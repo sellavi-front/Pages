878393bf64bd86538ed334bf531b91ce6ead8dd8
Закир Гафиятов <gafiyatovz@gmail.com>
2020-10-28 15:52:06 +0300

    a

--- module/CustomBanner/CustomBanner.js
+++ module/CustomBanner/CustomBanner.js
@@ -17,5 +17,5 @@ const CustomBanner = (doc, obj) => {
       </div>`,
   );
 
-  banner.style.background.cssText = 'background: url(https://res.cloudinary.com/gz-company/image/upload/v1603099813/SSLAB/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F.._gyr7wr.jpg)';
+  banner.style.background.cssText = `background: url(${obj.urlImg})`;
 };
