fb333a8d54e2b9a53c478e914a09be624a48e59b
Закир Гафиятов <gafiyatovz@gmail.com>
2020-10-28 15:49:46 +0300

    a

--- module/CustomBanner/CustomBanner.js
+++ module/CustomBanner/CustomBanner.js
@@ -10,12 +10,12 @@ const CustomBanner = (doc, obj) => {
           <div class="banner__caption">
             <h1>${obj.title}</h1>
             <a href="${obj.link}">
-              ${obj.subtitle}
+              ${obj.linkContent}
             </a>
           </div>
         </div>
       </div>`,
   );
 
-  banner.style.background = 'https://res.cloudinary.com/gz-company/image/upload/v1603099813/SSLAB/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F.._gyr7wr.jpg';
+  banner.style.background.cssText = 'background: url(https://res.cloudinary.com/gz-company/image/upload/v1603099813/SSLAB/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F.._gyr7wr.jpg)';
 };
