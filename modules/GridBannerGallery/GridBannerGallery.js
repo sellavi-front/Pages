ede0e84c8684cd99f646a9edd7d9765bdbcf6531
Закир Гафиятов <gafiyatovz@gmail.com>
2020-10-30 16:52:32 +0300

    test

--- modules/GridBannerGallery/GridBannerGallery.js
+++ modules/GridBannerGallery/GridBannerGallery.js
@@ -1,4 +1,54 @@
-import template from './template';
+const template = `
+<div class="grid-banner">
+  <div class="grid-banner__item">
+    <div class="grid-banner__text-container">
+      <h1 class="grid-banner__text-container_title">Детская одежда</h1>
+      <p class="grid-banner__text-container_subtitle">для девочек</p>
+      <button class="grid-banner__text-container_discount">
+        <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
+        <span class="grid-banner__text-container_discount_subtitle">на всю одежду по промокоду SALE30</span>
+      </button>
+    </div>
+  </div>
+</div>
+<div class="grid-banner">
+  <div class="grid-banner__item">
+    <div class="grid-banner__text-container">
+    </div>
+    <img src="https://res.cloudinary.com/gz-company/image/upload/v1604059787/Interno/1_cotuxs.png" alt="" class="grid-banner__item_img">
+  </div>
+</div>
+<div class="grid-banner">
+  <div class="grid-banner__item">
+    <div class="grid-banner__text-container">
+    </div>
+    <img src="https://res.cloudinary.com/gz-company/image/upload/v1604059787/Interno/1_cotuxs.png" alt="" class="grid-banner__item_img">
+  </div>
+  <div class="grid-banner__item">
+    <div class="grid-banner__text-container">
+    </div>
+    <img src="https://res.cloudinary.com/gz-company/image/upload/v1604059787/Interno/1_cotuxs.png" alt="" class="grid-banner__item_img">
+  </div>
+</div>
+<div class="grid-banner">
+  <div class="grid-banner__item">
+    <div class="grid-banner__text-container">
+    </div>
+    <img src="https://res.cloudinary.com/gz-company/image/upload/v1604059787/Interno/1_cotuxs.png" alt="" class="grid-banner__item_img">
+  </div>
+</div>
+<div class="grid-banner">
+  <div class="grid-banner__item">
+    <div class="grid-banner__text-container">
+    </div>
+    <img src="https://res.cloudinary.com/gz-company/image/upload/v1604059787/Interno/1_cotuxs.png" alt="" class="grid-banner__item_img">
+  </div>
+  <div class="grid-banner__item">
+    <div class="grid-banner__text-container">
+    </div>
+    <img src="https://res.cloudinary.com/gz-company/image/upload/v1604059787/Interno/1_cotuxs.png" alt="" class="grid-banner__item_img">
+  </div>
+</div>`;
 
 export default class GridBannerGalery {
   constructor(elem) {
