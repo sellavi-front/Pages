e160523bee684debac261178173897f57f6f0b7f
Закир Гафиятов <gafiyatovz@gmail.com>
2020-10-30 18:22:29 +0300

    100843 banner template

--- modules/GridBannerGallery/template.js
+++ modules/GridBannerGallery/template.js
@@ -10,38 +10,15 @@ const template = `
       </button>
     </div>
   </div>
-</div>
-<div class="grid-banner">
   <div class="grid-banner__item">
     <div class="grid-banner__text-container">
-    </div>
-  </div>
-</div>
-<div class="grid-banner">
-  <div class="grid-banner__item">
-    <div class="grid-banner__text-container">
-    </div>
-  </div>
-  <div class="grid-banner__item">
-    <div class="grid-banner__text-container">
-    </div>
-  </div>
-</div>
-<div class="grid-banner">
-  <div class="grid-banner__item">
-    <div class="grid-banner__text-container">
-    </div>
-  </div>
-</div>
-<div class="grid-banner">
-  <div class="grid-banner__item">
-    <div class="grid-banner__text-container">
-    </div>
-  </div>
-  <div class="grid-banner__item">
-    <div class="grid-banner__text-container">
+      <h1 class="grid-banner__text-container_title">Детская одежда</h1>
+      <p class="grid-banner__text-container_subtitle">для девочек</p>
+      <button class="grid-banner__text-container_discount">
+        <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
+        <span class="grid-banner__text-container_discount_subtitle">на всю одежду по промокоду SALE30</span>
+      </button>
     </div>
   </div>
 </div>`;
-
 export default template;
