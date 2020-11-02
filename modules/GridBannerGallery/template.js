b2128b78f8c6e8d0b78d8b2c75cd9355f8497807
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-02 11:29:13 +0300

    template

--- modules/GridBannerGallery/template.js
+++ modules/GridBannerGallery/template.js
@@ -1,72 +1,80 @@
 const template = `
 <div class="banner-gallery">
-<div class="grid-banner">
-  <div class="grid-banner__item">
-    <div class="grid-banner__text-container">
-      <h2 class="grid-banner__text-container_title">Детская одежда</h2>
-      <p class="grid-banner__text-container_subtitle">для девочек</p>
-      <button class="grid-banner__text-container_discount">
-        <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
-        <span class="grid-banner__text-container_discount_subtitle">на всю одежду по промокоду SALE30</span>
-      </button>
-    </div>
-  </div>
-</div>
-<div class="grid-banner">
-  <div class="grid-banner__item">
-    <div class="grid-banner__text-container">
-      <h2 class="grid-banner__text-container_title">Детская одежда</h2>
-      <p class="grid-banner__text-container_subtitle">для мальчиков</p>
-      <button class="grid-banner__text-container_discount">
-        <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
-        <span class="grid-banner__text-container_discount_subtitle">на всю одежду по промокоду SALE30</span>
-      </button>
-    </div>
-  </div>
-</div>
-<div class="grid-banner__two-item">
-  <div class="grid-banner">
+  <div class="grid-banner grid-banner-1">
     <div class="grid-banner__item">
       <div class="grid-banner__text-container">
-        <h2 class="grid-banner__text-container_title">Мягкие игрушки</h2>
+        <h2 class="grid-banner__text-container_title">Детская одежда</h2>
+        <p class="grid-banner__text-container_subtitle">для девочек</p>
         <button class="grid-banner__text-container_discount">
           <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
-          <span class="grid-banner__text-container_discount_subtitle">с 1 июля по 1 сентября 2020</span>
+          <span class="grid-banner__text-container_discount_subtitle"
+            >на всю одежду по промокоду SALE30</span
+          >
         </button>
       </div>
     </div>
   </div>
-  <div class="grid-banner">
-    <div class="grid-banner__item"></div>
-  </div>
-</div>
-<div class="grid-banner">
-  <div class="grid-banner__item">
-    <div class="grid-banner__text-container">
-      <h2 class="grid-banner__text-container_title">Электронные книги</h2>
-    </div>
-  </div>
-</div>
-<div class="grid-banner__two-item">
-  <div class="grid-banner">
+  <div class="grid-banner grid-banner-2">
     <div class="grid-banner__item">
       <div class="grid-banner__text-container">
-        <h2 class="grid-banner__text-container_title">Barbie</h2>
+        <h2 class="grid-banner__text-container_title">Детская одежда</h2>
+        <p class="grid-banner__text-container_subtitle">для мальчиков</p>
         <button class="grid-banner__text-container_discount">
           <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
-          <span class="grid-banner__text-container_discount_subtitle">с 1 июля по 1 сентября 2020</span>
+          <span class="grid-banner__text-container_discount_subtitle"
+            >на всю одежду по промокоду SALE30</span
+          >
         </button>
       </div>
     </div>
   </div>
-  <div class="grid-banner">
+  <div class="grid-banner__two-item">
+    <div class="grid-banner grid-banner-3">
+      <div class="grid-banner__item">
+        <div class="grid-banner__text-container">
+          <h2 class="grid-banner__text-container_title">Мягкие игрушки</h2>
+          <button class="grid-banner__text-container_discount">
+            <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
+            <span class="grid-banner__text-container_discount_subtitle"
+              >с 1 июля по 1 сентября 2020</span
+            >
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="grid-banner grid-banner-4">
+      <div class="grid-banner__item"></div>
+    </div>
+  </div>
+  <div class="grid-banner grid-banner-5">
     <div class="grid-banner__item">
       <div class="grid-banner__text-container">
-        <h2 class="grid-banner__text-container_title">Другие товары</h2>
+        <h2 class="grid-banner__text-container_title">Электронные книги</h2>
       </div>
     </div>
   </div>
-</div>
+  <div class="grid-banner__two-item">
+    <div class="grid-banner grid-banner-6">
+      <div class="grid-banner__item">
+        <div class="grid-banner__text-container">
+          <h2 class="grid-banner__text-container_title">Barbie</h2>
+          <button class="grid-banner__text-container_discount">
+            <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
+            <span class="grid-banner__text-container_discount_subtitle"
+              >с 1 июля по 1 сентября 2020</span
+            >
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="grid-banner grid-banner-7">
+      <div class="grid-banner__item">
+        <div class="grid-banner__text-container">
+          <h2 class="grid-banner__text-container_title">Другие товары</h2>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 `;
 
