67b25646e6a6df4c0e13cff219db2d697159e3d5
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-02 12:32:58 +0300

    template

--- shops/100843/script.js
+++ shops/100843/script.js
@@ -1,6 +1,6 @@
 import GridBannerGallery from '../../modules/GridBannerGallery/GridBannerGallery.js';
 
-const contentArea = document.querySelector('.content-area');
+const contentArea = document.querySelector('body.common-home>.content-area');
 
 const gridBannerGallery = new GridBannerGallery(contentArea);
 
