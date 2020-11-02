87bd3a1b3060944b6a9454c759b35c9d27024be0
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-02 10:23:17 +0300

    template

--- modules/GridBannerGallery/template.js
+++ modules/GridBannerGallery/template.js
@@ -23,27 +23,30 @@ const template = `
     </div>
   </div>
 </div>
-<div class="grid-banner">
-  <div class="grid-banner__item">
+<div class="grid-banner__two-item">
+  <div class="grid-banner">
+    <div class="grid-banner__item">
+      <div class="grid-banner__text-container">
+        <h1 class="grid-banner__text-container_title">Детская одежда</h1>
+        <p class="grid-banner__text-container_subtitle">для девочек</p>
+        <button class="grid-banner__text-container_discount">
+          <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
+          <span class="grid-banner__text-container_discount_subtitle">на всю одежду по промокоду SALE30</span>
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="grid-banner">
+    <div class="grid-banner__item">
     <div class="grid-banner__text-container">
       <h1 class="grid-banner__text-container_title">Детская одежда</h1>
-      <p class="grid-banner__text-container_subtitle">для девочек</p>
+      <p class="grid-banner__text-container_subtitle">для мальчиков</p>
       <button class="grid-banner__text-container_discount">
         <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
         <span class="grid-banner__text-container_discount_subtitle">на всю одежду по промокоду SALE30</span>
       </button>
     </div>
   </div>
-  <div class="grid-banner__item">
-  <div class="grid-banner__text-container">
-    <h1 class="grid-banner__text-container_title">Детская одежда</h1>
-    <p class="grid-banner__text-container_subtitle">для мальчиков</p>
-    <button class="grid-banner__text-container_discount">
-      <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
-      <span class="grid-banner__text-container_discount_subtitle">на всю одежду по промокоду SALE30</span>
-    </button>
-  </div>
-</div>
 </div>
 `;
 
