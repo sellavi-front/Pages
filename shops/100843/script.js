dc5efecc45772ad72cb922f6bf43494c82653d93
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-02 16:21:02 +0300

    template

--- shops/100843/script.js
+++ shops/100843/script.js
@@ -2,6 +2,11 @@ import GridBannerGallery from '../../modules/GridBannerGallery/GridBannerGallery
 
 const contentArea = document.querySelector('.common-home .content-area');
 
-const gridBannerGallery = new GridBannerGallery(contentArea);
+if (
+  location.href === 'https://internokids.ru/' ||
+  location.href === 'https://internokids.ru/?from_admin'
+) {
+  const gridBannerGallery = new GridBannerGallery(contentArea);
 
-gridBannerGallery.render();
+  gridBannerGallery.render();
+}
