49a84c331eac33def18de27a8eb451cb15b52132
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-05 10:39:00 +0300

    Модуль переноса элемента в нужный контейнер

--- modules/AddElemInContainer/AddElemInContainer.js
+++ modules/AddElemInContainer/AddElemInContainer.js
@@ -44,7 +44,7 @@ class AddElemInContainer {
     console.log('findElements => ', this.addElem)
     const elem = this.copyType(this.addElem);
     const cont = this.container;
-    console.log('findElements => elem ', elem);
+    console.log('findElements => elem ', [elem, cont]);
     //this.delete(this.addElem);
     return [elem, cont];
   }
