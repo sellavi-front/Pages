a45a769b9178013738771499634220ed80a0d7ef
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-05 16:39:31 +0300

    add link for main banners

--- modules/GridBannerGallery/template.js
+++ modules/GridBannerGallery/template.js
@@ -1,38 +1,38 @@
 const template = `
 <div class="banner-gallery">
-  <div class="grid-banner grid-banner-1">
-  <a href="детская-одежда/для-девчонок/">
-    <div class="grid-banner__item">
-      <div class="grid-banner__text-container">
-        <h2 class="grid-banner__text-container_title">Детская одежда</h2>
-        <p class="grid-banner__text-container_subtitle">для девочек</p>
-          <button class="grid-banner__text-container_discount">
-            <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
-            <span class="grid-banner__text-container_discount_subtitle"
-              >на всю одежду по промокоду SALE30</span
-            >
-          </button>
-      </div>
-    </div>
-    </a>
-  </div>
-  <div class="grid-banner grid-banner-2">
-  <a href="детская-одежда/для-девчонок/">
-    <div class="grid-banner__item">
-      <div class="grid-banner__text-container">
-        <h2 class="grid-banner__text-container_title">Детская одежда</h2>
-        <p class="grid-banner__text-container_subtitle">для мальчиков</p>
+<div class="grid-banner grid-banner-1">
+<a href="детская-одежда/для-девчонок/">
+  <div class="grid-banner__item">
+    <div class="grid-banner__text-container">
+      <h2 class="grid-banner__text-container_title">Детская одежда</h2>
+      <p class="grid-banner__text-container_subtitle">для девочек</p>
         <button class="grid-banner__text-container_discount">
           <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
           <span class="grid-banner__text-container_discount_subtitle"
             >на всю одежду по промокоду SALE30</span
           >
         </button>
-      </div>
     </div>
-    </a>
   </div>
-  <div class="grid-banner__two-item">
+  </a>
+</div>
+<div class="grid-banner grid-banner-2">
+<a href="детская-одежда/для-девчонок/">
+  <div class="grid-banner__item">
+    <div class="grid-banner__text-container">
+      <h2 class="grid-banner__text-container_title">Детская одежда</h2>
+      <p class="grid-banner__text-container_subtitle">для мальчиков</p>
+      <button class="grid-banner__text-container_discount">
+        <h3 class="grid-banner__text-container_discount_title">Скидка 30%</h3>
+        <span class="grid-banner__text-container_discount_subtitle"
+          >на всю одежду по промокоду SALE30</span
+        >
+      </button>
+    </div>
+  </div>
+  </a>
+</div>
+<div class="grid-banner__two-item">
   <a href="детская-одежда/для-девчонок/">
     <div class="grid-banner grid-banner-3">
         <div class="grid-banner__item">
@@ -46,45 +46,45 @@ const template = `
             </button>
           </div>
         </div>
-      </a>
     </div>
+  </a>
+  <a href="детская-одежда/для-девчонок/">
     <div class="grid-banner grid-banner-4">
-      <a href="детская-одежда/для-девчонок/">
-        <div class="grid-banner__item"></div>
-      </a>
+      <div class="grid-banner__item"></div>
+    </div>
+  </a>
+</div>
+<a href="детская-одежда/для-девчонок/">
+  <div class="grid-banner grid-banner-5">
+    <div class="grid-banner__item">
+      <div class="grid-banner__text-container">
+        <h2 class="grid-banner__text-container_title">Электронные книги</h2>
+        <p class="grid-banner__text-container_subtitle">для детей</p>
+      </div>
     </div>
   </div>
-  <div class="grid-banner grid-banner-5">
-    <a href="детская-одежда/для-девчонок/">
+ </a>
+<div class="grid-banner__two-item">
+  <a href="детская-одежда/для-девчонок/">
+    <div class="grid-banner grid-banner-6">
       <div class="grid-banner__item">
         <div class="grid-banner__text-container">
-          <h2 class="grid-banner__text-container_title">Электронные книги</h2>
-          <p class="grid-banner__text-container_subtitle">для детей</p>
+          <img class="grid-banner__img-title" src="https://res.cloudinary.com/gz-company/image/upload/v1604306396/Interno/1280px-Barbie_Logo_1_jrr1g0.png" alt="Barbie">
+          <p class="grid-banner__text-container_subtitle">куклы для девочек</p>
         </div>
       </div>
     </a>
   </div>
-  <div class="grid-banner__two-item">
-    <a href="детская-одежда/для-девчонок/">
-      <div class="grid-banner grid-banner-6">
-        <div class="grid-banner__item">
-          <div class="grid-banner__text-container">
-            <img class="grid-banner__img-title" src="https://res.cloudinary.com/gz-company/image/upload/v1604306396/Interno/1280px-Barbie_Logo_1_jrr1g0.png" alt="Barbie">
-            <p class="grid-banner__text-container_subtitle">куклы для девочек</p>
-          </div>
+  <a href="детская-одежда/для-девчонок/">
+    <div class="grid-banner grid-banner-7">
+      <div class="grid-banner__item">
+        <div class="grid-banner__text-container">
+          <h2 class="grid-banner__text-container_title">Другие товары</h2>
         </div>
-      </a>
+      </div>
     </div>
-    <a href="детская-одежда/для-девчонок/">
-      <div class="grid-banner grid-banner-7">
-        <div class="grid-banner__item">
-          <div class="grid-banner__text-container">
-            <h2 class="grid-banner__text-container_title">Другие товары</h2>
-          </div>
-        </div>
-      </div>
-    </a>
-  </div>
+  </a>
+</div>
 </div>
 `;
 
