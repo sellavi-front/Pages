f7685a46be957149a6da414cf9e53a7f4f1cc5cb
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-05 11:01:28 +0300

    Модуль переноса элемента в нужный контейнер

--- modules/AddElemInContainer/AddElemInContainer.js
+++ modules/AddElemInContainer/AddElemInContainer.js
@@ -16,16 +16,13 @@ class AddElemInContainer {
     this.addElem = addElem;
     this.d = d;
     [this.el, this.cont] = this.findElements();
-    console.log('constructor => ', this.findElements())
   }
 
   copyArr(arr) {
-    console.log('copyArr =>', arr);
     return Array.from(arr);
   }
 
   copyEl(el) {
-    console.log('copyEl => ', el);
     return el.cloneNode(true);
   }
 
@@ -35,23 +32,18 @@ class AddElemInContainer {
   }
 
   copyType(data) {
-    console.log('copyType =>', this.copyEl(data));
     if (data.length) this.copyArr(data);
     return this.copyEl(data);
   }
 
   findElements() {
-    console.log('findElements => ', this.addElem)
     const elem = this.copyType(this.addElem);
     const cont = this.container;
-    console.log('findElements => elem ', [elem, cont]);
     this.delete(this.addElem);
     return [elem, cont];
   }
 
   render() {
-    console.log('this.cont: ', this.cont);
-    console.log('render this.elem', this.el.outterHTML);
     //this.cont.forEach((elem, idx) => {
       //console.log('elem', elem);
       //console.log('idx', idx);
