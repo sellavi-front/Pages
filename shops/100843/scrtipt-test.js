7c3ede1f453da22abd490f451590efdd2f8369ea
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-06 12:13:26 +0300

    test new GridGallery

--- shops/100843/scrtipt-test.js
+++ shops/100843/scrtipt-test.js
@@ -2,8 +2,61 @@ import GridBannerGalleryTest from '../../modules/GridBannerGalleryTest/GridBanne
 
 const contentArea = document.querySelector('.titles');
 
+const bannerContent = [
+  {
+    class: 'grid-banner-2',
+    link: 'детская-одежда/для-девчонок/',
+    title: 'Детская одежда',
+    subtitle: 'Для девочек',
+    button: {
+      title: 'Скидка 30%',
+      text: 'на всю одежду по промокоду SALE30',
+    },
+  },
+  {
+    class: 'grid-banner-2',
+    link: 'детская-одежда/для-девчонок/',
+    title: 'Детская одежда',
+    subtitle: 'Для девочек',
+    button: {
+      title: 'Скидка 30%',
+      text: 'на всю одежду по промокоду SALE30',
+    },
+  },
+  {
+    class: 'grid-banner-2',
+    link: 'детская-одежда/для-девчонок/',
+    title: 'Детская одежда',
+    subtitle: 'Для девочек',
+    button: {
+      title: 'Скидка 30%',
+      text: 'на всю одежду по промокоду SALE30',
+    },
+  },
+  {
+    class: 'grid-banner-2',
+    link: 'детская-одежда/для-девчонок/',
+    title: 'Детская одежда',
+    subtitle: 'Для девочек',
+    button: {
+      title: 'Скидка 30%',
+      text: 'на всю одежду по промокоду SALE30',
+    },
+  },
+  {
+    class: 'grid-banner-2',
+    link: 'детская-одежда/для-девчонок/',
+    title: 'Детская одежда',
+    subtitle: 'Для девочек',
+    button: {
+      title: 'Скидка 30%',
+      text: 'на всю одежду по промокоду SALE30',
+    },
+  },
+];
+
 if (location.href === 'https://internokids.ru/sales/') {
-  const grid = new GridBannerGalleryTest(contentArea);
+  const grid = new GridBannerGalleryTest(contentArea, bannerContent);
   grid.render();
 
   // gridBannerGalleryTest.render();
