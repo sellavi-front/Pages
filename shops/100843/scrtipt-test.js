6ab363ba20bed324c11f44edc62182fb9a76ad94
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-06 12:37:37 +0300

    test new GridGallery

--- shops/100843/scrtipt-test.js
+++ shops/100843/scrtipt-test.js
@@ -1,12 +1,9 @@
 import GridBannerGalleryTest from '../../modules/GridBannerGalleryTest/GridBannerGalleryTest.js';
-import content from './content/content';
+import content from './content/content.js';
 
 const contentArea = document.querySelector('.titles');
-
 
 if (location.href === 'https://internokids.ru/sales/') {
   const grid = new GridBannerGalleryTest(contentArea, content);
   grid.render();
-
-  // gridBannerGalleryTest.render();
 }
