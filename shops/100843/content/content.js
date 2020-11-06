20aea29f9802013f924f49da3925bdce474aa1db
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-06 15:55:12 +0300

    100843

--- shops/100843/content/content.js
+++ shops/100843/content/content.js
@@ -1,9 +1,19 @@
 const content = [
   {
-    class: 'grid-banner-2',
+    class: 'grid-banner-1',
     link: 'детская-одежда/для-девчонок/',
     title: 'Детская одежда',
     subtitle: 'Для девочек',
+    button: {
+      title: 'Скидка 30%',
+      text: 'на всю одежду по промокоду SALE30',
+    },
+  },
+  {
+    class: 'grid-banner-2',
+    link: 'детская-одежда/для-мальчишек/',
+    title: 'Детская одежда',
+    subtitle: 'Для мальчиков',
     button: {
       title: 'Скидка 30%',
       text: 'на всю одежду по промокоду SALE30',
@@ -11,42 +21,52 @@ const content = [
   },
   {
     class: 'grid-banner-3',
-    link: 'детская-одежда/для-девчонок/',
-    title: 'Детская одежда',
-    subtitle: 'Для девочек',
+    link: 'игрушки/мягкие-игрушки/',
+    title: 'Мягкие игрушки',
+    subtitle: '',
     button: {
-      title: 'Скидка 30%',
-      text: 'на всю одежду по промокоду SALE30',
+      title: 'Скидка 50%',
+      text: 'с 1 июля по 1 сентября 2020',
     },
   },
   {
     class: 'grid-banner-4',
-    link: 'детская-одежда/для-девчонок/',
-    title: 'Детская одежда',
-    subtitle: 'Для девочек',
+    link: 'lego/',
+    title: '',
+    subtitle: '',
     button: {
-      title: 'Скидка 30%',
-      text: 'на всю одежду по промокоду SALE30',
+      title: '',
+      text: '',
     },
   },
   {
     class: 'grid-banner-5',
     link: 'детская-одежда/для-девчонок/',
-    title: 'Детская одежда',
-    subtitle: 'Для девочек',
+    title: 'Электронные книги',
+    subtitle: 'Для детей',
     button: {
-      title: 'Скидка 30%',
-      text: 'на всю одежду по промокоду SALE30',
+      title: '',
+      text: '',
     },
   },
   {
     class: 'grid-banner-6',
-    link: 'детская-одежда/для-девчонок/',
-    title: 'Детская одежда',
-    subtitle: 'Для девочек',
+    link: 'игрушки/barbie/',
+    title: 'Barbie',
+    subtitle: 'куклы для девочек',
     button: {
-      title: 'Скидка 30%',
-      text: 'на всю одежду по промокоду SALE30',
+      title: '',
+      text: '',
+    },
+  },
+  {
+    class: 'grid-banner-7',
+    link: 'детская-одежда/для-девчонок/',
+    title: 'Другие товары',
+    subtitle: '',
+    button: {
+      title: '',
+      text: '',
     },
   },
 ];
