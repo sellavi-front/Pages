5d8b77869bc053bc5d9911ea66525dd1f4c12052
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-06 12:36:42 +0300

    test new GridGallery

---
+++ shops/100843/content/content.js
@@ -0,0 +1,54 @@
+const content = [
+  {
+    class: 'grid-banner-2',
+    link: 'детская-одежда/для-девчонок/',
+    title: 'Детская одежда',
+    subtitle: 'Для девочек',
+    button: {
+      title: 'Скидка 30%',
+      text: 'на всю одежду по промокоду SALE30',
+    },
+  },
+  {
+    class: 'grid-banner-3',
+    link: 'детская-одежда/для-девчонок/',
+    title: 'Детская одежда',
+    subtitle: 'Для девочек',
+    button: {
+      title: 'Скидка 30%',
+      text: 'на всю одежду по промокоду SALE30',
+    },
+  },
+  {
+    class: 'grid-banner-4',
+    link: 'детская-одежда/для-девчонок/',
+    title: 'Детская одежда',
+    subtitle: 'Для девочек',
+    button: {
+      title: 'Скидка 30%',
+      text: 'на всю одежду по промокоду SALE30',
+    },
+  },
+  {
+    class: 'grid-banner-5',
+    link: 'детская-одежда/для-девчонок/',
+    title: 'Детская одежда',
+    subtitle: 'Для девочек',
+    button: {
+      title: 'Скидка 30%',
+      text: 'на всю одежду по промокоду SALE30',
+    },
+  },
+  {
+    class: 'grid-banner-6',
+    link: 'детская-одежда/для-девчонок/',
+    title: 'Детская одежда',
+    subtitle: 'Для девочек',
+    button: {
+      title: 'Скидка 30%',
+      text: 'на всю одежду по промокоду SALE30',
+    },
+  },
+];
+
+export default content;
--- shops/100843/scrtipt-test.js
+++ shops/100843/scrtipt-test.js
@@ -1,62 +1,11 @@
 import GridBannerGalleryTest from '../../modules/GridBannerGalleryTest/GridBannerGalleryTest.js';
+import content from './content/content';
 
 const contentArea = document.querySelector('.titles');
 
-const bannerContent = [
-  {
-    class: 'grid-banner-2',
-    link: 'детская-одежда/для-девчонок/',
-    title: 'Детская одежда',
-    subtitle: 'Для девочек',
-    button: {
-      title: 'Скидка 30%',
-      text: 'на всю одежду по промокоду SALE30',
-    },
-  },
-  {
-    class: 'grid-banner-2',
-    link: 'детская-одежда/для-девчонок/',
-    title: 'Детская одежда',
-    subtitle: 'Для девочек',
-    button: {
-      title: 'Скидка 30%',
-      text: 'на всю одежду по промокоду SALE30',
-    },
-  },
-  {
-    class: 'grid-banner-2',
-    link: 'детская-одежда/для-девчонок/',
-    title: 'Детская одежда',
-    subtitle: 'Для девочек',
-    button: {
-      title: 'Скидка 30%',
-      text: 'на всю одежду по промокоду SALE30',
-    },
-  },
-  {
-    class: 'grid-banner-2',
-    link: 'детская-одежда/для-девчонок/',
-    title: 'Детская одежда',
-    subtitle: 'Для девочек',
-    button: {
-      title: 'Скидка 30%',
-      text: 'на всю одежду по промокоду SALE30',
-    },
-  },
-  {
-    class: 'grid-banner-2',
-    link: 'детская-одежда/для-девчонок/',
-    title: 'Детская одежда',
-    subtitle: 'Для девочек',
-    button: {
-      title: 'Скидка 30%',
-      text: 'на всю одежду по промокоду SALE30',
-    },
-  },
-];
 
 if (location.href === 'https://internokids.ru/sales/') {
-  const grid = new GridBannerGalleryTest(contentArea, bannerContent);
+  const grid = new GridBannerGalleryTest(contentArea, content);
   grid.render();
 
   // gridBannerGalleryTest.render();
