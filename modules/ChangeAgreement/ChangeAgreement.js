f48be7fcfdde06a1bf9e8e77c9d0b1333d3284e3
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-08 12:33:26 +0300

    отладка

--- modules/ChangeAgreement/ChangeAgreement.js
+++ modules/ChangeAgreement/ChangeAgreement.js
@@ -5,6 +5,6 @@ export default class ChangeAgreement {
 
   change() {
     const checkbox = this.doc.querySelector('.checkbox>label');
-    checkbox.textContent = 'Согласен (-а) с условиями <a href="/terms-of-use/" style="text-decoration: none !important;"><b>Публичной оферты</b></a>';
+    checkbox.insertAdjacentHTML('beforeend', `Согласен (-а) с условиями <a href="/terms-of-use/" style="text-decoration: none !important;"><b>Публичной оферты</b></a>`);
   }
 }
