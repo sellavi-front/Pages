9469e12a60005edeb11f0c18779c9aec88da52a1
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-09 14:21:06 +0300

    испрваление ошибок

--- fixes/ChangeErrorEessage/ChangeErrorMessage.js
+++ fixes/ChangeErrorEessage/ChangeErrorMessage.js
@@ -1,16 +1,16 @@
 export default class ChangeErrorMessage {
   constructor() {
-    this.alerts = document.querySelectorAll('.text-danger');
+    this.btn = document.querySelector('#button-card');
   }
-
   setMessage() {
-    this.alerts(alert => {
+    console.log(this.alerts);
+    this.alerts((alert) => {
       const nameBlock = alert.parentElement.firstElementChild;
-      alert.innerHTML = `Поле ${nameBlock.textContent.toLowerCase()} обязательно!`
-    })
+      alert.innerHTML = `Поле ${nameBlock.textContent.toLowerCase()} обязательно!`;
+    });
   }
 
   change() {
-    $('#button-cart').on('click', this.setMessage());
+    this.btn.addEventListener('click', this.setMessage(document.querySelectorAll('.text-danger')));
   }
 }
