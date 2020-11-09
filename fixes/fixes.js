4a6b36fca80bb6ea6114bfda353ea8d3e40f1e44
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-09 14:37:22 +0300

    a

--- fixes/fixes.js
+++ fixes/fixes.js
@@ -1,4 +1,5 @@
 import ChangeAgreement from './ChangeAgreement/ChangeAgreement.js';
 import ChangeErrorMessage from './ChangeErrorEessage/ChangeErrorMessage.js';
+import TranslateAboutUs from './TranslateAboutUs/TranslateAboutUs.js';
 
-export { ChangeAgreement, ChangeErrorMessage };
+export { ChangeAgreement, ChangeErrorMessage, TranslateAboutUs };
