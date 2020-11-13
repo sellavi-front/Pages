0a52cf08ab754652d2f339120478cf4bdb417a79
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-13 14:01:50 +0300

    styles

--- shops/101470/index.js
+++ shops/101470/index.js
@@ -1,4 +1,4 @@
-import  DeleteElements from '../../modules/DeleteElements/DeleteElements.js';
+import DeleteElements from '../../modules/DeleteElements/DeleteElements.js';
 
 if (document.querySelector('.wrapper')) {
   const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
@@ -6,14 +6,13 @@ if (document.querySelector('.wrapper')) {
   const cartText = document.querySelector('#cart > span');
   const navigation = document.querySelector('.navigation-wrapper>div>.navigation');
   const headerLogo = headerWrapper.querySelector('.logo');
+  const forDelete = [search, cartTextheaderWrapper];
 
-  const forDelete  = [search, cartText];
   const deleteElements = new DeleteElements(forDelete);
-
-  deleteElements.remove();
 
   headerWrapper.classList.add('container-fluid');
   headerWrapper.classList.remove('container');
-console.log(navigation);
+
   headerLogo.insertAdjacentHTML('afterend', navigation.outerHTML);
+  deleteElements.remove();
 }
