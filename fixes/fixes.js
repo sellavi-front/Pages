102a2b189fa11ff5ece26554544f7f9f3395f345
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-13 15:35:05 +0300

    fixes footer copy

--- fixes/fixes.js
+++ fixes/fixes.js
@@ -1,5 +1,6 @@
 import ChangeAgreement from './ChangeAgreement/ChangeAgreement.js';
 import ChangeErrorMessage from './ChangeErrorEessage/ChangeErrorMessage.js';
 import TranslateAboutUs from './TranslateAboutUs/TranslateAboutUs.js';
+import FooterCopy from './FooterCopy/FooterCopy,js';
 
 export { ChangeAgreement, ChangeErrorMessage, TranslateAboutUs };
