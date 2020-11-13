529777cc2fc32b33e5181c0d26f00779d5fc1026
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-13 14:28:11 +0300

    big banner

--- shops/101470/index.js
+++ shops/101470/index.js
@@ -1,4 +1,5 @@
 import DeleteElements from '../../modules/DeleteElements/DeleteElements.js';
+import BigBanner from '../../modules/BigBanner/BigBanner.js';
 
 if (document.querySelector('.wrapper')) {
   const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
@@ -6,13 +7,23 @@ if (document.querySelector('.wrapper')) {
   const cartText = document.querySelector('#cart > span');
   const navigation = document.querySelector('.navigation-wrapper>div>.navigation');
   const headerLogo = headerWrapper.querySelector('.logo');
+
   const forDelete = [search, cartText, headerWrapper];
 
+  const objBanner = {
+    title: 'Коврики для ЙОГИ',
+    link: '#',
+    linkContent: 'В КАТАЛОГЕ',
+  };
+
   const deleteElements = new DeleteElements(forDelete);
+  const bigBanner = new BigBanner(document, objBanner);
 
   headerWrapper.classList.add('container-fluid');
   headerWrapper.classList.remove('container');
 
   headerLogo.insertAdjacentHTML('afterend', navigation.outerHTML);
   deleteElements.remove();
+
+  bigBanner.render();
 }
