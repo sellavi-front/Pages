1adacc8c9db843b91c4b19d910317317cf3b546f
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-13 12:03:34 +0300

    styles

--- shops/101470/index.js
+++ shops/101470/index.js
@@ -1,13 +1,16 @@
 import  DeleteElements from '../../modules/DeleteElements/DeleteElements.js';
 
 if (document.querySelector('.wrapper')) {
+  const headerWrapper = document.querySelector('.header-wrapper>div');
   const search = document.querySelector('.header-search');
   const cartText = document.querySelector('#cart > span');
+  const navigation = document.querySelector('.navigation');
 
   const forDelete  = [search, cartText];
   const deleteElements = new DeleteElements(forDelete);
 
-  console.log(forDelete);
-
   deleteElements.remove();
+
+  headerWrapper.querySelector('.logo').insertAdjacetnHTML('afterend', navigation);
+
 }
