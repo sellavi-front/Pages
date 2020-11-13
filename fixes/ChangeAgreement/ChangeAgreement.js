d4757c38e98007604ffe8f66658a4f3542f55ce1
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-13 17:08:49 +0300

    new Change Agreement

--- fixes/ChangeAgreement/ChangeAgreement.js
+++ fixes/ChangeAgreement/ChangeAgreement.js
@@ -1,11 +1,10 @@
 export default class ChangeAgreement {
-  constructor(doc) {
-    this.doc = doc;
+  constructor() {
+    this.checkbox = document.querySelector('.checkbox>label');
   }
 
   change() {
-    const checkbox = this.doc.querySelector('.checkbox>label');
-    checkbox.insertAdjacentHTML('beforeend', `Согласен (-а) с условиями <a href="/terms-of-use/" style="text-decoration: none !important;"><b>Публичной оферты</b></a>`);
+    this.checkbox.insertAdjacentHTML('beforeend', `Согласен (-а) с условиями <a href="/terms-of-use/" style="text-decoration: none !important;"><b>Публичной оферты</b></a>`);
   }
 }
 
