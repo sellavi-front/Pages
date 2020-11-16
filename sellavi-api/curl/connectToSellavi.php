8b03f4143e3735a9853b70cb7237663208119b54
Coospir <eugene.starodubov@gmail.com>
2020-11-16 12:11:13 +0300

    Added some fixes

--- sellavi-api/curl/connectToSellavi.php
+++ sellavi-api/curl/connectToSellavi.php
@@ -1,4 +1,4 @@
-<?php
+<?
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
@@ -7,26 +7,14 @@ class Connection {
 
     public static function getSellavi($apiURL, $secret){
         $ch = curl_init();
-
-        // Will return the response, if false it print the response
-
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'accept: application/json',
             'x-token: ' . $secret
         ));
-
-        // Set the url
-
         curl_setopt($ch, CURLOPT_URL, $apiURL);
-
-        // Execute
-
         $result = curl_exec($ch);
-
         curl_close($ch);
         echo $result;
     }
 }
-
-?>
