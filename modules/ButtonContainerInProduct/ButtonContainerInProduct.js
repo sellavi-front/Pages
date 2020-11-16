01f88de393d87e3bf416cb1fb9479b3c7b68729a
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-16 11:36:12 +0300

    переписал методы на работу с массивом данных

--- modules/ButtonContainerInProduct/ButtonContainerInProduct.js
+++ modules/ButtonContainerInProduct/ButtonContainerInProduct.js
@@ -1,23 +1,33 @@
+import AddElemInContainer from '../AddElemInContainer/AddElemInContainer.js'
+
 export default class ButtonContainerInProduct {
-  constructor(addToCart, addToWishList) {
+  constructor(addToCart, addToWishList, container) {
+    this.container = container;
     this.addToCart = addToCart;
     this.addToWishList = addToWishList;
   }
 
   createContainer() {
-    addToCart.forEach((btn) => {
+    this.container.forEach((btn, i) => {
       btn.insertAdjacentHTML(
         'beforebegin',
-        `<div class="buttons__container"></div>`,
+        `<div class="buttons__container">${this.addToWishList[i]}${this.addToCart[i]}</div>`,
       );
     });
   }
 }
 
+const priceActions = document.querySelectorAll('.price_actions');
 const addToCart = document.querySelectorAll('.add_to_cart');
 const addToWishList = document.querySelectorAll('.add_to_wishlist');
 
-const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, addToWishList);
+const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, addToWishList, priceActions);
 
 buttonContainerInProduct.createContainer();
+
+// const btnContainer = document.querySelectorAll('.buttons__container')
+
+// const addCartInProductCard = new AddElemInContainer(addToCart, btnContainer);
+// const addLikeInProductCard = new AddElemInContainer(addToWishList, btnContainer);
+
 //${this.addToCart.outerHTML}${this.addToWishList.outerHTML}
