0aaa68d7f00d09adc07cbb98f4db21b9a0b99f3f
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-17 16:12:17 +0300

    cart

--- shops/102063/index.js
+++ shops/102063/index.js
@@ -5,8 +5,9 @@ import '../../fixes/fixes.js';
 
 if (document.querySelector('.wrapper')) {
   const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
-
+  const hideCart = document.querySelector('.header-wrapper>.container>.header-cart');
   const buttonsInHeader = new ButtonListInHeader(headerWrapper);
 
   buttonsInHeader.setTemplate(headerWrapper);
+  hideCart.remove();
 }
