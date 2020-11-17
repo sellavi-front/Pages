6a3cb9b69faf4061da287cbe78fb9b2671c00af0
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-17 15:42:40 +0300

    иконки в хэдэр

--- shops/102063/index.js
+++ shops/102063/index.js
@@ -1,1 +1,12 @@
+import ButtonListInHeader from '../../modules/ButtonListInHeader/ButtonListInHeader.js'
+
 import '../../fixes/fixes.js';
+
+
+if (document.querySelector('.wrapper')) {
+  const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
+
+  const buttonsInHeader = new ButtonListInHeader(headerWrapper);
+
+  buttonsInHeader.setTemplate(headerWrapper);
+}
