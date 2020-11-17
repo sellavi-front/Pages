2bc3c4e89a507e6477a4801660e877b52ae24e3e
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-17 14:20:03 +0300

    Фикс скрытия загрузки скриптов и страницы

--- fixes/fixes.js
+++ fixes/fixes.js
@@ -2,5 +2,6 @@ import ChangeAgreement from './ChangeAgreement/ChangeAgreement.js';
 import ChangeErrorMessage from './ChangeErrorEessage/ChangeErrorMessage.js';
 import TranslateAboutUs from './TranslateAboutUs/TranslateAboutUs.js';
 import FooterCopy from './FooterCopy/FooterCopy.js';
+import HideLoading from './HideLoading/HideLoading.js';
 
-export { ChangeAgreement, ChangeErrorMessage, TranslateAboutUs, FooterCopy };
+export { ChangeAgreement, ChangeErrorMessage, TranslateAboutUs, FooterCopy, HideLoading };
