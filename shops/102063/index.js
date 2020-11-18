eca4bbf97e1f7eb334016347c20741ab67a4bc5a
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-18 16:23:57 +0300

    styles

--- shops/102063/index.js
+++ shops/102063/index.js
@@ -3,17 +3,32 @@ import SmallCategory from '../../components/SmallCategory/SmallCategory.js'
 import { DeleteClasses } from '../../modules/modules.js';
 import '../../fixes/fixes.js';
 
+const bootstrapClasses = [
+  'col-lg-4',
+  'col-xl-3',
+  'mb-0',
+  'mb-md-4',
+  'col-12',
+  'col-sm-12',
+  'col-sm-6',
+  'col-md-6',
+  'col-md-3',
+  'col-lg-6',
+  'col-xl-4',
+  'mb-4',
+  'container'
+];
 
 if (document.querySelector('.wrapper')) {
   const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
   const hideCart = document.querySelector('.header-wrapper>.container>.header-cart');
   const linkcatalog = document.querySelector('.flexy.has_children:first-child');
   const pageSection = document.querySelector('.page-section');
-
-  const onDeleteClasses = ['container']
+  const productItem =  document.querySelectorAll('.product-item');
 
   const buttonsInHeader = new ButtonListInHeader(headerWrapper);
-  const delClasses = new DeleteClasses(pageSection, onDeleteClasses)
+  const delClasses = new DeleteClasses(pageSection, bootstrapClasses)
+  const delSelectorsProductItem = new DeleteClasses(productItem, bootstrapClasses)
 
   buttonsInHeader.setTemplate(headerWrapper);
   hideCart.remove();
@@ -22,5 +37,6 @@ if (document.querySelector('.wrapper')) {
   pageSection.classList.add('.container-fluid');
 
   delClasses.findAndDelete();
+  delSelectorsProductItem.findAndDelete();
 }
 
