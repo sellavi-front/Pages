a44aa12cb4baae9ce8f890e718a89f6aa213e372
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-18 14:38:48 +0300

    catalog icon

--- shops/102063/index.js
+++ shops/102063/index.js
@@ -6,8 +6,12 @@ import '../../fixes/fixes.js';
 if (document.querySelector('.wrapper')) {
   const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
   const hideCart = document.querySelector('.header-wrapper>.container>.header-cart');
+  const linkcatalog = document.querySelector('.flexy.has_children:first-child');
+
   const buttonsInHeader = new ButtonListInHeader(headerWrapper);
 
   buttonsInHeader.setTemplate(headerWrapper);
   hideCart.remove();
+
+  linkcatalog.insertAdjacentHTML('afterbegin', `<i class="fas fa-bars"></i>`);
 }
