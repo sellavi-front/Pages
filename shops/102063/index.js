25312c211f69cd290ae0baf55356c6f0514eef5e
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-18 15:59:24 +0300

    удаление класса container

--- shops/102063/index.js
+++ shops/102063/index.js
@@ -1,6 +1,6 @@
 import ButtonListInHeader from '../../modules/ButtonListInHeader/ButtonListInHeader.js'
 import SmallCategory from '../../components/SmallCategory/SmallCategory.js'
-
+import { DeleteClasses } from '../../modules/modules.js';
 import '../../fixes/fixes.js';
 
 
@@ -9,11 +9,18 @@ if (document.querySelector('.wrapper')) {
   const hideCart = document.querySelector('.header-wrapper>.container>.header-cart');
   const linkcatalog = document.querySelector('.flexy.has_children:first-child');
   const pageSection = document.querySelector('.page-section');
+
+  const onDeleteClasses = ['container']
+
   const buttonsInHeader = new ButtonListInHeader(headerWrapper);
+  const delClasses = new DeleteClasses(pageSection, onDeleteClasses)
 
   buttonsInHeader.setTemplate(headerWrapper);
   hideCart.remove();
 
   linkcatalog.insertAdjacentHTML('afterbegin', `<i class="fas fa-bars"></i>`);
+  pageSection.classList.add('.container-fluid');
+
+  delClasses.findAndDelete();
 }
 
