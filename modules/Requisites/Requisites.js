4643f8a54c079fbbdf085313fef41d6a1d95e349
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-18 10:40:45 +0300

    рекивзиты

--- modules/Requisites/Requisites.js
+++ modules/Requisites/Requisites.js
@@ -14,8 +14,9 @@
 // }
 
 export default class Requisites {
-  constructor(obj) {
+  constructor(obj, target) {
     this.obj = obj;
+    this.target = target;
     this.container = document.querySelector('.requsitess');
     this.arr = [];
   }
@@ -35,7 +36,7 @@ export default class Requisites {
   setTemplate() {
     console.log(this.setData());
     this.container.insertAdjacentHTML(
-      'afterbegin',
+      this.target,
       `
         <div class="requsites__data">
           <h4 class="requsites__title">Реквизиты продавца</h4>
@@ -45,21 +46,26 @@ export default class Requisites {
   }
 }
 
-if (location.href.includes('term-of-use')) {
-  const data = {
-    ogranization: 'ИП ЩЕРБАКОВ АЛЕКСАНДР СЕРГЕЕВИЧ',
-    orgAdress: 'г. Екатеринбург, ул. Щербакова 4 офис 302',
-    mailAdress: 'г. Екатеринбург, ул. Щербакова 4 офис 302',
-    inn: '667105593531',
-    ogrn: '317665800123460',
-    bankName: 'УРАЛЬСКИЙ БАНК ПАО СБЕРБАНК',
-    accountNumber: '40802810016540071804',
-    corrNumber: '30101810500000000674',
-    bik: '046577674',
-    director: 'ЩЕРБАКОВ АЛЕКСАНДР СЕРГЕЕВИЧ',
-  };
+const data = {
+  ogranization: 'ИП ЩЕРБАКОВ АЛЕКСАНДР СЕРГЕЕВИЧ',
+  orgAdress: 'г. Екатеринбург, ул. Щербакова 4 офис 302',
+  mailAdress: 'г. Екатеринбург, ул. Щербакова 4 офис 302',
+  inn: '667105593531',
+  ogrn: '317665800123460',
+  bankName: 'УРАЛЬСКИЙ БАНК ПАО СБЕРБАНК',
+  accountNumber: '40802810016540071804',
+  corrNumber: '30101810500000000674',
+  bik: '046577674',
+  director: 'ЩЕРБАКОВ АЛЕКСАНДР СЕРГЕЕВИЧ',
+};
 
-  const req = new Requisites(data);
+if (location.href.includes('term-of-use')) {
+  const req = new Requisites(data, 'afterbegin');
 
   req.setTemplate();
+}
+
+if (location.href.includes('contact-info')) {
+  const root = document.querySelector('.contact-info > .media-list > .media:last-child');
+  const req = new Requisites(data, 'afterend');
 }
