51357e8bcd7a19d2de1062f2a58205040ab5f292
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-18 16:07:14 +0300

    delete module

--- modules/modules.js
+++ modules/modules.js
@@ -1,10 +1,23 @@
-import './AddElemInContainer/AddElemInContainer.js';
-import './BigBanner/BigBanner.js';
-import './ButtonContainerInProduct/ButtonContainerInProduct.js';
-import './ButtonListInHeader/ButtonListInHeader.js';
-import './DeleteClasses/DeleteClasses.js';
-import './DeleteElements/DeleteElements.js';
-import './FetchApi/FetchApi.js';
-import './GridBannerGallery/GridBannerGallery.js';
-import './LikeInCard/LikeInCard.js';
-import './Requisites/Requisites.js';
+import AddElemInContainer from './AddElemInContainer/AddElemInContainer.js';
+import BigBanner from './BigBanner/BigBanner.js';
+import ButtonContainerInProduct from './ButtonContainerInProduct/ButtonContainerInProduct.js';
+import ButtonListInHeader from './ButtonListInHeader/ButtonListInHeader.js';
+import DeleteClasses from './DeleteClasses/DeleteClasses.js';
+import DeleteElements from './DeleteElements/DeleteElements.js';
+import FetchApi from './FetchApi/FetchApi.js';
+import GridBannerGallery from './GridBannerGallery/GridBannerGallery.js';
+import LikeInCard from './LikeInCard/LikeInCard.js';
+import Requisites from './Requisites/Requisites.js';
+
+export {
+  AddElemInContainer,
+  BigBanner,
+  ButtonListInHeader,
+  ButtonContainerInProduct,
+  DeleteClasses,
+  DeleteElements,
+  FetchApi,
+  GridBannerGallery,
+  LikeInCard,
+  Requisites
+}
