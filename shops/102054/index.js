08eeaeeeb0101369b49e7712ef4d20302c3770cf
Coospir <eugene.starodubov@gmail.com>
2020-11-19 13:27:40 +0300

    Updated

--- shops/102054/index.js
+++ shops/102054/index.js
@@ -23,10 +23,16 @@ window.onload = () => {
     elem.toggleClass('scrolled', threshold);
     if(document.querySelector("header").classList.contains("scrolled")) {
       document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo.navbar__block > div > button").style.cssText = "color: #333"
-      document.querySelector(".navbar_item").style.cssText = "color: #333"
+      document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo.navbar__block > a:nth-child(3)").style.cssText = "color: #333"
+      document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo.navbar__block > a:nth-child(4)").style.cssText = "color: #333"
+      document.querySelector("#search > button > i").style.cssText = "color: #333"
+      document.querySelector("#cart").style.cssText = "color: #333"
     } else {
       document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo.navbar__block > div > button").style.cssText = "color: #fff"
-      document.querySelector(".navbar_item").style.cssText = "color: #fff"
+      document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo.navbar__block > a:nth-child(3)").style.cssText = "color: #fff"
+      document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo.navbar__block > a:nth-child(4)").style.cssText = "color: #fff"
+      document.querySelector("#search > button > i").style.cssText = "color: #fff"
+      document.querySelector("#cart").style.cssText = "color: #fff"
     }
   }
   $(window).on({ scroll: scrolled });
