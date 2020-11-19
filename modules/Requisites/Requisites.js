42466d4ffea3595c95b5fe8ce9925f9b6e2d95fe
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-19 12:57:47 +0300

    реквизиты

--- modules/Requisites/Requisites.js
+++ modules/Requisites/Requisites.js
@@ -57,29 +57,3 @@ export default class Requisites {
     }
   }
 }
-
-const data = {
-  'Организация' : 'ИП ЩЕРБАКОВ АЛЕКСАНДР СЕРГЕЕВИЧ',
-  'Юр. Адрес': 'г. Екатеринбург, ул. Щербакова 4 офис 302',
-  'Почтовый адрес': 'г. Екатеринбург, ул. Щербакова 4 офис 302',
-  'ИНН': '667105593531',
-  'ОГРН': '317665800123460',
-  'Банк': 'УРАЛЬСКИЙ БАНК ПАО СБЕРБАНК',
-  'Рассчетный счет': '40802810016540071804',
-  'Корр. Счёт': '30101810500000000674',
-  'БИК': '046577674',
-  'Директор': 'ЩЕРБАКОВ АЛЕКСАНДР СЕРГЕЕВИЧ',
-};
-
-if (location.href.includes('term-of-use')) {
-  const req = new Requisites(data, 'afterbegin');
-
-  req.setTemplate();
-}
-
-if (location.href.includes('contact')) {
-  const media = document.querySelector('.contact-info > .media-list > .media:last-child');
-
-  const req = new Requisites(data, 'afterend', media);
-  req.setTemplate();
-}
