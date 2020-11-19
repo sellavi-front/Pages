ff29e338670988eee3f0963f1683798b0f2571c6
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-19 16:28:18 +0300

    search

--- shops/101470/index.js
+++ shops/101470/index.js
@@ -39,7 +39,7 @@ if (document.querySelector('.wrapper')) {
   const cartText = document.querySelector('#cart > span');
   const icoUser = document.querySelector('.icon-user');
   const headerSearch = document.querySelector('.header-search');
-  const buttonList = document.querySelector('.button__list');
+  //const buttonList = document.querySelector('.button__list');
   const navigation = document.querySelector('.navigation-wrapper>div>.navigation');
   const headerLogo = headerWrapper.querySelector('.logo');
   const footerContentText = document.querySelectorAll('.footer_content_text');
@@ -76,7 +76,6 @@ if (document.querySelector('.wrapper')) {
   headerLogo.insertAdjacentHTML('afterend', navigation.outerHTML);
   footerWrapper.insertAdjacentHTML('beforeend', footerWidgetCol2List.outerHTML);
 
-  buttonList.insertAdjacentHTML('afterbegin', headerSearch.outerHTML);
   deleteElements.remove();
 
   deleteClassesFooter.findAndDelete();
