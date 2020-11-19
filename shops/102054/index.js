ddc89cf93d84e67115e6d3023ca6e5683e3cb0a6
Coospir <eugene.starodubov@gmail.com>
2020-11-19 13:44:53 +0300

    Updated

--- shops/102054/index.js
+++ shops/102054/index.js
@@ -27,12 +27,14 @@ window.onload = () => {
       document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo.navbar__block > a:nth-child(4)").style.cssText = "color: #333"
       document.querySelector("#search > button > i").style.cssText = "color: #333"
       document.querySelector("#cart").style.cssText = "color: #333"
+      document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo.navbar__block > div > div").cssText = ""
+
     } else {
       document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo.navbar__block > div > button").style.cssText = "color: #fff"
       document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo.navbar__block > a:nth-child(3)").style.cssText = "color: #fff"
       document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo.navbar__block > a:nth-child(4)").style.cssText = "color: #fff"
       document.querySelector("#search > button > i").style.cssText = "color: #fff"
-      document.querySelector("#cart").style.cssText = "color: #fff"
+      document.querySelector(".drop-content a").style.cssText = "color: #333"
     }
   }
   $(window).on({ scroll: scrolled });
