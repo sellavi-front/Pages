9758d572a67286132c5cbed02e17359fa2b31412
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-19 14:11:31 +0300

    slider

---
+++ shops/102063/utils/bootstrapClasses.js
@@ -0,0 +1,15 @@
+export default bootstrapClasses = [
+  'col-lg-4',
+  'col-xl-3',
+  'mb-0',
+  'mb-md-4',
+  'col-12',
+  'col-sm-12',
+  'col-sm-6',
+  'col-md-6',
+  'col-md-3',
+  'col-lg-6',
+  'col-xl-4',
+  'mb-4',
+  'container'
+];
