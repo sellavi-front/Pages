eea9a7d7b7adc1c4e25b655d790f24db80a2f195
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-20 11:29:05 +0300

    styles header

--- shops/100967/index.js
+++ shops/100967/index.js
@@ -1,7 +1,9 @@
 import ButtonListInHeader from  '../../modules/ButtonListInHeader/ButtonListInHeader.js'
 
 const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
+const hideCart = document.querySelector('.header-wrapper>.container>.header-cart');
 
 const buttonsInHeader = new ButtonListInHeader(headerWrapper);
 
 buttonsInHeader.setTemplate(headerWrapper);
+hideCart.remove();
