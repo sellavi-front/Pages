f8b00d31bbb8ba9a2a41c36d79dcc17778962bc4
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-20 14:08:45 +0300

    fix ico header

--- shops/100967/index.js
+++ shops/100967/index.js
@@ -1,17 +1,21 @@
-import ButtonListInHeader from  '../../modules/ButtonListInHeader/ButtonListInHeader.js'
+import ButtonListInHeader from '../../modules/ButtonListInHeader/ButtonListInHeader.js';
 
 const icons = {
-  cart: '<img src="https://res.cloudinary.com/gz-company/image/upload/v1605863312/Miru/assets/bag-black_li2hqx.svg">',
-  like: '<img src="https://res.cloudinary.com/gz-company/image/upload/v1605863437/Miru/assets/icon_di3am3.svg">',
+  cart:
+    '<img src="https://res.cloudinary.com/gz-company/image/upload/v1605863312/Miru/assets/bag-black_li2hqx.svg">',
+  like:
+    '<img src="https://res.cloudinary.com/gz-company/image/upload/v1605863437/Miru/assets/icon_di3am3.svg">',
+};
+
+if (document.querySelector('.wrapper')) {
+  const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
+  const hideCart = document.querySelector('.header-wrapper>.container>.header-cart');
+  const icoUser = document.querySelector('.button__list_elem.icon-user');
+  const buttonsInHeader = new ButtonListInHeader(headerWrapper, icons);
+
+  buttonsInHeader.setTemplate();
+  hideCart.remove();
+  icoUser.remove();
+
+  console.log(icoUser);
 }
-
-const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
-const hideCart = document.querySelector('.header-wrapper>.container>.header-cart');
-const icoUser = document.querySelector('.button__list_elem.icon-user');
-const buttonsInHeader = new ButtonListInHeader(headerWrapper, icons);
-
-buttonsInHeader.setTemplate();
-hideCart.remove();
-icoUser.remove();
-
-console.log(icoUser);
