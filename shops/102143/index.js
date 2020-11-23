081a66516a7a2c0c488655b778c778308051face
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-23 14:59:43 +0300

    102143 requsites

---
+++ shops/102143/index.js
@@ -0,0 +1,15 @@
+import Requisites from '../../modules/Requisites/Requisites.js';
+import requsites from './utils/requsites.js'
+
+if (location.href.includes('term-of-use')) {
+  const req = new Requisites(requsites, 'beforeend', 'ИП ТЮМЕНЦЕВА МАРИЯ ВАЛЕРЬЕВНА');
+
+  req.setTemplate();
+}
+
+if (location.href.includes('contact')) {
+  const media = document.querySelector('.contact-info > .media-list > .media:last-child');
+
+  const req = new Requisites(requsites, 'afterend', 'ИП ТЮМЕНЦЕВА МАРИЯ ВАЛЕРЬЕВНА', media);
+  req.setTemplate();
+}
