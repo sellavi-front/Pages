a0e90b2b41de9acc020dbd106d21a45594fa745f
Coospir <eugene.starodubov@gmail.com>
2020-11-23 17:16:23 +0300

    New links in navbar

--- shops/100980/index.js
+++ shops/100980/index.js
@@ -9,16 +9,13 @@ window.onload = () => {
   let logo = document.querySelector("#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div > div.logo")
   logo.classList.add("navbar__block")
   let navbar = `
-    <div class="navbar_item dropdown">
-      <button class="dropbtn">О компании</button>
-      <div class="dropdown-content">
-        <a>Как работаем</a>
-        <a>Отзывы</a>
-        <a>Контакты</a>
-      </div>
-    </div>
-    <a class="navbar_item" href="https://store102054.sellavi.com/man/">Поставщики</a>
-    <a class="navbar_item" href="https://store102054.sellavi.com/woman/">Преимущества</a>
+    <a class="navbar_item" href="#">Все товары</a>
+    <a class="navbar_item" href="#">О компании</a>
+    <a class="navbar_item" href="#">Доставка</a>
+    <a class="navbar_item" href="#">Оплата и возврат</a>
+    <a class="navbar_item" href="#">Комплексное оснащение</a>
+    <a class="navbar_item" href="#">Готовые решения</a>
+    <a class="navbar_item" href="#">Контакты</a>
   `
   logo.insertAdjacentHTML("beforeend", navbar)
 
