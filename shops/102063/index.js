227b88d0ad4a1f35037ff68ff2e8cbc8b77d0871
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-11-24 18:32:33 +0300

    test for error of footer

--- shops/102063/index.js
+++ shops/102063/index.js
@@ -8,10 +8,6 @@ import sliderData from './utils/sliderData.js';
 import bootstrapClasses from './utils/bootstrapClasses.js';
 import dataSmallCat from './utils/dataSmallCat.js'
 
-/* catalog add sections */
-document.querySelector('#home > div > footer > div.footer-widgets > div.container-fluid > div').insertAdjacentHTML('beforeend', '<div class="col-md-3"><div class="widget widget-categories"><h4 class="widget-title">Каталог</h4><ul><li><a href="https://store102063.sellavi.com/%D0%B0%D0%BA%D0%B2%D0%B0%D1%84%D0%BE%D1%80/" role="link">Аквафор</a></li><li><a href="https://store102063.sellavi.com/%D0%B1%D0%B0%D1%80%D1%8C%D0%B5%D1%80/" role="link">Барьер</a></li><li><a href="https://store102063.sellavi.com/%D0%B3%D0%B5%D0%B9%D0%B7%D0%B5%D1%80/" role="link">Гейзер</a></li></ul></div></div>');
-
-
 
 if (document.querySelector('.wrapper')) {
   const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
@@ -54,6 +50,11 @@ if (document.querySelector('.common-home')) {
   slider.setOptions();
 
 }
+
+if (document.querySelector('#home > div > footer > div.footer-widgets > div.container-fluid > div')) {
+  /* catalog add sections */
+  document.querySelector('#home > div > footer > div.footer-widgets > div.container-fluid > div').insertAdjacentHTML('beforeend', '<div class="col-md-3"><div class="widget widget-categories"><h4 class="widget-title">Каталог</h4><ul><li><a href="https://store102063.sellavi.com/%D0%B0%D0%BA%D0%B2%D0%B0%D1%84%D0%BE%D1%80/" role="link">Аквафор</a></li><li><a href="https://store102063.sellavi.com/%D0%B1%D0%B0%D1%80%D1%8C%D0%B5%D1%80/" role="link">Барьер</a></li><li><a href="https://store102063.sellavi.com/%D0%B3%D0%B5%D0%B9%D0%B7%D0%B5%D1%80/" role="link">Гейзер</a></li></ul></div></div>');
+} else {alert('error');}
 
 /* description */
 const oldNode = document.querySelector('#tab-description');
