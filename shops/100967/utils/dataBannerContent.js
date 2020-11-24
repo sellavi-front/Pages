b921470341b45f184d7637d2f03a8f43ae3af77b
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-24 15:49:13 +0300

    contentBanner

--- shops/100967/utils/dataBannerContent.js
+++ shops/100967/utils/dataBannerContent.js
@@ -4,7 +4,7 @@ const dataBannerContent = [
     text: 'с 1 по 15 декабря 2020',
     linkSrc: '#',
     linkContent: 'Подробнее',
-    banner: 'https://res.cloudinary.com/gz-company/image/upload/v1606219149/Miru/Group_86_1_gtbyfu.png'
+    banner: 'https://res.cloudinary.com/gz-company/image/upload/v1606222046/Miru/Group_33_kdi7yl.png'
   }
 ]
 
