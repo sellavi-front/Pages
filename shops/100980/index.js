902d5648529df1d50371fc066144fbd638c1dd7a
Coospir <eugene.starodubov@gmail.com>
2020-11-24 11:59:45 +0300

    Added pictures and grid

--- shops/100980/index.js
+++ shops/100980/index.js
@@ -20,24 +20,31 @@ window.onload = () => {
   logo.insertAdjacentHTML("beforeend", navbar)
   let banner = `
   <div class="banner__container">
-  <div class="banner__item">
-    <div class="banner__column">
-      <div class="banner__column__item">
-        <h4>Проектируем, оснащаем оборудованием и запускаем кулинарные пространства</h4>
+    <div class="banner__item">
+      <div class="banner__column">
+        <div class="banner__column__item">
+          <h4>Проектируем, оснащаем оборудованием и запускаем кулинарные пространства</h4>
+          <div class="info__block">
+              <div class="info__block__item">
+
+              </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="banner__item">
-    <div class="banner__column">
-      <div class="banner__column__item">
-        <form class="banner__submit__form">
-          <h6>Оставьте заявку прямо сейчас</h6>
-          <input class="banner_submit__form__input" type="text" placeholder="Ваше имя">
-          <input class="banner_submit__form__input" type="text" placeholder="Ваш телефон">
-          <input class="banner_submit__form__input" type="text" placeholder="Ваш E-Mail">
-        </form>
+    <div class="banner__item">
+      <div class="banner__column">
+        <div class="banner__column__item">
+          <form class="banner__submit__form">
+            <h6>Оставьте заявку прямо сейчас</h6>
+            <input class="banner_submit__form__input" type="text" placeholder="Ваше имя">
+            <input class="banner_submit__form__input" type="text" placeholder="Ваш телефон">
+            <input class="banner_submit__form__input" type="text" placeholder="Ваш E-Mail">
+            <p>Получите предложение и детальный план реализации проекта</p>
+            <button type="button" class="banner__submit__form__btn">Получить предложение</button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
 </div>`
 }
