f7f9b4f36475f68156262b77a24dc1f0efe755c5
Coospir <eugene.starodubov@gmail.com>
2020-11-24 16:51:57 +0300

    Adaptive ContentLayoutOne

--- shops/100980/index.js
+++ shops/100980/index.js
@@ -55,20 +55,22 @@ window.onload = () => {
         </div>
     </div>`
   let contentLayoutOne = `
-    <div class="content__layout__container">
-      <div class="content__layout__container__item">
-        <img src="https://coospir.github.io/Pages/shops/100980/media/Group.png" />
-      </div>
-      <div class="content__layout__container__item">
-        <div class="content__layout__container__item__description">
-        <h1>Компания Tradequip</h1>
-        <p>Эксперт по проектированию, оснащению оборудованием и запуску кулинарных пространств.</p>
+    <div class="container">
+      <div class="content__layout__container">
+        <div class="content__layout__container__item">
+          <img src="https://coospir.github.io/Pages/shops/100980/media/Group.png" />
+        </div>
+        <div class="content__layout__container__item">
+          <div class="content__layout__container__item__description">
+          <h1>Компания Tradequip</h1>
+          <p>Эксперт по проектированию, оснащению оборудованием и запуску кулинарных пространств.</p>
 
-        <p>Уже более 3 лет мы помогаем открывать фудкорты, рестораны, кафе, бары, пекарни и пищевые производства.</p>
+          <p>Уже более 3 лет мы помогаем открывать фудкорты, рестораны, кафе, бары, пекарни и пищевые производства.</p>
 
-        <p>В портфеле компании есть разработанные технологами готовые комплекты оборудования и профессионального инвентаря для любой кухни мира.</p>
+          <p>В портфеле компании есть разработанные технологами готовые комплекты оборудования и профессионального инвентаря для любой кухни мира.</p>
 
-        <p>Наш продукт это созданные по всем стандартам и качествам кулинарные пространства с уникальным дизайном и собственным фирменным стилем.</p>
+          <p>Наш продукт это созданные по всем стандартам и качествам кулинарные пространства с уникальным дизайном и собственным фирменным стилем.</p>
+          </div>
         </div>
       </div>
     </div>
