3ac8ddac99d1d2340b9cb6014d53eaf479592ce1
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-25 16:00:44 +0300

    адаптив для блока

--- shops/100967/index.js
+++ shops/100967/index.js
@@ -115,6 +115,6 @@ if (document.querySelector('.common-home')) {
   // Коробка которая вылезает за баннер
   bannerContentItemImg.insertAdjacentHTML(
     'afterbegin',
-        `<img class="img-1" src="https://res.cloudinary.com/gz-company/image/upload/v1606308936/Miru/assets/Mock%D0%B0%D0%B2%D0%BF%D0%B2%D0%B0%D0%BFup_1_4_2_stp1sm.png">`,
+        `<img class="img-1" src="https://res.cloudinary.com/gz-company/image/upload/v1606223895/Miru/Group_88_1_lfzmtk.png">`,
   );
 }
