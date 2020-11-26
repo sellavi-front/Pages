aaa08f0f3712ce5f1c202fa8645ace3886bdc067
Coospir <eugene.starodubov@gmail.com>
2020-11-26 16:00:35 +0300

    Updated component

--- components/GetCategoriesFromNavbar/GetCategoriesFromNavbar.js
+++ components/GetCategoriesFromNavbar/GetCategoriesFromNavbar.js
@@ -1,31 +1,20 @@
 import CreateAddContainer from '../../modules/CreateAddContainer/CreateAddContainer.js';
 
-/*function GetCategoriesFromNavbar() {
-  let result = []
-  let nodeList = document.querySelectorAll("header>.navigation-wrapper>.container>.navigation>.nav.sf-menu>li")
-  nodeList.forEach((item, index) => {
-    result[index] = {
-      name: item.firstElementChild.innerText,
-      link: item.firstElementChild.href
-    }
-  })
-  return result
-}*/
-
-
 export default class GetCategoriesFromNavbar {
-  constructor(list, title, container, target) {
+  constructor(list, arr, title, container, target) {
     this.list = list;
+    this.arr = arr;
     this.title = title;
     this.container = container;
     this.target = target;
   }
 
   getCategoriesFromNavbar() {
-    return this.list.map(item => {
-     return {
-          name: item.firstElementChild.outerHTML
-      }
+    return this.list.map((item, index) => {
+       return {
+            img: this.arr[index],
+            name: item.firstElementChild.outerHTML
+       }
     })
   }
 
@@ -42,6 +31,7 @@ export default class GetCategoriesFromNavbar {
     return this.getCategoriesFromNavbar().map(
       (item) => `
         <div class="categories__container__item">
+        <p>${item.img}</p>
           ${item.name}
         </div>
       `,
