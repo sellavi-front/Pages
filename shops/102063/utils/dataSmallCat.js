31a3d20c055bf56a2515305a6d8f54649ff777d2
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-11-26 11:57:52 +0300

    change photos in custom category

--- shops/102063/utils/dataSmallCat.js
+++ shops/102063/utils/dataSmallCat.js
@@ -1,32 +1,32 @@
 const dataSmallCat = [
   {
     href: '#',
-    src: 'https://images.unsplash.com/photo-1526868452228-6590028c2fac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606380941/water_filter_small_inmdvg.png',
     title: 'Очистка воды в квартиры',
   },
   {
     href: '#',
-    src: 'https://images.unsplash.com/photo-1580940040649-e3d55edb1e0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80',
+    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606380941/photo-1580940040649-e3d55edb1e0c_xhycja.png',
     title: 'Очистка воды в дом',
   },
   {
     href: '#',
-    src: 'https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=625&q=80',
+    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606380941/photo-1593642634443-44adaa06623a_uq3dyu.png',
     title: 'Очистка воды в офис',
   },
   {
     href: '#',
-    src: 'https://images.unsplash.com/photo-1569966459694-faf2bf4af6c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606380941/photo-1569966459694-faf2bf4af6c6_ssouyg.png',
     title: 'Фильтры для кафе и ресторанов',
   },
   {
     href: '#',
-    src: 'https://images.unsplash.com/photo-1594807928657-90490c2dae91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606380941/photo-1594807928657-90490c2dae91_fpnfcx.png',
     title: 'Обслуживание',
   },
   {
     href: '#',
-    src: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606380941/%D0%91%D1%84%D1%8B%D0%B0%D1%8B%D0%B2%D0%B0%D1%8B%D0%B2%D0%B0%D0%B5%D0%B7_%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-3_xtvubr.png',
     title: 'Анализ воды',
   },
 ];
