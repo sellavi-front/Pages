eddd0b720fb6437b22db1079a7e2c804b0d88e24
Coospir <eugene.starodubov@gmail.com>
2020-11-26 16:18:52 +0300

    Updated component

--- shops/100980/index.js
+++ shops/100980/index.js
@@ -84,11 +84,17 @@ window.onload = () => {
     document.querySelector("#home > div.wrapper.wrapper-closed > div.content-area > section > div").setAttribute("class", "container-fluid")
     let nodeList = document.querySelectorAll("header>.navigation-wrapper>.container>.navigation>.nav.sf-menu>li")
     let imgArr = [
-      'test',
-      'asdasd',
-      'weq',
-      'sadd',
-      'qwe',
+      'https://coospir.github.io/Pages/shops/100980/media/categories/dez_sredstv.png',
+      'https://coospir.github.io/Pages/shops/100980/media/categories/line.png',
+      'https://coospir.github.io/Pages/shops/100980/media/categories/meh_oborud.png',
+      'https://coospir.github.io/Pages/shops/100980/media/categories/neutral.png',
+      'https://coospir.github.io/Pages/shops/100980/media/categories/posud.png',
+      'https://coospir.github.io/Pages/shops/100980/media/categories/prof-coffee.png',
+      'https://coospir.github.io/Pages/shops/100980/media/categories/prof_inv.png',
+      'https://coospir.github.io/Pages/shops/100980/media/categories/teplo.png',
+      'https://coospir.github.io/Pages/shops/100980/media/categories/torg-oborud.png',
+      'https://coospir.github.io/Pages/shops/100980/media/categories/pekar.png',
+      'https://coospir.github.io/Pages/shops/100980/media/categories/holod.png',
     ]
     let categories = new GetCategoriesFromNavbar(Array.from(nodeList), imgArr, "Каталог товаров", customSection, "afterbegin")
     categories.render()
