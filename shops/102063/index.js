a154ccfe5a16fc112876288d203202540c72431f
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-11-26 10:33:26 +0300

    главная fix

--- shops/102063/index.js
+++ shops/102063/index.js
@@ -51,7 +51,7 @@ if (document.querySelector('.common-home')) {
 
 }
 
-document.querySelector('#home > div > header > div.navigation-wrapper').insertAdjacentHTML('afterbegin', '<div><a href="https://store102063.sellavi.com/">Главная</a></div>');
+// document.querySelector('#home > div > header > div.navigation-wrapper').insertAdjacentHTML('afterbegin', '<div><a href="https://store102063.sellavi.com/">Главная</a></div>');
 
 
 /* description */
