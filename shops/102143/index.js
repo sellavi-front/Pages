47af0457952a7b1b40af4f8d8a995d7c9d2fe642
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-27 17:32:16 +0300

    social icons

--- shops/102143/index.js
+++ shops/102143/index.js
@@ -39,6 +39,7 @@ if (document.querySelector('.common-home')) {
 
   saleBanner.render();
   imgNearText.render();
+  social.render()
 
   const bannerContentItemBg = document.querySelector('.banner-content__item_bg > picture');
   bannerContentItemBg.insertAdjacentHTML('afterbegin', `<source media="(max-width: 495px)" src="https://res.cloudinary.com/gz-company/image/upload/v1606481504/ThaiCosmetic/Group_75_lfbifj.png"> </source>`)
