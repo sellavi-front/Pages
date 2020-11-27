99adda306ad483a77529f56397d32b400035f0b6
root <root@DESKTOP-I76FJL8.localdomain>
2020-11-27 11:54:44 +0300

    updated

--- shops/102121/index.js
+++ shops/102121/index.js
@@ -7,8 +7,6 @@ let customBanner = document.querySelector(".custom_banner")
 let customSection = document.querySelector(".custom_section")
 
 if(window.location.pathname === '/' || window.location.pathname === '/?from_admin') {
-  /* Delete main page */
-  document.querySelector("#undefined-sticky-wrapper > header > div.navigation-wrapper > div > nav > ul > li:nth-child(1)").remove()
   let nodeList = document.querySelectorAll("header>.navigation-wrapper>.container>.navigation>.nav.sf-menu>li")
   let categories = new GetCategoriesFromNavbar(Array.from(nodeList), imgArr, "Каталог товаров", customSection, "afterbegin")
   categories.render()
