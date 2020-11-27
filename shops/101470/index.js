b80b96d87d6c9f8c87f1fb545321674a2ea37ecc
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-11-27 15:10:10 +0300

    change logo

--- shops/101470/index.js
+++ shops/101470/index.js
@@ -108,7 +108,7 @@ if (document.querySelector('.common-home')) {
   bigBanner.render();
   horizontTitleDecription.render();
 
-  logo.setAttribute('src', 'https://res.cloudinary.com/gz-company/image/upload/v1606396562/1985/Image-1_3_ubaczw.png');
+  logo.setAttribute('src', 'https://res.cloudinary.com/gz-company/image/upload/v1605624670/1985/Image-1_2_zvkexd.png');
 }
 
 if (document.querySelector('.product-item')) {
