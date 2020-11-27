9768f3bb6a13ef2ab4e25edebb587c1e0ce41e4a
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-27 15:47:29 +0300

    add exampleUse

---
+++ components/ContentBannerTypeOne/exampleUse.js
@@ -0,0 +1,20 @@
+const dataBannerContent = [
+  {
+    title: 'Cкидка на всю продукцию Miru <b>15%</b>',
+    text: 'с 1 по 15 декабря 2020',
+    linkSrc: '#',
+    linkContent: 'Подробнее',
+    banner: 'https://res.cloudinary.com/gz-company/image/upload/v1606308936/Miru/assets/Mock%D0%B0%D0%B2%D0%BF%D0%B2%D0%B0%D0%BFup_1_4_2_stp1sm.png',
+    srcset: 'https://res.cloudinary.com/gz-company/image/upload/v1606305782/Miru/assets/Group_96_2_li6urs.png',
+  }
+]
+
+export default dataBannerContent;
+
+const contentBanner = new ContentBannerTypeOne(
+  document.querySelector('body>.wrapper>.content-area>.page-section'),
+  'beforeEnd',
+  dataBannerContent,
+);
+
+contentBanner.render();
