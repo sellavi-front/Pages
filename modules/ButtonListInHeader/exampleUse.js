a1785aba03812c25bc71886f4b9074568f221f95
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-27 12:50:32 +0300

    правки по большому баннеру

--- modules/ButtonListInHeader/exampleUse.js
+++ modules/ButtonListInHeader/exampleUse.js
@@ -4,7 +4,7 @@
   const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
   const hideCart = document.querySelector('.header-wrapper>div>.header-cart');
 
-  const buttonsInHeader = new ButtonListInHeader(headerWrapper, icons);
+  const buttonsInHeader = new ButtonListInHeader(headerWrapper);
 
   hideCart.remove();
 // End
