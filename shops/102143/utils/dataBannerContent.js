d2c7ec446d247b4b0fa391d5b9b67a81e9d9b1d0
Закир Гафиятов <gafiyatovz@gmail.com>
2020-11-27 16:28:12 +0300

    about us

--- shops/102143/utils/dataBannerContent.js
+++ shops/102143/utils/dataBannerContent.js
@@ -4,7 +4,7 @@ const dataBannerContent = [
     text: 'с 1 по 15 декабря 2020',
     linkSrc: '#',
     linkContent: 'Подробнее',
-    banner: 'https://res.cloudinary.com/gz-company/image/upload/v1606481454/ThaiCosmetic/Group_74_jti6ma.png',
+    banner: 'https://res.cloudinary.com/gz-company/image/upload/v1606483305/ThaiCosmetic/apteka-opt_2_1_w7woin.png',
     srcset: 'https://res.cloudinary.com/gz-company/image/upload/v1606481504/ThaiCosmetic/Group_75_lfbifj.png',
   }
 ]
