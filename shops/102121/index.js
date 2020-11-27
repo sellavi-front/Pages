a343538338ff18ae2d2b1bc05d3657cf951f3b06
root <root@DESKTOP-I76FJL8.localdomain>
2020-11-27 11:31:38 +0300

    updated shop

--- shops/102121/index.js
+++ shops/102121/index.js
@@ -1,5 +1,13 @@
-import Requisites from '../../modules/Requisites/Requisites.js';
+import Requisites from '../../modules/Requisites/Requisites.js'
 import requsites from './utils/requsites.js'
+import GetCategoriesFromNavbar from "../../components/GetCategoriesFromNavbar/GetCategoriesFromNavbar.js"
+import imgArr from "./utils/images.js"
+
+if(window.location.pathname === '/' || window.location.pathname === '/?from_admin') {
+  let nodeList = document.querySelectorAll("header>.navigation-wrapper>.container>.navigation>.nav.sf-menu>li")
+  let categories = new GetCategoriesFromNavbar(Array.from(nodeList), imgArr, "Каталог товаров", customSection, "afterbegin")
+  categories.render()
+}
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Харламова Зоя Николаевна');
---
+++ shops/102121/utils/images.js
@@ -0,0 +1,8 @@
+const imgArr = [
+  'https://vsepariki.com/upload/iblock/020/02008b7cc71aaee35e91fdfd39064fb3.jpg',
+  'http://hair-wig.ru/images/speasyimagegallery/albums/3/images/ew01.jpg',
+  'http://hair-wig.ru/images/speasyimagegallery/albums/3/images/ew17.jpg'
+]
+
+export default imgArr
+
