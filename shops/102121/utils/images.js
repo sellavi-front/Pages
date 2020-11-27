26ace2c4faaf0735776b62b287f0eebdc7e0c314
root <root@DESKTOP-I76FJL8.localdomain>
2020-11-27 13:01:18 +0300

    test

--- shops/102121/utils/images.js
+++ shops/102121/utils/images.js
@@ -1,7 +1,7 @@
 const imgArr = [
-  'https://coospir.github.io/Pages/shops/102121/media/nakladki.jpg',
-  'https://coospir.github.io/Pages/shops/102121/media/iskusstv.jpg',
-  'https://coospir.github.io/Pages/shops/102121/media/natural.jpg'
+  'https://res.cloudinary.com/dd2edlvdy/image/upload/v1606470353/nakladki_fwvq7h.jpg',
+  'https://res.cloudinary.com/dd2edlvdy/image/upload/v1606470343/iskusstv_jv5fza.jpg',
+  'https://res.cloudinary.com/dd2edlvdy/image/upload/v1606470343/natural_qh8cjz.jpg'
 ]
 
 export default imgArr
