da06b104864f6737a958885785c8c79baa2d1f68
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-11-27 14:44:27 +0300

    add if statements

--- shops/101977/index.js
+++ shops/101977/index.js
@@ -2,11 +2,13 @@ import Requisites from '../../modules/Requisites/Requisites.js';
 import requsites from './utils/requsites.js';
 import ButtonListInHeader from '../../modules/ButtonListInHeader/ButtonListInHeader';
 
-const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
-const hideCart = document.querySelector('.header-wrapper>div>.header-cart');
-const buttonsInHeader = new ButtonListInHeader(headerWrapper);
-buttonsInHeader.setTemplate()
-hideCart.remove();
+if (location.href.includes('/')) {
+  const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
+  const hideCart = document.querySelector('.header-wrapper>div>.header-cart');
+  const buttonsInHeader = new ButtonListInHeader(headerWrapper);
+  buttonsInHeader.setTemplate()
+  hideCart.remove();
+}
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Гралько Людмила Викторовна');
