e81e98bcd14bee7fd2c24bc6c8a995f894806d86
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-11-29 15:27:17 +0300

    change analysis

--- components/SmallCategory/exampleUse.js
+++ components/SmallCategory/exampleUse.js
@@ -27,7 +27,7 @@ const data = [
   },
   {
     href: '#',
-    src: 'https://images.unsplash.com/photo-1578496481449-cf2e845cc00c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
+    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606652783/photo-1578496481449-cf2e845cc00c_sbkxt4.png',
     title: 'Анализ воды',
   },
 ];
