d5d22d30823ccf286604ac1297e6d3083f0f70fe
Coospir <eugene.starodubov@gmail.com>
2020-11-30 16:01:10 +0300

    Created pages structure

--- shops/100980/pages/aboutCompany.js
+++ shops/100980/pages/aboutCompany.js
@@ -2,9 +2,12 @@ import contentOptimizer from "./contentOptimizer.js";
 
 let sectionTitles = document.querySelector(".page-section.titles")
 let information = document.querySelector(".page-section.color.information")
+
 const aboutCompanyTemplate = `
   <div class="container-fluid">
-    <div class="bread__crumbs_container"></div>
+    <div class="bread__crumbs_container">
+        <a href="/">Главная</a>><a href="/about-company/" class="active__page">О компании</a></div>
+    </div>
   </div>
 `
 
