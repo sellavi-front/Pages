48cd0d4fd3e489dba6fde606a600064333477d5a
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-11-30 12:02:00 +0300

    delete log

--- shops/101977/index.js
+++ shops/101977/index.js
@@ -4,8 +4,6 @@ import ButtonListInHeader from '../../modules/ButtonListInHeader/ButtonListInHea
 import BigBanner from '../../modules/BigBanner/BigBanner.js';
 import objBanner from './utils/bannerText.js';
 
-console.log(objBanner);
-
 const bigBanner = new BigBanner('https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606724800/jan-kopriva-sh_7sFEFICI-unsplash_1_gc6uba.png', objBanner);
 bigBanner.render()
 
