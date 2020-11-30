0121a702b976ca1ce2c3031290926b807310908c
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-11-30 11:28:52 +0300

    import BigBanner js and css

--- modules/BigBanner/exampleUse.js
+++ modules/BigBanner/exampleUse.js
@@ -1,2 +1,2 @@
-const bigBanner = new BigBanner('https://res.cloudinary.com/gz-company/image/upload/v1606466129/ThaiCosmetic/Group_73_yqnh52.png');
+const bigBanner = new BigBanner('https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606724800/jan-kopriva-sh_7sFEFICI-unsplash_1_gc6uba.png');
 bigBanner.render()
--- shops/101977/index.js
+++ shops/101977/index.js
@@ -1,12 +1,16 @@
 import Requisites from '../../modules/Requisites/Requisites.js';
 import requsites from './utils/requsites.js';
 import ButtonListInHeader from '../../modules/ButtonListInHeader/ButtonListInHeader.js';
+import BigBanner from '../../modules/BigBanner/BigBanner';
 
-  const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
-  const hideCart = document.querySelector('.header-wrapper>div>.header-cart');
-  const buttonsInHeader = new ButtonListInHeader(headerWrapper);
-  buttonsInHeader.setTemplate()
-  hideCart.remove();
+const bigBanner = new BigBanner('https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606724800/jan-kopriva-sh_7sFEFICI-unsplash_1_gc6uba.png');
+bigBanner.render()
+
+const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
+const hideCart = document.querySelector('.header-wrapper>div>.header-cart');
+const buttonsInHeader = new ButtonListInHeader(headerWrapper);
+buttonsInHeader.setTemplate();
+hideCart.remove();
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Гралько Людмила Викторовна');
