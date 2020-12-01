d962e872f220311eea57237ecb7d219340b9c586
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-01 12:23:57 +0300

    fixes

--- fixes/fixes.js
+++ fixes/fixes.js
@@ -1,7 +1,7 @@
-import ChangeAgreement from './ChangeAgreement/ChangeAgreement.js';
-import ChangeErrorMessage from './ChangeErrorEessage/ChangeErrorMessage.js';
-import TranslateAboutUs from './TranslateAboutUs/TranslateAboutUs.js';
-import FooterCopy from './FooterCopy/FooterCopy.js';
+import ChangeAgreement from './js/ChangeAgreement/ChangeAgreement.js';
+import ChangeErrorMessage from './js/ChangeErrorEessage/ChangeErrorMessage.js';
+import TranslateAboutUs from './js/TranslateAboutUs/TranslateAboutUs.js';
+import FooterCopy from './js/FooterCopy/FooterCopy.js';
 // import HideLoading from './HideLoading/HideLoading.js';
 
 export default { ChangeAgreement, ChangeErrorMessage, TranslateAboutUs, FooterCopy };
