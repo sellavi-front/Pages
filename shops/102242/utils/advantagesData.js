8a20e4005b00ea1289e167c6867cb0e1f854af97
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-01 17:46:41 +0300

    advantages

--- shops/102242/utils/advantagesData.js
+++ shops/102242/utils/advantagesData.js
@@ -3,19 +3,19 @@
     {
       img: 'https://res.cloudinary.com/gz-company/image/upload/v1606833197/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/IMG_5335_1_sio0ap.png',
       textTitle: '<img src="https://res.cloudinary.com/gz-company/image/upload/v1606833091/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/icons8-%D1%80%D1%83%D0%BB%D0%B5%D1%82%D0%BA%D0%B0-%D0%B4%D0%BB%D1%8F-%D1%88%D0%B8%D1%82%D1%8C%D1%8F_1_fw5oce.png">',
-      text: 'Индивидуальный пошив',
+      text: '<span>Индивидуальный пошив</span>',
       link: ''
     },
     {
       img: 'https://res.cloudinary.com/gz-company/image/upload/v1606833197/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/IMG_9732_1_xw5e8s.png',
       textTitle: '<img src="https://res.cloudinary.com/gz-company/image/upload/v1606833091/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/icons8-%D0%B5%D0%B2%D1%80%D0%BE%D0%BF%D0%B0_1_i8xlw1.png">',
-      text: 'Качественные материалы из Европы',
+      text: '<span>Качественные материалы из Европы</span>',
       link: ''
     },
     {
       img: 'https://res.cloudinary.com/gz-company/image/upload/v1606833198/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/IMG_8835_1_uughcs.png',
       textTitle: '<img src="https://res.cloudinary.com/gz-company/image/upload/v1606833103/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/icons8-%D0%BF%D0%BE%D1%88%D0%B8%D0%B2-%D0%BC%D1%83%D0%B6%D1%81%D0%BA%D0%BE%D0%B8%CC%86-%D0%BE%D0%B4%D0%B5%D0%B6%D0%B4%D1%8B_1_ly4glj.png">',
-      text: 'Собственное производство',
+      text: '<span>Собственное производство</span>',
       link: ''
     },
   ]
