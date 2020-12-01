a5fdfc2b3e18730b044488e3525ba8de60b2a27e
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-01 22:59:24 +0300

    test styled-components

--- shops/102015/modules/index.js
+++ shops/102015/modules/index.js
@@ -7,7 +7,7 @@ const Container = styled.div`
   height: 300px;
 `;
 
-render(
+document.querySelector('.custom_section').insertAdjacentElement('beforeend',
     <Container>
     </Container>
     );
