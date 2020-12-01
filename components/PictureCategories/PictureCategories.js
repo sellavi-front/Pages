1c3712195fcbac55c90f703414f39200fe481e98
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-01 16:13:59 +0300

    rename container

--- components/PictureCategories/PictureCategories.js
+++ components/PictureCategories/PictureCategories.js
@@ -3,7 +3,7 @@ import DeleteClasses from '../../modules/DeleteClasses/DeleteClasses.js';
 const selectors = ['col-md-6', 'mt-4'];
 
 if (document.querySelector('.common-home')) {
-  const categorie = document.querySelector("body > div.wrapper.wrapper-closed > div.content-area > section.page-section.homefeatured_category > div > div > div");
+  const categorie = document.querySelectorAll("body > div.wrapper.wrapper-closed > div.content-area > section.page-section.homefeatured_category > div > div > div");
   console.log(categorie);
   const forDelete = new DeleteClasses(categorie, selectors);
   forDelete.findAndDelete();
