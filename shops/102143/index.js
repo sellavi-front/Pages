2958fc75daffbea2aff687b90f8e69644e3c3311
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-01 12:01:10 +0300

    fixes

--- shops/102143/index.js
+++ shops/102143/index.js
@@ -1,5 +1,5 @@
-import '../../fixes/TranslateAboutUs/TranslateAboutUs.js'
-import '../../fixes/FooterCopy/FooterCopy.js';
+import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
+import '../../fixes/js/FooterCopy/FooterCopy.js';
 
 import ButtonListInHeader from '../../modules/ButtonListInHeader/ButtonListInHeader.js';
 import Requisites from '../../modules/Requisites/Requisites.js';
