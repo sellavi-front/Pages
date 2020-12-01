26e300455d4678928373ee7ed78f017813edd37c
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-01 11:46:59 +0300

    new shop and link styles fix

---
+++ shops/102242/index.js
@@ -0,0 +1,17 @@
+import Requisites from '../../modules/Requisites/Requisites.js';
+import requsites from './utils/requsites.js'
+
+import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
+
+// if (location.href.includes('term-of-use')) {
+//   const req = new Requisites(requsites, 'beforeend', 'ИП Харламова Зоя Николаевна');
+
+//   req.setTemplate();
+// }
+
+// if (location.href.includes('contact')) {
+//   const media = document.querySelector('.contact-info > .media-list > .media:last-child');
+
+//   const req = new Requisites(requsites, 'afterend', 'ИП Харламова Зоя Николаевна', media);
+//   req.setTemplate();
+// }
