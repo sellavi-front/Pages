becfe2d7d3e6fdc7d3a5e7a94b9dafdf4a6e0079
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-12-01 16:17:48 +0300

    quick fix )

--- shops/101977/index.js
+++ shops/101977/index.js
@@ -34,3 +34,5 @@ oldNavBar.remove();
 document.querySelector('#search > div.search_field > input').removeAttribute('placeholder');
 
 document.querySelector('#home > div > header > div.header-wrapper > div.container > nav').insertAdjacentHTML('beforebegin', '<span>AL`DOMA</span>');
+
+document.querySelector('#home > div > div > div.main-slider.content_center > div.custom_banner > div > div > div.banner__caption > a').insertAdjacentHTML('beforebegin', '<div>От производителя оптом и в розницу</div>');
