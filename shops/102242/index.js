8e799b9ed079e2f559d9d6faff594c2574f7722f
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-01 12:20:11 +0300

    fixes

--- shops/102242/index.js
+++ shops/102242/index.js
@@ -2,6 +2,13 @@ import Requisites from '../../modules/Requisites/Requisites.js';
 import requsites from './utils/requsites.js'
 
 import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
+import VideoBanner from '../../components/VideoBanner/VideoBanner.js';
+import constants from './constants/constants.js';
+
+
+if (document.querySelector('.common-home')) {
+  const videoBanner = new VideoBanner(constants.srcVideo);
+}
 
 // if (location.href.includes('term-of-use')) {
 //   const req = new Requisites(requsites, 'beforeend', 'ИП Харламова Зоя Николаевна');
