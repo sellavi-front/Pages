a78e3eeda61c3a80a6784d65692078ae3754ca4f
Coospir <eugene.starodubov@gmail.com>
2020-12-02 15:52:49 +0300

    updated 102054

--- shops/102054/index.js
+++ shops/102054/index.js
@@ -64,7 +64,7 @@ window.onload = () => {
               Your browser does not support the video tag.
           </video>
            <div class="video__banner__text__content">
-              <h1 class="video__banner__text__content__header"><a href="#">Новая коллекция</a></h1>
+              <h1 class="video__banner__text__content__header"><a href="/man/">Новая коллекция</a></h1>
            </div>
       </div>
     `)
