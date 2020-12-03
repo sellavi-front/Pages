6c4f9fe99e1da6571a6f04dbb069b7dfc65a60c8
Coospir <eugene.starodubov@gmail.com>
2020-12-03 16:14:31 +0300

    Updated DropDownList

--- shops/100980/index.js
+++ shops/100980/index.js
@@ -9,7 +9,7 @@ import renderDeliveryTemplate from "./pages/delivery/delivery.js"
 let pages = {
   home: document.querySelector(".common-home"),
   delivery: document.querySelector(".information-information-3585"),
-  complex: document.querySelector(".information-information-3586")
+  complex: document.querySelector(".information-information-3586"),
   about: document.querySelector(".information-information-3588")
 }
 
