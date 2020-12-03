1df080e753f241c3d61a92b318ae9c8d044544e5
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-12-03 12:05:45 +0300

    del leader

--- shops/101977/index.js
+++ shops/101977/index.js
@@ -53,9 +53,5 @@ document.querySelector('#home > div > div > div.main-slider.content_center > div
 
 document.querySelector('#home > div > div > section > div.about > div > div > div > div > h2').insertAdjacentHTML('beforeend', '<div class="flex-logo-text"><div class="thisLogo"></div><div class="thisLogoText">Al`DOMA</div></div>');
 
-document.querySelector('body>.wrapper>.content-area>.page-section').insertAdjacentHTML('beforeend', '<div class="leader"></div>');
-document.querySelector('#home > div > div > section > div.leader').insertAdjacentHTML('afterbegin', '<div class="leader-title">Станьте дилером и получите<p>дополнительные преимущества</p></div>');
-document.querySelector('.leader').insertAdjacentHTML('beforeend', '<div class="three-flex-block"><div class="first-block"><div class="left-side block1"></div><div class="right-side block1">Выгодные цены</div></div><div class="second-block"><div class="left-side block2"></div><div class="right-side block2">Бесплатную доставку</div></div><div class="third-block"><div class="left-side block3"></div><div class="right-side block3">Бонусы по итогам года</div></div></div>');
-document.querySelector('.leader').insertAdjacentHTML('beforeend', '<button type="button" class="be-leader">Стать лидером</button>');
 document.querySelector('body>.wrapper>.content-area>.page-section').insertAdjacentHTML('beforeend', '<div class="map-flex"><div class="our-contacts"><div>Контакты</div><div class="adress"><div>Адрес: </div><div>г.Волгоград, ул.Бурейская, д.8</div></div><div class="phone"><div>Телефон: </div><div>Техническая служба</div><div>Отдел продаж</div></div><div class="emails"><div>E-mail: </div><div>Ссылка</div></div></div><iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A7efb2b1c48455fd2e1c95a8256260ab3e86490a95490800b620cd291dff510af&amp;source=constructor" width="600" height="400" frameborder="0"></iframe></div>');
 document.querySelector('.footer').remove();
