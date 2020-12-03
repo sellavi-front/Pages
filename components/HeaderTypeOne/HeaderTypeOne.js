bab79a8c1d8ffc37d62f86871b3d58865e98e08c
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-03 16:43:53 +0300

    adaptive

--- components/HeaderTypeOne/HeaderTypeOne.js
+++ components/HeaderTypeOne/HeaderTypeOne.js
@@ -4,8 +4,20 @@ import NavTranslateTo from '../../modules/NavTranslateTo/NavTranslateTo.js';
 const headerWidgets = document.querySelector('.header_widgets');
 const headerContainer = document.querySelector('.header-wrapper>div');
 const headerLogo = document.querySelector('.header-wrapper .logo');
-
+const wishList = `
+  <div class="header_wishlist">
+    <a
+      id="wishlist"
+      class="position-relative"
+      href="https://${host}/index.php?route=account/wishlist"
+      role="link"
+    >
+      <i class="far fa-heart"></i>
+    </a>
+  </div>
+`
 new CreateAddContainer(headerContainer, 'beforeend', headerWidgets.outerHTML).render();
 headerWidgets.remove();
 
+new CreateAddContainer(headerWidgets, 'afterbegin', wishList)
 new NavTranslateTo(headerLogo, 'afterend').translate();
