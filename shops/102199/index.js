a2d23f86ac1a9addecce82f4a5bdb28bd0fcec7c
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-03 17:21:34 +0300

    create template for shop

--- shops/102199/index.js
+++ shops/102199/index.js
@@ -1,7 +1,6 @@
 import Requisites from '../../modules/Requisites/Requisites.js';
 import requsites from './utils/requsites.js'
-import '../../components/HeaderTypeOne/HeaderTypeOne.js';
-
+import '../../templates/TemplateTypeOne/TemplateTypeOne.js'
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Абдинов Эльдар Гейдар Оглы');
 
