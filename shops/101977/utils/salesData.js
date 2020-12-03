21769a9a15db1e536bf9eca44564559639f35c6a
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-12-03 14:19:52 +0300

    change all carts

--- shops/101977/utils/salesData.js
+++ shops/101977/utils/salesData.js
@@ -7,19 +7,19 @@
       link: '#'
     },
     {
-      img: 'https://res.cloudinary.com/gz-company/image/upload/v1606225301/Miru/%D0%B2%D0%B0%D0%BF_1_ua8yn7.png',
+      img: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606993848/scale_1200_1_q8q2k3.png',
       textTitle: 'Акция. ',
       text: 'Продукция Miru 1 day Flat Pack  с 1 по 31 декабря со скидкой 15%',
       link: '#'
     },
     {
-      img: 'https://res.cloudinary.com/gz-company/image/upload/v1606225406/Miru/138-package-bofghfgx-mockup_1_w95153.png',
+      img: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606993849/adrian-dascal-Ce_gQ7Z0eAc-unsplash_1_1_n9dpfk.png',
       textTitle: 'Акция. ',
       text: 'Продукция Miru 1 day Flat Pack  с 1 по 31 декабря со скидкой 15%',
       link: '#'
     },
     {
-      img: 'https://res.cloudinary.com/gz-company/image/upload/v1606225406/Miru/138-package-bofghfgx-mockup_1_w95153.png',
+      img: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606993848/fotis-nakos-YvHhZlcqTTE-unsplash_1_1_bda7lq.png',
       textTitle: 'Акция. ',
       text: 'Продукция Miru 1 day Flat Pack  с 1 по 31 декабря со скидкой 15%',
       link: '#'
