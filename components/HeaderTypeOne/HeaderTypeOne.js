68d120974dd13ce8dd1fa0897ea77dd7521c86b6
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-03 15:01:25 +0300

    hader type one

--- components/HeaderTypeOne/HeaderTypeOne.js
+++ components/HeaderTypeOne/HeaderTypeOne.js
@@ -3,4 +3,4 @@ import CreateAddContainer from '../../modules/CreateAddContainer/CreateAddContai
 const headerWidgets = document.querySelector('.header_widgets');
 const headerContainer = document.querySelector('.header-wrapper>div');
 
-new CreateAddContainer(headerContainer, 'beforeend', headerWidgets).render();
+new CreateAddContainer(headerContainer, 'beforeend', headerWidgets.outerHTML).render();
