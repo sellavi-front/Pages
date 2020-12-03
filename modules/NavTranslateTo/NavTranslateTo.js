fa298184949f892567a15593678185a4fe57525c
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-03 15:49:35 +0300

    new module TranslateNav

--- modules/NavTranslateTo/NavTranslateTo.js
+++ modules/NavTranslateTo/NavTranslateTo.js
@@ -4,10 +4,12 @@ export default class NavTranslateTo {
   constructor(container, target) {
     this.container = container;
     this.target = target;
+    this.wrapper = document.querySelector('.navigation-wrapper');
     this.nav = document.querySelector('.navigation-wrapper .navigation');
   }
 
   translate() {
     new CreateAddContainer(this.container, this.target, this.nav.outerHTML).render();
+    this.wrapper.remove();
   }
 }
