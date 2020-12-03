bbd43be54abbeedf92b08427ff759a24953d56fb
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-12-03 14:33:16 +0300

    fix

--- shops/101977/utils/salesData.js
+++ shops/101977/utils/salesData.js
@@ -2,26 +2,26 @@
   const salesData = [
     {
       img: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606993848/unnamed_1_gekvma.png',
-      textTitle: 'Акция. ',
-      text: 'Продукция Miru 1 day Flat Pack  с 1 по 31 декабря со скидкой 15%',
+      textTitle: 'Отлично пенится. ',
+      text: 'Благодаря уникальным моющим свойствам, пенообразованию и текучести бесконтактный шампунь проникает в места недоступные при ручной мойке.',
       link: '#'
     },
     {
       img: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606993848/scale_1200_1_q8q2k3.png',
-      textTitle: 'Акция. ',
-      text: 'Продукция Miru 1 day Flat Pack  с 1 по 31 декабря со скидкой 15%',
+      textTitle: 'Справляется с различными видами загрязнений. ',
+      text: 'Устойчивые пятна грязи, сажа, соль, смола, птичий помет, следы от насекомых и пр.',
       link: '#'
     },
     {
       img: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606993849/adrian-dascal-Ce_gQ7Z0eAc-unsplash_1_1_n9dpfk.png',
-      textTitle: 'Акция. ',
-      text: 'Продукция Miru 1 day Flat Pack  с 1 по 31 декабря со скидкой 15%',
+      textTitle: 'Экономичный расход. ',
+      text: 'Наша продукция - это концентрат разбавляемый перед использованием. Что делает шампуни экономичными в использовании',
       link: '#'
     },
     {
       img: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606993848/fotis-nakos-YvHhZlcqTTE-unsplash_1_1_bda7lq.png',
-      textTitle: 'Акция. ',
-      text: 'Продукция Miru 1 day Flat Pack  с 1 по 31 декабря со скидкой 15%',
+      textTitle: 'Эффект полировки. ',
+      text: 'Автошампунь - это мягкое мыло, которое не только удаляет грязь но и наносит тонкий защитный слой против царапин и придает блеск кузову.',
       link: '#'
     },
   ]
