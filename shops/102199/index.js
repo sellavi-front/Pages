f41ee19bab39ab18c6148312588140ffda5667e6
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-03 14:57:21 +0300

    hader type one

--- shops/102199/index.js
+++ shops/102199/index.js
@@ -1,5 +1,6 @@
 import Requisites from '../../modules/Requisites/Requisites.js';
 import requsites from './utils/requsites.js'
+import '../../components/HeaderTypeOne/HeaderTypeOne.js';
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Абдинов Эльдар Гейдар Оглы');
