4537023eed8c5f14072e06b8ba7ece406d194a69
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-04 16:00:51 +0300

    new req

--- fixes/js/Alert/Alert.js
+++ fixes/js/Alert/Alert.js
@@ -1,4 +1,4 @@
-const btns = document.querySelectorAll('.product-item>.add_to_wishlist>btn');
+const btns = document.querySelectorAll('.product-item .add_to_wishlist btn');
 const banner = document.querySelector('.main-slider');
 
 console.log(btns)
