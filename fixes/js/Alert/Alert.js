8fd14a126a4efa2efab109ee0e90a3bee3e1ea5b
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-04 16:28:37 +0300

    new req

--- fixes/js/Alert/Alert.js
+++ fixes/js/Alert/Alert.js
@@ -10,10 +10,11 @@ if (document.querySelector('.common-home')) {
     console.log(document.querySelector('.alert'));
     btn.addEventListener('click', console.log(123))
     btn.addEventListener('click', (e) => {
+      const alert = document.querySelector('.alert');
       console.log(e.target)
       setTimeout(() => {
-        console.log(document.querySelector('.alert'));
-        banner.insertAdjacentHTML('afterend', document.querySelector('.alert'));
+        console.log(alert);
+        banner.insertAdjacentHTML('afterend', alert);
       }, 2000);
     });
   });
