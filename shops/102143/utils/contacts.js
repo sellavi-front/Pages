1bbc80a16ac07a0d60be1800819b6cf66fdda8a0
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-04 12:00:42 +0300

    fix

--- shops/102143/utils/contacts.js
+++ shops/102143/utils/contacts.js
@@ -2,17 +2,17 @@ const contactData = [
   {
     icon: 'https://res.cloudinary.com/gz-company/image/upload/v1606490032/ThaiCosmetic/icons8-%D0%BE%D1%82%D0%BA%D1%80%D1%8B%D1%82%D1%8C-%D1%8D%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%BE%D0%BD%D0%BD%D0%BE%D0%B5-%D0%BF%D0%B8%D1%81%D1%8C%D0%BC%D0%BE_1_qdvvse.png',
     text: 'zakaz@zakaz@123.ru',
-    link: 'mailto:zakaz@zakaz@123.ru'
+    link: 'mailto:m_tyumentseva@mail.ru'
   },
   {
     icon: 'https://res.cloudinary.com/gz-company/image/upload/v1606490032/ThaiCosmetic/icons8-place-marker_2_g4gw0x.png',
-    text: 'Город столиц Пресненская набережная д.8 ст.1, Пн-Сб: с 9:30 до 19:00',
+    text: 'Комсомольск-на-Амуре, Ул. Васянина 10, ТЦ Вега 3 этаж, бутик 37',
     link: ''
   },
   {
-    icon: 'https://res.cloudinary.com/gz-company/image/upload/v1606490032/ThaiCosmetic/icons8-place-marker_2_g4gw0x.png',
+    icon: 'https://res.cloudinary.com/gz-company/image/upload/v1606488610/ThaiCosmetic/icons8-whatsapp_1_r1u6ry.png',
     text: '+7 (495) 999-99-88',
-    link: 'tel:+74959999988'
+    link: 'tel:8 (924) 313-3393'
   }
 ]
 
