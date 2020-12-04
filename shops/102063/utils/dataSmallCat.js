c7c228cf608e9b2f3789b31bb9a17600c0431bf2
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-12-04 15:58:05 +0300

    fix links

--- shops/102063/utils/dataSmallCat.js
+++ shops/102063/utils/dataSmallCat.js
@@ -1,31 +1,31 @@
 const dataSmallCat = [
   {
-    href: '#',
+    href: 'https://store102063.sellavi.com/filter-water-in-flat/',
     src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606380941/water_filter_small_inmdvg.png',
     title: 'Очистка воды в квартиры',
   },
   {
-    href: '#',
+    href: 'https://store102063.sellavi.com/filter-water-in-house/',
     src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606380941/photo-1580940040649-e3d55edb1e0c_xhycja.png',
     title: 'Очистка воды в дом',
   },
   {
-    href: '#',
+    href: 'https://store102063.sellavi.com/filter-water-in-office/',
     src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606380941/photo-1593642634443-44adaa06623a_uq3dyu.png',
     title: 'Очистка воды в офис',
   },
   {
-    href: '#',
+    href: 'https://store102063.sellavi.com/filter-in-caffe-and-rest/',
     src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606380941/photo-1569966459694-faf2bf4af6c6_ssouyg.png',
     title: 'Фильтры для кафе и ресторанов',
   },
   {
-    href: '#',
+    href: 'https://store102063.sellavi.com/service/',
     src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606380941/photo-1594807928657-90490c2dae91_fpnfcx.png',
     title: 'Обслуживание',
   },
   {
-    href: '#',
+    href: 'https://store102063.sellavi.com/water-analysis/',
     src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606652783/photo-1578496481449-cf2e845cc00c_sbkxt4.png',
     title: 'Анализ воды',
   },
