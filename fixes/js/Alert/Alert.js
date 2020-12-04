740aaf6a35f1f8626e3bf90b2be7a4dfe8ee3616
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-04 16:06:43 +0300

    new req

--- fixes/js/Alert/Alert.js
+++ fixes/js/Alert/Alert.js
@@ -1,14 +1,18 @@
-const btns = document.querySelectorAll('.product-item .add_to_wishlist btn');
-const banner = document.querySelector('.main-slider');
+if (document.querySelector('.common-home')) {
+  const btns = document.querySelectorAll(
+    '.products > .product-item > div > .price_actions > .buttons__container > .add_to_wishlist > button',
+  );
+  const banner = document.querySelector('.main-slider');
 
-console.log(btns)
-console.log(banner);
+  console.log(btns);
+  console.log(banner);
 
-btns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    setTimeout(() => {
-      console.log(document.querySelector('.alert'))
-      banner.insertAdjacentHTML('afterend', document.querySelector('.alert'))
-    }, 500)
+  btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      setTimeout(() => {
+        console.log(document.querySelector('.alert'));
+        banner.insertAdjacentHTML('afterend', document.querySelector('.alert'));
+      }, 500);
+    });
   });
-});
+}
