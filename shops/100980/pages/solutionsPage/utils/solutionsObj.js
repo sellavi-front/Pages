3867069fe29aa1ecc362f44f0a9f812fc33f2f2f
Coospir <eugene.starodubov@gmail.com>
2020-12-04 18:49:39 +0300

    Updated header 100980

--- shops/100980/pages/solutionsPage/utils/solutionsObj.js
+++ shops/100980/pages/solutionsPage/utils/solutionsObj.js
@@ -1,68 +1,84 @@
 export const solutionObj = {
   cafe: {
     title: 'Кафе',
-    countPlacesOne: 'Кафе на 10 мест',
-    countPlacesTwo: 'Кафе на 30 мест',
+    countPlaces: [
+      'Кафе на 10 мест',
+      'Кафе на 30 мест'
+    ],
     buttonGetSolution: "<a>Получить готовое решение</a>",
     img: 'https://res.cloudinary.com/dd2edlvdy/image/upload/v1607093005/daan-evers-tKN1WXrzQ3s-unsplash_1_qllwp1.png'
   },
 
   bar: {
     title: 'Бары',
-    countPlacesOne: 'Бар на 20 мест',
-    countPlacesTwo: 'Бар на 40 мест',
+    countPlaces: [
+      'Бар на 20 мест',
+      'Бар на 40 мест'
+    ],
     buttonGetSolution: "<a>Получить готовое решение</a>",
     img: 'https://res.cloudinary.com/dd2edlvdy/image/upload/v1607093005/helena-yankovska-sJOy9pveFbU-unsplash_1_yxbj2t.png'
   },
 
   pizza: {
     title: 'Пиццерия',
-    countPlacesOne: 'Пиццерия на 20 мест',
-    countPlacesTwo: 'Пиццерия на 40 мест',
+    countPlaces: [
+      'Пиццерия на 20 мест',
+      'Пиццерия на 40 мест'
+    ],
     buttonGetSolution: "<a>Получить готовое решение</a>",
     img: 'https://res.cloudinary.com/dd2edlvdy/image/upload/v1607092992/peter-bravo-de-los-rios-K70I7ecmXqg-unsplash_1_d3xkmq.png'
   },
 
   fastFood: {
     title: 'Кафе фаст-фуд',
-    countPlacesOne: 'Кафе "Бургер" на 20 мест',
-    countPlacesTwo: 'Прилавок Шаурма',
-    countPlacesThree: 'Кафе фаст-фуд на 40 мест',
+    countPlaces: [
+      'Кафе "Бургер" на 20 мест',
+      'Прилавок Шаурма',
+      'Кафе фаст-фуд на 40 мест'
+    ],
     buttonGetSolution: "<a>Получить готовое решение</a>",
     img: 'https://res.cloudinary.com/dd2edlvdy/image/upload/v1607092993/jonathan-borba-8l8Yl2ruUsg-unsplash_1_jz7bub.png'
   },
 
   diningHall: {
     title: 'Столовые',
-    countPlacesOne: 'Столовая на 50 мест',
-    countPlacesTwo: 'Столовая на 100 мест',
-    countPlacesThree: 'Столовая на 200 мест',
+    countPlaces: [
+      'Столовая на 50 мест',
+      'Столовая на 100 мест',
+      'Столовая на 200 мест'
+    ],
     buttonGetSolution: "<a>Получить готовое решение</a>",
     img: 'https://res.cloudinary.com/dd2edlvdy/image/upload/v1607092992/medina-catering-AdtqezIqv7c-unsplash_1_jrmlel.png'
   },
 
   restaurants: {
     title: 'Рестораны',
-    countPlacesOne: 'Ресторан на 50 мест',
-    countPlacesTwo: 'Ресторан на 100 мест',
+    countPlaces: [
+      'Ресторан на 50 мест',
+      'Ресторан на 100 мест'
+    ],
     buttonGetSolution: "<a>Получить готовое решение</a>",
     img: 'https://res.cloudinary.com/dd2edlvdy/image/upload/v1607092992/robert-mathews-xXaLfz6V9rQ-unsplash_1_1_zkvp0f.png'
   },
 
   lines: {
     title: 'Линии раздач',
-    countPlacesOne: 'Линия раздачи "Патша"',
-    countPlacesTwo: 'Линия раздачи "Аста"',
-    countPlacesThree: 'Линия раздачи "Премьер"',
-    countPlacesFour: 'Линия раздачи "Ривьера"',
+    countPlaces: [
+      'Линия раздачи "Патша"',
+      'Линия раздачи "Аста"',
+      'Линия раздачи "Премьер"',
+      'Линия раздачи "Ривьера"'
+    ],
     buttonGetSolution: "<a>Получить готовое решение</a>",
     img: 'https://res.cloudinary.com/dd2edlvdy/image/upload/v1607093006/5_1_nnuxdd.png'
   },
 
   shops: {
     title: 'Магазины',
-    countPlacesOne: 'Магазин "Прилавок-продавец"',
-    countPlacesTwo: 'Магазин "Мини-маркет"',
+    countPlaces: [
+      'Магазин "Прилавок-продавец"',
+      'Магазин "Мини-маркет"'
+    ],
     buttonGetSolution: "<a>Получить готовое решение</a>",
     img: 'https://res.cloudinary.com/dd2edlvdy/image/upload/v1607093005/daan-evers-tKN1WXrzQ3s-unsplash_1_qllwp1.png'
   },
@@ -73,29 +89,26 @@ export const solutionObj = {
 let renderSolutions = () => {
   let solutionsArr = Object.values(solutionObj)
   solutionsArr.map(item => {
-    console.log(item)
+    return `
+    <div class="solutions__row">
+      <div class="solutions__column">
+        <h1>${item.title}</h1>
+        ${item.countPlaces}
+      </div>
+
+      <div class="solutions__column_links">
+        ${item.buttonGetSolution}
+      </div>
+
+      <div class="solutions__column">
+        <img src="${item.img}" alt="img" />
+      </div>
+    </div>
+    `
   })
 }
 
 renderSolutions()
 
-export const solutionsRowLayout = `
-<div class="solutions__row">
-      <div class="solutions__column">
-        <h1>${solutionObj.cafe.title}</h1>
-        <p>${solutionObj.cafe.countPlacesOne}</p>
-        <p>${solutionObj.cafe.countPlacesTwo}</p>
-      </div>
-
-      <div class="solutions__column_links">
-        ${solutionObj.cafe.buttonGetSolution}
-        ${solutionObj.cafe.buttonGetSolution}
-      </div>
-
-      <div class="solutions__column">
-        <img src="${solutionObj.cafe.img}" alt="img" />
-      </div>
-</div>
-`
 
 
