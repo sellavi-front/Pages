212a25f255b371f556222aba84b7a170354ee538
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-04 16:40:22 +0300

    new req

--- shops/102143/index.js
+++ shops/102143/index.js
@@ -89,11 +89,9 @@ if (document.querySelector('.common-home')) {
     console.log(document.querySelector('.alert'));
     btn.addEventListener('click', console.log(123))
     btn.addEventListener('click', (e) => {
-      const alert = document.querySelector('.alert');
-      console.log(e.target)
       setTimeout(() => {
         console.log(alert);
-        banner.insertAdjacentHTML('afterend', alert);
+        banner.insertAdjacentHTML('afterend', document.querySelector('.alert'));
       }, 2000);
     });
   });
