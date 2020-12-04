342398f3a6403b90f9b1779da2fcb5045099da56
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-04 13:15:28 +0300

    fix

--- shops/102143/utils/contacts.js
+++ shops/102143/utils/contacts.js
@@ -7,7 +7,7 @@ const contactData = [
   {
     icon: 'https://res.cloudinary.com/gz-company/image/upload/v1606490032/ThaiCosmetic/icons8-place-marker_2_g4gw0x.png',
     text: 'Комсомольск-на-Амуре, Ул. Васянина 10, ТЦ Вега 3 этаж, бутик 37',
-    link: ''
+    link: 'https://yandex.ru/maps/11453/komsomolsk-at-amur/?ll=136.999691%2C50.543320&mode=search&oid=1790097491&ol=biz&sll=37.622504%2C55.753215&sspn=1.089020%2C0.498768&text=%D0%9A%D0%BE%D0%BC%D1%81%D0%BE%D0%BC%D0%BE%D0%BB%D1%8C%D1%81%D0%BA-%D0%BD%D0%B0-%D0%90%D0%BC%D1%83%D1%80%D0%B5%2C%20%D0%A3%D0%BB.%20%D0%92%D0%B0%D1%81%D1%8F%D0%BD%D0%B8%D0%BD%D0%B0%2010%2C%20%D0%A2%D0%A6%20%D0%92%D0%B5%D0%B3%D0%B0%203%20%D1%8D%D1%82%D0%B0%D0%B6%2C%20%D0%B1%D1%83%D1%82%D0%B8%D0%BA%2037&z=16'
   },
   {
     icon: 'https://res.cloudinary.com/gz-company/image/upload/v1607075173/ThaiCosmetic/icons8-whatsapp_1_byeep2.svg',
