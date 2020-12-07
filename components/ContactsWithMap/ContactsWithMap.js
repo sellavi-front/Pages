0689e93473404f431afa0b2c443e3a172dc616b3
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-07 18:20:40 +0300

    contact with map

--- components/ContactsWithMap/ContactsWithMap.js
+++ components/ContactsWithMap/ContactsWithMap.js
@@ -48,29 +48,3 @@ export default class ContactsWithMap {
     createAddContainer.render();
   }
 }
-
-const data = {
-  contacts: [
-    {
-      title: 'Адрес',
-      contact: 'г. Москва',
-    },
-    {
-      title: 'Телефон',
-      contact: '+7(926)367-97-77',
-    },
-    {
-      title: 'E-mail',
-      contact: 'magazin1170@mail.ru	',
-    },
-  ],
-  map: `
-  <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ace6b1a8aa5dbad763aca6289fa9858ead326365147735f99a5869dcddb66f547&amp;source=constructor" width="500" height="400" frameborder="0"></iframe>
-  `,
-};
-
-const cont = document.querySelector('.custom_section')
-
-const map = new ContactsWithMap(cont, 'beforeend', data);
-
-map.render();
---
+++ shops/102199/utils/contactData.js
@@ -0,0 +1,21 @@
+const contactData = {
+  contacts: [
+    {
+      title: 'Адрес',
+      contact: 'г. Москва',
+    },
+    {
+      title: 'Телефон',
+      contact: '+7(926)367-97-77',
+    },
+    {
+      title: 'E-mail',
+      contact: 'magazin1170@mail.ru	',
+    },
+  ],
+  map: `
+  <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ace6b1a8aa5dbad763aca6289fa9858ead326365147735f99a5869dcddb66f547&amp;source=constructor" width="500" height="400" frameborder="0"></iframe>
+  `,
+};
+
+export default contactData;
