ce37f9a8b5115b536ba75262f56d0c3b1927a5e4
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-07 17:24:01 +0300

    delete selectors in product

--- shops/102199/index.js
+++ shops/102199/index.js
@@ -1,6 +1,9 @@
 import Requisites from '../../modules/Requisites/Requisites.js';
 import requsites from './utils/requsites.js'
+
+import '../../fixes/js/DeleteSelectorProductItem/DeleteSelectorProductItem.js';
 import '../../templates/TemplateTypeOne/TemplateTypeOne.js'
+
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Абдинов Эльдар Гейдар Оглы');
 
