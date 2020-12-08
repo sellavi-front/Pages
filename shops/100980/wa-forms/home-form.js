cded6628e8eedc0ad8c4a1a9c94732ec3e2fdd04
Coospir <eugene.starodubov@gmail.com>
2020-12-08 18:05:32 +0300

    updated 100980

--- shops/100980/wa-forms/home-form.js
+++ shops/100980/wa-forms/home-form.js
@@ -1,14 +1,12 @@
 import WhatsAppWidget from "../../../components/WhatsAppWidget/wa-widget.js"
 
 let sendHomeForm = () => {
-    let homeFormBtn = document.querySelector(".banner__submit__form__btn")
-    console.log(homeFormBtn)
     let homeFormName = document.querySelector('[name="home__form_name"]').value;
     let homeFormPhone = document.querySelector('[name="home__form_phone"]').value;
     let homeFormEmail = document.querySelector('[name="home__form_email"]').value;
     let arrHome = [homeFormName, homeFormPhone, homeFormEmail]
 
-    let sender = new WhatsAppWidget(homeFormBtn, "79324247663", "Детальный план реализации проекта", arrHome, "Расскажите подробнее, пожалуйста.")
+    let sender = new WhatsAppWidget("79324247663", "Детальный план реализации проекта", arrHome, "Расскажите подробнее, пожалуйста.")
     return sender
 }
 
