7cd00647ef9e7db93c259948a8e3027d38980a8b
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-12-08 17:04:03 +0300

    fix cart page

--- shops/101977/index.js
+++ shops/101977/index.js
@@ -99,10 +99,12 @@ if (location.pathname !== '/') {
   document.querySelector('#home > div > header > div.header-wrapper > div.container > div.header-cart.sticky').style.cssText = 'display: flex; justify-content: center; align-self: center;';
   }
 
-  if (screen.width <= 480 ) {
+  if (screen.width <= 480) {
     document.querySelector('#home > div > div').style.cssText = 'margin-top: 60px;';
     document.querySelector('#home > div > header > div.header-wrapper > div.container > div.header-cart.sticky > div > div.dropdown.cart_drop_down').style.cssText = 'display: none;';
+    if (location.pathname !== '/cart/') {
     document.querySelector('#sidebar').style.cssText = 'margin-bottom: 20px;';
+    }
   }
 }
 
