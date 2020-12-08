44a6946ac83a93285de85c8ce81e2c84625465ee
Coospir <eugene.starodubov@gmail.com>
2020-12-08 10:49:50 +0300

    updated 100980

--- shops/100980/pages/solutionsPage/utils/steps.js
+++ shops/100980/pages/solutionsPage/utils/steps.js
@@ -2,25 +2,21 @@ const solutionsStepsLayout = `
   <div class="solutions__steps_container">
     <div class="solutions__steps_row">
         <div class="solutions__steps_item">
-            <div class="steps__rectangle"></div>
             <img src="" alt="img" />
             <h6>Проектирование и расстановка оборудования</h6>
         </div>
 
         <div class="solutions__steps_column">
-            <div class="steps__rectangle"></div>
             <img src="" alt="img" />
             <h6>Комплексное оснащение</h6>
         </div>
 
         <div class="solutions__steps_column">
-            <div class="steps__rectangle"></div>
             <img src="" alt="img" />
             <h6>Монтаж и запуск в эксплуатацию оборудования</h6>
         </div>
 
         <div class="solutions__steps_column">
-            <div class="steps__rectangle"></div>
             <img src="" alt="img" />
             <h6>Кредитование и рассрочка</h6>
         </div>
