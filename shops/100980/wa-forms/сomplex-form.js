cf66c269cc13b1d4daf5899fcc9b0bf5d3041df6
Coospir <eugene.starodubov@gmail.com>
2020-12-08 16:55:19 +0300

    updated 100980

--- shops/100980/wa-forms/сomplex-form.js
+++ shops/100980/wa-forms/сomplex-form.js
@@ -6,7 +6,7 @@ let sendComplexForm = () => {
   let complexFormEmail = document.querySelector('[name="complex__form_email"]').value;
   let complexFormSquare = document.querySelector('[name="complex__form_square"]').value;
   let complexFormCountSeats = document.querySelector('[name="complex__form_count_seats"]').value;
-  let arrHome = [complexFormName, complexFormPhone, complexFormEmail, "площадь: " + complexFormSquare + "м3", "кол-во мест: " + complexFormCountSeats + " мест"]
+  let arrHome = [complexFormName, complexFormPhone, complexFormEmail, "площадь: " + complexFormSquare + "м2", "кол-во мест: " + complexFormCountSeats + " мест"]
   let sender = new WhatsAppWidget("79324247663", "Расчет стоимости коммерческого предложения", arrHome, "Расскажите подробнее, пожалуйста.")
   sender.call()
 }
