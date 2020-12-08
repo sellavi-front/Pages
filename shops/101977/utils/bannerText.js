593f14919cd21ba6cb24dc9af4a714e535ee28d4
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-12-08 11:22:50 +0300

    fix link and title to order in banner

--- shops/101977/utils/bannerText.js
+++ shops/101977/utils/bannerText.js
@@ -1,7 +1,7 @@
 const objBanner = {
     title: 'Автошампуни для бесконтактной мойки',
-    link: '#',
-    linkContent: 'Сделать заказ'
+    link: 'https://aldoma.ru/%D0%B0%D0%B2%D1%82%D0%BE%D1%88%D0%B0%D0%BC%D0%BF%D1%83%D0%BD%D0%B8/',
+    linkContent: 'Товары'
 };
 
 export default objBanner;
