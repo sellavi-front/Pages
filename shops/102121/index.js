31353b36a215a1a643b2ca75cbb34ae2db9dd588
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-08 18:32:38 +0300

    co

--- shops/102121/index.js
+++ shops/102121/index.js
@@ -9,7 +9,7 @@ import contactData from './utils/contactData.js'
 import '../../templates/TemplateTypeOne/TemplateTypeOne.js';
 import ContactsWithMap from '../../components/ContactsWithMap/ContactsWithMap.js';
 
-if (document.querySelector('.wrapper')) {
+if (document.querySelector('.common-home')) {
   const customSection = document.querySelector('.custom_section');
   customSection.classList.add('container');
 
