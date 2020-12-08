c566a9d2dd2085825c23085a52f7f5bba6cf2208
Coospir <eugene.starodubov@gmail.com>
2020-12-08 11:28:26 +0300

    updated 100980

--- shops/100980/pages/solutionsPage/utils/steps.js
+++ shops/100980/pages/solutionsPage/utils/steps.js
@@ -2,22 +2,22 @@ const solutionsStepsLayout = `
   <div class="solutions__steps_container">
     <div class="solutions__steps_row">
         <div class="solutions__steps_item">
-            <img class="steps__img" src="" alt="img" />
+            <img class="steps__img" src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607415977/Vector_plchom.png" alt="img" />
             <h6 class="steps__title">Проектирование и расстановка оборудования</h6>
         </div>
 
         <div class="solutions__steps_item">
-            <img class="steps__img" src="" alt="img" />
+            <img class="steps__img" src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607415977/Vector_1_ca4tup.png" alt="img" />
             <h6 class="steps__title">Комплексное оснащение</h6>
         </div>
 
         <div class="solutions__steps_item">
-            <img class="steps__img" src="" alt="img" />
+            <img class="steps__img" src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607415977/Vector_2_mbavi0.png" alt="img" />
             <h6>Монтаж и запуск в эксплуатацию оборудования</h6>
         </div>
 
         <div class="solutions__steps_item">
-            <img class="steps__img" src="" alt="img" />
+            <img class="steps__img" src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607415977/Vector_3_skex5d.png" alt="img" />
             <h6 class="steps__title">Кредитование и рассрочка</h6>
         </div>
     </div>
