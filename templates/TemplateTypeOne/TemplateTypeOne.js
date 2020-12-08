92e4046ba343b3a05b02782e1b4f9890782acc06
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-08 12:19:19 +0300

    shop

--- templates/TemplateTypeOne/TemplateTypeOne.js
+++ templates/TemplateTypeOne/TemplateTypeOne.js
@@ -1,2 +1,10 @@
 import '../../components/HeaderTypeOne/HeaderTypeOne.js';
 import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
+import SocialIconsTypeOne from '../../components/SocialIconsTypeOne/SocialIconsTypeOne.js'
+
+if (document.querySelector('.wrapper') && !location.href.includes('checkout')) {
+  const footerContainer = document.querySelector('.footer-widgets>div');
+
+  new SocialIconsTypeOne(footerContainer, 'beforeend').render();
+
+}
