c7e5e506ae6adb1786e5d094efca17f0a58ca3c3
Coospir <eugene.starodubov@gmail.com>
2020-12-09 17:40:29 +0300

    test

--- shops/102169/blocks/custom-section-banner/custom-section-banner.js
+++ shops/102169/blocks/custom-section-banner/custom-section-banner.js
@@ -3,7 +3,7 @@ let customSectionBannerLayout = `
         <div class="section__banner_column">
             <div class="section__banner_row">
               <div class="section__banner_col">
-                  <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607518422/sellavi/102169/Component_2_1_svffuu.png" alt="img" />
+                  <img class="photo__custom_banner" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607518422/sellavi/102169/Component_2_1_svffuu.png" alt="img" />
               </div>
               <div class="section__banner_col">
                   <p class="section__banner_title">Нашу косметику можно приобрести, как в нашем интернет магазине, так и в других городах России.</p>
@@ -15,7 +15,7 @@ let customSectionBannerLayout = `
                       <li class="distributor">г. Иваново, ул. Калинина 17, Тел. 89106679837</li>
                   </ul>
                   <p class="marketplaces__container">Наша продукция на маркет плейсах</p>
-                  <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607518334/sellavi/102169/Component_1_2_wk6aux.png" alt="img" />
+                  <img class="disphoto__custom_banner" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607518334/sellavi/102169/Component_1_2_wk6aux.png" alt="img" />
                   <p class="wanna__be__distributor">Хотите и вы стать нашим дистрибьютором? Присоединяйтесь!</p>
                   <p class="wanna__be__distributor">Отправляйте запрос на почту zakaz@organic-stories.ru</p>
               </div>
