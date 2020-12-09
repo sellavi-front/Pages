080fb8087a0cd97c6890d2473678f124a2f37e1e
Coospir <eugene.starodubov@gmail.com>
2020-12-09 11:57:01 +0300

    added banner

--- shops/102169/index.js
+++ shops/102169/index.js
@@ -1,6 +1,6 @@
 import pages from './pages/pages.js'
 import './blocks/header/header.js'
-import renderHomeBanner from "./blocks/banner/banner";
+import renderHomeBanner from "./blocks/banner/banner.js";
 
 if(pages.home) {
   renderHomeBanner()
