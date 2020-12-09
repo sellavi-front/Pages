6118d6fb26af3b1effaf6a092852bd6a8b5741ab
Coospir <eugene.starodubov@gmail.com>
2020-12-09 15:28:22 +0300

    update 102169

--- shops/102169/blocks/banner/banner.js
+++ shops/102169/blocks/banner/banner.js
@@ -7,7 +7,21 @@ let bannerLayout = `
             <p class="banner__description">Коллекция средств по уходу за волосами, телом и лицом без силиконов, сульфатов и парабенов</p>
             <p class="banner__phone">+7(915) 094-06-19</p>
             <div class="banner__social">
+                <div class="social__item">
+                    <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607516742/sellavi/102169/Group_t9ghfm.png" alt="social" />
+                </div>
 
+                <div class="social__item">
+                    <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607516752/sellavi/102169/Group_3_pgihcu.png" alt="social" />
+                </div>
+
+                <div class="social__item">
+                    <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607516743/sellavi/102169/Group_1_o2dhef.png" alt="social" />
+                </div>
+
+                <div class="social__item">
+                    <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607516743/sellavi/102169/Group_2_dagjbp.png" alt="social" />
+                </div>
             </div>
         </div>
         </div>
