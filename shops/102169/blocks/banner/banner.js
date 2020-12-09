1faa9ca05a70e2b5dab5e35640f64df278c38596
Coospir <eugene.starodubov@gmail.com>
2020-12-09 14:59:06 +0300

    update 102169

--- shops/102169/blocks/banner/banner.js
+++ shops/102169/blocks/banner/banner.js
@@ -4,14 +4,14 @@ let bannerLayout = `
         <div class="banner__column">
             <h1>Натуральная косметика
             "ORGANIC STORIES"</h1>
-            <p>Коллекция средств по уходу за волосами, телом и лицом без силиконов, сульфатов и парабенов</p>
+            <p class="banner__description">Коллекция средств по уходу за волосами, телом и лицом без силиконов, сульфатов и парабенов</p>
         </div>
     </div>
     <div class="banner__row">
         <div class="banner__column">
             <p class="banner__phone">+7(915) 094-06-19</p>
             <div class="banner__social">
-                
+
             </div>
         </div>
     </div>
