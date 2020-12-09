9575554fe01d223456151e430d6f42565ed93695
Coospir <eugene.starodubov@gmail.com>
2020-12-09 17:05:00 +0300

    test

--- shops/102169/index.js
+++ shops/102169/index.js
@@ -2,6 +2,7 @@ import pages from './pages/pages.js'
 import './blocks/header/header.js'
 import renderHomeBanner from "./blocks/banner/banner.js"
 import renderSectionBanner from './blocks/custom-section-banner/custom-section-banner.js'
+import './../../fixes/js/TranslateAboutUs/TranslateAboutUs.js';
 
 if(pages.home) {
   renderHomeBanner()
