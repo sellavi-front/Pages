49f7ba98306e013c4804b7e136b3c2daadd58f25
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-09 16:44:29 +0300

    new social data for shop

--- templates/TemplateTypeOne/TemplateTypeOne.js
+++ templates/TemplateTypeOne/TemplateTypeOne.js
@@ -3,7 +3,6 @@ import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
 import SocialIconsTypeOne from '../../components/SocialIconsTypeOne/SocialIconsTypeOne.js'
 
 if (document.querySelector('.wrapper') && !location.href.includes('checkout')) {
-  const footerContainer = document.querySelector('.footer-widgets>div');
 
   /* Remove bootstrap col width seclector */
   const footerWidgetsCol = document.querySelectorAll('.footer-widgets > div > div > div');
@@ -24,6 +23,8 @@ if (document.querySelector('.wrapper') && !location.href.includes('checkout')) {
                                      **/
 
 if (document.querySelector('.wrapper') && !location.href.includes('checkout')) {
+  const footerContainer = document.querySelector('.footer-widgets>div');
+
   new SocialIconsTypeOne(footerContainer, 'beforeend').render();
 
   const socIcons = document.querySelector('.soc-icons>.container');
