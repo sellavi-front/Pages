22ec9f474a0d6be171b9eb3be95a4a07ed474557
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-09 18:04:26 +0300

    new shops

--- shops/102159/index.js
+++ shops/102159/index.js
@@ -1,9 +1,14 @@
 import Requisites from '../../modules/Requisites/Requisites.js';
 import { requsites, dataContact } from './utils/requsites.js';
 
+import '../../fixes/js/FooterCopy/FooterCopy.js';
+import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js';
+import '../../fixes/js/ChangeAgreement/ChangeAgreement.js';
+
+
 
 if (location.href.includes('term-of-use')) {
-  const req = new Requisites(requsites, 'beforeend', 'ИП Макеев Алексей Валерьевич');
+  const req = new Requisites(requsites, 'beforeend', 'ИП Зюзина Оксана Викторовна');
 
   req.setTemplate();
 }
@@ -11,17 +16,17 @@ if (location.href.includes('term-of-use')) {
 if (location.href.includes('contact')) {
   const media = document.querySelector('.contact-info > .media-list > .media:last-child');
 
-  const req = new Requisites(requsites, 'afterend', 'ИП Макеев Алексей Валерьевич', media);
+  const req = new Requisites(requsites, 'afterend', 'ИП Зюзина Оксана Викторовна', media);
   req.setTemplate();
 }
 
 if (location.href.includes('policy')) {
-  const req = new Requisites(requsites, 'afterend', 'ИП Макеев Алексей Валерьевич', document.querySelector("#content > div > div"));
+  const req = new Requisites(requsites, 'afterend', 'ИП Зюзина Оксана Викторовна', document.querySelector("#content > div > div"));
   req.setTemplate();
 }
 
 if (location.href.includes('pay-delivery')) {
-  const req = new Requisites(requsites, 'afterend', 'ИП Макеев Алексей Валерьевич', document.querySelector("#content > div > div"), dataContact);
+  const req = new Requisites(requsites, 'afterend', 'ИП Зюзина Оксана Викторовна', document.querySelector("#content > div > div"), dataContact);
   req.setTemplate();
 }
 
