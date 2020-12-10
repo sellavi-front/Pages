1f572a8cbea1842e3305236c2ab831265d27b50f
Coospir <eugene.starodubov@gmail.com>
2020-12-10 14:10:35 +0300

    test

--- shops/102169/blocks/custom-section-banner/feedback-form.js
+++ shops/102169/blocks/custom-section-banner/feedback-form.js
@@ -1,18 +1,16 @@
 import WhatsAppWidget from "../../../../components/WhatsAppWidget/wa-widget.js";
 
-let sendHomeForm = () => {
-  let sender = null
-  let homeFormName = document.querySelector(".__container>.form__input:nth-child(1)").value;
-  let homeFormPhone = document.querySelector(".__container>.form__input:nth-child(2)").value;
-  let arrHome = [homeFormName, homeFormPhone]
-  sender = new WhatsAppWidget("79150940619", "Хочу стать дилером!", arrHome, "Расскажите подробнее, пожалуйста.")
-  return sender
-}
+
 
 let sender = () => {
   let homeFormBtn = document.querySelector(".form__button")
   homeFormBtn.addEventListener("click", () => {
-    console.log(sendHomeForm.sendWA())
+    let sender = null
+    let homeFormName = document.querySelector(".__container>.form__input:nth-child(1)").value;
+    let homeFormPhone = document.querySelector(".__container>.form__input:nth-child(2)").value;
+    let arrHome = [homeFormName, homeFormPhone]
+    sender = new WhatsAppWidget("79150940619", "Хочу стать дилером!", arrHome, "Расскажите подробнее, пожалуйста.")
+    sender.sendWA()
   })
 }
 
