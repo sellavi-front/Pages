afb0358a1a1a9a04c53b93f2fa0a8642cbd2b4a6
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-10 12:52:03 +0300

    data

--- shops/102242/utils/contentBannerData.js
+++ shops/102242/utils/contentBannerData.js
@@ -4,7 +4,7 @@ const contentBannerData = [
     text: '<span class="bold">550р</span> скидка на все',
     linkSrc: '#',
     linkContent: 'подарок шелковая резинка',
-    banner: 'https://res.cloudinary.com/gz-company/image/upload/v1607591760/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/Group_13_se4xam.png',
+    banner: 'https://res.cloudinary.com/gz-company/image/upload/v1607591804/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/IMG_4578_1_blkmza.png',
     srcset: 'https://res.cloudinary.com/gz-company/image/upload/v1607591804/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/IMG_4578_1_blkmza.png',
   }
 ]
