9e28a0336aabfe2e04be0efebc246998764cb118
Coospir <eugene.starodubov@gmail.com>
2020-12-11 18:59:20 +0300

    updated 100980

--- shops/100980/blocks/customSection/howWeWork/howWeWork.js
+++ shops/100980/blocks/customSection/howWeWork/howWeWork.js
@@ -7,7 +7,7 @@ let howWeWorkLayout = `
           <div class="step__item">
               <img src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607355654/Component_10_w0qdmw.png" alt="img" />
               <h6>ЗАЯВКА</h6>
-              <p>Вы звоните нам или оставляете заявку через форму обратной связи и с Вами связывается наш менеджер</p>
+              <p class="howDesc">Вы звоните нам или оставляете заявку через форму обратной связи и с Вами связывается наш менеджер</p>
           </div>
       </div>
 
@@ -15,7 +15,7 @@ let howWeWorkLayout = `
           <div class="step__item">
               <img src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607355654/Component_11_iq4qz9.png" alt="img" />
               <h6>ВЫЕЗД</h6>
-              <p>Менеджер уточняет детали по проекту, производим бесплатно осмотр и замер помещения</p>
+              <p class="howDesc">Менеджер уточняет детали по проекту, производим бесплатно осмотр и замер помещения</p>
           </div>
       </div>
 
@@ -24,7 +24,7 @@ let howWeWorkLayout = `
           <div class="step__item">
               <img src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607355653/Component_12_dgu8vc.png" alt="img" />
               <h6>ДОГОВОР</h6>
-              <p>Делаем предварительный план расстановки оборудования и подписываем договор на создание проектной документации и визуализации помещения. Срок выполнения: 2 недели.</p>
+              <p class="howDesc">Делаем предварительный план расстановки оборудования и подписываем договор на создание проектной документации и визуализации помещения. Срок выполнения: 2 недели.</p>
           </div>
       </div>
 
@@ -32,7 +32,7 @@ let howWeWorkLayout = `
           <div class="step__item">
               <img src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607355651/Component_13_enmdfs.png" alt="img" />
               <h6>ПРОИЗВОДСТВО</h6>
-              <p>Делаем предварительный план расстановки оборудования и подписываем договор на создание проектной документации и визуализации помещения. Срок выполнения: 2 недели.</p>
+              <p class="howDesc">Делаем предварительный план расстановки оборудования и подписываем договор на создание проектной документации и визуализации помещения. Срок выполнения: 2 недели.</p>
           </div>
       </div>
 
@@ -40,7 +40,7 @@ let howWeWorkLayout = `
           <div class="step__item">
               <img src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607355650/Component_14_ztxgnb.png" alt="img" />
               <h6>МОНТАЖ</h6>
-              <p>Выполняем строительно-монтажные работы в помещении, выполняем оснащение оборудованием. Срок выполнения: 2-4 недели.</p>
+              <p class="howDesc">Выполняем строительно-монтажные работы в помещении, выполняем оснащение оборудованием. Срок выполнения: 2-4 недели.</p>
           </div>
       </div>
 
@@ -49,7 +49,7 @@ let howWeWorkLayout = `
           <div class="step__item">
               <img src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607355651/Component_15_h4dmig.png" alt="img" />
               <h6>ЗАПУСК</h6>
-              <p>Производим тестовый запуск всех систем и оборудования. Устанавливаем вывеску и размещаем менюборды. Срок выполнения 3 дня.</p>
+              <p class="howDesc">Производим тестовый запуск всех систем и оборудования. Устанавливаем вывеску и размещаем менюборды. Срок выполнения 3 дня.</p>
           </div>
       </div>
 
@@ -57,7 +57,7 @@ let howWeWorkLayout = `
           <div class="step__item">
               <img src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607355650/Component_16_bxf9no.png" alt="img" />
               <h6>ОКОНЧАНИЕ</h6>
-              <p>Подписываем с вами документы по завершению работ и вводу оборудования в эксплуатацию. Срок выполнения 2 дня.</p>
+              <p class="howDesc">Подписываем с вами документы по завершению работ и вводу оборудования в эксплуатацию. Срок выполнения 2 дня.</p>
           </div>
       </div>
 
@@ -65,7 +65,7 @@ let howWeWorkLayout = `
           <div class="step__item">
               <img src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607355651/Component_17_o4tkrz.png" alt="img" />
               <h6>ФОТОСЕССИЯ</h6>
-              <p>Выполняем официальный запуск. Делаем фотосессию и видеосъёмку.</p>
+              <p class="howDesc">Выполняем официальный запуск. Делаем фотосессию и видеосъёмку.</p>
           </div>
       </div>
 
@@ -73,7 +73,7 @@ let howWeWorkLayout = `
           <div class="step__item">
               <img src="https://res.cloudinary.com/dd2edlvdy/image/upload/v1607355650/Component_18_pwx10g.png" alt="img" />
               <h6>ОТЗЫВ</h6>
-              <p>Благодарим вас за совместную работу и просим написать для нас отзыв на фирменном бланке компании.</p>
+              <p class="howDesc">Благодарим вас за совместную работу и просим написать для нас отзыв на фирменном бланке компании.</p>
           </div>
       </div>
 
