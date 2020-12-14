c5f3d6bffd348629e228f5defc13615b3e92a3cf
m4medbekov <m4medbekov@gmail.com>
2020-12-14 13:17:32 +0300

    imports

--- shops/102015/index.js
+++ shops/102015/index.js
@@ -1,6 +1,27 @@
 import Requisites from '../../modules/Requisites/Requisites.js';
 import requsites from './utils/requsites.js';
-import '../../templates/TemplateTypeOne/TemplateTypeOne.js';
+//import '../../templates/TemplateTypeOne/TemplateTypeOne.js';
+import '../../components/HeaderTypeTwo/HeaderTypeTwo.js';
+import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
+
+
+if (document.querySelector('.wrapper') && !location.href.includes('checkout')) {
+
+  /* Remove bootstrap col width seclector */
+  const footerWidgetsCol = document.querySelectorAll('.footer-widgets > div > div > div');
+  footerWidgetsCol.forEach(el => {
+    el.classList.remove('col-md-3');
+  })
+  /* Add title for soc icons */
+
+
+  const catergoryBanners = document.querySelector('.homefeatured_category>div');
+  if (catergoryBanners) {
+    catergoryBanners.insertAdjacentHTML('afterbegin', `<h2 class="section-title mb-4 mt-4"><span> Категории </span></h2>`)
+  }
+}
+
+
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Окутина Марина Юрьевна');
@@ -14,3 +35,4 @@ if (location.href.includes('contact')) {
   const req = new Requisites(requsites, 'afterend', 'ИП Окутина Марина Юрьевна', media);
   req.setTemplate();
 }
+
