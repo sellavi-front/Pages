3e5418353b47e6d18330d53edfce0e46c9fb0e10
Coospir <eugene.starodubov@gmail.com>
2020-12-14 12:34:57 +0300

    updated 100980

--- shops/102169/index.js
+++ shops/102169/index.js
@@ -2,7 +2,8 @@ import pages from './pages/pages.js'
 import './blocks/header/header.js'
 import renderHomeBanner from "./blocks/banner/banner.js"
 import renderSectionBanner from './blocks/custom-section-banner/custom-section-banner.js'
-import './../../fixes/js/TranslateAboutUs/TranslateAboutUs.js';
+import './../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
+import './../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
 
 if(pages.home) {
   renderHomeBanner()
