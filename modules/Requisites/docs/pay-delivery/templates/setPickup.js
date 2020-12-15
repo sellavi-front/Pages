49eb3b37b5ed18b8702164ba18939060ef9657d1
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-15 19:06:32 +0300

    test render pay-delivery information

--- modules/Requisites/docs/pay-delivery/templates/setPickup.js
+++ modules/Requisites/docs/pay-delivery/templates/setPickup.js
@@ -1,4 +1,5 @@
 export const setPickup = (obj) => {
+  console.log(obj);
   return `
   <div class="d-flex content-delivery">
     <section class="delivery__right">
@@ -6,16 +7,16 @@ export const setPickup = (obj) => {
       <p class="delivery__right-small">
         <span class="delivery__right-subtitle">Бесплатно</span>
         <br />
-        Только по ${obj}
+        Только по ${obj.city}
       </p>
     </section>
     <section class="delivery__content">
-      <p class="delivery__content-title">${obj}</p>
+      <p class="delivery__content-title">${obj.adress}</p>
       <p class="delivery__content-label">
-        Только в ${obj} по выбору покупателя возможно получение заказа у сотрудника нашей компании
-        в согласованном с ним по телефону ${obj} на месте. В этом случае оплата товара
+        Только в ${obj.city} по выбору покупателя возможно получение заказа у сотрудника нашей компании
+        в согласованном с ним по телефону ${obj.phone} на месте. В этом случае оплата товара
         производится только наличными или банковской картой непосредственно при получении товара (без
-        предоплаты в режиме онлайн). Время работы нашего сотрудника: ${obj} При получении товара обязательно проверь комплектацию товара и наличие чека.
+        предоплаты в режиме онлайн). Время работы нашего сотрудника: ${obj.timeWork} При получении товара обязательно проверь комплектацию товара и наличие чека.
       </p>
     </section>
   </div>`;
