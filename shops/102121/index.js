b34e584d2219e0a8f1381aec3e2ec63c2cea1554
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-15 15:12:43 +0300

    origin main

--- shops/102121/index.js
+++ shops/102121/index.js
@@ -3,7 +3,6 @@ import { requsites, dataContact } from './utils/requsites.js';
 import '../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js';
 import '../../fixes/js/ChangeAgreement/ChangeAgreement.js';
-import SocialIconsTypeOne from '../../components/SocialIconsTypeOne/SocialIconsTypeOne.js'
 
 import contactData from './utils/contactData.js'
 
@@ -17,10 +16,6 @@ if (document.querySelector('.common-home')) {
   const map = new ContactsWithMap(customSection, 'beforeend', contactData);
   map.render();
 }
-
-// if (document.querySelector('.wrapper') && !location.href.includes('checkout')) {
-//   new SocialIconsTypeOne(footerContainer, 'beforeend').render();
-// }
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Ляховский Александр Владимирович');
@@ -44,40 +39,3 @@ if (location.href.includes('pay-delivery')) {
   const req = new Requisites(requsites, 'afterend', 'ИП Ляховский Александр Владимирович', document.querySelector("#content > div > div"), dataContact);
   req.setTemplate();
 }
-
-
-// import GetCategoriesFromNavbar from "../../components/GetCategoriesFromNavbar/GetCategoriesFromNavbar.js"
-// import imgArr from "./utils/images.js"
-// import BigBanner from '../../modules/BigBanner/BigBanner.js'
-// import ButtonListInHeader from '../../modules/ButtonListInHeader/ButtonListInHeader.js';
-// //import "../../fixes/fixes.js";
-
-// let customBanner = document.querySelector(".custom_banner")
-// let customSection = document.querySelector(".custom_section")
-
-// const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
-// const hideCart = document.querySelector('.header-wrapper>div>.header-cart');
-
-// const buttonsInHeader = new ButtonListInHeader(headerWrapper);
-// buttonsInHeader.setTemplate();
-// hideCart.remove();
-
-// if(window.location.pathname === '/' || window.location.pathname === '/?from_admin') {
-//   let nodeList = document.querySelectorAll("header>.navigation-wrapper>.container>.navigation>.nav.sf-menu>li")
-//   let categories = new GetCategoriesFromNavbar(Array.from(nodeList), imgArr, "Каталог товаров", customBanner, "afterend")
-//   categories.render()
-
-//   const objBanner = {
-//     title: 'Wigstyle',
-//     link: '#',
-//     linkContent: 'Подробнее'
-//   };
-
-//   const bigBanner = new BigBanner(objBanner, 'https://images.unsplash.com/photo-1565357419076-6acd4a10094e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
-//   bigBanner.render();
-// }
-
-// if(window.location.pathname === '/contact/') {
-//   document.querySelector("#home > div.wrapper.wrapper-closed > div.content-area > div > section > div > div > div.col-md-4 > div > div > div:nth-child(3) > div").setAttribute("class", "media-body")
-// }
-
