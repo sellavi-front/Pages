d92fc598987591345645d052612341f1fc785d76
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-16 16:14:59 +0300

    products for 102367

--- shops/102367/index.js
+++ shops/102367/index.js
@@ -1,2 +1,14 @@
 import '../../templates/TemplateTypeTwo/TemplateTypeTwo.js'
+
 import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
+
+import '../../components/FormBasic/FormBasic.js';
+
+import senderData from './utils/senderData.js';
+
+if (document.querySelector('.common-home')) {
+  const cstmSection = document.querySelector('.custom_section');
+
+  const sender = new FormBasic(cstmSection, 'beforeend', senderData);
+  sender.render();
+}
