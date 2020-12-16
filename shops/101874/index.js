1d3b1adecf06e8e45f81e74299173d7a5b6ef1d3
m4medbekov <m4medbekov@gmail.com>
2020-12-16 12:02:13 +0300

    add new folder 101874 + equa

---
+++ shops/101874/index.js
@@ -0,0 +1,16 @@
+import Requisites from '../../modules/Requisites/Requisites.js';
+import requsites from './utils/requsites.js'
+import '../../fixes/js/FooterCopy/FooterCopy.js';
+
+if (location.href.includes('term-of-use')) {
+  const req = new Requisites(requsites, 'beforeend', 'ИП"Руколеев Станислав Александрович"');
+
+  req.setTemplate();
+}
+
+if (location.href.includes('contact')) {
+  const media = document.querySelector('.contact-info > .media-list > .media:last-child');
+
+  const req = new Requisites(requsites, 'afterend', 'ИП"Руколеев Станислав Александрович"', media);
+  req.setTemplate();
+}
