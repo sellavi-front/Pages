9e517c7bbb34ab41e87f5fdbe7cbec47f534650a
m4medbekov <m4medbekov@gmail.com>
2020-12-16 17:56:01 +0300

    del fucking zak component

--- shops/102255/index.js
+++ shops/102255/index.js
@@ -1,23 +1,9 @@
 import Requisites from '../../modules/Requisites/Requisites.js';
 import requsites from './utils/requsites.js'
 import '../../fixes/js/FooterCopy/FooterCopy.js';
-import '../../templates/TemplateTypeOne/TemplateTypeOne.js';
-
-if (document.querySelector('.wrapper') && !location.href.includes('checkout')) {
-
-  /* Remove bootstrap col width seclector */
-  const footerWidgetsCol = document.querySelectorAll('.footer-widgets > div > div > div');
-  footerWidgetsCol.forEach(el => {
-    el.classList.remove('col-md-3');
-  })
-  /* Add title for soc icons */
+//import '../../templates/TemplateTypeOne/TemplateTypeOne.js';
 
 
-  const catergoryBanners = document.querySelector('.homefeatured_category>div');
-  if (catergoryBanners) {
-    catergoryBanners.insertAdjacentHTML('afterbegin', `<h2 class="section-title mb-4 mt-4"><span> Категории </span></h2>`)
-  }
-}
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ООО"Санитары Столицы"');
