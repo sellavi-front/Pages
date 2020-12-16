0a2d73d045e15944c0a6a1ba0f09f0a9f6123fa5
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-16 18:51:37 +0300

    style form

--- shops/102367/index.js
+++ shops/102367/index.js
@@ -8,6 +8,18 @@ import SocIconsTypeOne from '../../components/SocialIconsTypeOne/SocialIconsType
 import senderData from './utils/senderData.js';
 
 if (document.querySelector('.common-home')) {
+
+  // reset cut image in categories
+  const imgCat = document.querySelectorAll(".page-section.homefeatured_category > div > div > div > div > div > a > picture > img");
+  imgCat.forEach(cat => {
+    const src = cat.getAttribute('src');
+
+    let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
+
+    cat.setAttribute('src', replacedSrc);
+  })
+  // rend reset cit img
+
   const latestSection = document.querySelector('.page-section.latest-section > .container');
 
   const sender = new FormBasic(latestSection, 'beforeend', senderData);
