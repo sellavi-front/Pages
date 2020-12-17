2b56b656a59b0dc16a0353bda9150ea3270ea1b4
m4medbekov <m4medbekov@gmail.com>
2020-12-17 17:05:17 +0300

    add newBar

--- shops/102255/index.js
+++ shops/102255/index.js
@@ -3,6 +3,9 @@ import requsites from './utils/requsites.js'
 import '../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../templates/TemplateTypeOne/TemplateTypeOne.js';
 
+let newNav = document.querySelector('.nav.sf-menu')
+document.querySelector('section.page-section.latest-section').insertAdjacentHTML('afterbegin', `<div class="container"><div class="newNavTitle">Категории</div>${newNav.outerHTML}</div>`)
+
 // add user icon in header
 document.querySelector('.header_widgets').insertAdjacentHTML('afterbegin', '<div class="header_user"><a id="user" class="position-relative" href="https://store102255.sellavi.com/login/" role="link"><i class="far fa-user"></i></a></div>');
 
