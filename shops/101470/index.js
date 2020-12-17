b6767b6c5461ab260e4474dc67433a3b00a79699
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-17 11:10:43 +0300

    style form

--- shops/101470/index.js
+++ shops/101470/index.js
@@ -41,6 +41,11 @@ window.onload = () => {
 
 if (document.querySelector('.wrapper')) {
 
+  const cntctsSimple = document.querySelector('.widget-categories .contacts-simple');
+
+  cntctsSimple.classList.add('col-md-3');
+  cntctsSimple.querySelector('.contacts-simple__title').classList.add('widget-title');
+
   const tex = document.querySelector('footer > div.footer-widgets > div.container > div > div:nth-child(1) > div > div > ul > li:nth-child(3) > a').textContent;
   localStorage.setItem('phone', tex);
 
@@ -187,7 +192,7 @@ if (location.href.includes('contact')) {
 }
 
 if (location.href === 'https://store101470.sellavi.com/?from_admin' && 'https://store101470.sellavi.com/') {
-  console.log('test');
+
   document.querySelector('body').insertAdjacentHTML('afterbegin', '<div class="banner-title-button"><a href="https://store101470.sellavi.com/%D0%B4%D0%BB%D1%8F-%D0%B4%D0%BE%D0%BC%D0%B0/">товары</a></div>');
 }
 
