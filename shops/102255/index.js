780b101c3659196d677bed5dca5be6a5174ce935
m4medbekov <m4medbekov@gmail.com>
2020-12-17 18:28:30 +0300

    add title to product container

--- shops/102255/index.js
+++ shops/102255/index.js
@@ -3,11 +3,16 @@ import requsites from './utils/requsites.js'
 import '../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../templates/TemplateTypeOne/TemplateTypeOne.js';
 
-let newNav = document.querySelector('.nav.sf-menu')
-document.querySelector('section.page-section.latest-section').insertAdjacentHTML('afterbegin', `<div class="container"><div class="newNavTitle">Категории</div>${newNav.outerHTML}</div>`)
+
 
 // add user icon in header
 document.querySelector('.header_widgets').insertAdjacentHTML('afterbegin', '<div class="header_user"><a id="user" class="position-relative" href="https://store102255.sellavi.com/login/" role="link"><i class="far fa-user"></i></a></div>');
+
+let newNav = document.querySelector('.nav.sf-menu')
+document.querySelector('section.page-section.latest-section').insertAdjacentHTML('afterbegin', `<div class="container"><div class="newNavTitle">Категории</div>${newNav.outerHTML}</div>`)
+
+
+document.querySelector('#home > div > div > section.page-section.latest-section > div:nth-child(2)').insertAdjacentHTML('beforebegin', '<div class="container"><div class="productTitle">Новинки</div></div>');
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ООО"Санитары Столицы"');
