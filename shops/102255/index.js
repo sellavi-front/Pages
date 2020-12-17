46b0098a7a4948b9b3b210f99e8763879a37fa1a
m4medbekov <m4medbekov@gmail.com>
2020-12-18 00:49:22 +0300

    test

--- shops/102255/index.js
+++ shops/102255/index.js
@@ -17,7 +17,10 @@ import pages from "./utils/pages.js"
 document.querySelector('.header_widgets').insertAdjacentHTML('afterbegin', '<div class="header_user"><a id="user" class="position-relative" href="https://store102255.sellavi.com/login/" role="link"><i class="far fa-user"></i></a></div>');
 
 if(pages.home) {
+
+  
   let nodeList = document.querySelectorAll("header>.navigation-wrapper>.container>.navigation>.nav.sf-menu>li")
+  const customSection = document.querySelector('.custom_section');
   let categories = new GetCategoriesFromNavbar(Array.from(nodeList), imgArr, "Каталог товаров", customSection, "afterbegin")
   categories.render()
 }
