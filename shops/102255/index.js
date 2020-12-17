2f8d23c3d5d2a3254666f8731f3ad1199dc1c12b
m4medbekov <m4medbekov@gmail.com>
2020-12-18 00:23:06 +0300

    back user icon

--- shops/102255/index.js
+++ shops/102255/index.js
@@ -2,13 +2,13 @@ import Requisites from '../../modules/Requisites/Requisites.js';
 import requsites from './utils/requsites.js'
 import '../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../templates/TemplateTypeOne/TemplateTypeOne.js';
-//import ContactsWithMap from '../../components/ContactsWithMap/ContactsWithMap.js';
-//import contactData from './utils/contactData.js'
+import ContactsWithMap from '../../components/ContactsWithMap/ContactsWithMap.js';
+import contactData from './utils/contactData.js'
 
 
 
 
-// document.querySelector('.header_widgets').insertAdjacentHTML('afterbegin', '<div class="header_user"><a id="user" class="position-relative" href="https://store102255.sellavi.com/login/" role="link"><i class="far fa-user"></i></a></div>');
+document.querySelector('.header_widgets').insertAdjacentHTML('afterbegin', '<div class="header_user"><a id="user" class="position-relative" href="https://store102255.sellavi.com/login/" role="link"><i class="far fa-user"></i></a></div>');
 
 // let newNav = document.querySelector('.nav.sf-menu')
 // document.querySelector('section.page-section.latest-section').insertAdjacentHTML('afterbegin', `<div class="container"><div class="newNavTitle">Категории</div>${newNav.outerHTML}</div>`)
