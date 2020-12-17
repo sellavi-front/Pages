971753f6fe5bab9b3eb00ecca1fcfd5d036fcdf0
m4medbekov <m4medbekov@gmail.com>
2020-12-18 00:06:38 +0300

    commited for test

--- shops/102255/index.js
+++ shops/102255/index.js
@@ -1,27 +1,20 @@
 import Requisites from '../../modules/Requisites/Requisites.js';
 import requsites from './utils/requsites.js'
 import '../../fixes/js/FooterCopy/FooterCopy.js';
-import '../../templates/TemplateTypeOne/TemplateTypeOne.js';
-import ContactsWithMap from '../../components/ContactsWithMap/ContactsWithMap.js';
-import contactData from './utils/contactData.js'
-
-if (document.querySelector('.common-home')) {
-  const customSection = document.querySelector('.custom_section');
-  customSection.classList.add('container');
-
-  const map = new ContactsWithMap(customSection, 'beforeend', contactData);
-  map.render();
-
-}
-
-// add user icon in header
-document.querySelector('.header_widgets').insertAdjacentHTML('afterbegin', '<div class="header_user"><a id="user" class="position-relative" href="https://store102255.sellavi.com/login/" role="link"><i class="far fa-user"></i></a></div>');
-
-let newNav = document.querySelector('.nav.sf-menu')
-document.querySelector('section.page-section.latest-section').insertAdjacentHTML('afterbegin', `<div class="container"><div class="newNavTitle">Категории</div>${newNav.outerHTML}</div>`)
+//import '../../templates/TemplateTypeOne/TemplateTypeOne.js';
+//import ContactsWithMap from '../../components/ContactsWithMap/ContactsWithMap.js';
+//import contactData from './utils/contactData.js'
 
 
-document.querySelector('#home > div > div > section.page-section.latest-section > div:nth-child(2)').insertAdjacentHTML('beforebegin', '<div class="container"><div class="productTitle">Новинки</div></div>');
+
+
+// document.querySelector('.header_widgets').insertAdjacentHTML('afterbegin', '<div class="header_user"><a id="user" class="position-relative" href="https://store102255.sellavi.com/login/" role="link"><i class="far fa-user"></i></a></div>');
+
+// let newNav = document.querySelector('.nav.sf-menu')
+// document.querySelector('section.page-section.latest-section').insertAdjacentHTML('afterbegin', `<div class="container"><div class="newNavTitle">Категории</div>${newNav.outerHTML}</div>`)
+
+
+// document.querySelector('#home > div > div > section.page-section.latest-section > div:nth-child(2)').insertAdjacentHTML('beforebegin', '<div class="container"><div class="productTitle">Новинки</div></div>');
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ООО"Санитары Столицы"');
