67a7c86a55f879d459105bcfb005b37d0fd0a067
Coospir <eugene.starodubov@gmail.com>
2020-12-17 11:01:58 +0300

    updated

--- shops/102169/blocks/footer/footer.js
+++ shops/102169/blocks/footer/footer.js
@@ -1,6 +1,6 @@
 let socialLayout = `
-    <li class="social__li"><h4 class="widget-title">Мы в соц. сетях</h4></li>
-    <li>
+    <li><h4 class="widget-title">Мы в соц. сетях</h4></li>
+    <li class="social__li">
         <a href="#"><img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607516742/sellavi/102169/Group_t9ghfm.png" alt="instagram"></a>
         <a href="#"><img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607516743/sellavi/102169/Group_1_o2dhef.png" alt="wa"></a>
     </li>
