6588cee0c55162cc969d567a9a8d12ffbacbb39a
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-18 15:29:53 +0300

    color choise

--- shops/102242/index.js
+++ shops/102242/index.js
@@ -113,8 +113,8 @@ if (document.querySelector('.wrapper')) {
 if (document.querySelector('.product-item')) {
   document.querySelectorAll('body.ltr.ru > div > div.content-area > section.page-section.latest-section > div > div > div > div > div.media > a > img').forEach(cat => {
     const src = cat.getAttribute('src');
-console.log(src)
     let replacedSrc = src.replace(/\/if_ar_gt_2:1\/c_fill,h_252,w_225,q_100\/c_fill,h_252,w_525\/if_else\/c_pad,h_252,q_100,w_225/gi, '');
+    console.log(replacedSrc)
 
     cat.setAttribute('src', replacedSrc);
   })
