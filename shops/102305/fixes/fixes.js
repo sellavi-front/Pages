447f427983ea64c93d966c535c8f063444b00cb2
Coospir <eugene.starodubov@gmail.com>
2020-12-18 11:19:23 +0300

    updated

---
+++ shops/102305/fixes/fixes.js
@@ -0,0 +1,15 @@
+export default class Fixes {
+  deleteUnnecessaryClasses() {
+    document.querySelector(".main-slider ").remove()
+  }
+
+  updateImgCategories() {
+    const imgCat = document.querySelectorAll(".page-section.homefeatured_category > div > div > div > div > div > a > picture > img");
+    imgCat.forEach(cat => {
+      const src = cat.getAttribute('src');
+      let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
+      cat.setAttribute('src', replacedSrc);
+    })
+  }
+
+}
--- shops/102305/index.js
+++ shops/102305/index.js
@@ -1,1 +1,6 @@
 import './../../templates/TemplateTypeTwo/TemplateTypeTwo.js'
+import Fixes from "./fixes/fixes.js";
+
+let fixes = new Fixes()
+fixes.deleteUnnecessaryClasses()
+fixes.updateImgCategories()
