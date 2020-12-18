249a4a92f7c17bc243daf1e47eeebcfe42b2b407
Coospir <eugene.starodubov@gmail.com>
2020-12-18 13:59:05 +0300

    updated

--- shops/102305/index.js
+++ shops/102305/index.js
@@ -5,3 +5,10 @@ import Fixes from "./fixes/fixes.js";
 let fixes = new Fixes()
 fixes.deleteUnnecessaryClasses()
 fixes.updateImgCategories()
+
+document.querySelector(".content-area>.page-section>.container>.row>div:nth-child(1)>.thumbnail>.media>.media-link>.caption>.caption-wrapper>.caption-inner>.caption-title")
+  .insertAdjacentHTML("afterend",
+    `<div class="card">
+            <small class="count__products">Более 2000 товаров</small>
+            <a href="#" class="more__category">Смотреть</a>
+          </div>`)
