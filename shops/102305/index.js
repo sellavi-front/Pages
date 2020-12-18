9e81ee0635854e3b6108a68b0ef4029d489419a6
Coospir <eugene.starodubov@gmail.com>
2020-12-18 14:49:01 +0300

    updated

--- shops/102305/index.js
+++ shops/102305/index.js
@@ -5,29 +5,6 @@ import Fixes from "./fixes/fixes.js";
 let fixes = new Fixes()
 fixes.deleteUnnecessaryClasses()
 fixes.updateImgCategories()
+fixes.insertTitleToCategories()
 
-document.querySelector(".content-area>.page-section>.container>.row>div:nth-child(1)>.thumbnail>.media>.media-link>.caption>.caption-wrapper>.caption-inner>.caption-title")
-  .insertAdjacentHTML("afterend",
-    `<div class="category__card">
-            <small class="count__products">Более 100 товаров</small>
-            <a href="#" class="more__category">Смотреть</a>
-          </div>`)
 
-document.querySelector(".content-area>.page-section>.container>.row>div:nth-child(2)>.thumbnail>.media>.media-link>.caption>.caption-wrapper>.caption-inner>.caption-title")
-  .insertAdjacentHTML("afterend",
-    `<div class="category__card">
-            <small class="count__products">Более 6000 товаров</small>
-            <a href="#" class="more__category">Смотреть</a>
-          </div>`)
-
-document.querySelector(".content-area>.page-section>.container>.row>div:nth-child(3)>.thumbnail>.media>.media-link>.caption>.caption-wrapper>.caption-inner>.caption-title")
-  .insertAdjacentHTML("afterend",
-    `<div class="category__card">
-            <small class="count__products">Более 2000 товаров</small>
-            <a href="#" class="more__category">Смотреть</a>
-          </div>`)
-
-document.querySelector(".sf-menu>li[data-nav-part='home']").remove()
-document.querySelector(".sf-menu>li[data-nav-part='5764']").remove()
-document.querySelector(".sf-menu>li[data-nav-part='5763']").remove()
-document.querySelector(".sf-menu>li[data-nav-part='5762']").remove()
