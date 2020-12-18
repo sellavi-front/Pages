e7a554bbd70bee45b16f65759b4eca3df6ef05d7
Coospir <eugene.starodubov@gmail.com>
2020-12-18 15:02:57 +0300

    updated

--- shops/102305/index.js
+++ shops/102305/index.js
@@ -1,6 +1,6 @@
 import './../../templates/TemplateTypeTwo/TemplateTypeTwo.js'
 import "../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
-import "../../fixes/fixes.js"
+//import "../../fixes/fixes.js"
 import ContactsWithMap from "../../components/ContactsWithMap/ContactsWithMap.js"
 import contactData from "./utils/utils.js"
 import Fixes from "./fixes/fixes.js"
@@ -13,7 +13,6 @@ fixes.insertTitleToCategories()
 if (document.querySelector('.common-home')) {
   const customSection = document.querySelector('.custom_section');
   customSection.classList.add('container');
-
   const map = new ContactsWithMap(customSection, 'beforeend', contactData);
   map.render();
 }
