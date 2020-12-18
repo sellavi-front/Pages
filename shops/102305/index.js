76b73ea117721f92863af6586a27ad49c4d1410f
Coospir <eugene.starodubov@gmail.com>
2020-12-18 14:50:26 +0300

    updated

--- shops/102305/index.js
+++ shops/102305/index.js
@@ -1,5 +1,6 @@
 import './../../templates/TemplateTypeTwo/TemplateTypeTwo.js'
 import "../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js";
+import "../../fixes/fixes.js";
 import Fixes from "./fixes/fixes.js";
 
 let fixes = new Fixes()
