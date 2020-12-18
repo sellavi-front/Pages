b65f1d04bb1535738a851a8b4f9f1a6d98e26d40
Coospir <eugene.starodubov@gmail.com>
2020-12-18 14:57:48 +0300

    updated

--- shops/102305/index.js
+++ shops/102305/index.js
@@ -1,11 +1,21 @@
 import './../../templates/TemplateTypeTwo/TemplateTypeTwo.js'
-import "../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js";
-import "../../fixes/fixes.js";
-import Fixes from "./fixes/fixes.js";
+import "../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
+import "../../fixes/fixes.js"
+import ContactsWithMap from "../../components/ContactsWithMap/ContactsWithMap.js"
+import contactData from "./utils/utils.js"
+import Fixes from "./fixes/fixes.js"
 
 let fixes = new Fixes()
 fixes.deleteUnnecessaryClasses()
 fixes.updateImgCategories()
 fixes.insertTitleToCategories()
+
+if (document.querySelector('.common-home')) {
+  const customSection = document.querySelector('.custom_section');
+  customSection.classList.add('container');
+
+  const map = new ContactsWithMap(customSection, 'beforeend', contactData);
+  map.render();
+}
 
 
---
+++ shops/102305/utils/utils.js
@@ -0,0 +1,21 @@
+const contactData = {
+  title: 'Контакты',
+  contacts: [
+    {
+      title: 'Адрес',
+      contact: 'г.Волгоград, ул.Бурейская, д.8',
+    },
+    {
+      title: 'Телефон',
+      contact: '<a href="tel:+7(915)024-06-73">+7(915)024-06-73</a>',
+    },
+    {
+      title: 'E-mail',
+      contact: '<a href="mailto:denis@smktrade.ru">denis@smktrade.ru</a>',
+    },
+  ],
+  map:
+    `<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A98fc2ee65d128a92c1c5f8e7fd92940c6594ae968609816e47be4caa2fa4ce70&amp;source=constructor" frameborder="0"></iframe>`
+};
+
+export default contactData
