79080fa741bdc040e449133367cd2713fba0ea15
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-20 11:08:12 +0300

    color choise

--- shops/102121/index.js
+++ shops/102121/index.js
@@ -15,6 +15,17 @@ if (document.querySelector('.common-home')) {
 
   const map = new ContactsWithMap(customSection, 'beforeend', contactData);
   map.render();
+
+  // reset cut image in categories
+  const imgCat = document.querySelectorAll(".page-section.homefeatured_category > div > div > div > div > div > a > picture > img");
+  imgCat.forEach(cat => {
+    const src = cat.getAttribute('src');
+
+    let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
+
+    cat.setAttribute('src', replacedSrc);
+  })
+  // rend reset cit img
 }
 
 if (location.href.includes('term-of-use')) {
