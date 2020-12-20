c1b8f5322bef33a7d68635a264e9895a15780390
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-20 16:17:51 +0300

    color choise

--- shops/102121/index.js
+++ shops/102121/index.js
@@ -9,6 +9,7 @@ import contactData from './utils/contactData.js'
 import '../../templates/TemplateTypeOne/TemplateTypeOne.js';
 import ContactsWithMap from '../../components/ContactsWithMap/ContactsWithMap.js';
 import '../../components/HeaderTypeTwo/HeaderTypeTwo.js'
+import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
 
 if (document.querySelector('.common-home')) {
   const customSection = document.querySelector('.custom_section');
