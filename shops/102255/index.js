789bfeac81751bbea5f4e631ffc589bcbe99f974
m4medbekov <63649921+m4medbekov@users.noreply.github.com>
2020-12-21 18:21:03 +0300

    Fix button name.

--- shops/102255/index.js
+++ shops/102255/index.js
@@ -16,7 +16,7 @@ if (document.querySelector('.common-home')) {
 
 if (document.querySelector('.common-home')) {
   document.querySelector('#banner0 > div > div > div:nth-child(2) > div > div > div > div > div > div > h3 > span').insertAdjacentHTML('afterend', '<span class="second-title-text2">Заголовок 2 Заголовок 2 Заголовок 2</span>')
-  document.querySelector('.caption-subtitle .second-title-text2').insertAdjacentHTML('afterend', '<div class="button-in-banner">Подробнее</div>')
+  document.querySelector('.caption-subtitle .second-title-text2').insertAdjacentHTML('afterend', '<div class="button-in-banner2">Подробнее</div>')
 }
 
 if (document.querySelector('.common-home')) {
