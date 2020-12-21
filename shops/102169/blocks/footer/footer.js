c083f1e3508b4579ada33badfb698d025c92497e
Coospir <eugene.starodubov@gmail.com>
2020-12-21 14:08:25 +0300

    updated 102169

--- shops/102169/blocks/footer/footer.js
+++ shops/102169/blocks/footer/footer.js
@@ -2,7 +2,7 @@ let socialLayout = `
     <li><h4 class="widget-title">Мы в соц. сетях</h4></li>
     <li class="social__li">
         <a href="https://www.instagram.com/_organic_stories_/" target="_blank"><img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607516742/sellavi/102169/Group_t9ghfm.png" alt="instagram"></a>
-        <a target="_blank" onclick="window.open('https://wa.me/79150940619?text=Здравствуйте!%20Заинтересовала%20Ваша%20продукция.')"><img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607516743/sellavi/102169/Group_1_o2dhef.png" alt="wa"></a>
+        <a target="_blank" href="https://wa.me/79150940619?text=Здравствуйте!%20Заинтересовала%20Ваша%20продукция."><img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607516743/sellavi/102169/Group_1_o2dhef.png" alt="wa"></a>
     </li>
 `
 
