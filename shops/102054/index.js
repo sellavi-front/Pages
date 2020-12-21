a105e2f37ccf1011024ff3275c0a1224d582f5b4
Coospir <eugene.starodubov@gmail.com>
2020-12-21 18:58:53 +0300

    updated

--- shops/102054/index.js
+++ shops/102054/index.js
@@ -84,13 +84,13 @@ window.onload = () => {
     titleNewCollection.classList.add("title__new__collection")
     let productRowItems = oldProductRow.querySelectorAll(".product-item")
 
-    let productsBlock = `
-    <div class="products__block">
-        <div class="products__block__banner"><img alt="photo" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607527479/sellavi/102054/5T3A9736-min_bmsxax.jpg" onclick="location.href='https://whitemoon.shop/1/'" /></div>
-        <div class="products__block__items"></div>
-    </div>
-    `
-    titleNewCollection.insertAdjacentHTML("afterend", productsBlock)
+    // let productsBlock = `
+    // <div class="products__block">
+    //     <div class="products__block__banner"><img alt="photo" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607527479/sellavi/102054/5T3A9736-min_bmsxax.jpg" onclick="location.href='https://whitemoon.shop/1/'" /></div>
+    //     <div class="products__block__items"></div>
+    // </div>
+    // `
+    // titleNewCollection.insertAdjacentHTML("afterend", productsBlock)
     let productRowBlock = document.querySelector(".products__block__items")
     productRowItems.forEach(item => {
       productRowBlock.insertAdjacentHTML("afterbegin", item.outerHTML)
