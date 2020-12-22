b67fb219618251f677653b5057cbf8f2c4fe2a5d
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-22 16:00:25 +0300

    popup

--- shops/102233/index.js
+++ shops/102233/index.js
@@ -33,12 +33,15 @@ if (document.querySelector('.common-home')) {
 }
 
 if (document.querySelector('.wrapper') && !location.href.includes('checkout')) {
-  const footerContainer = document.querySelector('.footer-widgets>div');
+  const footerContainer = document.querySelector('.footer-widgets>div>div');
 
-  new SocialIconsTypeOne(footerContainer, 'beforeend', socIconsData).render();
+  new SocIconsTypeOne(footerContainer, 'beforeend', socialIcons).render();
 
-  const socIcons = document.querySelector('.soc-icons>.container');
-  socIcons.insertAdjacentHTML('afterbegin', `<p class="soc-icons__title">Мы в соц.сетях</p>`)
+  const socIconsContainer = document.querySelector('.soc-icons>.container');
+  socIconsContainer.insertAdjacentHTML('afterbegin', `<h4 class="widget-title soc-icons__title">Мы в соц.сетях</h4>`);
+
+  const socIcons = document.querySelector('.soc-icons');
+  socIcons.classList.add('col-md-3');
 }
 
 
