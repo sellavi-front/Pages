0343917817605fa20fe87f76c597916e4966c65e
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-22 11:36:51 +0300

    popup

--- shops/102143/index.js
+++ shops/102143/index.js
@@ -41,6 +41,7 @@ if (document.querySelector('.wrapper')) {
   const contactsClass = new ContactsTypeOne(footerWidgets, 'afterbegin', contactData);
 
   contactsClass.render();
+  const termUse = document.querySelectorAll('.footer_content_wrapper >ul>li>a');
 
   termUse.forEach(el => {
     if(el.textContent.toLowerCase() === 'условия обслуживания') {
