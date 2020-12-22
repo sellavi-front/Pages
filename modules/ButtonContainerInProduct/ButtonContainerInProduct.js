773ed18f93ab2887a60dee649e74d606ffca40b7
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-22 18:50:12 +0300

    popup

--- modules/ButtonContainerInProduct/ButtonContainerInProduct.js
+++ modules/ButtonContainerInProduct/ButtonContainerInProduct.js
@@ -37,6 +37,25 @@ export default class ButtonContainerInProduct {
   buttonContainerInProduct.createContainer();
   buttonContainerInProduct.delete(addToCart);
   buttonContainerInProduct.delete(wishList);
+
+  setTimeout(() => {
+    const price = document.querySelectorAll('.product-item .price');
+    const addToCart = document.querySelectorAll('.add_to_cart');
+
+    if (document.querySelectorAll('.add_to_wishlist').length === 0) {
+      var wishList = document.querySelectorAll('.remove_from_wishlist');
+    } else {
+      var wishList = document.querySelectorAll('.add_to_wishlist');
+    }
+
+
+    const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, wishList, price);
+
+    buttonContainerInProduct.createContainer();
+    buttonContainerInProduct.delete(addToCart);
+    buttonContainerInProduct.delete(wishList);
+
+  }, 4000)
 // const btnContainer = document.querySelectorAll('.buttons__container')
 
 // const addCartInProductCard = new AddElemInContainer(addToCart, btnContainer);
