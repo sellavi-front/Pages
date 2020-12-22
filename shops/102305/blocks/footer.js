d728ec04a3500d62b799bf05dfb286c210ae6388
Coospir <eugene.starodubov@gmail.com>
2020-12-22 13:50:27 +0300

    updated

---
+++ shops/102305/blocks/footer.js
@@ -0,0 +1,17 @@
+import SocialIconsTypeOne from "../../../components/SocialIconsTypeOne/SocialIconsTypeOne.js"
+
+export default class Footer {
+  addIcons() {
+    if (document.querySelector('.wrapper')) {
+      const footerContainer = document.querySelector('.footer-widgets>div>div');
+
+      new SocialIconsTypeOne(footerContainer, 'beforeend', socIconsData).render();
+
+      const socIconsContainer = document.querySelector('.soc-icons>.container');
+      socIconsContainer.insertAdjacentHTML('afterbegin', `<h4 class="widget-title soc-icons__title">Мы в соц.сетях</h4>`);
+
+      const socIcons = document.querySelector('.soc-icons');
+      socIcons.classList.add('col-md-3');
+    }
+  }
+}
