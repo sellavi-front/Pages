045321efaa3a0694a29484d7761b67026fc1393d
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-22 13:01:12 +0300

    popup

--- components/VideoBanner/VideoBanner.js
+++ components/VideoBanner/VideoBanner.js
@@ -12,7 +12,10 @@ export default class VideoBanner {
     console.log(this.obj);
     return this.obj.map(item => {
       return `
-        <video preload="yes" width='100%' height='100%' src="${item.src}" autoplay muted="muted" loop playsinline poster="https://res.cloudinary.com/gz-company/image/upload/v1608630243/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2020-12-22_%D0%B2_12.43.43_yorxxv.png">
+        <video preload="yes" width='100%' height='100%' src="${item.src}" autoplay muted="muted" loop playsinline
+        ${
+          item.poster ? `poster="${item.poster}"` : ''
+        }>
             Your browser does not support the video tag.
         </video>
       `
