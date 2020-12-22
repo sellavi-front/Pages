bce6f75ff5cbd302597ba38f992b118ede9a52d2
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-22 17:56:30 +0300

    popup

--- shops/102242/constants/constants.js
+++ shops/102242/constants/constants.js
@@ -1,6 +1,6 @@
 const bannerContent = {
   src: "https://res.cloudinary.com/gz-company/video/upload/v1606814203/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/VIDEO_CORSETS_ip0o1g.mp4",
-  poster: 'https://res.cloudinary.com/gz-company/image/upload/v1608631107/%D0%9A%D0%BE%D1%80%D1%81%D0%B5%D1%82%D1%8B/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2020-12-22_%D0%B2_12.58.09_aifulg.png',
+  poster: 'https://res.cloudinary.com/rostislavsellavi/image/upload/v1608648872/%D1%81%D0%B6%D0%B0%D1%82%D1%8B%D0%B8%CC%86_ckebsv.png',
   caption: 'Корсеты премиум класса'
 }
 
