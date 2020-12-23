252a1a37c2e8134a876e1ffe5a22dbc4f3b4fb42
Coospir <eugene.starodubov@gmail.com>
2020-12-23 17:36:27 +0300

    updated

--- shops/101263/blocks/banner.js
+++ shops/101263/blocks/banner.js
@@ -1,6 +1,6 @@
 export default class Banner {
   updateImgCategories() {
-    const imgCat = document.querySelectorAll(".custom_banner");
+    const imgCat = document.querySelectorAll(".content-area>.main-slider>#banner0>.owl-wrapper-outer>.owl-wrapper>.owl-item>.item>.img-bg>.img-fluid")
     imgCat.forEach(cat => {
       const src = cat.getAttribute('src');
       let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
--- shops/101263/index.js
+++ shops/101263/index.js
@@ -5,7 +5,8 @@ import Banner from "./blocks/banner.js"
 import pages from "./utils/pages/pages.js"
 
 if(pages.home) {
-
+  let Banner = new Banner()
+  Banner.updateImgCategories()
 }
 
 
