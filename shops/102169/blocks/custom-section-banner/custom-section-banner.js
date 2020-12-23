c3cfa83488e3b1f03a8e39f341f909dbd73294f0
Coospir <eugene.starodubov@gmail.com>
2020-12-23 15:07:50 +0300

    updated

--- shops/102169/blocks/custom-section-banner/custom-section-banner.js
+++ shops/102169/blocks/custom-section-banner/custom-section-banner.js
@@ -14,15 +14,15 @@ let customSectionBannerLayout = `
             <p class="section__banner_title">Нашу косметику можно приобрести, как в нашем интернет магазине, так и в других городах России.</p>
             <p class="distributors">Представляем наших дистрибьюторов.</p>
             <ul class="distributors__list">
-                <li class="distributor">г. Санкт-Петербург, Сеть Аптек "Аптека.Хелп". <a href="https://apteka.help/"><b>Сайт</b></a></li>
-                <li class="distributor">г. Томск, <a href="https://instagram.com/eco_cosmetique/"><b>Instagram</b></a></li>
-                <li class="distributor">г. Екатеринбург, ул. Ильича 31, Тел. +7(909)011-12-30. <a href="https://www.instagram.com/ekbrose/"><b>Instagram</b></a>, <a href="https://ekbrose.ru/"><b>Сайт</b></a></li>
-                <li class="distributor">г. Иваново, ул. Калинина 17, Тел. 89106679837. <a href="https://www.instagram.com/nota37/"><b>Instagram</b></a></li>
+                <li class="distributor">г. Санкт-Петербург, Сеть Аптек "Аптека.Хелп". <a href="https://apteka.help/"><img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608725196/sellavi/102169/globe_u4ra42.png" alt="site"/></a></li>
+                <li class="distributor">г. Томск, <a href="https://instagram.com/eco_cosmetique/"><img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607940084/sellavi/102169/87390_gg5in2.png" alt="site"/></a></li>
+                <li class="distributor">г. Екатеринбург, ул. Ильича 31, Тел. +7(909)011-12-30. <a href="https://www.instagram.com/ekbrose/"><img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607940084/sellavi/102169/87390_gg5in2.png" alt="site"/></a>, <a href="https://ekbrose.ru/"><img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608725196/sellavi/102169/globe_u4ra42.png" alt="site"/></a></li>
+                <li class="distributor">г. Иваново, ул. Калинина 17, Тел. 89106679837. <a href="https://www.instagram.com/nota37/"><img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1607940084/sellavi/102169/87390_gg5in2.png" alt="site"/></a></li>
             </ul>
             <p class="marketplaces__container">Наша продукция на маркет плейсах</p>
-            <p class="distributor"><a href="https://www.wildberries.ru/brands/os"><img class="disphoto__custom_banner" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608724531/sellavi/102169/wild.svg" alt="img"/></a></p>
-            <p class="distributor"><a href="https://www.ozon.ru/search/?from_global=true&text=organic+stories"><img class="disphoto__custom_banner" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608724566/sellavi/102169/ozon.svg" alt="img"/></a></p>
-            <p class="distributor"><a href="https://saterno.ru/organic-stories"><img class="disphoto__custom_banner" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608724696/sellavi/102169/unnamed_1_k0ni2l.svg" alt="img"/></a></p>
+            <p class="distributor"><a href="https://www.wildberries.ru/brands/os" target="_blank"><img class="disphoto__custom_banner"  src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608724531/sellavi/102169/wild.svg" alt="img"/></a></p>
+            <p class="distributor"><a href="https://www.ozon.ru/search/?from_global=true&text=organic+stories" target="_blank"><img class="disphoto__custom_banner" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608724566/sellavi/102169/ozon.svg" alt="img"/></a></p>
+            <p class="distributor"><a href="https://saterno.ru/organic-stories" target="_blank"><img class="disphoto__custom_banner" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608724696/sellavi/102169/unnamed_1_k0ni2l.svg" alt="img"/></a></p>
             <p class="wanna__be__distributor"><b>Хотите и вы стать нашим дистрибьютором? Присоединяйтесь!</b></p>
             <p class="wanna__be__distributor"><b>Отправляйте запрос на почту zakaz@organic-stories.ru</b></p>
         </div>
