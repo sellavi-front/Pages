052afa60f79974cb87b655d8ec7b913b630fbc73
m4medbekov <m4medbekov@gmail.com>
2020-12-23 15:06:10 +0300

    fix

--- shops/102416/utils/navToHeader.js
+++ shops/102416/utils/navToHeader.js
@@ -3,7 +3,6 @@ function navToHeader() {
     const newNav = oldNav.cloneNode(true);
     document.querySelector('.logo').insertAdjacentHTML('afterend', newNav.outerHTML);
     oldNav.remove();
-    const oldNav = document.querySelector('.nav.sf-menu');
 }
 
 export {navToHeader}
