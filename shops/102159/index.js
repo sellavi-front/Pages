39a0fcc006eb1984bd0f038064d2f857b464e7f1
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-24 15:29:09 +0300

    banner

--- shops/102159/index.js
+++ shops/102159/index.js
@@ -5,18 +5,19 @@ import '../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js';
 import '../../fixes/js/ChangeAgreement/ChangeAgreement.js';
 
-import '../../templates/TemplateTypeTwo/TemplateTypeTwo.js'
-import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
+import '../../templates/TemplateTypeTwo/TemplateTypeTwo.js';
+import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
 import ColorChoise from '../../components/ColorChoise/ColorChoise.js';
 
-
 if (document.querySelector('wrapper')) {
-
   // optimize logo quality
   const logo = document.querySelector('.header-wrapper .logo img');
   const src = logo.getAttribute('src');
 
-  let replacedSrc = src.replace(/\/c_fit,g_center,h_176,q_100,w_430\/c_pad,h_176,w_430\/v1608639340/gi, '');
+  let replacedSrc = src.replace(
+    /\/c_fit,g_center,h_176,q_100,w_430\/c_pad,h_176,w_430\/v1608639340/gi,
+    '',
+  );
 
   logo.setAttribute('src', replacedSrc);
   // end optimize logo quality
@@ -24,18 +25,24 @@ if (document.querySelector('wrapper')) {
   const socIcons = document.querySelector('.social-icons > li > a');
 
   socIcons.textContent = '';
-  socIcons.insertAdjacentHTML('beforeend', `<img src="https://res.cloudinary.com/gz-company/image/upload/v1608811788/icons8-instagram_1_vtwnvk.svg">`);
+  socIcons.insertAdjacentHTML(
+    'beforeend',
+    `<img src="https://res.cloudinary.com/gz-company/image/upload/v1608811788/icons8-instagram_1_vtwnvk.svg">`,
+  );
 
+  const termUse = document.querySelectorAll('.footer_content_wrapper >ul>li>a');
+
+  termUse.forEach((el) => {
+    if (el.textContent.toLowerCase() === 'условия обслуживания') {
+      el.closest('li').remove();
+    }
+  });
 }
-
 
 if (document.querySelector('.product-product')) {
   const colorChoise = new ColorChoise();
   colorChoise.render();
 }
-
-
-
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Зюзина Оксана Викторовна');
@@ -51,12 +58,22 @@ if (location.href.includes('contact')) {
 }
 
 if (location.href.includes('policy')) {
-  const req = new Requisites(requsites, 'afterend', 'ИП Зюзина Оксана Викторовна', document.querySelector("#content > div > div"));
+  const req = new Requisites(
+    requsites,
+    'afterend',
+    'ИП Зюзина Оксана Викторовна',
+    document.querySelector('#content > div > div'),
+  );
   req.setTemplate();
 }
 
 if (location.href.includes('pay-delivery')) {
-  const req = new Requisites(requsites, 'afterend', 'ИП Зюзина Оксана Викторовна', document.querySelector("#content > div > div"), dataContact);
+  const req = new Requisites(
+    requsites,
+    'afterend',
+    'ИП Зюзина Оксана Викторовна',
+    document.querySelector('#content > div > div'),
+    dataContact,
+  );
   req.setTemplate();
 }
-
