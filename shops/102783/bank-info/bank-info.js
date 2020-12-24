2545e3899cdcdfd70b42513036b3b5e59b30165a
Coospir <eugene.starodubov@gmail.com>
2020-12-24 18:32:32 +0300

    updated

---
+++ shops/102783/bank-info/bank-info.js
@@ -0,0 +1,15 @@
+import Requisites from '../../../modules/Requisites/Requisites.js';
+import requisites from './../utils/requsites.js'
+
+if (location.href.includes('term-of-use')) {
+  const req = new Requisites(requisites, 'beforeend', 'ИП КОРСАКОВ ЭДУАРД ВЛАДИМИРОВИЧ');
+
+  req.setTemplate();
+}
+
+if (location.href.includes('contact')) {
+  const media = document.querySelector('.contact-info > .media-list > .media:last-child');
+
+  const req = new Requisites(requsites, 'afterend', 'ИП КОРСАКОВ ЭДУАРД ВЛАДИМИРОВИЧ', media);
+  req.setTemplate();
+}
