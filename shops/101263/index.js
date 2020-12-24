1e683f880826b021c6aa7fdf5e07b107010b1127
Coospir <eugene.starodubov@gmail.com>
2020-12-24 12:20:00 +0300

    updated

--- shops/101263/index.js
+++ shops/101263/index.js
@@ -4,6 +4,10 @@ import "../../fixes/fixes.js"
 import Banner from "./blocks/banner.js"
 import pages from "./utils/pages/pages.js"
 import Advantages from "./blocks/advantages.js"
+import Functions from "./utils/functions/functions.js"
+
+let Fixes = new Functions()
+Fixes.deleteSpanBuyBtn()
 
 if(pages.home) {
   let banner = new Banner()
