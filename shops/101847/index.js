8c79785b168154549121db322d908a00252b78ae
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-24 18:27:22 +0300

    new shop

--- shops/101847/index.js
+++ shops/101847/index.js
@@ -21,3 +21,13 @@ if (location.href.includes('/policy')) {
   const req = new Requisites(requsites, 'afterend', 'ИП Зобнина Вера Сергеевна', document.querySelector("#content > div > div"));
   req.setTemplate();
 }
+
+if (document.querySelector('.wrapper')) {
+  const termUse = document.querySelectorAll('.footer_content_wrapper >ul>li>a');
+
+  termUse.forEach(el => {
+       if(el.textContent.toLowerCase() === 'условия обслуживания') {
+            el.closest('li').remove();
+       }
+  })
+}
