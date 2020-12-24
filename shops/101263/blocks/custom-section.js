bce2f1ac2c645cd37d379c50e0c74608fb556272
Coospir <eugene.starodubov@gmail.com>
2020-12-24 16:04:19 +0300

    updated

--- shops/101263/blocks/custom-section.js
+++ shops/101263/blocks/custom-section.js
@@ -3,9 +3,9 @@ import customSectionLayout from "../layouts/custom-section-layout.js"
 export default class CustomSection {
   render() {
     if(screen.width <= 768) {
-      document.querySelector(".custom-section").insertAdjacentHTML("beforeend", customSectionLayout.mobile)
+      document.querySelector(".custom_section").insertAdjacentHTML("beforeend", customSectionLayout.mobile)
     } else if(screen.width > 768) {
-      document.querySelector(".custom-section").insertAdjacentHTML("beforeend", customSectionLayout.desktop)
+      document.querySelector(".custom_section").insertAdjacentHTML("beforeend", customSectionLayout.desktop)
     }
   }
 }
