ad182c4e93a9816c207e34036e2b6f26a7640e3b
m4medbekov <m4medbekov@gmail.com>
2020-12-24 15:02:40 +0300

    Add map

--- shops/102416/index.js
+++ shops/102416/index.js
@@ -11,6 +11,8 @@ import {displaySecondInfo} from './utils/bannerSecondInfo.js';
 import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
 import FormBasic from '../../components/FormBasic/FormBasic.js';
 import senderData from './utils/senderData.js';
+import ContactsWithMap from '../../components/ContactsWithMap/ContactsWithMap.js';
+import contactData from './utils/contactData.js';
 
 if (document.querySelector('.common-home')) {
   document.querySelector('.page-section.homefeatured_category').insertAdjacentHTML('beforebegin', '<div class="pre-category-title"><div class="container"><span>Категории</span></div></div>');
@@ -30,20 +32,28 @@ const sender = new FormBasic(latestSection, 'beforeend', senderData);
 sender.render();
 
 const senderForm = document.querySelector('.sender-basic form');
-  const dataForm = {};
-  senderForm.addEventListener('input', (e) => {
-    if (e.target.getAttribute('name') === 'userName') {
-      dataForm.name = e.target.value;
-    }
+const dataForm = {};
+senderForm.addEventListener('input', (e) => {
+  if (e.target.getAttribute('name') === 'userName') {
+    dataForm.name = e.target.value;
+  }
 
-    if (e.target.getAttribute('name') === 'phone') {
-      dataForm.phone = e.target.value;
-    }
-  })
+  if (e.target.getAttribute('name') === 'phone') {
+    dataForm.phone = e.target.value;
+  }
+})
 
-  senderForm.querySelector('button').addEventListener('click', () => {
-    window.open(`https://wa.me/79267979561?text=Здравствуйте!%0D%0AМеня%20зовут%20${dataForm.name}.%20Я%20заинтересовался%20вашим%20товаром.%20Пожалуйста%20свяжитесь%20со%20мной%20${dataForm.phone}`, '_blank');
-  });
+senderForm.querySelector('button').addEventListener('click', () => {
+  window.open(`https://wa.me/79267979561?text=Здравствуйте!%0D%0AМеня%20зовут%20${dataForm.name}.%20Я%20заинтересовался%20вашим%20товаром.%20Пожалуйста%20свяжитесь%20со%20мной%20${dataForm.phone}`, '_blank');
+});
+
+if (document.querySelector('.common-home')) {
+  const customSection = document.querySelector('.custom_section');
+  customSection.classList.add('container');
+
+  const map = new ContactsWithMap(customSection, 'beforeend', contactData);
+  map.render();
+}
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Орлова Елена Геннадьевна');
