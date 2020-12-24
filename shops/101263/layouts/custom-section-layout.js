a6efb01aaf9c70a8836f1bfd7197994a0418d937
Coospir <eugene.starodubov@gmail.com>
2020-12-24 16:11:25 +0300

    updated

--- shops/101263/layouts/custom-section-layout.js
+++ shops/101263/layouts/custom-section-layout.js
@@ -2,13 +2,13 @@ let customSectionLayout = {
   desktop:
     `
     <div class="container">
-        <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608812295/sellavi/101263/Frame_28_uepdeh.svg" alt="custom"/>
+        <img class="custom_section_photo" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608812295/sellavi/101263/Frame_28_uepdeh.svg" alt="custom"/>
     </div>
     `,
   mobile:
     `
     <div class="container">
-        <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608812346/sellavi/101263/Component_2_bgzeyt.svg" alt="custom"/>
+        <img class="custom_section_photo" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608812346/sellavi/101263/Component_2_bgzeyt.svg" alt="custom"/>
     </div>
     `
 }
