7254f8c16e370eb549788641908a80b5581adf67
Coospir <eugene.starodubov@gmail.com>
2020-12-24 16:25:32 +0300

    updated

--- shops/101263/blocks/custom-banner.js
+++ shops/101263/blocks/custom-banner.js
@@ -3,9 +3,9 @@ import customBannerLayout from "../layouts/custom-banner-layout.js"
 export default class CustomBanner {
   render() {
     if(screen.width <= 768) {
-      document.querySelector(".custom_section").insertAdjacentHTML("beforeend", customBannerLayout.mobile)
+      document.querySelector(".custom_banner").insertAdjacentHTML("afterbegin", customBannerLayout.mobile)
     } else if(screen.width > 768) {
-      document.querySelector(".custom_section").insertAdjacentHTML("beforeend", customBannerLayout.desktop)
+      document.querySelector(".custom_banner").insertAdjacentHTML("afterbegin", customBannerLayout.desktop)
     }
   }
 }
