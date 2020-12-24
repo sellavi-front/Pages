c8dff5554ea7aec184e867860b6a5cec99c8fbbb
Coospir <eugene.starodubov@gmail.com>
2020-12-24 12:41:30 +0300

    updated

--- shops/101263/utils/functions/functions.js
+++ shops/101263/utils/functions/functions.js
@@ -1,7 +1,5 @@
 export default class Functions {
   deleteSpanBuyBtn() {
-    if(document.querySelector(".product-category") || document.querySelector(".common-home")) {
       document.querySelectorAll("#home>.wrapper>.content-area>.page-section>.container:nth-child(2)>.products>.product-item>.thumbnail>.price_actions>.buttons__container>.add_to_cart>.btn>span").forEach(e => e.parentNode.removeChild(e));
-    }
   }
 }
