eb7c22badd31566dd9d230c66a6c7bdcb597c91c
Coospir <eugene.starodubov@gmail.com>
2020-12-24 12:55:11 +0300

    updated

--- shops/101263/utils/pages/pages.js
+++ shops/101263/utils/pages/pages.js
@@ -1,6 +1,6 @@
 let pages = {
   home: document.querySelector(".common-home"),
-  productCategory: document.querySelector("#home>.product-category")
+  productCategory: document.querySelector("body#home.product-category")
 }
 
 export default pages
