72e1ab41ad55d6dfe2c261dd9e2bd8886de8f8a6
Coospir <eugene.starodubov@gmail.com>
2020-12-24 15:03:38 +0300

    updated

--- shops/101263/index.js
+++ shops/101263/index.js
@@ -5,6 +5,23 @@ import Banner from "./blocks/banner.js"
 import pages from "./utils/pages/pages.js"
 import Advantages from "./blocks/advantages.js"
 import Functions from "./utils/functions/functions.js"
+import DeleteClasses from '../../modules/DeleteClasses/DeleteClasses.js'
+
+const bootstrapClasses = [
+  'col-lg-4',
+  'col-xl-3',
+  'mb-0',
+  'mb-md-4',
+  'col-12',
+  'col-sm-12',
+  'col-sm-6',
+  'col-md-6',
+  'col-md-3',
+  'col-lg-6',
+  'col-xl-4',
+  'mb-4',
+  'container'
+];
 
 
 
@@ -14,6 +31,11 @@ if(pages.home) {
   let advantages = new Advantages()
   advantages.render()
   let Fixes = new Functions()
+  if (document.querySelector('.product-item')) {
+    const productItems = document.querySelectorAll('.product-item');
+    const deleteSelectors = new DeleteClasses(productItems, bootstrapClasses);
+    deleteSelectors.findAndDelete();
+  }
 }
 
 if(pages.productCategory) {
