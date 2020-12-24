d6f4e735ee492a728eaa2a35aacda1fe7a9d3d3d
Coospir <eugene.starodubov@gmail.com>
2020-12-24 16:24:04 +0300

    updated

--- shops/101263/index.js
+++ shops/101263/index.js
@@ -1,7 +1,7 @@
 import "../../components/HeaderTypeTwo/HeaderTypeTwo.js"
 import "../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
 import "../../fixes/fixes.js"
-import Banner from "./blocks/banner.js"
+//import Banner from "./blocks/banner.js"
 import pages from "./utils/pages/pages.js"
 import Advantages from "./blocks/advantages.js"
 import Functions from "./utils/functions/functions.js"
