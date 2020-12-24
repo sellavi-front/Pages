3dbc6a5d228fe91f73abb3cf85062c2160bc565a
Coospir <eugene.starodubov@gmail.com>
2020-12-24 12:23:24 +0300

    updated

--- shops/101263/blocks/banner.js
+++ shops/101263/blocks/banner.js
@@ -6,5 +6,12 @@ export default class Banner {
       let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
       cat.setAttribute('src', replacedSrc);
     })
+
+    const imgCat2 = document.querySelectorAll(".content-area>.main-slider>#banner0>.owl-wrapper-outer>.owl-wrapper>.owl-item>.item>.img-bg>source")
+    imgCat2.forEach(cat => {
+      const src = cat.getAttribute('src');
+      let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
+      cat.setAttribute('src', replacedSrc);
+    })
   }
 }
