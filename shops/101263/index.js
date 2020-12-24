27320ebb7887552c232c6aa2d1d92e782e27988e
Coospir <eugene.starodubov@gmail.com>
2020-12-24 11:55:01 +0300

    updated

--- shops/101263/index.js
+++ shops/101263/index.js
@@ -3,10 +3,13 @@ import "../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
 import "../../fixes/fixes.js"
 import Banner from "./blocks/banner.js"
 import pages from "./utils/pages/pages.js"
+import Advantages from "./blocks/advantages.js"
 
 if(pages.home) {
   let banner = new Banner()
   banner.updateImgCategories()
+  let advantages = new Advantages()
+  advantages.render()
 }
 
 
---
+++ shops/101263/utils/advantages-object.js
@@ -0,0 +1,19 @@
+export const adv = [
+  {
+    icon:
+      'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608799997/sellavi/101263/kosher-food.svg',
+    text: 'Более 100 товаров',
+  },
+  {
+    icon: 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608626891/sellavi/102305/Car.svg',
+    text: 'Бесплатная доставка от 2500р',
+  },
+  {
+    icon: 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608626891/sellavi/102305/Smile.svg',
+    text: 'Приятное обслуживание',
+  },
+  {
+    icon: 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608626891/sellavi/102305/Sale.svg',
+    text: 'Скидки выходного дня',
+  }
+];
