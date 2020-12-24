ad2f1edd54751b534f6d650818d6fb42ac803fc6
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-24 14:23:20 +0300

    banner

--- shops/102159/index.js
+++ shops/102159/index.js
@@ -12,7 +12,7 @@ import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
 if (document.querySelector('wrapper')) {
 
   // optimize logo quality
-  const logo = document.querySelector('.logo');
+  const logo = document.querySelector('.header-wrapper .logo');
   const src = logo.getAttribute('src');
 
   let replacedSrc = src.replace(/\/c_fit,g_center,h_176,q_100,w_430\/c_pad,h_176,w_430\/v1608639340/gi, '');
