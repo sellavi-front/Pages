c7736d30019ce47f00a2242486f1a522dc8ec519
Coospir <eugene.starodubov@gmail.com>
2020-12-24 12:28:58 +0300

    updated

--- shops/101263/blocks/banner.js
+++ shops/101263/blocks/banner.js
@@ -6,12 +6,6 @@ export default class Banner {
       let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
       cat.setAttribute('src', replacedSrc);
     })
-
-    const imgCat2 = document.querySelectorAll(".content-area>.main-slider>#banner0>.owl-wrapper-outer>.owl-wrapper>.owl-item>.item>.img-bg>source")
-    imgCat2.forEach(cat => {
-      const src = cat.getAttribute('srcset');
-      let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
-      cat.setAttribute('srcset', replacedSrc);
-    })
+    document.querySelectorAll(".content-area>.main-slider>#banner0>.owl-wrapper-outer>.owl-wrapper>.owl-item>.item>.img-bg>source").setAttribute('srcset', 'https://cdn.sellavi.com/image/upload/v1608731970/ru/clients/101263/a6badf471eb3c898e4899515a3c0fda8d28aa081.jpg')
   }
 }
