a9fb7999d6b79faf91a4565be6702cf314c79600
m4medbekov <m4medbekov@gmail.com>
2020-12-24 18:08:01 +0300

    Second banner

--- shops/102416/utils/bannerSecondInfo.js
+++ shops/102416/utils/bannerSecondInfo.js
@@ -1,5 +1,6 @@
 function displaySecondInfo() {
     document.querySelector('#banner0 > div > div > div > div > div > div > div > div > div > h3').insertAdjacentHTML('afterend', '<div class="second-title">Свежие, уникальные и инновационные дизайны постельного белья премиум класса от лучших производителей Европы</div>');
+    document.querySelector('#banner0 > div > div > div:nth-child(2) > div > div > div > div > div > div > h3').insertAdjacentHTML('afterend', '<div class="second-banner-second-text">Комплектование люлек, детских кроваток, спальных мест для подростков</div>');
 }
 
 export {displaySecondInfo}
