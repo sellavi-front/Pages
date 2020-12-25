8d07ba46409a2f7502d010d9057a47f612c35967
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-25 15:54:02 +0300

    new shop

--- shops/102273/js/main.js
+++ shops/102273/js/main.js
@@ -1,1 +1,2 @@
 import './utils/main.js'
+import './pages/main-page.js'
--- shops/102273/js/pages/main-page.js
+++ shops/102273/js/pages/main-page.js
@@ -1,17 +1,19 @@
-if (document.querySelector('.common-home')) {
-  // reset cut image in categories
-  const imgCat = document.querySelectorAll(
-    '.page-section.homefeatured_category > div > div > div > div > div > a > picture > img',
-  );
-  imgCat.forEach((cat) => {
-    const src = cat.getAttribute('src');
-
-    let replacedSrc = src.replace(
-      /\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi,
-      '',
+export default (function() {
+  if (document.querySelector('.common-home')) {
+    // reset cut image in categories
+    const imgCat = document.querySelectorAll(
+      '.page-section.homefeatured_category > div > div > div > div > div > a > picture > img',
     );
+    imgCat.forEach((cat) => {
+      const src = cat.getAttribute('src');
 
-    cat.setAttribute('src', replacedSrc);
-  });
-  // rend reset cit img
-}
+      let replacedSrc = src.replace(
+        /\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi,
+        '',
+      );
+
+      cat.setAttribute('src', replacedSrc);
+    });
+    // rend reset cit img
+  }
+})();
