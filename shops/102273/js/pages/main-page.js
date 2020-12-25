0f639eb60c56691e6e11e6def725378c37d00613
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-25 16:01:41 +0300

    new shop

--- shops/102273/js/pages/main-page.js
+++ shops/102273/js/pages/main-page.js
@@ -15,5 +15,9 @@ export default (function() {
       cat.setAttribute('src', replacedSrc);
     });
     // rend reset cit img
+
+    const btnCategory = document.querySelectorAll(".common-home > div > div.content-area > section.page-section.homefeatured_category > div > div > div > div > div > a > div > div > div > span");
+
+    btnCategory.forEach(btn => btn.textContent = 'Подробнее');
   }
 })();
