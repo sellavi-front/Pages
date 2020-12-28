9b7a273681edf597b14a4e068f8be678e1e0f9b8
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-28 16:11:32 +0300

    cstm cat

--- shops/101371/index.js
+++ shops/101371/index.js
@@ -1,3 +1,21 @@
-import '../../components/PictureCategories/PictureCategories.js'
+import '../../components/PictureCategories/PictureCategories.js';
 
 let iasd;
+
+if (document.querySelector('.common-home')) {
+  // reset cut image in categories
+  const imgCat = document.querySelectorAll(
+    '.page-section.homefeatured_category > div > div > div > div > div > a > picture > img',
+  );
+  imgCat.forEach((cat) => {
+    const src = cat.getAttribute('src');
+
+    let replacedSrc = src.replace(
+      /\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi,
+      '',
+    );
+
+    cat.setAttribute('src', replacedSrc);
+  });
+  // rend reset cit img
+}
