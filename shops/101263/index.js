57903d9f4a76943ce678d018a1ba4bbb37e10a5e
Coospir <eugene.starodubov@gmail.com>
2020-12-28 17:56:58 +0300

    updated

--- shops/101263/index.js
+++ shops/101263/index.js
@@ -62,9 +62,9 @@ if(pages.home) {
   imgCat.forEach(cat => {
     const src = cat.getAttribute('src');
 
-    let replacedSrc = src.replace(//if_ar_gt_2:1/c_fill,h_300,w_300,dpr_2/if_else/c_pad,h_300,w_300,dpr_2/if_end/gi, '');
+    let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
 
-      cat.setAttribute('src', replacedSrc));
+      cat.setAttribute('src', replacedSrc);
   })
 }
 
