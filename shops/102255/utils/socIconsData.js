5e1a9a0b44924e96f1eaf4f9479bc83e5f9403a7
Ruslan Mamedbekov <m4medbekov@gmail.com>
2020-12-28 12:33:57 +0300

    fix bug with footer width

--- shops/102255/utils/socIconsData.js
+++ shops/102255/utils/socIconsData.js
@@ -1,7 +1,7 @@
 const socIconsData = [
   {
     src: 'https://res.cloudinary.com/gz-company/image/upload/v1606295327/Miru/assets/icons/icons8-whatsapp_1_kh2mdl.svg',
-    link: 'https://wa.me/79518925622',
+    link: 'https://wa.me/79251552557',
   },
 ];
 
