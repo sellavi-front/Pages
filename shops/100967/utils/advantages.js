46e974209c7f77fe97010a58a25c07ff0f7df010
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-28 16:36:49 +0300

    refresh miru

---
+++ shops/100967/utils/advantages.js
@@ -0,0 +1,17 @@
+const advantages = [
+  {
+    icon:
+      'https://res.cloudinary.com/gz-company/image/upload/v1606138421/Miru/eye-scanner_1_mmtu8g.png',
+    text: 'Легендарная оптика знаменитого японского качества дарит пользователю безупречное зрение и уверенность в себе, вдохновляя на смелые цели и высокие достижения.',
+  },
+  {
+    icon: 'https://res.cloudinary.com/gz-company/image/upload/v1606138444/Miru/view_1_f35d0y.png',
+    text: 'Современные гидрофильные материалы, а также – технологии изготовления изделий и обработки поверхности линз – обеспечивают устойчивость к пересыханию для комфорта при ношении в любом режиме.',
+  },
+  {
+    icon: 'https://res.cloudinary.com/gz-company/image/upload/v1606138421/Miru/eye_1_jo1grj.png',
+    text: 'Контактные линзы Menicon отличаются исключительной мягкостью и удобством, которые достигаются за счёт низкого модуля упругости.',
+  },
+];
+
+export default advantages;
---
+++ shops/100967/utils/dataBannerContent.js
@@ -0,0 +1,12 @@
+const dataBannerContent = [
+  {
+    title: 'Cкидка на Miru <b>15%</b>',
+    text: 'с 1 по 15 декабря 2020',
+    linkSrc: '#',
+    linkContent: 'Подробнее',
+    banner: 'https://res.cloudinary.com/gz-company/image/upload/v1606308936/Miru/assets/Mock%D0%B0%D0%B2%D0%BF%D0%B2%D0%B0%D0%BFup_1_4_2_stp1sm.png',
+    srcset: 'https://res.cloudinary.com/gz-company/image/upload/v1606305782/Miru/assets/Group_96_2_li6urs.png',
+  }
+]
+
+export default dataBannerContent;
