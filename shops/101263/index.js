63ab9849a91fd44d6ad72ef4d2b973e01198bc1a
Coospir <eugene.starodubov@gmail.com>
2020-12-29 16:47:52 +0300

    updated

--- shops/101263/index.js
+++ shops/101263/index.js
@@ -26,10 +26,27 @@ const bootstrapClasses = [
 
 
 if(pages.home) {
-  let banner = new Banner()
+  /*let banner = new Banner()
   banner.updateImgCategories()
   let customBanner = new CustomBanner()
-  customBanner.render()
+  customBanner.render()*/
+  if(window.screen.width >= 575) {
+    /*const imgCat = document.querySelectorAll(".content-area>.main-slider>#banner0>.owl-wrapper-outer>.owl-wrapper>.owl-item>.item>.img-bg>.img-fluid")
+    imgCat.forEach(cat => {
+      const src = cat.getAttribute('src');
+      let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
+      cat.setAttribute('src', replacedSrc);
+    })*/
+    document.querySelector(".custom_banner").insertAdjacentHTML("afterbegin", `<div class="container"><img src="https://cdn.sellavi.com/image/upload/v1608731970/ru/clients/101263/a6badf471eb3c898e4899515a3c0fda8d28aa081.jpg" alt="banner"/></div>`)
+  } else {
+    /*const imgCat = document.querySelectorAll(".content-area>.main-slider>#banner0>.owl-wrapper-outer>.owl-wrapper>.owl-item>.item>.img-bg>.img-fluid")
+    imgCat.forEach(cat => {
+      const src = cat.getAttribute('src');
+      let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
+      cat.setAttribute('src', replacedSrc);
+    })*/
+    document.querySelector(".custom_banner").insertAdjacentHTML("afterbegin", `<div class="container"><img src="https://cdn.sellavi.com/image/upload/q_auto:420/v1608731984/ru/clients/101263/5ab4a1a11b389c4dc868d48c69c86948e1ed0050.jpg" alt="banner"/></div>`)
+  }
   let advantages = new Advantages()
   advantages.render()
   if (document.querySelector('.product-item')) {
