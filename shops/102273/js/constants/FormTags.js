faad810cd715b5bb483ba1faf676684d21c341ff
Закир Гафиятов <gafiyatovz@gmail.com>
2020-12-29 12:28:39 +0300

    refresh styles

--- shops/102273/js/constants/FormTags.js
+++ shops/102273/js/constants/FormTags.js
@@ -1,27 +1,37 @@
 const FormTags = [
   {
-    name: 'label',
-    content: 'Текст контент вашего тега',
+    name: 'input',
     attributes: {
-      selector: 'class="sender-form__input"',
+      selector: 'class="form__input"',
       type: 'type="name"',
+      name: 'name="userName"',
       placeholder: 'placeholder="Ваше Имя"',
     },
   },
   {
-      name: 'input',
-      attributes: {
-        selector: 'class="sender-form__input"',
-        type: 'type="email"',
-        placeholder: 'placeholder="Ваш e-mail"',
-      },
+    name: 'input',
+    attributes: {
+      selector: 'class="form__input"',
+      type: 'type="email"',
+      name: 'name="email"',
+      placeholder: 'placeholder="Ваш e-mail"',
     },
+  },
   {
     name: 'input',
     attributes: {
-      selector: 'class="sender-form__input"',
+      selector: 'class="form__input"',
       type: 'type="tel"',
+      name: 'name="phone"',
       placeholder: 'placeholder="Ваш телефон"',
+    },
+  },
+  {
+    name: 'button',
+    content: 'Оставить заявку',
+    attributes: {
+      selector: 'class="form__button"',
+      type: 'type="button"',
     },
   },
 ]
