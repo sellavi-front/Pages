bdd84a7e853f066f5d56c0bb101158fb0e2a7c4a
Coospir <eugene.starodubov@gmail.com>
2020-12-29 12:10:53 +0300

    updated

--- shops/101263/index.js
+++ shops/101263/index.js
@@ -59,12 +59,18 @@ if(pages.home) {
   });
 
   const imgCat = document.querySelectorAll(".page-section.homefeatured_category > div > div > div > div > div > a > picture > img");
+  const sourceCat = document.querySelector(".common-home>.wrapper>.content-area>.main-slider>#banner0>.owl-wrapper-outer>.owl-wrapper>.owl-item>.item>.img-bg>source")
+
+  sourceCat.forEach( source => {
+    const srcset = cat.getAttribute('srcset');
+    let replacedSrc1 = srcset.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
+    source.setAttribute('src', replacedSrc1);
+  })
+
   imgCat.forEach(cat => {
     const src = cat.getAttribute('src');
-
-    let replacedSrc = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
-
-      cat.setAttribute('src', replacedSrc);
+    let replacedSrc2 = src.replace(/\/if_ar_gt_2\:1\/c_fill\,h_300\,w_300\,dpr_2\/if_else\/c_pad\,h_300\,w_300\,dpr_2\/if_end/gi, '');
+    cat.setAttribute('src', replacedSrc2);
   })
 }
 
