fbcd9bb259c45d9419ebe333370f0200c6522d1b
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-05 17:18:05 +0300

    new shops

--- shops/102759/index.js
+++ shops/102759/index.js
@@ -4,6 +4,12 @@ import { requsites, dataContact } from './utils/requsites.js';
 import '../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js';
 import '../../fixes/js/ChangeAgreement/ChangeAgreement.js';
+import BigBanner from '../../modules/BigBanner/BigBanner.js'
+
+
+if (document.querySelector('.common-home')) {
+  new BigBanner('https://res.cloudinary.com/gz-company/image/upload/v1606466129/ThaiCosmetic/Group_73_yqnh52.png').render()
+}
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Скотарева Наталия Анатольевна');
