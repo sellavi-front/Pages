96de3dfc2266968c45843dff409205e1ad6fc2ae
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-05 17:56:47 +0300

    new shops

--- shops/103094/index.js
+++ shops/103094/index.js
@@ -3,6 +3,7 @@ import requsites from './utils/requsites.js'
 import contactData from './utils/contactData.js'
 import '../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
+import { dataContact } from '../102159/utils/requsites.js';
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Кучма Виктория Геннадьевна');
@@ -10,14 +11,14 @@ if (location.href.includes('term-of-use')) {
   req.setTemplate();
 }
 
-if (location.href.includes('contact')) {
+if (location.href.includes('/contact')) {
   const media = document.querySelector('.contact-info > .media-list > .media:last-child');
 
   const req = new Requisites(requsites, 'afterend', 'ИП Кучма Виктория Геннадьевна', media);
   req.setTemplate();
 }
 
-if (location.href.includes('policy')) {
+if (location.href.includes('/policy')) {
   const req = new Requisites(
     requsites,
     'afterend',
@@ -27,13 +28,13 @@ if (location.href.includes('policy')) {
   req.setTemplate();
 }
 
-if (location.href.includes('pay-delivery')) {
+if (location.href.includes('/pay-delivery')) {
   const req = new Requisites(
     requsites,
     'afterend',
     'ИП Кучма Виктория Геннадьевна',
     document.querySelector('#content > div > div'),
-    contactData,
+    dataContact,
   );
   req.setTemplate();
 }
