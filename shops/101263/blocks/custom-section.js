f69798fe676d634221b200473a3c4d2216623d10
Coospir <eugene.starodubov@gmail.com>
2021-01-08 18:15:48 +0300

    updated

--- shops/101263/blocks/custom-section.js
+++ shops/101263/blocks/custom-section.js
@@ -8,7 +8,7 @@ export default class CustomSection {
 
 
     if(screen.width <= 768) {
-      //document.querySelector(".custom_section").insertAdjacentHTML("beforeend", customSectionLayout.mobile)
+      document.querySelector(".custom_section").insertAdjacentHTML("beforeend", customSectionLayout.mobile)
     } else if(screen.width > 768) {
       document.querySelector(".custom_section").insertAdjacentHTML("beforeend", customSectionLayout.desktop)
     }
