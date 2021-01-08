9c086360ea41cf7adc0e383ec88e58c5a2540e97
Coospir <eugene.starodubov@gmail.com>
2021-01-08 16:14:54 +0300

    updated

--- shops/103062/components/banner/banner.js
+++ shops/103062/components/banner/banner.js
@@ -2,8 +2,8 @@ import images from "../../images/images.js"
 
 let renderBanner = () => {
   let customBanner = document.querySelector(".custom_banner")
-  let imgDesktop = `<img class="banner__desktop centered" src="${images.bannerDesktop}" alt="banner-desktop"/>`
-  let imgMobile = `<img class="banner__mobile centered" src="${images.bannerMobile}" alt="banner-mobile"/>`
+  let imgDesktop = `<img class="img-fluid" src="${images.bannerDesktop}" alt="banner-desktop"/>`
+  let imgMobile = `<img class="img-fluid" src="${images.bannerMobile}" alt="banner-mobile"/>`
   if(screen.width <= 768) {
     customBanner.insertAdjacentHTML('afterbegin', imgMobile)
   } else {
