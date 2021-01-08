680abcc8ca39d382e1ae665a0289c7737b1d26dc
Coospir <eugene.starodubov@gmail.com>
2021-01-08 15:55:38 +0300

    updated

--- shops/103062/components/banner/banner.js
+++ shops/103062/components/banner/banner.js
@@ -1,7 +1,13 @@
+import images from "../../images/images";
 let renderBanner = () => {
   let customBanner = document.querySelector(".custom_banner")
-  let containerFluid = "<div class='container-fluid'></div>"
-  customBanner.insertAdjacentHTML('afterbegin', containerFluid)
+  let imgDesktop = `<div class='container-fluid'><img src="${images.bannerDesktop}"</div>`
+  let imgMobile = `<div class='container-fluid'><img src="${images.bannerMobile}"</div>`
+  if(screen.width <= 768) {
+    customBanner.insertAdjacentHTML('afterbegin', imgMobile)
+  } else {
+    customBanner.insertAdjacentHTML('afterbegin', imgDesktop)
+  }
 }
 
 export default renderBanner
---
+++ shops/103062/images/images.js
@@ -0,0 +1,6 @@
+const images = {
+  bannerDesktop: 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1610110365/sellavi/103062/Frame_4_i7qd2o.svg',
+  bannerMobile: 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1610110362/sellavi/103062/Frame_nuiryo.svg'
+}
+
+export default images
