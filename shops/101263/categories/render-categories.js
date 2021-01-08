a74c421415025b57774f39dfef83251d8b207317
Coospir <eugene.starodubov@gmail.com>
2021-01-08 18:09:43 +0300

    updated

--- shops/101263/categories/render-categories.js
+++ shops/101263/categories/render-categories.js
@@ -3,6 +3,7 @@ import customSectionLayout from "../layouts/custom-section-layout.js";
 let renderCategories = () => {
   let layout = `
   <div class="container">
+    <h3 class="float-left">Категории</h3>
     <div class="categories__container">
         <div class="categories__item">
             <a href="${categories.water.link}"><img src="${categories.water.img}" alt="category"></a>
