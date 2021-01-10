0c719416c8a7be93f86198de277a41864d0677aa
Coospir <eugene.starodubov@gmail.com>
2021-01-10 12:27:39 +0300

    updated

--- shops/101263/layouts/custom-banner-layout.js
+++ shops/101263/layouts/custom-banner-layout.js
@@ -1,13 +1,13 @@
 let customBannerLayout = {
   desktop:
     `
-    <div class="container-fluid banner">
+    <div class="container-fluid mainBanner">
         <img class="custom_banner_photo" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608731750/sellavi/101263/Banner-PC.svg" alt="custom"/>
     </div>
     `,
   mobile:
     `
-    <div class="container-fluid banner">
+    <div class="container-fluid mainBanner">
         <img class="custom_banner_photo" src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1608815679/sellavi/101263/Component_1_1_tduab6.svg" alt="custom"/>
     </div>
     `
