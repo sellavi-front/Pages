143fe8190c3fc6ac5b43ed986410ed0e88cd0f8f
Coospir <eugene.starodubov@gmail.com>
2021-01-11 17:26:48 +0300

    updated

--- shops/103062/components/categories/categories.js
+++ shops/103062/components/categories/categories.js
@@ -1,6 +1,6 @@
 let fixCategories = () => {
-  document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(1) > div > div > a > picture").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/c83ae92794c00af6d4103a65f731cb01381b6835.jpg")
-  document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(2) > div > div > a > picture").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/6aa122b875e287c26e941abb67d188136f4d7143.jpg")
+  document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(1) > div > div > a > picture > img").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/c83ae92794c00af6d4103a65f731cb01381b6835.jpg")
+  document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(2) > div > div > a > picture > img").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/6aa122b875e287c26e941abb67d188136f4d7143.jpg")
 }
 
 export default fixCategories
