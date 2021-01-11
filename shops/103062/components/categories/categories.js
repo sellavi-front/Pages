cec1d3b5512e64170a257cb8ca80070ddfdc8a61
Coospir <eugene.starodubov@gmail.com>
2021-01-11 17:02:14 +0300

    updated

---
+++ shops/103062/components/categories/categories.js
@@ -0,0 +1,6 @@
+let fixCategories = () => {
+  document.querySelector(".img-bg:first-child img").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/c83ae92794c00af6d4103a65f731cb01381b6835.jpg")
+  document.querySelector(".img-bg:last-child img").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/6aa122b875e287c26e941abb67d188136f4d7143.jpg")
+}
+
+export default fixCategories
