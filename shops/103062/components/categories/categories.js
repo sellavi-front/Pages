a5532274cf21b7eb43056ff8056a94fd8eaff4e4
Coospir <eugene.starodubov@gmail.com>
2021-01-11 17:46:51 +0300

    updated

--- shops/103062/components/categories/categories.js
+++ shops/103062/components/categories/categories.js
@@ -3,6 +3,8 @@ let fixCategories = () => {
   document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(2) > div > div > a > picture > img").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/6aa122b875e287c26e941abb67d188136f4d7143.jpg")
   document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(2) > div > div > a > div").remove()
   document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(1) > div > div > a > div").remove()
+
+  document.querySelector("#home > div > div.content-area > section > div").insertAdjacentHTML("afterbegin", "<h2 class='categories-title'>Категории</h2>")
 }
 
 export default fixCategories
