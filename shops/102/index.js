dd5a6e99d9a5088cd03976f0fdabeb2857907ba1
Ruslan Mamedbekov <m4medbekov@gmail.com>
2021-01-12 10:46:34 +0300

    del overflow

---
+++ shops/102/index.js
@@ -0,0 +1,18 @@
+import Requisites from '../../modules/Requisites/Requisites.js';
+import requsites from './utils/requsites.js';
+import '../../fixes/js/FooterCopy/FooterCopy.js';
+import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
+
+
+if (location.href.includes('term-of-use')) {
+  const req = new Requisites(requsites, 'beforeend', 'ИП Северинов Алексей Вальерьевич');
+
+  req.setTemplate();
+}
+
+if (location.href.includes('contact')) {
+  const media = document.querySelector('.contact-info > .media-list > .media:last-child');
+
+  const req = new Requisites(requsites, 'afterend', 'ИП Северинов Алексей Вальерьевич', media);
+  req.setTemplate();
+}
