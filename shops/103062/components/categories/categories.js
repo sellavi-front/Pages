0c216adb163e4fc7507da068e18e495db545ea87
Coospir <eugene.starodubov@gmail.com>
2021-01-12 12:53:14 +0300

    updated

--- shops/103062/components/categories/categories.js
+++ shops/103062/components/categories/categories.js
@@ -2,7 +2,6 @@ import pages from "../../pages/pages.js"
 
 let fixHomeCategories = () => {
   if(pages.home) {
-    console.log(333)
     document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(1) > div > div > a > picture > img").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/c83ae92794c00af6d4103a65f731cb01381b6835.jpg")
     document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(2) > div > div > a > picture > img").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/6aa122b875e287c26e941abb67d188136f4d7143.jpg")
     document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(2) > div > div > a > div").remove()
@@ -13,7 +12,6 @@ let fixHomeCategories = () => {
 
 let fixCategory = () => {
   if(pages.productCategory) {
-    console.log(222)
     document.querySelector("#home > div > div.content-area > section.page-section.titles").remove()
     document.querySelector("#home > div > div.content-area > section.page-section.subcategories_top > div > div").remove()
   }
