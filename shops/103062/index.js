a6740afa830eea745af9dc07758deb8dd5cd162e
Coospir <eugene.starodubov@gmail.com>
2021-01-12 16:34:20 +0300

    updated

--- shops/103062/index.js
+++ shops/103062/index.js
@@ -3,7 +3,7 @@
 import {fixHomeCategories, fixCategory} from "./components/categories/categories.js"
 //import './../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
 import pages from "./pages/pages.js";
-import '../../components/HeaderTypeTwo/HeaderTypeTwo.js'
+//import '../../components/HeaderTypeTwo/HeaderTypeTwo.js'
 import '../../components/PictureCategories/PictureCategories.js'
 
 if(pages.home) {
