cc1cb7e097be05c9f3b59e267108b3d59151954b
Ruslan Mamedbekov <m4medbekov@gmail.com>
2021-01-12 15:36:17 +0300

    fix location

--- shops/102585/index.js
+++ shops/102585/index.js
@@ -3,7 +3,7 @@ import requsites from './utils/requsites.js';
 import '../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
 
-if (location.href.includes('common-home')) {
+if (location.href.includes('/')) {
   document.querySelector('.page-section.latest-section').insertAdjacentHTML('afterend', '<div class="contactsSection"><div class="container"><div class="contacts-flex"><div class="contacts-title"><span>Контакты</span></div><div class="otherInfo"><div class="number"><div class="number-img"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1610451452/Group_208_ydygyb.png" alt=""></div><div class="title-and-number"><div class="number-title"><span>Номер телефона</span></div><div class="real-number"><span>+7 912 683 45 03</span></div></div></div><div class="email"><div class="email-img"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1610451450/Group_209_ye2yi4.png" alt=""></div><div class="title-and-email"><div class="email-title"><span>Почтовый адрес</span></div><div class="real-email"><span>zali-shopping@mail.ru</span></div></div></div></div></div></div></div>');
 }
 
