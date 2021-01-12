16e9bf243e05fab21709e3fda7ca21bc17c33835
Coospir <eugene.starodubov@gmail.com>
2021-01-12 16:43:22 +0300

    updated

--- shops/103062/index.js
+++ shops/103062/index.js
@@ -1,17 +1,16 @@
-//import './fixes/fixes.js'
-//import renderBanner from "./components/banner/banner.js"
-//import {fixHomeCategories, fixCategory} from "./components/categories/categories.js"
-//import './../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
-//import pages from "./pages/pages.js";
-//import '../../components/HeaderTypeTwo/HeaderTypeTwo.js'
-//import '../../components/PictureCategories/PictureCategories.js'
+import './fixes/fixes.js'
+import renderBanner from "./components/banner/banner.js"
+import {fixHomeCategories, fixCategory} from "./components/categories/categories.js"
+import './../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
+import pages from "./pages/pages.js";
+import '../../components/HeaderTypeTwo/HeaderTypeTwo.js'
+import '../../components/PictureCategories/PictureCategories.js'
 
-/*
 if(pages.home) {
-  //renderBanner()
+  renderBanner()
   fixHomeCategories()
 }
 
 if(pages.productCategory) {
   fixCategory()
-}*/
+}
