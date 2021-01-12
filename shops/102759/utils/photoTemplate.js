e4dc56964546b9c5ec30fe2562083d9cfb032760
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-12 18:03:58 +0300

    new shops

--- shops/102759/utils/photoTemplate.js
+++ shops/102759/utils/photoTemplate.js
@@ -1,42 +1,12 @@
 export const data = [
   {
     quote: {
-      text: '«Мода – это не то, что существует только в платьях. Мода в небе, на улице. Мода имеет дело с идеями: как мы живём и что вокруг нас происходит»',
+      text:
+        '«Мода – это не то, что существует только в платьях. Мода в небе, на улице. Мода имеет дело с идеями: как мы живём и что вокруг нас происходит»',
       author: 'Коко Шанель',
     },
-    images: [
-      {
-        src:
-          'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
-        alt: '',
-        selectors: ['w-50'],
-      },
-      {
-        src:
-          'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
-        alt: '',
-        selectors: ['w-50'],
-      },
-      {
-        src:
-          'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
-        alt: '',
-        selectors: ['w-50'],
-      },
-      {
-        src:
-          'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
-        alt: '',
-        selectors: ['w-50'],
-      },
-    ],
-  },
-  {
-    quote: {
-      text: '«Мода – это не то, что существует только в платьях. Мода в небе, на улице. Мода имеет дело с идеями: как мы живём и что вокруг нас происходит»',
-      author: 'Коко Шанель',
-    },
-    images: [
+    rows: [
+      images: [
         {
           src:
             'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
@@ -50,6 +20,58 @@ export const data = [
           selectors: ['w-50'],
         },
       ],
+      [
+        {
+          src:
+            'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
+          alt: '',
+          selectors: ['w-50'],
+        },
+        {
+          src:
+            'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
+          alt: '',
+          selectors: ['w-50'],
+        },
+      ],
+    ],
+  },
+  {
+    quote: {
+      text:
+        '«Мода – это не то, что существует только в платьях. Мода в небе, на улице. Мода имеет дело с идеями: как мы живём и что вокруг нас происходит»',
+      author: 'Коко Шанель',
+    },
+    rows: {
+      images: [
+        {
+          src:
+            'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
+          alt: '',
+          selectors: ['w-50'],
+        },
+        {
+          src:
+            'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
+          alt: '',
+          selectors: ['w-50'],
+        },
+      ],
+      images: [
+        {
+          src:
+            'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
+          alt: '',
+          selectors: ['w-50'],
+        },
+        {
+          src:
+            'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
+          alt: '',
+          selectors: ['w-50'],
+        },
+      ],
+    },
   },
 ];
 
@@ -71,33 +93,35 @@ export class GridImgWithQuote {
     `;
   }
 
-  setRowImg(images) {
-    return `<div class="row d-flex flex-nowrap">${this.setImg(images)}</div>`;
+  setRowImg(rows) {
+    return rows.map(row => `<div class="row d-flex flex-nowrap">${this.setImg(row.images)}</div>`);
   }
 
   setImg(images) {
-    return images.map((img) => {
-      return `<img class="" src="${img.src}" alt="${img.alt}">`;
-    }).join('');
+    return images
+      .map((img) => {
+        return `<img class="" src="${img.src}" alt="${img.alt}">`;
+      })
+      .join('');
   }
 
   render() {
     return `
     <div class="gallery d-flex">
-      ${
-        this.props.map(row => {
+      ${this.props
+        .map((row) => {
           return `
           <div class="col p3">
             <div class="photo-grid w-100 d-flex flex-column">
-              ${this.setRowImg(row.images)}
+              ${this.setRowImg(row.rows)}
             </div>
           </div>
           <div class="col p3">
             ${this.setQuote(row.quote)}
           </div>
-          `
-        }).join('')
-      }
+          `;
+        })
+        .join('')}
   </div>
     `;
   }
