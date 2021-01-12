628a77903ad9802e89b542a0ab237f6907c4d1d3
Coospir <eugene.starodubov@gmail.com>
2021-01-12 11:36:58 +0300

    updated

--- shops/103062/components/categories/categories.js
+++ shops/103062/components/categories/categories.js
@@ -1,15 +1,18 @@
 import pages from "../../pages/pages.js"
 
 let fixCategories = () => {
-  document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(1) > div > div > a > picture > img").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/c83ae92794c00af6d4103a65f731cb01381b6835.jpg")
-  document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(2) > div > div > a > picture > img").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/6aa122b875e287c26e941abb67d188136f4d7143.jpg")
-  document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(2) > div > div > a > div").remove()
-  document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(1) > div > div > a > div").remove()
+  if(pages.home) {
+    console.log(333)
+    document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(1) > div > div > a > picture > img").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/c83ae92794c00af6d4103a65f731cb01381b6835.jpg")
+    document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(2) > div > div > a > picture > img").setAttribute("src", "https://cdn.sellavi.com/image/upload/ru/clients/103062/6aa122b875e287c26e941abb67d188136f4d7143.jpg")
+    document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(2) > div > div > a > div").remove()
+    document.querySelector("#home > div > div.content-area > section > div > div > div:nth-child(1) > div > div > a > div").remove()
+    document.querySelector("#home > div > div.content-area > section > div").insertAdjacentHTML("afterbegin", "<h2 class='categories-title'>Категории</h2>")
+  }
 
-  document.querySelector("#home > div > div.content-area > section > div").insertAdjacentHTML("afterbegin", "<h2 class='categories-title'>Категории</h2>")
-  console.log(213123)
-  if(window.location.pathname === '/available/' || window.location.pathname === '/joint-procurement/') {
-    console.log(1)
+
+  if(pages.productCategory) {
+    console.log(222)
     document.querySelector("#home > div > div.content-area > section.page-section.titles").remove()
     document.querySelector("#home > div > div.content-area > section.page-section.subcategories_top > div > div").remove()
   }
--- shops/103062/pages/pages.js
+++ shops/103062/pages/pages.js
@@ -1,5 +1,6 @@
 const pages = {
   home: document.querySelector(".common-home"),
+  productCategory: document.querySelector(".product-category"),
   available: document.querySelector(".product-category-6091"),
   buying: document.querySelector(".product-category-6092")
 }
