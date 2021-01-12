8f7633fb9ff9ab5b286b6a910d932d989653d8f5
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-12 17:37:07 +0300

    new shops

--- shops/102759/utils/photoTemplate.js
+++ shops/102759/utils/photoTemplate.js
@@ -17,6 +17,18 @@ export const data = [
         alt: '',
         selectors: ['w-50'],
       },
+      {
+        src:
+          'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
+        alt: '',
+        selectors: ['w-50'],
+      },
+      {
+        src:
+          'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
+        alt: '',
+        selectors: ['w-50'],
+      },
     ],
   },
   {
@@ -24,20 +36,23 @@ export const data = [
       text: '«Мода – это не то, что существует только в платьях. Мода в небе, на улице. Мода имеет дело с идеями: как мы живём и что вокруг нас происходит»',
       author: 'Коко Шанель',
     },
-    images: [
-      {
-        src:
-          'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
-        alt: '',
-        selectors: ['w-50'],
-      },
-      {
-        src:
-          'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
-        alt: '',
-        selectors: ['w-50'],
-      },
+    row: [
+      [
+        {
+          src:
+            'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
+          alt: '',
+          selectors: ['w-50'],
+        },
+        {
+          src:
+            'https://res.cloudinary.com/depgheppz/image/upload/v1610453170/02640A45-9C29-4E65-AA27-C02FB8A32FFC_jzbqw2.png',
+          alt: '',
+          selectors: ['w-50'],
+        },
+      ],
     ],
+
   },
 ];
 
