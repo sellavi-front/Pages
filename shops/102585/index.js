69c443271be35c033fd8bd33d6669492e920fe6c
Ruslan Mamedbekov <m4medbekov@gmail.com>
2021-01-13 17:16:34 +0300

    fix checkout-checkout

--- shops/102585/index.js
+++ shops/102585/index.js
@@ -9,6 +9,11 @@ if (document.querySelector('.product-category')) {
   document.querySelector('div#content').insertAdjacentHTML('beforebegin', '<div class="preprod-title"><div class="container"><div class="soms-title">Новинки</div></div></div>');
 }
 
+if (document.querySelector('.checkout-checkout')) {
+  document.querySelector('#home > div.content-area > section > div.flexwrap.checkout_form > div.register_block > div.form_checkout > div.shipment-reloader > div > h2').innerHTML = 'Шаг 2: Способ доставки';
+  document.querySelector('#home > div.content-area > section > div.flexwrap.checkout_form > div.register_block > div.form_checkout > div.payment-method > h2').innerHTML = 'Шаг 3: Способ оплаты';
+}
+
 if (document.querySelector('.common-home')) {
   document.querySelector('.page-section.latest-section').insertAdjacentHTML('afterend', '<div class="contactsSection"><div class="container"><div class="contacts-flex"><div class="contacts-title"><span>Контакты</span></div><div class="otherInfo"><div class="number"><div class="number-img"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1610451452/Group_208_ydygyb.png" alt=""></div><div class="title-and-number"><div class="number-title"><span>Номер телефона</span></div><div class="real-number"><span>8 919 721-55-83</span></div></div></div><div class="email"><div class="email-img"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1610451450/Group_209_ye2yi4.png" alt=""></div><div class="title-and-email"><div class="email-title"><span>Почтовый адрес</span></div><div class="real-email"><span>severinovsquash@gmail.com</span></div></div></div></div></div></div></div>');
 }
