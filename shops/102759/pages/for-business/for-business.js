6c1d7806047a9734b537ffeb8244eb29d81895dc
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-13 16:39:02 +0300

    new shops

--- shops/102759/pages/for-business/for-business.js
+++ shops/102759/pages/for-business/for-business.js
@@ -2,13 +2,13 @@ import InfoBlock from './components/InfoBlock.js';
 
 const aboutData = [
   {
-    image: 'https://res.cloudinary.com/gz-company/image/upload/v1606228219/Miru/Miru_1_day_Flat_Pack_pattern-01_1_cg520r.png',
+    image: 'https://res.cloudinary.com/depgheppz/image/upload/v1610545117/olesia-buyar-HCRYIjtGybs-unsplash_1_upetz1.png',
     title: 'Мы не хотим просто продавать одежду оптом',
     ph1: ' Мы хотим найти сильных партнеров! Партнеров, которые будут разделять наши принципы и подходы.',
     ph2: ' Спрос на наш бренд превышает текущее предложение, при этом для нас важнее не столько продать больше одежды, сколько предоставить нашим клиентам лучшие возможности и лучший сервис.'
   },
   {
-    image: 'https://res.cloudinary.com/gz-company/image/upload/v1606228219/Miru/Miru_1_day_Flat_Pack_pattern-01_1_cg520r.png',
+    image: 'https://res.cloudinary.com/depgheppz/image/upload/v1610545117/pina-messina-qQKv7r1BaRw-unsplash_1_qf58wt.png',
     title: 'Условия',
     ph1: `
         <ol class="info-block__list">
