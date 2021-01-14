5dea5295b6525480f34f74a870349a9962b46597
Ruslan Mamedbekov <m4medbekov@gmail.com>
2021-01-14 11:28:58 +0300

    fix

--- shops/103009/index.js
+++ shops/103009/index.js
@@ -2,6 +2,8 @@ import Requisites from '../../modules/Requisites/Requisites.js';
 import {requsites, dataContact } from './utils/requsites.js'
 import '../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
+import '../../templates/TemplateTypeTwo/TemplateTypeTwo.js';
+
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Балинец Наталья Александровна');
