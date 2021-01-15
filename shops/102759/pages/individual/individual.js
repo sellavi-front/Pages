c1a9f07adb2953355b3611606d3e53debf209d91
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-15 15:46:57 +0300

    new shops

--- shops/102759/pages/individual/individual.js
+++ shops/102759/pages/individual/individual.js
@@ -21,7 +21,7 @@ const data = [
 
 if (location.href.includes('/individual')) {
   const lastSection = document.querySelector('#content > .row > div');
-
+console.log(123);
   const pageHeader = document.querySelector(".page-header > h1");
   pageHeader.textContent = 'Пошив одежды на заказ';
   pageHeader.insertAdjacentHTML('afterend', `<p class="page-header__subtitle">Действительно качественный пошив вашей одежды</p>`)
