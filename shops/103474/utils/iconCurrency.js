28ff01b4fabf94a59554e118632e6b286c56c140
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-17 17:53:12 +0300

    new shops

--- shops/103474/utils/iconCurrency.js
+++ shops/103474/utils/iconCurrency.js
@@ -57,7 +57,7 @@
     setTimeout(() => {
       console.log('enter');
       changeIcon(document.querySelectorAll('.single-price > div'), icon);
-      changeIcon(document.querySelectorAll('.checkout_table .text-right'), icon);
+      changeIcon(document.querySelectorAll('.checkout_table .text-right.total_table__sum'), icon);
       changeIcon(document.querySelectorAll('.cart_drop_down .cart-norm-qty'), icon);
       changeIcon(document.querySelectorAll('.order_list_section__total-lines > div > .text-right'), icon);
 
