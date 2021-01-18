f0e5103ad4440dfbda99d7e9d62d30995b43879a
Coospir <eugene.starodubov@gmail.com>
2021-01-18 16:15:41 +0300

    updated

--- shops/103062/images/images.js
+++ shops/103062/images/images.js
@@ -1,5 +1,5 @@
 const images = {
-  bannerDesktop: 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1610975203/sellavi/103062/Frame_320_a77p0t.svg',
+  bannerDesktop: 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1610975728/sellavi/103062/Frame_320_1_ysrkzt.svg',
   bannerMobile: 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1610975232/sellavi/103062/Frame_316_bxbg9s.svg'
 }
 
