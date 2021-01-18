a1d922f11f27c19cbda96c97b45d76817c10e767
Coospir <eugene.starodubov@gmail.com>
2021-01-18 11:36:52 +0300

    updated

--- shops/101263/index.js
+++ shops/101263/index.js
@@ -40,7 +40,7 @@ if(pages.home) {
   } else {
     document.querySelector(".custom_banner").insertAdjacentHTML("afterbegin", `<div class="container"><img class="bannerImg" src="https://cdn.sellavi.com/image/upload/q_auto:420/v1608731984/ru/clients/101263/5ab4a1a11b389c4dc868d48c69c86948e1ed0050.jpg" alt="banner"/></div>`)
   }
-  document.querySelector("#home > div > div.content-area > section > div:nth-child(2) > h2 > span").insertAdjacentHTML("afterend", `<i class="fa fa-fire"></i>`)
+  document.querySelector("#home > div > div.content-area > section > div:nth-child(2) > h2 > span").insertAdjacentHTML("afterend", `<i class="fa fa-fire hits"></i>`)
   let customSection = new CustomSection()
   customSection.render()
 
