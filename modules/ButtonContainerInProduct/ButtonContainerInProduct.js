2206c64d112a784e58634b53775a9a130fe1cc65
Coospir <eugene.starodubov@gmail.com>
2021-01-19 13:11:14 +0300

    updated

--- modules/ButtonContainerInProduct/ButtonContainerInProduct.js
+++ modules/ButtonContainerInProduct/ButtonContainerInProduct.js
@@ -22,33 +22,39 @@ export default class ButtonContainerInProduct {
   }
 }
 
+let arrHeights = []
 let oldHeight = document.querySelector(".row.products.grid").clientHeight
+oldHeight.push(arrHeights)
 document.querySelector(".row.products.grid").addEventListener("transitionend", () => {
   let newHeight = document.querySelector(".row.products.grid").clientHeight
-  if(oldHeight <= newHeight) {
-    console.log("Listener of height row products is working!")
-    const price = document.querySelectorAll('.product-item .price');
-    const addToCart = document.querySelectorAll('.add_to_cart');
+  newHeight.push(arrHeights)
+  arrHeights.map((item, i) => {
+    if(item[i] > item[i+1]) {
+      console.log("Listener of height row products is working!")
+      const price = document.querySelectorAll('.product-item .price');
+      const addToCart = document.querySelectorAll('.add_to_cart');
 
-    if (document.querySelectorAll('.add_to_wishlist').length === 0) {
-      var wishList = document.querySelectorAll('.remove_from_wishlist');
-    } else {
-      var wishList = document.querySelectorAll('.add_to_wishlist');
-    }
-
-
-    const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, wishList, price);
-
-    buttonContainerInProduct.createContainer();
-    buttonContainerInProduct.delete(addToCart);
-    buttonContainerInProduct.delete(wishList);
-
-
-    let imgEl = document.querySelectorAll('.img-fluid.lazy');
-    for (let i = 0; i < imgEl.length; i++) {
-      if (imgEl[i].getAttribute('data-src')) {
-        imgEl[i].setAttribute('src', imgEl[i].getAttribute('data-src'));
+      if (document.querySelectorAll('.add_to_wishlist').length === 0) {
+        var wishList = document.querySelectorAll('.remove_from_wishlist');
+      } else {
+        var wishList = document.querySelectorAll('.add_to_wishlist');
       }
-    }
-  }
+
+
+      const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, wishList, price);
+
+      buttonContainerInProduct.createContainer();
+      buttonContainerInProduct.delete(addToCart);
+      buttonContainerInProduct.delete(wishList);
+
+
+      let imgEl = document.querySelectorAll('.img-fluid.lazy');
+      for (let i = 0; i < imgEl.length; i++) {
+        if (imgEl[i].getAttribute('data-src')) {
+          imgEl[i].setAttribute('src', imgEl[i].getAttribute('data-src'));
+        }
+      }
+    } else console.log("Smth wrong with height")
+  })
+
 })
