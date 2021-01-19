b26eb8aca6cced0711c50ae2489db137502ccae7
Coospir <eugene.starodubov@gmail.com>
2021-01-19 12:03:30 +0300

    updated

--- shops/103062/index.js
+++ shops/103062/index.js
@@ -1,7 +1,7 @@
 import './fixes/fixes.js'
 import renderBanner from "./components/banner/banner.js"
 import {fixHomeCategories, fixCategory} from "./components/categories/categories.js"
-import './../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
+//import './../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
 import pages from "./pages/pages.js";
 import '../../components/HeaderTypeTwo/HeaderTypeTwo.js'
 import '../../components/PictureCategories/PictureCategories.js'
