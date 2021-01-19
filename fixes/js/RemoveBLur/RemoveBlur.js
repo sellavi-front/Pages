5145ab32ce364f34375bd53bdba8f3b1a254cdd4
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-19 19:03:22 +0300

    new fix for pagination products

--- fixes/js/RemoveBLur/RemoveBlur.js
+++ fixes/js/RemoveBLur/RemoveBlur.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-  if (document.querySelector('.product-item')) {
+  if (document.querySelector('body.product-category') || document.querySelector('body.common-home .products')) {
     let target = document.querySelector('#content > div > div.row.products.grid');
     const config = {
       childList: true,
@@ -8,7 +8,8 @@ window.onload = () => {
       for (let mutation of mutationsList) {
         if (mutation.type === 'childList') {
           target.querySelectorAll('.product-item img').forEach((prod) => {
-            prod.setAttribute('src', prod.getAttribute('src').replace(/e_blur:2000/gi, ''));
+            const cutStr = prod.getAttribute('src').includes('e_blur:2000') ? prod.getAttribute('src') : null;
+            prod.setAttribute('src', cutStr.replace(/e_blur:2000/gi, ''));
           });
         }
       }
