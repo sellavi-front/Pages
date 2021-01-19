09ab85ab9d4a4713cc090879cd02ac9523099f39
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-19 14:42:20 +0300

    develop lazy load product

--- modules/ButtonContainerInProduct/test.js
+++ modules/ButtonContainerInProduct/test.js
@@ -6,56 +6,31 @@ export default class ButtonContainerInProduct {
   }
 
   delete(element) {
-    element.forEach(el => el.remove());
+    element.forEach((el) => el.remove());
   }
 
   createContainer() {
     this.container.forEach((btn, i) => {
-      btn.insertAdjacentHTML(
-        'afterend',
-        `<div class="buttons__container">${this.addToWishList[i].outerHTML}${this.addToCart[i].outerHTML}</div>`,
-      );
+      if (!btn.nextSibling.classList.contains('button__container')) {
+        btn.insertAdjacentHTML(
+          'afterend',
+          `<div class="buttons__container">${this.addToWishList[i].outerHTML}${this.addToCart[i].outerHTML}</div>`,
+        );
+      }
     });
-
-    // this.delete(addToCart);
-    // this.delete(addToWishList);
   }
 }
 
-// const price = document.querySelectorAll('.product-item .price');
-// const addToCart = document.querySelectorAll('.add_to_cart');
-
-// if (document.querySelectorAll('.add_to_wishlist').length === 0) {
-//   var wishList = document.querySelectorAll('.remove_from_wishlist');
-// } else {
-//   var wishList = document.querySelectorAll('.add_to_wishlist');
-// }
-
-
-// const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, wishList, price);
-
-// buttonContainerInProduct.createContainer();
-// buttonContainerInProduct.delete(addToCart);
-// buttonContainerInProduct.delete(wishList);
-
-
-// let imgEl = document.querySelectorAll('.img-fluid.lazy');
-// for (let i = 0; i < imgEl.length; i++) {
-//   if (imgEl[i].getAttribute('data-src')) {
-//     imgEl[i].setAttribute('src', imgEl[i].getAttribute('data-src'));
-//   }
-// }
-
-const productWrapper = document.querySelector("#home > div > div.content-area > section.page-section.latest-section > div > div");
-let oldHeight = document.querySelector(".row.products.grid").clientHeight;
-const arrHeightFrame = [
-  productWrapper.clientHeight,
-]
-productWrapper.addEventListener("transitionend", (e) => {
+const productWrapper = document.querySelector(
+  '#home > div > div.content-area > section.page-section.latest-section > div > div',
+);
+let oldHeight = document.querySelector('.row.products.grid').clientHeight;
+const arrHeightFrame = [productWrapper.clientHeight];
+productWrapper.addEventListener('transitionend', (e) => {
   console.log('event', e.target);
-  let newHeight = document.querySelector(".row.products.grid").clientHeight
-  if(oldHeight <= newHeight) {
-    console.log("Listener of height row products is working!")
+  let newHeight = document.querySelector('.row.products.grid').clientHeight;
+  if (oldHeight <= newHeight) {
+    console.log('Listener of height row products is working!');
     const price = document.querySelectorAll('.product-item .price');
     const addToCart = document.querySelectorAll('.add_to_cart');
 
@@ -65,13 +40,11 @@ productWrapper.addEventListener("transitionend", (e) => {
       var wishList = document.querySelectorAll('.add_to_wishlist');
     }
 
-
     const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, wishList, price);
 
     buttonContainerInProduct.createContainer();
     buttonContainerInProduct.delete(addToCart);
     buttonContainerInProduct.delete(wishList);
-
 
     let imgEl = document.querySelectorAll('.img-fluid.lazy');
     for (let i = 0; i < imgEl.length; i++) {
@@ -79,12 +52,8 @@ productWrapper.addEventListener("transitionend", (e) => {
         imgEl[i].setAttribute('src', imgEl[i].getAttribute('data-src'));
       }
     }
-  } else console.log("Smth wrong with height")
-
-})
-
-
-
+  } else console.log('Smth wrong with height');
+});
 
 //   const price = document.querySelectorAll('.product-item .price');
 // const addToCart = document.querySelectorAll('.add_to_cart');
@@ -94,7 +63,6 @@ productWrapper.addEventListener("transitionend", (e) => {
 // } else {
 //   var wishList = document.querySelectorAll('.add_to_wishlist');
 // }
-
 
 //   const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, wishList, price);
 
@@ -111,7 +79,6 @@ productWrapper.addEventListener("transitionend", (e) => {
 // } else {
 //   var wishList = document.querySelectorAll('.add_to_wishlist');
 // }
-
 
 //   const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, wishList, price);
 
