08744679786f1a105d4af3891bb61ff1fe3dbe24
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-19 14:59:25 +0300

    develop lazy load product

--- modules/ButtonContainerInProduct/test.js
+++ modules/ButtonContainerInProduct/test.js
@@ -11,7 +11,7 @@ export default class ButtonContainerInProduct {
 
   createContainer() {
     this.container.forEach((btn, i) => {
-      console.log(btn.nextSibling.previousElementSibling);
+      console.log(btn.closest('div'));
       // if (!btn.nextSibling.classList.contains('button__container')) {
       btn.insertAdjacentHTML(
         'afterend',
