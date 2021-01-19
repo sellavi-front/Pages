31f264359cea86433766b02f955df23e4b432f58
Coospir <eugene.starodubov@gmail.com>
2021-01-19 12:48:22 +0300

    updated

--- modules/ButtonContainerInProduct/ButtonContainerInProduct.js
+++ modules/ButtonContainerInProduct/ButtonContainerInProduct.js
@@ -22,21 +22,29 @@ export default class ButtonContainerInProduct {
   }
 }
 
-  const price = document.querySelectorAll('.product-item .price');
-const addToCart = document.querySelectorAll('.add_to_cart');
+let oldHeight = document.querySelector("#content > div > div.row.products.grid").clientHeight
+document.querySelector("#content > div > div.row.products.grid").addEventListener("transitionend", () => {
+  let newHeight = document.querySelector("#content > div > div.row.products.grid").clientHeight
+  if(oldHeight < newHeight) {
+    const price = document.querySelectorAll('.product-item .price');
+    const addToCart = document.querySelectorAll('.add_to_cart');
 
-if (document.querySelectorAll('.add_to_wishlist').length === 0) {
- var wishList = document.querySelectorAll('.remove_from_wishlist');
-} else {
-  var wishList = document.querySelectorAll('.add_to_wishlist');
-}
+    if (document.querySelectorAll('.add_to_wishlist').length === 0) {
+      var wishList = document.querySelectorAll('.remove_from_wishlist');
+    } else {
+      var wishList = document.querySelectorAll('.add_to_wishlist');
+    }
 
 
-  const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, wishList, price);
+    const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, wishList, price);
 
-  buttonContainerInProduct.createContainer();
-  buttonContainerInProduct.delete(addToCart);
-  buttonContainerInProduct.delete(wishList);
+    buttonContainerInProduct.createContainer();
+    buttonContainerInProduct.delete(addToCart);
+    buttonContainerInProduct.delete(wishList);
+  }
+})
+
+
 
 setTimeout(() => {
   const price = document.querySelectorAll('.product-item .price');
--- shops/103062/index.js
+++ shops/103062/index.js
@@ -1,15 +1,10 @@
 import './fixes/fixes.js'
 import renderBanner from "./components/banner/banner.js"
 import {fixHomeCategories, fixCategory} from "./components/categories/categories.js"
-//import './../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
+import './../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
 import pages from "./pages/pages.js";
 import '../../components/HeaderTypeTwo/HeaderTypeTwo.js'
 import '../../components/PictureCategories/PictureCategories.js'
-
-
-document.body.addEventListener('DOMSubtreeModified', function () {
-  document.title = 'DOM Changed at ' + new Date();
-}, false);
 
 if(pages.home) {
   //renderBanner()
