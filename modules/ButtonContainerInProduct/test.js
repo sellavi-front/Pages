8203d586fb68d77a547abf2b1e76aef29fa4706c
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-19 14:11:49 +0300

    new shops

---
+++ modules/ButtonContainerInProduct/test.js
@@ -0,0 +1,66 @@
+export default class ButtonContainerInProduct {
+  constructor(addToCart, addToWishList, container) {
+    this.container = container;
+    this.addToCart = addToCart;
+    this.addToWishList = addToWishList;
+  }
+
+  delete(element) {
+    element.forEach(el => el.remove());
+  }
+
+  createContainer() {
+    this.container.forEach((btn, i) => {
+      btn.insertAdjacentHTML(
+        'afterend',
+        `<div class="buttons__container">${this.addToWishList[i].outerHTML}${this.addToCart[i].outerHTML}</div>`,
+      );
+    });
+
+    // this.delete(addToCart);
+    // this.delete(addToWishList);
+  }
+}
+
+  const price = document.querySelectorAll('.product-item .price');
+const addToCart = document.querySelectorAll('.add_to_cart');
+
+if (document.querySelectorAll('.add_to_wishlist').length === 0) {
+ var wishList = document.querySelectorAll('.remove_from_wishlist');
+} else {
+  var wishList = document.querySelectorAll('.add_to_wishlist');
+}
+
+
+  const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, wishList, price);
+
+  buttonContainerInProduct.createContainer();
+  buttonContainerInProduct.delete(addToCart);
+  buttonContainerInProduct.delete(wishList);
+
+setTimeout(() => {
+  const price = document.querySelectorAll('.product-item .price');
+const addToCart = document.querySelectorAll('.add_to_cart');
+
+if (document.querySelectorAll('.add_to_wishlist').length === 0) {
+ var wishList = document.querySelectorAll('.remove_from_wishlist');
+} else {
+  var wishList = document.querySelectorAll('.add_to_wishlist');
+}
+
+
+  const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, wishList, price);
+
+  buttonContainerInProduct.createContainer();
+  buttonContainerInProduct.delete(addToCart);
+  buttonContainerInProduct.delete(wishList);
+}, 4000)
+
+setTimeout (() => {
+  let imgEl = document.querySelectorAll('.img-fluid.lazy');
+  for (let i = 0; i < imgEl.length; i++) {
+    if (imgEl[i].getAttribute('data-src')) {
+      imgEl[i].setAttribute('src', imgEl[i].getAttribute('data-src'));
+    }
+  }
+}, 4000);
