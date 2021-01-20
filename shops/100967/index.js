002f1fde08006d28bc1b371c76ca5588e9e1f978
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-20 12:47:08 +0300

    add template structure for requsites

--- shops/100967/index.js
+++ shops/100967/index.js
@@ -26,7 +26,7 @@ import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
 
 const icons = {
   cart:
-    '<img src="https://res.cloudinary.com/gz-company/image/upload/v1606402883/Miru/assets/icons/icons8-%D0%BA%D0%BE%D1%80%D0%B7%D0%B8%D0%BD%D0%B0_1_1_gqdqiq.png" alt="cart">',
+    '<img src="https://res.cloudinary.com/depgheppz/image/upload/v1611136009/Vector_opwga3.svg" alt="cart">',
   like:
     '<img src="https://res.cloudinary.com/gz-company/image/upload/v1605863437/Miru/assets/icon_di3am3.svg">',
 };
