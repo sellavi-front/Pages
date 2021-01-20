7ebc3ca897c0b4937f55fec038706ba0dbb5db86
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-20 15:42:48 +0300

    add template structure for requsites

--- modules/ButtonContainerInProduct/ButtonContainerInProduct.js
+++ modules/ButtonContainerInProduct/ButtonContainerInProduct.js
@@ -11,23 +11,20 @@ export default class ButtonContainerInProduct {
 
   createContainer() {
     this.container.forEach((btn, i) => {
-      console.log(btn);
       btn.insertAdjacentHTML(
-        'beforeend',
+        'afterend',
         `<div class="buttons__container">${this.addToWishList[i].outerHTML}${this.addToCart[i].outerHTML}</div>`,
       );
     });
 
-    this.delete(addToCart);
-    this.delete(addToWishList);
+    // this.delete(addToCart);
+    // this.delete(addToWishList);
   }
 }
 
-const priceActions = document.querySelectorAll('.price_actions');
+  const price = document.querySelectorAll('.product-item .price');
 const addToCart = document.querySelectorAll('.add_to_cart');
-const addToWishList = document.querySelectorAll('.add_to_wishlist');
 
-<<<<<<< HEAD
 if (document.querySelectorAll('.add_to_wishlist').length === 0) {
  var wishList = document.querySelectorAll('.remove_from_wishlist');
 } else {
@@ -40,15 +37,3 @@ if (document.querySelectorAll('.add_to_wishlist').length === 0) {
   buttonContainerInProduct.createContainer();
   buttonContainerInProduct.delete(addToCart);
   buttonContainerInProduct.delete(wishList);
-=======
-const buttonContainerInProduct = new ButtonContainerInProduct(addToCart, addToWishList, priceActions);
-
-buttonContainerInProduct.createContainer();
-
-// const btnContainer = document.querySelectorAll('.buttons__container')
-
-// const addCartInProductCard = new AddElemInContainer(addToCart, btnContainer);
-// const addLikeInProductCard = new AddElemInContainer(addToWishList, btnContainer);
-
-//${this.addToCart.outerHTML}${this.addToWishList.outerHTML}
->>>>>>> eugene/eugene-dev
