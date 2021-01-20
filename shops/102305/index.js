5510d7d37b4856c64f23d419f6094889a964ff2e
Coospir <eugene.starodubov@gmail.com>
2021-01-20 14:32:32 +0300

    updated

--- shops/102305/index.js
+++ shops/102305/index.js
@@ -24,24 +24,6 @@ fixes.updateClasses()
 
 footer.addIcons()
 
-let target = document.querySelector("#content > div > div.row.products.grid");
-const config = {
-  childList: true,
-};
-const callback = function(mutationsList, observer) {
-  for (let mutation of mutationsList) {
-    if (mutation.type === 'childList') {
-      target.querySelectorAll('.product-item img').forEach(prod => {
-        console.log(prod)
-
-        prod.setAttribute('src', prod.getAttribute('src').replace(/e_blur:2000/gi, ''));
-      })
-    }
-  }
-};
-const observer = new MutationObserver(callback);
-observer.observe(target, config);
-
 if (document.querySelector('.common-home')) {
   //const customSection = document.querySelector('.custom_section')
   //customSection.classList.add('container')
