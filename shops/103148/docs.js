977293cabe7b5f5031f52e773205efa3394dd44c
Coospir <eugene.starodubov@gmail.com>
2021-01-20 16:41:26 +0300

    updated

--- shops/103148/docs.js
+++ shops/103148/docs.js
@@ -4,7 +4,7 @@ import '../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
 
 if (location.href.includes('term-of-use')) {
-  const req = new Requisites(requsites, 'beforeend', 'Название организации');
+  const req = new Requisites(requsites, 'beforeend', 'ИП Пономарева Кристина Вячеславовна');
 
   req.setTemplate();
 }
@@ -12,7 +12,7 @@ if (location.href.includes('term-of-use')) {
 if (location.href.includes('contact')) {
   const media = document.querySelector('.contact-info > .media-list > .media:last-child');
 
-  const req = new Requisites(requsites, 'afterend', 'Название организации', media);
+  const req = new Requisites(requsites, 'afterend', 'ИП Пономарева Кристина Вячеславовна', media);
   req.setTemplate();
 }
 
@@ -21,7 +21,7 @@ if (location.href.includes('policy')) {
   const req = new Requisites(
     requsites,
     'afterend',
-    'Название организации',
+    'ИП Пономарева Кристина Вячеславовна',
     document.querySelector('#content > div > div'),
   );
   req.setTemplate();
@@ -31,7 +31,7 @@ if (location.href.includes('pay-delivery')) {
   const req = new Requisites(
     requsites,
     'afterend',
-    'Название организации',
+    'ИП Пономарева Кристина Вячеславовна',
     document.querySelector('#content > div > div'),
     dataContact,
   );
