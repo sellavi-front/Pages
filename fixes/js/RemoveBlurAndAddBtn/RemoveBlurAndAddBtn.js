cf1571c3a23efdc49d7bbac70d104c7aee7d35c0
Coospir <eugene.starodubov@gmail.com>
2021-01-20 12:39:41 +0300

    updated

--- fixes/js/RemoveBlurAndAddBtn/RemoveBlurAndAddBtn.js
+++ fixes/js/RemoveBlurAndAddBtn/RemoveBlurAndAddBtn.js
@@ -22,32 +22,10 @@ export default class ButtonContainerInProductCard {
   }
 }
 
+const priceActions = document.querySelectorAll('.price_actions');
+const addToCart = document.querySelectorAll('.add_to_cart');
+const addToWishList = document.querySelectorAll('.add_to_wishlist');
 
-window.onload = () => {
-  if (document.querySelector('body.product-category') || document.querySelector('body.common-home .products')) {
-    let target = document.querySelector('body div.products');
-    const config = {
-      childList: true,
-    };
-    const callback = function (mutationsList, observer) {
-      for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-          target.querySelectorAll('.product-item img').forEach((prod) => {
-            if (prod.getAttribute('src').includes('e_blur:2000')) {
-              prod.setAttribute('src', prod.getAttribute('src').replace(/e_blur:2000/gi, ''));
-              const priceActions = document.querySelectorAll('.price_actions');
-              const addToCart = document.querySelectorAll('.add_to_cart');
-              const addToWishList = document.querySelectorAll('.add_to_wishlist');
+const buttonContainerInProductCard = new ButtonContainerInProductCard(addToCart, addToWishList, priceActions);
 
-              const buttonContainerInProductCard = new ButtonContainerInProductCard(addToCart, addToWishList, priceActions);
-
-              buttonContainerInProductCard.createContainer();
-            }
-          });
-        }
-      }
-    };
-    const observer = new MutationObserver(callback);
-    observer.observe(target, config);
-  }
-};
+buttonContainerInProductCard.createContainer();
