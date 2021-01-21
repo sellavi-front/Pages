7868ba5c10c4d8ada6940f1a983a014d9848529c
Coospir <eugene.starodubov@gmail.com>
2021-01-21 15:33:11 +0300

    updated

--- shops/102783/index.js
+++ shops/102783/index.js
@@ -2,22 +2,23 @@ import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
 import '../../fixes/js/ButtonContainerInProductCard/ButtonContainerInProductCard.js'
 import '../../fixes/js/RemoveBlur/RemoveBlur.js'
 
-import Requisites from '../../modules/Requisites/Requisites.js';
+import Requisites from '../../modules/Requisites/Requisites.js'
 import {requsites, dataContact } from './utils/requsites.js'
 import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
 import '../../fixes/js/FooterCopy/FooterCopy.js'
 
 if (location.href.includes('term-of-use')) {
-  const req = new Requisites(requsites, 'beforeend', 'ИП "Кучма Виктория Геннадьевна"');
-
-  req.setTemplate();
+  const req = new Requisites(requsites, 'beforeend', 'ИП "Кучма Виктория Геннадьевна"')
+  req.setTemplate()
 }
 
-if (location.href.includes('contact')) {
-  const media = document.querySelector('.contact-info > .media-list > .media:last-child');
+document.querySelector("#home > div > footer > div.footer-widgets > div.container > div > div:nth-child(1) > div > h4").innerHTML = "О нас"
 
-  const req = new Requisites(requsites, 'afterend', 'ИП "Кучма Виктория Геннадьевна"', media);
-  req.setTemplate();
+if (location.href.includes('contact')) {
+  const media = document.querySelector('.contact-info > .media-list > .media:last-child')
+
+  const req = new Requisites(requsites, 'afterend', 'ИП "Кучма Виктория Геннадьевна"', media)
+  req.setTemplate()
 }
 
 
@@ -26,9 +27,10 @@ if (location.href.includes('policy')) {
     requsites,
     'afterend',
     'ИП "Кучма Виктория Геннадьевна"',
+
     document.querySelector('#content > div > div'),
-  );
-  req.setTemplate();
+  )
+  req.setTemplate()
 }
 
 if (location.href.includes('pay-delivery')) {
@@ -38,6 +40,6 @@ if (location.href.includes('pay-delivery')) {
     'ИП "Кучма Виктория Геннадьевна"',
     document.querySelector('#content > div > div'),
     dataContact,
-  );
-  req.setTemplate();
+  )
+  req.setTemplate()
 }
