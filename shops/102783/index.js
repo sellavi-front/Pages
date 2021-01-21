c3409e27061fe621a0aacbb03e1096226dd08133
Coospir <eugene.starodubov@gmail.com>
2021-01-21 12:22:46 +0300

    updated

--- shops/102783/index.js
+++ shops/102783/index.js
@@ -1,7 +1,9 @@
 import Requisites from '../../modules/Requisites/Requisites.js';
 import {requsites, dataContact } from './utils/requsites.js'
 import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
-import './../../../fixes/js/FooterCopy/FooterCopy.js'
+import './../../fixes/js/FooterCopy/FooterCopy.js'
+import './../../fixes/js/RemoveBlur/RemoveBlur.js'
+import './../../fixes/js/ButtonContainerInProductCard/ButtonContainerInProductCard.js'
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП "Кучма Виктория Геннадьевна"');
