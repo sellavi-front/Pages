01b345aa015ec480d71cd6b8f287397ea7452e3b
Ruslan Mamedbekov <m4medbekov@gmail.com>
2021-01-22 16:12:43 +0300

    final link fix

--- shops/102255/index.js
+++ shops/102255/index.js
@@ -57,7 +57,7 @@ if (document.querySelector('.wrapper')) {
 // }
 
 if (document.querySelector('#home > nav.navigation.mobile_side_nav > ul > li:nth-child(10) > a')) {
-  document.querySelector('#home > nav.navigation.mobile_side_nav.opened.nav-opened > ul > li:nth-child(10) > a').setAttribute('href', 'https://xn-----7kcnbcc2drfbazh.com/%d1%81%d1%80%d0%b5%d0%b4%d1%81%d1%82%d0%b2%d0%be-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%b4%d0%b0-%d0%b8-%d0%be%d0%b3%d0%be%d1%80%d0%be%d0%b4%d0%b0/')
+  document.querySelector('#home > nav.navigation.mobile_side_nav > ul > li:nth-child(10) > a').setAttribute('href', 'https://xn-----7kcnbcc2drfbazh.com/%d1%81%d1%80%d0%b5%d0%b4%d1%81%d1%82%d0%b2%d0%be-%d0%b4%d0%bb%d1%8f-%d1%81%d0%b0%d0%b4%d0%b0-%d0%b8-%d0%be%d0%b3%d0%be%d1%80%d0%be%d0%b4%d0%b0/')
 }
   
 if (location.href.includes('term-of-use')) {
