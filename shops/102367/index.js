96fd9b31627ef399c435108908888c0c3aa78767
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-22 18:32:19 +0300

    new shops

--- shops/102367/index.js
+++ shops/102367/index.js
@@ -97,7 +97,7 @@ if (document.querySelector('.product-product')) {
 setTimeout(() => {
   console.log(document.querySelector("#product > div:nth-child(2) > div > button"));
   document.querySelector("#product > div:nth-child(2) > div > button").addEventListener('click', () => {
-      console.log(document.querySelectorAll('#product > div:nth-child(2) ul li > span'));
+      console.log(document.querySelectorAll(".dropdown-item > span"));
   })
 }, 1500)
 
