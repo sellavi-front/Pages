2a9b1a9c8905c3abf048eabb297c808d800a460d
Coospir <eugene.starodubov@gmail.com>
2021-01-22 15:42:14 +0300

    updated

--- shops/102783/index.js
+++ shops/102783/index.js
@@ -1,12 +1,12 @@
 import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
 import '../../fixes/js/ButtonContainerInProductCard/ButtonContainerInProductCard.js'
 import '../../fixes/js/RemoveBlur/RemoveBlur.js'
-
-import Requisites from '../../modules/Requisites/Requisites.js'
-import {requsites, dataContact } from './utils/requsites.js'
-import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
-/*import '../../fixes/js/FooterCopy/FooterCopy.js'*/
 import renderContactsInCustomSection from "./blocks/contacts/contacts.js"
+
+import Requisites from '../../modules/Requisites/Requisites.js';
+import {requsites, dataContact } from './utils/requsites.js'
+import '../../fixes/js/FooterCopy/FooterCopy.js';
+import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
 
 
 document.querySelector("body").insertAdjacentHTML("afterbegin", `
@@ -19,46 +19,47 @@ document.querySelector("body").insertAdjacentHTML("afterbegin", `
 
 setTimeout(function() {
   /*window.addEventListener("load", function () {*/
-    const preloader = document.querySelector(".custom-preloader");
-    preloader.className += " hidden";
+  const preloader = document.querySelector(".custom-preloader");
+  preloader.className += " hidden";
   /*});*/
 }, 1500)
 
+
 if (location.href.includes('term-of-use')) {
-  const req = new Requisites(requsites, 'beforeend', 'ИП "Кучма Виктория Геннадьевна"')
-  req.setTemplate()
+  const req = new Requisites(requsites, 'beforeend', 'ИП КОРСАКОВ ЭДУАРД ВЛАДИМИРОВИЧ');
+
+  req.setTemplate();
 }
 
-document.querySelector("#home > div > footer > div.footer-widgets > div.container > div > div:nth-child(1) > div > h4").innerHTML = "О нас"
-
 if (location.href.includes('contact')) {
-  const media = document.querySelector('.contact-info > .media-list > .media:last-child')
+  const media = document.querySelector('.contact-info > .media-list > .media:last-child');
 
-  const req = new Requisites(requsites, 'afterend', 'ИП "Кучма Виктория Геннадьевна"', media)
-  req.setTemplate()
+  const req = new Requisites(requsites, 'afterend', 'ИП КОРСАКОВ ЭДУАРД ВЛАДИМИРОВИЧ', media);
+  req.setTemplate();
 }
 
 
 if (location.href.includes('policy')) {
   const req = new Requisites(
-    requsites,
-    'afterend',
-    'ИП "Кучма Виктория Геннадьевна"',
-
-    document.querySelector('#content > div > div'),
-  )
-  req.setTemplate()
+      requsites,
+      'afterend',
+      'ИП КОРСАКОВ ЭДУАРД ВЛАДИМИРОВИЧ',
+      document.querySelector('#content > div > div'),
+  );
+  req.setTemplate();
 }
 
 if (location.href.includes('pay-delivery')) {
   const req = new Requisites(
-    requsites,
-    'afterend',
-    'ИП "Кучма Виктория Геннадьевна"',
-    document.querySelector('#content > div > div'),
-    dataContact,
-  )
-  req.setTemplate()
+      requsites,
+      'afterend',
+      'ИП КОРСАКОВ ЭДУАРД ВЛАДИМИРОВИЧ',
+      document.querySelector('#content > div > div'),
+      dataContact,
+  );
+  req.setTemplate();
 }
+
+
 
 renderContactsInCustomSection()
