04b66d4185025992718368af1ce7307cff2952c2
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-25 18:46:16 +0300

    new shops

--- shops/100967/index.js
+++ shops/100967/index.js
@@ -174,7 +174,7 @@ if (document.querySelector('.wrapper')) {
   const phh = document.querySelector('.phh');
   phh.insertAdjacentHTML('beforebegin', `<div class="docs">${footerDocs.outerHTML}</div>`);
 
-  document.querySelector("#home > nav.navigation.mobile_side_nav.opened.nav-opened > ul").insertAdjacentHTML('afterend', `<p class="d-flex justify-content-center align-items-center custom__text_ph">Бесплатная доставка</p>`);
+  // document.querySelector("#home > nav.navigation.mobile_side_nav.opened.nav-opened > ul").insertAdjacentHTML('afterend', `<p class="d-flex justify-content-center align-items-center custom__text_ph">Бесплатная доставка</p>`);
 }
 
 if (document.querySelector('.common-home')) {
