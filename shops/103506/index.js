8adfe3c627f4a27cc797bf9ab8c2f5653db932c7
Coospir <eugene.starodubov@gmail.com>
2021-01-26 11:52:04 +0300

    updated

--- shops/103506/index.js
+++ shops/103506/index.js
@@ -1,7 +1,8 @@
 import "./docs/requisites.js"
+import "./blocks/header/header.js"
 import Paths from "./path/paths.js"
 import Banner from "./blocks/banner/banner.js"
-import "./blocks/header/header.js"
+
 
 if(Paths.home) {
   let MainBanner = new Banner()
