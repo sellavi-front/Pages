6986d0a72214db5342bd86e1d05589e74bfe2a43
Coospir <eugene.starodubov@gmail.com>
2021-01-26 10:57:24 +0300

    updated

--- shops/103506/blocks/banner/banner.js
+++ shops/103506/blocks/banner/banner.js
@@ -2,7 +2,7 @@ import BigBanner from "../../../../modules/BigBanner/BigBanner.js"
 
 export default class Banner {
   render() {
-    const bigBanner = new BigBanner('https://res.cloudinary.com/dtqqfmyqb/image/upload/v1606724800/jan-kopriva-sh_7sFEFICI-unsplash_1_gc6uba.png');
+    const bigBanner = new BigBanner('https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1611647407/sellavi/103506/Frame_7_1_ohe1mv.jpg');
     bigBanner.render()
   }
 }
