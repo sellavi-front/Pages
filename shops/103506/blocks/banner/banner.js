d379b9f12b02a2f2ce6134731c1e99145965d7a9
Coospir <eugene.starodubov@gmail.com>
2021-01-26 11:35:24 +0300

    updated

--- shops/103506/blocks/banner/banner.js
+++ shops/103506/blocks/banner/banner.js
@@ -2,7 +2,7 @@ import BigBanner from "../../../../modules/BigBanner/BigBanner.js"
 
 export default class Banner {
   render() {
-    const bigBanner = new BigBanner('https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1611647407/sellavi/103506/Frame_7_1_ohe1mv.jpg');
+    const bigBanner = new BigBanner('https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1611649841/sellavi/103506/Frame_7_1_korbko.jpg');
     bigBanner.render()
   }
 }
--- shops/103506/index.js
+++ shops/103506/index.js
@@ -1,6 +1,7 @@
 import "./docs/requisites.js"
 import Paths from "./path/paths.js"
 import Banner from "./blocks/banner/banner.js"
+import "./blocks/header/header.js"
 
 if(Paths.home) {
   let MainBanner = new Banner()
