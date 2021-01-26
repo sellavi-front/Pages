1a76778307ec5be908a787d082cfa2ae303fd8e1
Coospir <eugene.starodubov@gmail.com>
2021-01-26 12:30:18 +0300

    updated

--- shops/103506/index.js
+++ shops/103506/index.js
@@ -1,5 +1,5 @@
 import "./docs/requisites.js"
-import "../../components/HeaderTypeTwo/HeaderTypeTwo.js"
+import "../../components/HeaderTypeThree/HeaderTypeThree.js"
 import Paths from "./path/paths.js"
 import Banner from "./blocks/banner/banner.js"
 
