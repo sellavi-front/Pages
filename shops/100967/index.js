f46bb723546bb64e28163f333fa55c4fc7edc745
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-26 11:02:02 +0300

    new shops

--- shops/100967/index.js
+++ shops/100967/index.js
@@ -179,8 +179,7 @@ if (document.querySelector('.wrapper')) {
   document.querySelectorAll(".panel-collapse > ul > li > a").forEach(el => {
     if (el.textContent.includes(' - ')) {
       let arr = el.textContent.split();
-      console.log(arr.split());
-
+      console.log(arr.textContent.split());
     }
   })
 }
