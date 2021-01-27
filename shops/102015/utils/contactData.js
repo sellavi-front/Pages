b82514ebad59b6bd115a8aa5c3ecdf56ca6d9dfe
m4medbekov <m4medbekov@gmail.com>
2021-01-27 17:41:20 +0300

    fix map

--- shops/102015/utils/contactData.js
+++ shops/102015/utils/contactData.js
@@ -15,7 +15,7 @@ const contactData = {
     },
   ],
   map: `
-  <iframe src="<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ab5ed2af75ef2b858d3522fe4bfde26739b7c58d5f1bcfe44f4863b33ffb41e6f&amp;source=constructor" width="600" height="400" frameborder="0"></iframe>
+  <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ab5ed2af75ef2b858d3522fe4bfde26739b7c58d5f1bcfe44f4863b33ffb41e6f&amp;source=constructor" width="600" height="400" frameborder="0"></iframe>
   `,
 };
 
