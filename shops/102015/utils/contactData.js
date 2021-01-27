1e91a584eb6700d5b103af87b194e9716bd96c1d
m4medbekov <m4medbekov@gmail.com>
2021-01-27 17:39:55 +0300

    fix map contact

--- shops/102015/utils/contactData.js
+++ shops/102015/utils/contactData.js
@@ -3,19 +3,19 @@ const contactData = {
   contacts: [
     {
       title: 'Адрес',
-      contact: 'Москва, ул. Белореченская, д. 22/66',
+      contact: 'MO. г.Красногорск, Изумрудные Холмы, Бульвар Космонавтов д.8',
     },
     {
       title: 'Телефон',
-      contact: '<a href="tel:+79251552557">+79251552557</a>',
+      contact: '<a href="tel:+79168018224">+79168018224</a>',
     },
     {
       title: 'E-mail',
-      contact: '<a href="mailto:5178080@mail.ru">5178080@mail.ru</a>',
+      contact: '<a href="mailto:marinaokutina@yandex.ru">marinaokutina@yandex.ru</a>',
     },
   ],
   map: `
-  <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ac3aa37f726fcc30a115c808ac4e7e037289c238be97d50c31646463c6bc5bc92&amp;source=constructor" frameborder="0"></iframe>
+  <iframe src="<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ab5ed2af75ef2b858d3522fe4bfde26739b7c58d5f1bcfe44f4863b33ffb41e6f&amp;source=constructor" width="600" height="400" frameborder="0"></iframe>
   `,
 };
 
