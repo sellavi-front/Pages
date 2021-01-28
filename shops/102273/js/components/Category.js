670b0554d54fbb97feb2763260840fa3adabe6dc
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-28 14:29:32 +0300

    styles

--- shops/102273/js/components/Category.js
+++ shops/102273/js/components/Category.js
@@ -1,4 +1,4 @@
-import BaseComponent from '../../../../components/BaseComponent/BaseComponent.js'
+import BaseComponent from '../../../../components/BaseComponent/BaseComponent.js';
 
 export default class Category extends BaseComponent {
   constructor(props, data) {
@@ -7,24 +7,26 @@ export default class Category extends BaseComponent {
   }
 
   getData() {
-    return this.data.content.map(elem => {
-      return `
+    return this.data.content
+      .map((elem) => {
+        return `
           <div class="icon-category__elem">
             <img class="icon-category__elem_ico" src="${elem.src}" alt="иконка категории">
             <a class="icon-category__elem_link" href="${elem.link}">
               ${elem.text}
             </a>
           </div>
-      `
-    }).join('')
+      `;
+      })
+      .join('');
   }
 
   getTemplate() {
     return `
     <div class="icon-category">
-      <h3 class="icon-category__title">${this.data.title}</h3>
+      ${this.data.title ? `<h3 class="icon-category__title">${this.data.title}</h3>` : ''}
       ${this.getData()}
-    </div>`
+    </div>`;
   }
 
   setTemplate() {
