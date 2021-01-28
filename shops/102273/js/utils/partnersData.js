ede57a150dfd357ddd2c726a081f584ade63eea6
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-28 17:54:51 +0300

    styles

--- shops/102273/js/utils/partnersData.js
+++ shops/102273/js/utils/partnersData.js
@@ -1,18 +1,18 @@
 const partnersData = [
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845435/102273/partners/logo_1_ibniu9.png',
     title: 'Партнер – компания ООО «КитМед». ',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах, Продажа и поставка медицинской техники.',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845434/102273/partners/22ea91ffceca62ceb4183cb0d5c2f9d3_1_lyka9v.png',
     title: 'Партнер – компания ООО «СпортМедИмпорт». ',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах, Продажа и поставка медицинской техники.',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845434/102273/partners/logo_1_1_di971p.png',
     title: 'Партнер – компания ООО «ПРОФ ФАРМ». ',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах, Продажа и поставка медицинской техники.',
     link: '#'
@@ -24,25 +24,25 @@ const partnersData = [
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845435/102273/partners/logo_2_1_f9njbc.png',
     title: 'Партнер – компания «Нижегородский областной Бароцентр»',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах, Производство и продажа медицинских барокамер.',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845434/102273/partners/image_16_wuakpj.png',
     title: 'Партнер – компания ООО «СИМТ» ',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах, Современные инновационные медицинские технологии',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845434/102273/partners/logo_1_2_bon4qm.png',
     title: 'Партнер – компания ООО «ЛЕОВИТ». ',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах, Производство и поставка специализированного профилактического питания для детоксикации организма ',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845435/102273/partners/logo_675_1_ustbxw.png',
     title: 'Партнер – компания ООО «Бренд Вотер»',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах, Производство экологически чистой воды с СТМ (собственной торговой маркой) заказчика.',
     link: '#'
@@ -54,49 +54,49 @@ const partnersData = [
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845435/102273/partners/MZMO_txt_big_6_1_jsxxpg.png',
     title: 'ГБУЗ МО «МОКПТД» ',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах    ',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845434/102273/partners/3ea266ff2bdf77f62d5cf8c9f4b4aa62_1_natwyj.png',
     title: 'ГБУЗ «ГВВ № 2 ДЗМ»',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845435/102273/partners/logo_2020_1_1_wqminu.png',
     title: 'ГБУЗ «ГКБ № № 64 м. В.В. Виноградова ДЗМ» ',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845435/102273/partners/logo_2_3_y76ucr.png',
     title: 'ГБУЗ «ГКБ № 57  им. Д. Д. Плетнёва ДЗМ» ',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845435/102273/partners/logo_3_1_bj4nz0.png',
     title: 'ФГБУ ГБ 119',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845435/102273/partners/logo2_1_hca890.png',
     title: 'Научно-практический центр медико-социальной реабилитации инвалидов имени Л.И. Швецовой.',
     text: 'Участие в социальных, медико–социальных и благотворительных Программах',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845435/102273/partners/logo_4_1_gutbkx.png',
     title: 'Партнер – компания МЦ «Милта Клиник».',
     text: 'Участие в медико–социальных и благотворительных Программах. Диагностика, консультации, лечение. Урология, гинекология.',
     link: '#'
   },
   {
-    src: '',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611845434/102273/partners/18199368_241860069551687_1471674001059348302_n_1_vliil9.png',
     title: '«Межрегиональный центр социальных информационных программ».',
     text: 'Участие в медико–социальных, учебных, развлекательных, информационных и благотворительных Программах',
     link: '#'
