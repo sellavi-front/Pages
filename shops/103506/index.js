82cf35ff5a4ca9f23ef891f347fc206c23f835d6
Coospir <eugene.starodubov@gmail.com>
2021-01-28 11:50:01 +0300

    Updated 1030506

--- shops/103506/index.js
+++ shops/103506/index.js
@@ -2,7 +2,12 @@ import "./docs/requisites.js"
 import "../../templates/TemplateTypeTwo/TemplateTypeTwo.js"
 import Paths from "./path/paths.js"
 
-
+if (document.querySelector('.header-wrapper')) {
+  const oldNav = document.querySelector('.navigation .nav.sf-menu');
+  const newNav = oldNav.cloneNode(true);
+  document.querySelector('#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div.container > div.logo').insertAdjacentHTML('afterend', newNav.outerHTML);
+  oldNav.remove();
+}
 
 if(Paths.home) {
 
