a06e43787ee1966ee1e618a7d59f6435046059d0
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-28 11:32:48 +0300

    styles

--- shops/102273/js/main.js
+++ shops/102273/js/main.js
@@ -1,2 +1,8 @@
 import './utils/main.js'
 import './pages/main-page.js'
+
+if (document.querySelector('.wrapper')) {
+  const logo = document.querySelector("#home > div > header > div.header-wrapper > div.container-fluid > div.logo > a > img");
+
+  logo.getAttribute('src').replace(/\/c_fit,g_center,h_176,q_100,w_430\/c_pad,h_176,w_430\/v1611822634/gi, '')
+}
