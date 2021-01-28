79c781fc6007328f18a72e800cf33b0669003f71
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-28 12:52:12 +0300

    styles

--- shops/102273/js/pages/innerPage.js
+++ shops/102273/js/pages/innerPage.js
@@ -1,9 +1,9 @@
-import FormBasic from '../../../../../components/FormBasic/FormBasic.js'
-import Category from '../../components/Category.js'
+import FormBasic from '../../../../components/FormBasic/FormBasic.js'
+import Category from '../components/Category.js'
 
-import senderData from '../../constants/senderData.js'
-import FormTags from '../../constants/FormTags.js'
-import categoryData from '../../utils/categoryData.js'
+import senderData from '../constants/senderData.js'
+import FormTags from '../constants/FormTags.js'
+import categoryData from '../constants/categoryData.js'
 
 export default (function () {
   if (document.querySelector('.product-category')) {
