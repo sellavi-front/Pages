ec1fc5abf6a43e3f65512f11e7caa2b0cc326c7b
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-28 11:12:50 +0300

    styles

---
+++ shops/102273/js/pages/category/category.js
@@ -0,0 +1,7 @@
+
+
+export default (function () {
+  if (document.querySelector('.product-category')) {
+    const bannerArea = document.querySelector('.content-area > .page-section.titles');
+  }
+})();
