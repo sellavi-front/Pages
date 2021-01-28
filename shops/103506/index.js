0fcd8382a2515ba44ddbd05660e8ab6e2e1ae10f
Coospir <eugene.starodubov@gmail.com>
2021-01-28 15:59:08 +0300

    Updated 1030506

--- shops/103506/index.js
+++ shops/103506/index.js
@@ -19,5 +19,5 @@ document.querySelector("#home > div.wrapper.wrapper-closed > footer > div.footer
 document.querySelector("#home > div.wrapper.wrapper-closed > footer > div.footer-widgets > div.container > div > div:nth-child(1) > div > div > ul > li:nth-child(1) > a").innerHTML = 'О нас'
 
 if(Paths.home) {
-
+  document.querySelector("#banner0 > div > div > div > div > picture > source").setAttribute("srcset", "https://cdn.sellavi.com/image/upload/ru/clients/103506/26d063c3bf4bf691704286bebed1d6fb15dd1142.jpg")
 }
