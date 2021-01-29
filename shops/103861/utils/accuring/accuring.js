e20e63f3889c45988172b09a995ed9f8f3275b04
Coospir <eugene.starodubov@gmail.com>
2021-01-29 17:42:13 +0300

    Updated 1030506

--- shops/103861/utils/accuring/accuring.js
+++ shops/103861/utils/accuring/accuring.js
@@ -17,7 +17,7 @@ if (location.href.includes('contact')) {
 }
 
 
-if (location.href.includes('pay-delivery')) {
+/*if (location.href.includes('pay-delivery')) {
   const req = new Requisites(
     requsites,
     'afterend',
@@ -26,4 +26,4 @@ if (location.href.includes('pay-delivery')) {
     dataContact,
   );
   req.setTemplate();
-}
+}*/
