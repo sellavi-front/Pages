dbe64db52dbc2b964d6e42ba67d2c88b3c6ae606
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-29 18:05:42 +0300

    styles

--- shops/102864/scripts/main.js
+++ shops/102864/scripts/main.js
@@ -5,14 +5,15 @@ if (pages.main) {
 
   // renderContactsInCustomSection();
 
-  document.querySelector('.custom_section').insertAdjacentHTML('beforeend', `
+  document.querySelector('.custom_section').insertAdjacentHTML('beforeend', `<div class="container">
     <div class="d-flex flex-wrap justify-content-between">
-      <img src="https://ru.sellavi.com/admin/l_out/102864-Ym5oaGJYbG1kbk55Ykc5M2VtUjZSRTA0TDFoSlduSldOR2M5UFE9PQ==" style="margin-right: 100px; max-width: 450px; width: 100%;">
+      <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611932692/102273/Frame_2_m4fgs8.png" style="margin-right: 100px; max-width: 450px; width: 100%;">
       <div>
         <h4>О нас</h4>
         <p>
         Я сделаю для вас браслет, который станет не просто украшением, а вашим личным талисманом на удачу, помощником эксклюзивного дизайна, из тех камней, которые подходят именно вам: по знаку Зодиака  или иным вашим личным пожеланиям.
         </p>
+      </div>
       </div>
     </div>
   `);
