e7db5f9287f904ee36908178e6beefc54d3248f8
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-29 12:10:10 +0300

    styles

--- shops/102273/js/pages/innerPages/desinfectedFrames.js
+++ shops/102273/js/pages/innerPages/desinfectedFrames.js
@@ -146,14 +146,42 @@ export default function desinfectedFrames() {
             </div>
           </div>
           <div style="margin: 0 0 40px 0">
-            <h5>Установленные многофункциональные дезинфицирующие арки AV GATE:</h5>
+            <h5 style="margin-bottom: 30px;"><b>Установленные многофункциональные дезинфицирующие арки AV GATE:</b></h5>
             <ul>
               <li>
                 <p>1.	ООО «Честный чек» м. Краснопресненская, г. Москва, ул. Рочдельская, 15. ст.16 а, 4 этаж.</p>
                 <div>
                   <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611910612/102273/IMG_20200918_172312_gbsxes.png" alt="" style="margin-right: 35px;">
-                  <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611910612/102273/IMG-20201005-WA0003_k64toq.png" alt="" style="margin-right: 35px;">
+                  <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611910612/102273/IMG-20201005-WA0003_k64toq.png" alt="" >
 
+                </div>
+              </li>
+              <li>
+                <p>2.	  Стройтанснефтегаз</p>
+                <div>
+                  <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611911178/102273/IMG-20201005-WA0006_iipxug.png" alt="" style="margin-right: 35px;">
+                  <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611911178/102273/IMG-20201005-WA0007_clw8uz.png" alt="" style="margin-right: 35px;">
+                  <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611911177/102273/IMG-20201005-WA0004_xm62ds.png" alt="">
+
+                </div>
+              </li>
+              <li>
+                <p>3.	ООО «ЦРПТ» г. Москва, ул. Рочдельская, д. 15, стр. 16А, БП «Трехгорная мануфактура»</p>
+                <div>
+                  <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611911273/102273/IMG-20201005-WA0009_upniac.png" alt="" style="margin-right: 35px;">
+                  <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611911273/102273/IMG-20201005-WA0008_hdkizl.png" alt="" style="margin-right: 35px;">
+                  <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611911273/102273/IMG-20201005-WA0010_lpdecz.png" alt="" >
+
+
+                </div>
+              </li>
+              <li>
+                <p>4.	ООО «ЦРПТ» г. Москва, ул. Рочдельская, д. 15, стр. 16А, БП «Трехгорная мануфактура».</p>
+                <div>
+                <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611911344/102273/IMG-20201005-WA0015_z1iedx.png" alt="" style="margin-right: 35px;">
+                <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611911344/102273/IMG-20201005-WA0013_syovyo.png" alt="" style="margin-right: 35px;">
+                <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611911344/102273/IMG-20201005-WA0014_omgzyw.png" alt="" style="margin-right: 35px;">
+                <img src="https://res.cloudinary.com/depgheppz/image/upload/v1611911344/102273/IMG-20201005-WA0012_vshyky.png" alt="" >
                 </div>
               </li>
             </ul>
