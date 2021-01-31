54879bb48d3a336434ea2dd2f7c58598f97b1ea6
Закир Гафиятов <gafiyatovz@gmail.com>
2021-01-31 11:19:36 +0300

    styles

--- shops/102244/index.js
+++ shops/102244/index.js
@@ -19,41 +19,41 @@ if (document.querySelector('.wrapper')) {
   document.querySelector("#home > div > header > div.header-wrapper > div").classList.add('container-fluid')
   document.querySelector("#home > div > header > div.header-wrapper > div").classList.remove('container')
 
-  // document.querySelector(
-  //   '#home > div > header > div.header-wrapper > div > nav > ul > li:nth-child(1) > div > a',
-  // ).textContent = '';
-  // document
-  //   .querySelector(
-  //     '#home > div > header > div.header-wrapper > div > nav > ul > li:nth-child(1) > div > a',
-  //   )
-  //   .insertAdjacentHTML(
-  //     'beforeend',
-  //     `
-  // <svg width="23" height="21" viewBox="0 0 23 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-  //   <rect width="23" height="1" fill="black"/>
-  //   <rect y="7" width="23" height="1" fill="black"/>
-  //   <rect y="14" width="23" height="1" fill="black"/>
-  // </svg>
-  // `,
-  //   );
+  document.querySelector(
+    '#home > div > header > div.header-wrapper > div > nav > ul > li:nth-child(1) > div > a',
+  ).textContent = '';
+  document
+    .querySelector(
+      '#home > div > header > div.header-wrapper > div > nav > ul > li:nth-child(1) > div > a',
+    )
+    .insertAdjacentHTML(
+      'beforeend',
+      `
+  <svg width="23" height="21" viewBox="0 0 23 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="23" height="1" fill="black"/>
+    <rect y="7" width="23" height="1" fill="black"/>
+    <rect y="14" width="23" height="1" fill="black"/>
+  </svg>
+  `,
+    );
 
-  // setTimeout(() => {
-  //   document.querySelector('.prevent_touch_banner.d-none').insertAdjacentHTML(
-  //     'afterend',
-  //     `
-  //       <div class="mobile-navigation-close">
-  //         <i class="fal fa-times"></i>
-  //       </div>
-  //     `,
-  //   );
-  // }, 500);
+  setTimeout(() => {
+    document.querySelector('.prevent_touch_banner.d-none').insertAdjacentHTML(
+      'afterend',
+      `
+        <div class="mobile-navigation-close">
+          <i class="fal fa-times"></i>
+        </div>
+      `,
+    );
+  }, 500);
 
-  // document.querySelector('.mobile-navigation-wrapper').addEventListener('click', (e) => {
-  //   document.querySelector('.mobile-navigation-close').addEventListener('click', () => {
-  //     const nav = document.querySelector('.navigation');
-  //     nav.classList.add('nav-closed');
-  //     nav.classList.remove('nav-opened');
-  //     document.querySelector('body').classList.remove('overflow-hidden');
-  //   });
-  // });
+  document.querySelector('.mobile-navigation-wrapper').addEventListener('click', (e) => {
+    document.querySelector('.mobile-navigation-close').addEventListener('click', () => {
+      const nav = document.querySelector('.navigation');
+      nav.classList.add('nav-closed');
+      nav.classList.remove('nav-opened');
+      document.querySelector('body').classList.remove('overflow-hidden');
+    });
+  });
 }
