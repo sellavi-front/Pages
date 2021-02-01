d7ffe506232c5562e88193610e3f462e1b550b9a
m4medbekov <m4medbekov@gmail.com>
2021-02-01 14:22:38 +0300

    fix

--- shops/103009/index.js
+++ shops/103009/index.js
@@ -94,3 +94,7 @@ if (location.href.includes('pay-delivery')) {
 // if (document.querySelector('.footer-widgets')) {
 //   document.querySelector('#home > div > footer > div.footer-widgets > div.container > div > div.soc-icons > div > div').insertAdjacentHTML('beforebegin', '<div class="title-socIcon">Мы в соцсетях</div>');
 // }   
+
+if (document.querySelector('.header-wrapper')) {
+  document.querySelector('header > div.header-wrapper > div.container > ul > li.flexy.no_children > a').innerHTML = 'Статьи';
+}
