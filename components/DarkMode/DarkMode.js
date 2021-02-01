347f35d2a0e8b665ae00567725b1efb7c4d839ba
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-01 20:42:52 +0300

    styles

--- components/DarkMode/DarkMode.js
+++ components/DarkMode/DarkMode.js
@@ -1,2 +1,15 @@
-document.querySelector('body').classList.add('dark-mode');
-localStorage.setItem('theme', 'dark');
+const themeBtn = document.querySelector('header .theme-checkbox');
+
+if (!themeBtn.checked) {
+  console.log('Тема не применена');
+}
+
+themeBtn.addEventListener('click', () => {
+  if (localStorage.getItem('theme') !== 'dark' && localStorage.getItem('theme')) {
+    localStorage.setItem('theme', 'dark');
+    localStorage.setItem('theme-checkbox', 'checked');
+  } else {
+    themeBtn.checked = true;
+    localStorage.removeItem('theme');
+  }
+});
