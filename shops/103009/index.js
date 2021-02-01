f2feaaaa74568be6c1d0ec5d3d098cfb715c6274
m4medbekov <m4medbekov@gmail.com>
2021-02-01 10:58:03 +0300

    dick

--- shops/103009/index.js
+++ shops/103009/index.js
@@ -10,7 +10,7 @@ import '../../fixes/js/ChangeAgreement/ChangeAgreement.js';
 
 
 document.querySelector('#home > div.wrapper.wrapper-closed > div.navigation_black_screen').remove();
-
+document.querySelector('#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div.container > ul > li.flexy.no_children > a').innerText = 'Статьи'
 // if (document.querySelector('.header-wrapper')) {
 //   document.querySelector('#home > div.wrapper.wrapper-closed > header > div.header-wrapper > div.container > ul > li.flexy.no_children > a').innerHTML = 'Статьи';
 // }
