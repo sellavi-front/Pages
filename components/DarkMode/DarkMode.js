2e38427ac6ccb20d8834b2f336486d209091ffa8
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-02 16:19:31 +0300

    styles

--- components/DarkMode/DarkMode.js
+++ components/DarkMode/DarkMode.js
@@ -1,38 +1,45 @@
 const _removeTheme = () => {
-  localStorage.removeItem('theme');
-  document.querySelector('body').classList.remove('dark-mode');
+    localStorage.removeItem('theme');
+    document.querySelector('body').classList.remove('dark-mode');
 };
 
 const _setTheme = (btn) => {
-  localStorage.setItem('theme', 'dark');
-  document.querySelector('body').classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark');
+    document.querySelector('body').classList.add('dark-mode');
 };
 
 const _hasTheme = (btn) => {
-  if (!localStorage.getItem('theme')) {
-    _setTheme(btn);
-  } else if (
-    localStorage.getItem('theme') === 'dark' &&
-    document.querySelector('body').classList.contains('dark-mode')
-  ) {
-    _removeTheme();
-  }
+    if (!localStorage.getItem('theme')) {
+        _setTheme(btn);
+    } else if (
+        localStorage.getItem('theme') === 'dark' &&
+        document.querySelector('body').classList.contains('dark-mode')
+    ) {
+        _removeTheme();
+    }
 };
 
 const _hasChecked = (btn) => {
-  if (
-    localStorage.getItem('theme') === 'dark' &&
-    !document.querySelector('body').classList.contains('dark-mode')
-  ) {
-    _setTheme(btn);
-    btn.checked = true;
-  }
+    if (
+        localStorage.getItem('theme') === 'dark' &&
+        !document.querySelector('body').classList.contains('dark-mode') &&
+        !location.href.includes('checkout')
+    ) {
+        _setTheme(btn);
+        btn.checked = true;
+    } else if (
+        localStorage.getItem('theme') === 'dark' &&
+        !document.querySelector('body').classList.contains('dark-mode') &&
+        location.href.includes('checkout')
+    ) {
+        _setTheme(btn);
+    }
 };
 
 const setDarkTheme = () => {
-  const themeBtn = document.querySelector('.button__list_elem .theme-checkbox');
-  _hasChecked(themeBtn);
-  themeBtn.addEventListener('input', () => _hasTheme(themeBtn));
+    const themeBtn = document.querySelector('.button__list_elem .theme-checkbox');
+    _hasChecked(themeBtn);
+    themeBtn.addEventListener('input', () => _hasTheme(themeBtn));
 };
 
 export default setDarkTheme;
