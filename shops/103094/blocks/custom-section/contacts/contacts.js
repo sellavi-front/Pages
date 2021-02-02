a9c194fbd6a4fde60df582dabd178cdef1ecff50
Coospir <eugene.starodubov@gmail.com>
2021-02-02 11:25:39 +0300

    Updated 1030506

--- shops/103094/blocks/custom-section/contacts/contacts.js
+++ shops/103094/blocks/custom-section/contacts/contacts.js
@@ -10,7 +10,7 @@ let renderContactsInCustomSection = () => {
         <div class="otherInfo">
         <div class="address">
               <div class="address-img">
-                  <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612250310/sellavi/103094/Component_6_knbges.jpg" alt="">
+                  <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612254293/sellavi/103094/Component_1_tyttwj.jpg" alt="">
               </div>
               <div class="title-and-address">
                 <div class="address-title">
@@ -24,7 +24,7 @@ let renderContactsInCustomSection = () => {
 
             <div class="number">
               <div class="number-img">
-                  <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612250329/sellavi/103094/Group_208_d3kw1v.jpg" alt="">
+                  <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612254293/sellavi/103094/Group_208_xqetc1.jpg" alt="">
               </div>
               <div class="title-and-number">
                 <div class="number-title">
@@ -38,7 +38,7 @@ let renderContactsInCustomSection = () => {
 
             <div class="email">
               <div class="email-img">
-                  <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612250343/sellavi/103094/Group_209_te1eh1.jpg" alt="">
+                  <img src="https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612254293/sellavi/103094/Group_209_qdgxhp.jpg" alt="">
               </div>
               <div class="title-and-email">
                 <div class="email-title">
