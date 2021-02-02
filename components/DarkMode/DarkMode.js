2eb2bf751442164134a833526826b1139ca4f130
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-02 10:30:28 +0300

    styles

--- components/DarkMode/DarkMode.js
+++ components/DarkMode/DarkMode.js
@@ -1,11 +1,9 @@
-
-
 const _removeTheme = () => {
   localStorage.removeItem('theme');
   document.querySelector('body').classList.remove('dark-mode');
 };
 
-const _setTheme = btn => {
+const _setTheme = (btn) => {
   localStorage.setItem('theme', 'dark');
   document.querySelector('body').classList.add('dark-mode');
   btn.checked = true;
@@ -14,10 +12,13 @@ const _setTheme = btn => {
 const _hasTheme = (btn) => {
   if (!localStorage.getItem('theme')) {
     _setTheme(btn);
-  } else if (localStorage.getItem('theme') && !document.querySelector('body').classList.contains('dark-mode')) {
+  } else if (
+    localStorage.getItem('theme') &&
+    !document.querySelector('body').classList.contains('dark-mode')
+  ) {
     _setTheme(btn);
   } else {
-    _removeTheme()
+    _removeTheme();
   }
 };
 
@@ -25,6 +26,6 @@ const setDarkTheme = () => {
   const themeBtn = document.querySelector('.button__list_elem .theme-checkbox');
   _hasTheme();
   themeBtn.addEventListener('input', () => _hasTheme(themeBtn));
-}
+};
 
 export default setDarkTheme;
