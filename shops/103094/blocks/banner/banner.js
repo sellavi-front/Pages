bc93fab23d54bb716356a84bf5eb94fa9aa7b75c
Coospir <eugene.starodubov@gmail.com>
2021-02-02 12:17:13 +0300

    Updated 1030506

--- shops/103094/blocks/banner/banner.js
+++ shops/103094/blocks/banner/banner.js
@@ -1,6 +1,6 @@
 let resetSizeBanner = () => {
-  //let newLink = "https://cdn.sellavi.com/image/upload/v1612186757/ru/clients/103094/6b584576f338311db0e8a87b9cf5a8a8261e31e1.webp"
-  //document.querySelector("#banner0 > div > div > div > div > picture > source").setAttribute("srcset", newLink)
+  let newLink = "https://cdn.sellavi.com/image/upload/v1612257265/ru/clients/103094/165829d93913748820ec43abf3bb9fa35066a765.webp"
+  document.querySelector("#banner0 > div > div > div > div > picture > source").setAttribute("srcset", newLink)
 }
 
 export default resetSizeBanner
