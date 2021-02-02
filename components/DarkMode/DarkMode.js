d642c8959b4025388911a3f7e5548becb57d25b9
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-02 08:44:22 +0300

    styles

--- components/DarkMode/DarkMode.js
+++ components/DarkMode/DarkMode.js
@@ -1,21 +1,24 @@
-const themeBtn = document.querySelector('header .theme-checkbox');
+const themeBtn = document.querySelector('.button__list_elem .theme-checkbox');
 const body = document.querySelector('body');
 
-const _hasDarkTheme = () => {
-  if (!localStorage.getItem('theme')) {
-    localStorage.setItem('theme', 'dark');
-    localStorage.setItem('theme-checkbox', 'checked');
-    body.classList.add('theme-dark');
-  } else {
-    themeBtn.checked = true;
-    localStorage.removeItem('theme');
-    body.classList.remove('theme-dark');
+const _removeTheme = () => {
+  localStorage.removeItem('theme');
+  body.classList.remove('dark-mode');
+};
+
+const _setTheme = () => {
+  localStorage.setItem('theme', 'dark');
+  body.classList.add('dark-mode');
+};
+
+const _hasItem = () => {
+  if (!themeBtn.checked && localStorage.getItem('theme') === 'dark') {
+    _setTheme();
+    themeBtn.chcked = true;
+  } else if (!localStorage.getItem('theme') === 'dark') {
   }
 };
 
-const setDarkTheme = () => {
-  _hasDarkTheme()
-  themeBtn.addEventListener('click', () => _hasDarkTheme());
-};
+themeBtn.addEventListener('click', () => _hasDarkTheme());
 
 export default setDarkTheme;
