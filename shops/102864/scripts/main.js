bccb4f08fa8b632be6ba83e8afe6700ac569f809
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-03 15:53:54 +0300

    styles

--- shops/102864/scripts/main.js
+++ shops/102864/scripts/main.js
@@ -19,6 +19,33 @@ if (pages.main) {
     </div>
   `,
   );
+
+  document.querySelector('.custom_section').insertAdjacentHTML(
+    'beforeend', `
+      <div class="d-flex flex-wrap justify-content-between">
+          <div class="d-flex">
+            <img src="https://res.cloudinary.com/depgheppz/image/upload/v1612356741/Group_7423519_svefer.png">
+            <p class="d-flex flex-column">
+              <b class="">Адрес</b>
+              <span>Адрес Адрес Адрес</span>
+            </p>
+          </div>
+          <div class="d-flex">
+            <img src="https://res.cloudinary.com/depgheppz/image/upload/v1612356741/Group_208_t0zswf.png">
+            <p class="d-flex flex-column">
+              <b class="">Адрес</b>
+              <span>Адрес Адрес Адрес</span>
+            </p>
+          </div>
+          <div class="d-flex">
+            <img src="https://res.cloudinary.com/depgheppz/image/upload/v1612356741/Group_209_xe0c8z.png">
+            <p class="d-flex flex-column">
+              <b class="">Адрес</b>
+              <span>Адрес Адрес Адрес</span>
+            </p>
+          </div>
+      </div>
+    `);
 }
 
 if (location.origin) {
