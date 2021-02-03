45b835c317399da0400f6a75479194e5ce2b1742
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-03 17:37:29 +0300

    styles

--- shops/103474/utils/index.js
+++ shops/103474/utils/index.js
@@ -1,26 +1,12 @@
 import "../../../components/HeaderTypeTwo/HeaderTypeTwo.js"
 import "../../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
 import "../../../fixes/fixes.js"
-import Banner from "../blocks/banner.js"
+// import Banner from "../blocks/banner.js"
 import pages from "./pages/pages.js"
 import Advantages from "../blocks/advantages.js"
 import Functions from "./functions/functions.js"
-import DeleteClasses from '../../../modules/DeleteClasses/DeleteClasses.js'
 import CustomSection from "../blocks/custom-section.js"
 
-const bootstrapClasses = [
-  'col-lg-4',
-  'mb-0',
-  'mb-md-4',
-  'col-12',
-  'col-sm-12',
-  'col-sm-6',
-  'col-md-6',
-  'col-lg-6',
-  'col-xl-4',
-/*  'mb-4',*/
-  'container'
-];
 
 if(pages.home) {
   /*let banner = new Banner()
