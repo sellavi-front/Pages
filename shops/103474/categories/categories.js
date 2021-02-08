915146559bb1bbf13efb1a8404544e929864f900
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-08 15:05:37 +0300

    styles

--- shops/103474/categories/categories.js
+++ shops/103474/categories/categories.js
@@ -1,49 +1,59 @@
-const categories = {
-  water: {
+const categories = [
+  {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612363467/103474/Frame_19_nryqdm.png',
-    link: '#'
+    link: '#',
+    text: '',
   },
-  fish: {
+  {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612363468/103474/Group_7423498_rdoxrb.png',
-    link: '#'
+    link: '#',
+    text: '',
   },
-  bakery: {
+  {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612363468/103474/Frame_51_bswfly.png',
-    link: '#'
+    link: '#',
+    text: '',
   },
-  chocolate: {
+  {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612363468/103474/Group_7423499_sijdbl.png',
-    link: '#'
+    link: '#',
+    text: '',
   },
-  waffles: {
+  {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612363467/103474/Frame_38_snoemi.png',
-    link: '#'
+    link: '#',
+    text: '',
   },
-  season: {
+  {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612363467/103474/Frame_49_l0kmsq.png',
-    link: '#'
+    link: '#',
+    text: '',
   },
-  snacks: {
+ {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612363468/103474/Frame_53_cskqbx.png',
-    link: '#'
+    link: '#',
+    text: '',
   },
-  humus: {
+  {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612363468/103474/Group_7423494_gby9tr.png',
-    link: '#'
+    link: '#',
+    text: '',
   },
-  halva: {
+  {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612363467/103474/cook-with-eggs-rustic-kitchen_1_vsbt3d.png',
-    link: '#'
+    link: '#',
+    text: '',
   },
-  cakes: {
+  {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612363468/103474/Group_7423495_yggpod.png',
-    link: '#'
+    link: '#',
+    text: '',
   },
-  other: {
+  {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612363467/103474/Frame_39_ff00mt.png',
-    link: '#'
+    link: '#',
+    text: '',
   },
-
-}
+]
 
 export default categories
