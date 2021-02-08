8dc31838f799d96aab79befe2d9e460a0222a534
Coospir <eugene.starodubov@gmail.com>
2021-02-08 14:20:12 +0300

    updated

---
+++ shops/102648/components/Products/Products.js
@@ -0,0 +1,3 @@
+import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
+import '../../fixes/js/ButtonContainerInProductCard/ButtonContainerInProductCard.js'
+import '../../fixes/js/RemoveBlur/RemoveBlur.js'
