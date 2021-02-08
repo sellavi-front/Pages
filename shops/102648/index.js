cd9c38b88d419998db59798ccca6883ef0e0ee12
Coospir <eugene.starodubov@gmail.com>
2021-02-08 17:40:34 +0300

    updated

--- shops/102648/index.js
+++ shops/102648/index.js
@@ -8,6 +8,14 @@ import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
 import '../../fixes/js/ButtonContainerInProductCard/ButtonContainerInProductCard.js'
 import '../../fixes/js/RemoveBlur/RemoveBlur.js'
 
+import pages from "./config/conditions.js"
+
+if(pages.main) {
+  document.querySelector("#banner0 > div > div > div > div > picture > source").setAttribute('srcset', 'https://cdn.sellavi.com/image/upload/v1612794865/ru/clients/102648/78a8f4a45eb55e0eb3fefadab7dca827462e58a6.webp')
+}
+
+
+
 
 
 
