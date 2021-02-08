f7077b71be35d1f2336aebff3445511feb07a0b9
Coospir <eugene.starodubov@gmail.com>
2021-02-08 15:48:38 +0300

    updated

--- shops/102648/components/CatalogBtn/CatalogBtn.js
+++ shops/102648/components/CatalogBtn/CatalogBtn.js
@@ -1,20 +1,18 @@
-window.onload = () => {
-  let oldCatalog = document.querySelector(".mobile-navigation-wrapper")
-  oldCatalog.classList.add("header-catalog")
-  let newCatalog = oldCatalog.cloneNode(true)
-  document.querySelector(".logo").insertAdjacentHTML("afterend", newCatalog.outerHTML)
-  oldCatalog.remove()
+let oldCatalog = document.querySelector(".mobile-navigation-wrapper")
+oldCatalog.classList.add("header-catalog")
+let newCatalog = oldCatalog.cloneNode(true)
+document.querySelector(".logo").insertAdjacentHTML("afterend", newCatalog.outerHTML)
+oldCatalog.remove()
 
-  let headerCatalog = document.querySelector(".header-catalog")
-  let headerCatalogLink = document.querySelector(".header-catalog a")
-  headerCatalogLink.insertAdjacentHTML("beforeend", `<span class="catalog-title">Каталог</span>`)
-  const listt = document.querySelectorAll('.flexy .panel-collapse > ul > li > a');
+let headerCatalog = document.querySelector(".header-catalog")
+let headerCatalogLink = document.querySelector(".header-catalog a")
+headerCatalogLink.insertAdjacentHTML("beforeend", `<span class="catalog-title">Каталог</span>`)
+const listt = document.querySelectorAll('.flexy .panel-collapse > ul > li > a');
 
-  listt.forEach((el) => {
-    const tr = el.textContent;
+listt.forEach((el) => {
+  const tr = el.textContent;
 
-    if(tr.includes(' - ')) {
-      el.textContent = tr.slice(2);
-    }
-  });
-}
+  if(tr.includes(' - ')) {
+    el.textContent = tr.slice(2);
+  }
+});
