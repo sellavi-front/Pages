b2d1f0abef05776bdc657851b80dff3a42adf126
Coospir <eugene.starodubov@gmail.com>
2021-02-08 15:05:56 +0300

    updated

--- shops/102648/components/Products/SecondColumnCategories/SecondColumnCategories.js
+++ shops/102648/components/Products/SecondColumnCategories/SecondColumnCategories.js
@@ -24,5 +24,4 @@ window.onload= () => {
       </div>
     `)
   }
-
 }
--- shops/102648/index.js
+++ shops/102648/index.js
@@ -1,8 +1,8 @@
 import '../../fixes/js/FooterCopy/FooterCopy.js';
 import './utils/accuring/accuring.js'
 import './components/Header/Header.js'
-import './components/Products/Products.js'
 import './components/CatalogBtn/CatalogBtn.js'
+import './components/Products/Products.js'
 
 
 
