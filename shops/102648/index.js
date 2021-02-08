c90aa6dbaf9f4f66b03fbde7155f06f641acbf96
Coospir <eugene.starodubov@gmail.com>
2021-02-08 17:31:23 +0300

    updated

--- shops/102648/index.js
+++ shops/102648/index.js
@@ -1,12 +1,12 @@
-import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
-import '../../fixes/js/ButtonContainerInProductCard/ButtonContainerInProductCard.js'
-import '../../fixes/js/RemoveBlur/RemoveBlur.js'
-
 import '../../fixes/js/FooterCopy/FooterCopy.js';
 import './utils/accuring/accuring.js'
 import './components/Header/Header.js'
 import './components/CatalogBtn/CatalogBtn.js'
 import './components/Products/Products.js'
+
+import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js'
+import '../../fixes/js/ButtonContainerInProductCard/ButtonContainerInProductCard.js'
+import '../../fixes/js/RemoveBlur/RemoveBlur.js'
 
 
 
