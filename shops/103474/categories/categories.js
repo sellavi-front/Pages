83ce65d3f979713376aaafa544646c6d4d8c346f
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-08 15:29:47 +0300

    styles

--- shops/103474/categories/categories.js
+++ shops/103474/categories/categories.js
@@ -1,58 +1,63 @@
 const categories = [
   {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_19_umfjsx.png',
     link: '#',
-    text: 'Молоко',
+    text: 'Кофе и чай',
+  },
+  {
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_50_bdvd2i.png',
+    link: '#',
+    text: 'Мясная продукция',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
     link: '#',
-    text: 'Молоко',
+    text: 'Хумусы и салаты',
   },
   {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_52_z3t2eg.png',
     link: '#',
-    text: 'Молоко',
+    text: 'Замороженная продукция',
   },
   {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_38_mcotzo.png',
     link: '#',
-    text: 'Молоко',
+    text: 'Снеки и сухие завтраки',
   },
   {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_49_aadszq.png',
     link: '#',
-    text: 'Молоко',
-  },
-  {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
-    link: '#',
-    text: 'Молоко',
+    text: 'Выпечка',
   },
  {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_53_yozxol.png',
     link: '#',
-    text: 'Молоко',
+    text: 'Сладости',
   },
   {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_56_tfxnce.png',
     link: '#',
-    text: 'Молоко',
+    text: 'Молочная продукция',
   },
   {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784188/103474/Frame_42_pobk7l.png',
     link: '#',
-    text: 'Молоко',
+    text: 'Все для выпечки',
   },
   {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784188/103474/Frame_49_1_md8fwy.png',
     link: '#',
-    text: 'Молоко',
+    text: 'Бакалея',
   },
   {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784188/103474/Frame_44_cntv1n.png',
     link: '#',
-    text: 'Молоко',
+    text: 'Детское питание',
+  },
+  {
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784188/103474/Frame_45_kqos0d.png',
+    link: '#',
+    text: 'Другое',
   },
 ]
 
