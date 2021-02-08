ddc794477f088b611d90a65e2da4087fabeda3a7
Coospir <eugene.starodubov@gmail.com>
2021-02-08 14:37:19 +0300

    updated

--- shops/102648/index.js
+++ shops/102648/index.js
@@ -2,7 +2,6 @@ import '../../fixes/js/FooterCopy/FooterCopy.js';
 import './utils/accuring/accuring.js'
 import './components/Header/Header.js'
 import './components/CatalogBtn/CatalogBtn.js'
-import './components/CatalogBtn/CatalogBtn.js';
 import './components/Products/Products.js';
 
 
