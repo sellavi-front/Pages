029419476dcbb7d8b776047fcec87b5b28f53765
Coospir <eugene.starodubov@gmail.com>
2021-02-08 15:11:19 +0300

    updated

--- shops/102648/components/Products/SecondColumnCategories/SecondColumnCategories.js
+++ shops/102648/components/Products/SecondColumnCategories/SecondColumnCategories.js
@@ -3,18 +3,25 @@ import pages from "../../../config/conditions.js"
 window.onload= () => {
   if(pages.main) {
     console.log('[Making New Columns..]')
+    // Getting categories ...
+    let oldCatalog = document.querySelector(".mobile-navigation-wrapper")
+    let newCatalog = oldCatalog.cloneNode(true)
+
+    // Getting product container ...
+    document.querySelector(".logo").insertAdjacentHTML("afterend", newCatalog.outerHTML)
     let oldProductContainer = document.querySelector("#home > div > div.content-area > section > div.container")
     let newProductContainer = oldProductContainer.cloneNode(true)
-
     let layoutProductContainer = newProductContainer.outerHTML
     oldProductContainer.remove()
 
+    // Creating new structure ...
     document.querySelector('.page-section').insertAdjacentHTML("afterbegin", `
       <div class="container">
         <div class='row'>
             <div class="col">
               <div class="categories__menu">
                   <h1>Categories</h1>
+                  ${ newCatalog.outerHTML }
               </div>
             </div>
             <div class="col">
