b3503a08588112b1a60bdf944d26b89eaa13d132
Coospir <eugene.starodubov@gmail.com>
2021-02-08 14:50:26 +0300

    updated

--- shops/102648/components/Products/SecondColumnCategories/SecondColumnCategories.js
+++ shops/102648/components/Products/SecondColumnCategories/SecondColumnCategories.js
@@ -2,15 +2,22 @@ import pages from "../../../config/conditions.js"
 
 window.onload= () => {
   if(pages.main) {
+
+    let oldProductContainer = document.querySelector("#home > div > div.content-area > section > div.container")
+    let newProductContainer = oldProductContainer.cloneNode(true)
+    oldProductContainer.remove()
+
     document.querySelector('.page-section').insertAdjacentHTML("afterbegin", `
       <div class='row'>
         <div class="col">
-            <div class="categories__block">
-                <h1>Categories</h1>
+            <div class="container">
+                <div class="categories__menu">
+                    <h1>Categories</h1>
+                </div>
             </div>
         </div>
         <div class="col">
-
+            ${newProductContainer}
         </div>
       </div>
     `)
