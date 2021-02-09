c98be0caf4bbf1056d803e94425dc2d4803ff079
Coospir <eugene.starodubov@gmail.com>
2021-02-09 14:15:17 +0300

    updated

--- shops/103506/index.js
+++ shops/103506/index.js
@@ -1,6 +1,5 @@
 import "./docs/requisites.js"
-import "../../templates/TemplateTypeTwo/TemplateTypeTwo.js"
+
 import "../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
 import "../../fixes/js/ButtonContainerInProductCard/ButtonContainerInProductCard.js"
 import "../../fixes/js/RemoveBlur/RemoveBlur.js"
-import "../../fixes/fixes.js"
