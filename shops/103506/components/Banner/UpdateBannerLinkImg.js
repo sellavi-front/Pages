40d8c3d59a820d4c175ec4b9758ea583e67769b1
Coospir <eugene.starodubov@gmail.com>
2021-02-09 18:28:02 +0300

    updated

--- shops/103506/components/Banner/UpdateBannerLinkImg.js
+++ shops/103506/components/Banner/UpdateBannerLinkImg.js
@@ -2,5 +2,6 @@ let source = document.querySelectorAll(".owl-carousel > div > div > div > div > 
 source.forEach(img => {
   let link = img.getAttribute('srcset')
   let newLink = link.replace('/c_pad,w_1900/', '')
+  console.log(newLink)
   img.setAttribute('srcset', newLink)
 })
