17ffb3cfdc4e090ce751960e1eb37f586df2c2fb
Coospir <eugene.starodubov@gmail.com>
2021-02-09 17:56:42 +0300

    updated

--- shops/103506/components/Products/Products.js
+++ shops/103506/components/Products/Products.js
@@ -1,1 +1,4 @@
+import "../../../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
+import "../../../../fixes/js/ButtonContainerInProductCard/ButtonContainerInProductCard.js"
+import "../../../../fixes/js/RemoveBlur/RemoveBlur.js"
 import './ButtonsGroup/UpdateButtonGroup.js'
--- shops/103506/index.js
+++ shops/103506/index.js
@@ -1,8 +1,5 @@
 import "./docs/requisites.js"
 
-import "../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
-import "../../fixes/js/ButtonContainerInProductCard/ButtonContainerInProductCard.js"
-import "../../fixes/js/RemoveBlur/RemoveBlur.js"
 
 import './components/Products/Products.js'
 import "../../fixes/js/FooterCopy/FooterCopy.js"
