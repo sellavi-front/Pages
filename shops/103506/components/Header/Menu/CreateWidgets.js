41f980bf3614ddab72a13d6ef9952d6448f11a0a
Coospir <eugene.starodubov@gmail.com>
2021-02-10 11:09:27 +0300

    updated

--- shops/103506/components/Header/Menu/CreateWidgets.js
+++ shops/103506/components/Header/Menu/CreateWidgets.js
@@ -4,7 +4,6 @@ let headerContainer = document.querySelector("body.ltr > div > header > div.head
 
 // Getting old elements
 let oldHeaderCart = document.querySelector(".header-cart")
-let oldSearchBlackScreen = document.querySelector(".search_black_screen")
 let oldHeaderSearch = document.querySelector(".header-search")
 
 // Deleting words from cart
@@ -12,7 +11,6 @@ document.querySelector("#cart > span.cart-norm-qty.d-none.d-lg-inline-block").re
 
 // Creating copy of widgets
 let newHeaderCart = oldHeaderCart.cloneNode(true)
-let newSearchBlackScreen = oldSearchBlackScreen.cloneNode(true)
 let newHeaderSearch = oldHeaderSearch.cloneNode(true)
 let wishList = `
     <a
