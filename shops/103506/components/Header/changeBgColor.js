4cbdee1214af195c5257f239e4e32f44f1629907
Coospir <eugene.starodubov@gmail.com>
2021-02-10 17:06:17 +0300

    updated

--- shops/103506/components/Header/changeBgColor.js
+++ shops/103506/components/Header/changeBgColor.js
@@ -1,8 +1,10 @@
 import Paths from "../../path/paths.js"
 
 const headerWrapper = document.querySelector(".header-wrapper")
+const burger = document.querySelector(".mobile-navigation-wrapper")
 if ( !Paths.home ) {
   console.log(1)
   headerWrapper.classList.add('colored')
+  burger.classList.add('colored')
 }
 
