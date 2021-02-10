1eaed53939d5688e741676132c8f66ffe62c2381
Coospir <eugene.starodubov@gmail.com>
2021-02-10 10:07:42 +0300

    updated

--- shops/103506/components/Header/Header.js
+++ shops/103506/components/Header/Header.js
@@ -1,4 +1,22 @@
 //import './../../../../components/HeaderTypeTwo/HeaderTypeTwo.js'
 
-let headerWidgets = document.querySelector(".header-wrapper>div").insertAdjacentHTML("beforeend", "<div class='header_widgets'></div>")
-console.log(headerWidgets)
+const headerWidgets = document.querySelector(".header-wrapper>div").insertAdjacentHTML("beforeend", "<div class='header_widgets'></div>")
+const headerContainer = document.querySelector('.header-wrapper>div');
+const cartI = document.querySelector('body .header-cart i');
+const wishList = `
+    <a
+      id="wishlist"
+      class="position-relative"
+      href="https://${location.host}/index.php?route=account/wishlist"
+      role="link"
+    >
+      <i class="far fa-heart"></i>
+    </a>
+`;
+
+headerContainer.insertAdjacentHTML(
+  'beforeend',
+  `<div class="header_widgets">${wishList}${cartI.outerHTML}</div>`,
+);
+/*
+cart.remove();*/
