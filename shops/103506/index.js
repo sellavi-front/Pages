561bfef3d31b97cba52b3d67813b9d93cacc2fd3
Coospir <eugene.starodubov@gmail.com>
2021-02-10 09:55:48 +0300

    updated

--- shops/103506/index.js
+++ shops/103506/index.js
@@ -1,4 +1,5 @@
 import "./docs/requisites.js"
 import './components/Banner/UpdateBannerLinkImg.js'
+import './components/Header/Header.js'
 import './components/Products/Products.js'
 import './components/Footer/Footer.js'
