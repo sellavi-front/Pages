10625b6c9fc43ff6901f6ce81c747193c01c684f
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-10 22:43:12 +0300

    styles

--- shops/103474/utils/index.js
+++ shops/103474/utils/index.js
@@ -89,3 +89,9 @@ if (location.href.includes('/checkout')) {
 }
 
 
+const logoSellavi = document.querySelector("#sellavi_logo > img");
+
+if (logoSellavi) {
+  logoSellavi.setAttribute('src', 'https://res.cloudinary.com/depgheppz/image/upload/v1612986109/Frame_4_l3scow.png');
+}
+
