5c739b2fb838b386787b6ba32a1e5bef731bd7b6
Coospir <eugene.starodubov@gmail.com>
2021-02-10 14:08:38 +0300

    updated

--- shops/103506/components/Header/changeBgColor.js
+++ shops/103506/components/Header/changeBgColor.js
@@ -4,5 +4,5 @@ const headerWrapper = document.querySelector(".header-wrapper")
 if ( !Paths.home ) {
   console.log(1)
   headerWrapper.classList.add('colored')
-  headerWrapper.style.background = '#eed6ae'
+  document.querySelector(".header-wrapper.colored").style.background = '#eed6ae'
 }
