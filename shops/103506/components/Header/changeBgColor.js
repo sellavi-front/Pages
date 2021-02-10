2b547036bd9b6809264ae0d86a43846aa51b246f
Coospir <eugene.starodubov@gmail.com>
2021-02-10 17:38:19 +0300

    updated

--- shops/103506/components/Header/changeBgColor.js
+++ shops/103506/components/Header/changeBgColor.js
@@ -14,5 +14,6 @@ if ( !Paths.home ) {
   headerWidgets.forEach(item => {
     item.classList.add('colored')
   })
+  document.querySelector("body.ltr> div > header > div.header-wrapper > div.container-fluid.d-flex > div.logo > a > img").setAttribute("src", 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612967822/sellavi/103506/Group_277_c28c6u.png')
 }
 
