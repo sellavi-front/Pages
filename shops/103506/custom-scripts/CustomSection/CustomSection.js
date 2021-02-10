7c1b2744fc6e3c3d120e0ad510a260237bf80c6b
Coospir <eugene.starodubov@gmail.com>
2021-02-10 15:44:16 +0300

    updated

--- shops/103506/custom-scripts/CustomSection/CustomSection.js
+++ shops/103506/custom-scripts/CustomSection/CustomSection.js
@@ -1,6 +1,6 @@
 const customSection = document.querySelector(".custom_section")
-let desktop = `<img src='https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612960805/sellavi/103506/Group_267_euicyq.png' alt='cacao'/>`
-let mobile = `<img src='https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612960802/sellavi/103506/Component_3_v9oqjs.png' alt='cacao'/>`
+let desktop = `<div class='container d-flex'><img class='d-features' src='https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612960805/sellavi/103506/Group_267_euicyq.png' alt='cacao'/></div>`
+let mobile = `<div class='container d-flex'><img class='m-features' src='https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612960802/sellavi/103506/Component_3_v9oqjs.png' alt='cacao'/></div>`
 
 if( screen.width > 480 ) {
   customSection.insertAdjacentHTML("afterbegin", desktop)
