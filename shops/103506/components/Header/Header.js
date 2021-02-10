962515d418a5588673ffa2470784225c92ea50b1
Coospir <eugene.starodubov@gmail.com>
2021-02-10 10:01:09 +0300

    updated

--- shops/103506/components/Header/Header.js
+++ shops/103506/components/Header/Header.js
@@ -1,1 +1,4 @@
-import './../../../../components/HeaderTypeTwo/HeaderTypeTwo.js'
+//import './../../../../components/HeaderTypeTwo/HeaderTypeTwo.js'
+
+let headerWidgets = document.querySelector(".header-wrapper>div").insertAdjacentHTML("beforeend", "<div class='header_widgets'></div>")
+console.log(headerWidgets)
