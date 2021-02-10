fafdf62a4b607a5a251d89f43c745273a84784f7
Coospir <eugene.starodubov@gmail.com>
2021-02-10 12:07:12 +0300

    updated

--- shops/103506/components/Header/Menu/CreateWidgets.js
+++ shops/103506/components/Header/Menu/CreateWidgets.js
@@ -1,76 +1,4 @@
-import CreateAddContainer from '../../../../../modules/CreateAddContainer/CreateAddContainer.js';
-import DeleteClasses from '../../../../../modules/DeleteClasses/DeleteClasses.js';
-import { collapseShow } from '../../../../../components/HeaderTypeTwo/scripts/collapseShow.js';
-
-document.querySelector("body.ltr > div > header > div.header-wrapper > div.container-fluid").insertAdjacentHTML("beforeend", `<div class='header_widgets'></div>`)
-const headerWidgets = document.querySelector('.header_widgets');
-const headerContainer = document.querySelector('.header-wrapper>div');
-//const headerLoginText = document.querySelector('.header_login>.d-none.d-lg-block');
-const navigationElements = document.querySelectorAll('.navigation .sf-menu li > a');
-
-const links = document.querySelectorAll('.mobile_side_nav_menu>.flexy>a');
-
-const wishList = `
-    <a
-      id="wishlist"
-      class="position-relative"
-      href="https://${location.host}/index.php?route=account/wishlist"
-      role="link"
-    >
-      <i class="far fa-heart"></i>
-    </a>
-`;
-
-if (document.querySelector('.wrapper')) {
-  const selectors = [];
-  const cartI = document.querySelector('body .header-cart i');
-
-  headerContainer.classList.add('container');
-  headerContainer.classList.remove('container-fluid');
-
-  cartI.classList.add('far');
-  cartI.classList.remove('fal');
-
-  //headerLoginText.remove();
-
-  // Add underline to nav elem for open page
-  navigationElements.forEach(elem => {
-    if (elem.getAttribute('href') == decodeURI(location.href) || location.pathname == elem.getAttribute('href')) {
-      elem.style.cssText = 'border-bottom: 2px solid;'
-    }
-  })
-
-  // END add underline to nav elem for open page
-
-  if (headerWidgets) {
-    const newCont = new CreateAddContainer(headerContainer, 'beforeend', headerWidgets.outerHTML);
-    newCont.render();
-
-    headerWidgets.remove();
-    collapseShow(links);
-    document
-      .querySelector('.header_widgets')
-      .insertAdjacentHTML('afterbegin', `<div class="header_wishlist">${wishList}</div>`);
-
-    //const headerLoginIco = document.querySelectorAll('.header_login>.d-none.d-sm-block.d-lg-none');
-
-    //headerLoginIco[0].classList.forEach((selector) => selectors.push(selector));
-
-    //const deleteSelectors = new DeleteClasses(headerLoginIco, selectors);
-    //deleteSelectors.findAndDelete();
-  } else {
-    headerContainer.insertAdjacentHTML(
-      'beforeend',
-      `<div class="header_widgets">${wishList}${cartI.outerHTML}</div>`,
-    );
-    cartI.remove();
-  }
-}
-
-
-
-
-/*let headerContainer = document.querySelector("body.ltr > div > header > div.header-wrapper > div.container-fluid > div.logo")
+let headerContainer = document.querySelector("body.ltr > div > header > div.header-wrapper > div.container-fluid > div.logo")
 
 // Getting old elements
 let oldHeaderCart = document.querySelector(".header-cart")
@@ -86,10 +14,21 @@ let wishList = `
     <a
       id="wishlist"
       class="position-relative"
-      href="https://${location.host}/index.php?route=account/wishlist"
+      href="https://${location.host}/index.php?route=login"
       role="link"
     >
-      <i class="far fa-heart"></i>
+      <i class="far fa-heart text-white"></i>
+    </a>
+`;
+
+let login = `
+    <a
+      id="login"
+      class="position-relative"
+      href="https://${location.host}/login/"
+      role="link"
+    >
+      <i class="fas fa-user text-white"></i>
     </a>
 `;
 
@@ -97,11 +36,13 @@ let widgetsBlock = `<div class='widgets_block'>
     ${newHeaderSearch.outerHTML}
     ${newHeaderCart.outerHTML}
     ${wishList}
+    ${login}
 </div>`
 
 headerContainer.insertAdjacentHTML("beforeend", widgetsBlock)
+
 oldHeaderSearch.remove()
-oldHeaderCart.remove()*/
+oldHeaderCart.remove()
 
 
 
