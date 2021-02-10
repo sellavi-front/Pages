5edcab1eedbbd15bbcdc4312cd9dabfe83215cac
Coospir <eugene.starodubov@gmail.com>
2021-02-10 18:17:24 +0300

    updated

--- shops/103506/components/Header/Menu/CreateWidgets.js
+++ shops/103506/components/Header/Menu/CreateWidgets.js
@@ -5,14 +5,14 @@ if( !Paths.checkout ) {
   document.querySelector("#cart > span").classList.remove("d-lg-none")
   let headerContainer = document.querySelector("body.ltr > div > header > div.header-wrapper > div.container-fluid.d-flex")
 
-// Getting old elements
+  // Getting old elements
   let oldHeaderCart = document.querySelector(".header-cart")
   let oldHeaderSearch = document.querySelector(".header-search")
 
-// Deleting words from cart
+  // Deleting words from cart
   document.querySelector("#cart > span.cart-norm-qty.d-none.d-lg-inline-block").remove()
 
-// Creating copy of widgets
+  // Creating copy of widgets
   let newHeaderCart = oldHeaderCart.cloneNode(true)
   let newHeaderSearch = oldHeaderSearch.cloneNode(true)
   let wishList = `
@@ -24,7 +24,7 @@ if( !Paths.checkout ) {
     >
       <i class="far fa-heart"></i>
     </a>
-`;
+    `;
 
   let login = `
     <a
@@ -35,21 +35,18 @@ if( !Paths.checkout ) {
     >
       <i class="fas fa-user"></i>
     </a>
-`;
+    `;
 
   let widgetsBlock = `<div class='widgets_block'>
-    ${newHeaderSearch.outerHTML}
-    ${wishList}
-    ${login}
-    ${newHeaderCart.outerHTML}
-</div>`
+      ${newHeaderSearch.outerHTML}
+      ${wishList}
+      ${login}
+      ${newHeaderCart.outerHTML}
+    </div>`
 
   headerContainer.insertAdjacentHTML("beforeend", widgetsBlock)
-
   oldHeaderSearch.remove()
   oldHeaderCart.remove()
-
-
 }
 
 
