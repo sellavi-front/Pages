48af2c04efbbc52bf4e36ee49e8a76804ba2e0d7
Coospir <eugene.starodubov@gmail.com>
2021-02-10 14:08:03 +0300

    updated

--- shops/103506/components/Header/changeBgColor.js
+++ shops/103506/components/Header/changeBgColor.js
@@ -2,6 +2,7 @@ import Paths from "../../path/paths.js"
 
 const headerWrapper = document.querySelector(".header-wrapper")
 if ( !Paths.home ) {
+  console.log(1)
   headerWrapper.classList.add('colored')
   headerWrapper.style.background = '#eed6ae'
 }
