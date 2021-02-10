3555c698b4b9f9cafc9c10373722dbcb915ee4db
Coospir <eugene.starodubov@gmail.com>
2021-02-10 10:42:36 +0300

    updated

--- shops/103506/components/Header/Header.js
+++ shops/103506/components/Header/Header.js
@@ -1,20 +1,2 @@
-//import './../../../../components/HeaderTypeTwo/HeaderTypeTwo.js'
+import './../../../../components/HeaderTypeOne/HeaderTypeOne.js'
 
-const headerContainer = document.querySelector('.common-home > div > header > div.header-wrapper > div.container-fluid')
-const wishList = `
-    <a
-      id="wishlist"
-      class="position-relative"
-      href="https://${location.host}/index.php?route=account/wishlist"
-      role="link"
-    >
-      <i class="far fa-heart"></i>
-    </a>
-`;
-
-/*headerContainer.insertAdjacentHTML(
-  'beforeend',
-  `<div class="header_widgets">${wishList}</div>`,
-);*/
-/*
-cart.remove();*/
