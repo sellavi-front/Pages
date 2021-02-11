f9e85fb59f25d2c415a71e246b85bc591a40175e
Coospir <eugene.starodubov@gmail.com>
2021-02-11 13:03:16 +0300

    updated 1030506

--- shops/103506/components/Header/Menu/Logo/LogoPositionChanger.js
+++ shops/103506/components/Header/Menu/Logo/LogoPositionChanger.js
@@ -1,6 +1,6 @@
 import Paths from "../../../../path/paths.js"
 
-if( !Paths.main ) {
+if( !(location.pathname === '/') ) {
   console.log(1)
   document.querySelector(".logo").classList.add('logo-edited')
 } else document.querySelector(".logo").classList.remove('logo-edited')
