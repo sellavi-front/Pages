ae5c3b78276490e84d7e86a982277585ef45c7ef
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-11 15:52:15 +0300

    styles

--- shops/102244/utils/contactData.js
+++ shops/102244/utils/contactData.js
@@ -15,7 +15,7 @@ const contactData = {
     },
   ],
   map: `
-  <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A732095fb7e4e29e95ef232fcc1898d689cdf61a2b239ada1ed8e5f10714023ca&amp;source=constructor" frameborder="0"></iframe>
+  <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A5fcef4af89c2758de51fc69bbdc2019c7e2f795b2e6f0fdbc15fac110a2ac9a7&amp;source=constructor" width="500" height="400" frameborder="0"></iframe>
   `,
 };
 
