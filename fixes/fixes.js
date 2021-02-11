8877fbb63179a74b53be5a0ad4c93f28eed40a42
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-11 18:52:21 +0300

    styles

--- fixes/fixes.js
+++ fixes/fixes.js
@@ -1,5 +1,5 @@
 import ChangeAgreement from './js/ChangeAgreement/ChangeAgreement.js';
-import ChangeErrorMessage from './js/ChangeErrorEessage/ChangeErrorMessage.js';
+// import ChangeErrorMessage from './js/ChangeErrorEessage/ChangeErrorMessage.js';
 import TranslateAboutUs from './js/TranslateAboutUs/TranslateAboutUs.js';
 import FooterCopy from './js/FooterCopy/FooterCopy.js';
 // import HideLoading from './HideLoading/HideLoading.js';
