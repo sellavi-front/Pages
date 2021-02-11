5f164db08ad4622b097abc169603db6764eec12e
Coospir <eugene.starodubov@gmail.com>
2021-02-11 12:24:02 +0300

    updated 1030506

--- shops/103506/components/Header/Menu/Logo/LogoPositionChanger.js
+++ shops/103506/components/Header/Menu/Logo/LogoPositionChanger.js
@@ -1,4 +1,4 @@
-import Paths from "../../path/paths.js"
+import Paths from "../../../../path/paths.js"
 
 if( !Paths.main ) {
   document.querySelector("body.ltr > div > header > div.header-wrapper.colored > div.container-fluid.d-flex > div.logo").classList.add('edited')
