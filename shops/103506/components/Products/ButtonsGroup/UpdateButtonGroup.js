78c35ef11a2005f52c338eee90ceada59c8dbc7d
Coospir <eugene.starodubov@gmail.com>
2021-02-11 14:14:30 +0300

    updated 1030506

--- shops/103506/components/Products/ButtonsGroup/UpdateButtonGroup.js
+++ shops/103506/components/Products/ButtonsGroup/UpdateButtonGroup.js
@@ -1,4 +1,4 @@
 let productBtnsMain = document.querySelectorAll(".content-area>.page-section>.container>.products>.product-item>.thumbnail>.price_actions")
-let productBtnsCategory = document.querySelector(".page-section.with-sidebar>div>div>.content>div>.products>div>.thumbnail>.price_actions")
+let productBtnsCategory = document.querySelectorAll(".page-section.with-sidebar>div>div>.content>div>.products>div>.thumbnail>.price_actions")
 productBtnsMain.forEach(btn => { btn.classList.remove('d-flex', 'justify-content-between') })
 productBtnsCategory.forEach(btn => { btn.classList.remove('d-flex', 'justify-content-between') })
