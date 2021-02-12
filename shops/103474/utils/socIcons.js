4ffbf7566e360185d77aeea4490f2f8f2fb5847e
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-12 17:39:34 +0300

    styles

--- shops/103474/utils/socIcons.js
+++ shops/103474/utils/socIcons.js
@@ -1,13 +1,13 @@
 const socIcons = [
 
   {
-    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1613051505/icons8-instagram_1_wysuzg.png',
+    src: 'https://res.cloudinary.com/gz-company/image/upload/v1606295327/Miru/assets/icons/icons8-instagram_1_rw7nv3.svg',
     link: 'https://www.instagram.com/kosherbrodsky/',
   },
   {
-    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1613051505/icons8-facebook-f_1_xqg5q1.png',
+    src: 'https://res.cloudinary.com/depgheppz/image/upload/v1613140765/icons8-facebook-f_1_1_vqui95.svg',
     link: 'https://www.facebook.com/kosher.brodsky'
-  }
+  },
 
 ];
 
