db853297263631619979d71e7d2aa9101bdd1af4
m4medbekov <m4medbekov@gmail.com>
2021-02-15 17:48:37 +0300

    fix

--- shops/103753/index.js
+++ shops/103753/index.js
@@ -240,12 +240,13 @@ if (document.querySelector('.common-home')) {
   document.querySelector('body').insertAdjacentHTML('afterbegin', '<div><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612528588/455_3_f62gqq.png"></div>');
 }
 
-  
-if (document.querySelector('.custom_banner')) {
-  document.querySelector('.custom_banner').insertAdjacentHTML('afterbegin', `
-<div class="container">
-    <video width="1100" autoplay loop>
-        <source src="https://res.cloudinary.com/richelle/video/upload/v1613390750/VID-20210214-WA0004_kxxz4g.mp4">Sorry, your browser doesn't support embedded videos.
-    </video>
-</div>`);
-} 
+window.onload = () => {
+  if (document.querySelector('.custom_banner')) {
+    document.querySelector('.custom_banner').insertAdjacentHTML('afterbegin', `
+  <div class="container">
+      <video width="1100" autoplay loop>
+          <source src="https://res.cloudinary.com/richelle/video/upload/v1613390750/VID-20210214-WA0004_kxxz4g.mp4">Sorry, your browser doesn't support embedded videos.
+      </video>
+  </div>`);
+  } 
+}
