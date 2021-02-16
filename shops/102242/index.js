9bb3d7b38e53f0c695a9ae285d2645eaaccc9442
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-16 12:28:21 +0300

    styles

--- shops/102242/index.js
+++ shops/102242/index.js
@@ -5,7 +5,7 @@
 import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js';
 import '../../fixes/js/ChangeErrorEessage/ChangeErrorMessage.js';
 // import '../../fixes/js/ChangeAgreement/ChangeAgreement.js';
-// import icons from './utils/icons/icons.js';
+import icons from './utils/icons/icons.js';
 
 import '../../components/PictureCategories/PictureCategories.js';
 
@@ -135,10 +135,10 @@ if (document.querySelector('.wrapper')) {
     like: document.querySelector('body .header_widgets .header_wishlist i'),
   };
 
-  // headerIcons.cart.classList.remove('fa-shopping-cart', 'far');
-  // headerIcons.cart.insertAdjacentHTML('afterbegin', icons.cart);
-  // headerIcons.like.classList.remove('far', 'fa-heart');
-  // headerIcons.like.insertAdjacentHTML('afterbegin', icons.like);
+  headerIcons.cart.classList.remove('fa-shopping-cart', 'far');
+  headerIcons.cart.insertAdjacentHTML('afterbegin', icons.cart);
+  headerIcons.like.classList.remove('far', 'fa-heart');
+  headerIcons.like.insertAdjacentHTML('afterbegin', icons.like);
 
   document
     .querySelectorAll('.page-section.homefeatured_category > div > div > div')
