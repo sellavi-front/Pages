bb5d9ac70c68389a9de7df0207ebc56a3a530a6a
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-16 17:32:29 +0300

    styles

--- shops/103474/index.js
+++ shops/103474/index.js
@@ -2,6 +2,5 @@ import '../../fixes/js/FooterCopy/FooterCopy.js'
 
 import './utils/index.js';
 import '../../fixes/js/ButtonContainerInProductCard/ButtonContainerInProductCard.js'
-import '../../fixes/js/RemoveBlur/RemoveBlur.js'
 import '../../fixes/js/RemoveBlurAndAddBtn/RemoveBlurAndAddBtn.js'
 import './utils/functions/iconCurrency.js'
