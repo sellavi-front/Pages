37157de1112d45952f09e036ad7da1ff7db127d7
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-17 17:59:11 +0300

    styles

--- fixes/js/Alert/Alert.js
+++ fixes/js/Alert/Alert.js
@@ -1,15 +1,16 @@
-if (document.querySelector('.common-home')) {
-  const btns = document.querySelectorAll('.add_to_wishlist > button');
-  const banner = document.querySelector('.main-slider');
+export default (() => {
+  if (document.querySelector('.common-home')) {
+    const btns = document.querySelectorAll('.add_to_wishlist > button');
+    const banner = document.querySelector('.main-slider');
 
-
-  btns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      setTimeout(() => {
-        const alert = document.querySelector('.alert');
-        banner.insertAdjacentHTML('afterend', alert.outerHTML);
-        alert.remove();
-      }, 800);
+    btns.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        setTimeout(() => {
+          const alert = document.querySelector('.alert');
+          banner.insertAdjacentHTML('afterend', alert.outerHTML);
+          alert.remove();
+        }, 800);
+      });
     });
-  });
-}
+  }
+})();
--- shops/102244/utils/fixes.js
+++ shops/102244/utils/fixes.js
@@ -1,4 +1,4 @@
-import '../../../fixes/js/Alert/Alert.js';
 import '../../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
 import '../../../fixes/js/ChangeAgreement/ChangeAgreement.js'
+import '../../../fixes/js/Alert/Alert.js';
