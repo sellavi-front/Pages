f1c5903f0e3dce9a6a711ec9c9a3a16fb86378da
Coospir <eugene.starodubov@gmail.com>
2021-02-17 15:16:43 +0300

    updated

--- shops/102648/components/Menu/Menu.js
+++ shops/102648/components/Menu/Menu.js
@@ -1,6 +1,8 @@
-let secondaryCategories = document.querySelectorAll(".mobile_side_nav_menu > li > .panel-collapse > .closed > li > a")
-let secondCatArr = Array.from(secondaryCategories)
-for (let i = 0; i < secondCatArr.length; i++) {
-  let c = secondCatArr[i].innerHTML.replace('-', '');
-  secondCatArr[i].innerHTML = c;
+if(document.querySelector(".mobile_side_nav_menu")) {
+  let secondaryCategories = document.querySelectorAll(".mobile_side_nav_menu > li > .panel-collapse > .closed > li > a")
+  let secondCatArr = Array.from(secondaryCategories)
+  for (let i = 0; i < secondCatArr.length; i++) {
+    let c = secondCatArr[i].innerHTML.replace('-', '');
+    secondCatArr[i].innerHTML = c;
+  }
 }
