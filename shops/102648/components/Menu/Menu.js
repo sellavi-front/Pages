31d754ff9bf0f93563a3eb1dbc91a6567ca42120
Coospir <eugene.starodubov@gmail.com>
2021-02-17 14:59:56 +0300

    updated

--- shops/102648/components/Menu/Menu.js
+++ shops/102648/components/Menu/Menu.js
@@ -1,5 +1,6 @@
 let secondaryCategories = document.querySelectorAll(".mobile_side_nav_menu > li > .panel-collapse > .closed > li > a")
 let secondCatArr = Array.from(secondaryCategories)
-secondCatArr.map(item => {
-  console.log(item)
+secondCatArr.map((item, i) => {
+  let newStr = item[i].innerHTML.replace('-', '');
+  item[i].innerHTML = newStr;
 })
