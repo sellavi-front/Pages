f2763a25de9dc655a87fa1a7fb0e3f727511663f
Coospir <eugene.starodubov@gmail.com>
2021-02-18 12:24:24 +0300

    updated

--- shops/requsites/index.js
+++ shops/requsites/index.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import '../../fixes/js/FooterCopy/FooterCopy.js';
-import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
-import pages from './config/conditions.js'
-import './utils/accuring/accuring.js'
-
-=======
 import './utils/fixes.js';
 import './utils/accuring/accuring.js';
->>>>>>> pages/main
+
