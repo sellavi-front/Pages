b0eb9d3aecceee0943a35cd8da5f6b2829aefa81
m4medbekov <m4medbekov@gmail.com>
2021-02-18 15:30:22 +0300

    fix

--- shops/103908/utils/socIconsData.js
+++
@@ -1,8 +0,0 @@
-const socIconsData = [
-  {
-    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612881580/instagram-brands_khnu1f.svg',
-    link: 'https://www.instagram.com/many_artbox/',
-  },
-];
-
-export default socIconsData;
