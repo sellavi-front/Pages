6d179691294022c3f714c048d688363a60c94442
m4medbekov <m4medbekov@gmail.com>
2021-02-18 15:27:01 +0300

    fix

---
+++ shops/103908/utils/socIconsData.js
@@ -0,0 +1,8 @@
+const socIconsData = [
+  {
+    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612881580/instagram-brands_khnu1f.svg',
+    link: 'https://www.instagram.com/many_artbox/',
+  },
+];
+
+export default socIconsData;
