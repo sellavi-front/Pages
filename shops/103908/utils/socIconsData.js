9df6e599a1c4a2352ca6d9dd2e3e364d2544beea
m4medbekov <m4medbekov@gmail.com>
2021-02-19 16:49:19 +0300

    fix

---
+++ shops/103908/utils/socIconsData.js
@@ -0,0 +1,20 @@
+const socIconsData = [
+  {
+    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612881580/instagram-brands_khnu1f.svg',
+    link: 'https://instagram.com/brasletnata.ru?igshid=1kn7dehn4xipy',
+  },
+  {
+    src: 'https://res.cloudinary.com/gz-company/image/upload/v1606295327/Miru/assets/icons/icons8-whatsapp_1_kh2mdl.svg',
+    link: 'https://wa.me/79175751641',
+  },
+  {
+    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613742283/icons8-viber_1_wtdjs4.png',
+    link: 'https://api.whatsapp.com/send?phone=79175751641',
+  },
+  {
+    src: 'https://res.cloudinary.com/gz-company/image/upload/v1608216460/102367/icons8-telegram-app_1_aawuoi.svg',
+    link: 'https://t.me/79175751641',
+  },
+];
+
+export default socIconsData;
