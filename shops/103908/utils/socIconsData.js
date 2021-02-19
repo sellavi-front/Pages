afc4e39995ce0abe2f58f2d45e5d2df39f957b70
m4medbekov <m4medbekov@gmail.com>
2021-02-19 16:56:36 +0300

    fix

--- shops/103908/utils/socIconsData.js
+++ shops/103908/utils/socIconsData.js
@@ -4,7 +4,7 @@ const socIconsData = [
     link: 'https://instagram.com/brasletnata.ru?igshid=1kn7dehn4xipy',
   },
   {
-    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613742760/icons8-%D1%82%D0%B5%D0%BB%D0%B5%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B0-app_1_1_q0l01r.png',
+    src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613742764/icons8-whatsapp_1_e1bbbz.png',
     link: 'https://wa.me/79175751641',
   },
   {
