fe9fd2d7ea736e517c89c7e9ef9f6145c3613502
Ruslan Mamedbekov <m4medbekov@gmail.com>
2021-02-21 17:02:11 +0300

    fix

--- shops/103908/index.js
+++ shops/103908/index.js
@@ -167,7 +167,7 @@ if (document.querySelector(".common-home")) {
 </div>
 <div class="row">
     <div class="col">
-        <a href="https://brasletnata.ru/%D0%B8%D0%BD%D0%B4%D0%B8%D0%B2%D0%B8%D0%B4%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613724622/Group_7423529_ktsnbo.png"></a>
+        <a href="https://brasletnata.ru/%D0%B8%D0%BD%D0%B4%D0%B8%D0%B2%D0%B8%D0%B4%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613916074/Group_7423529_xvgiks.png"></a>
     </div>
 </div>
 </div>`) 
