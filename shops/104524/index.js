6235c178c612beaaf5a43fb6cf03129f3923b355
m4medbekov <m4medbekov@gmail.com>
2021-02-22 12:44:43 +0300

    fix

--- shops/104524/index.js
+++ shops/104524/index.js
@@ -1,158 +1,180 @@
 import Requisites from '../../modules/Requisites/Requisites.js';
-import {requsites, dataContact } from './utils/requsites.js'
+import {
+  requsites,
+  dataContact
+} from './utils/requsites.js'
 import '../../fixes/js/FooterCopy/FooterCopy.js';
 import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
-import '../../templates/TemplateTypeTwo/TemplateTypeTwo.js';
 import '../../fixes/js/ChangeAgreement/ChangeAgreement.js';
 import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
 import SocialIconsTypeOne from '../../components/SocialIconsTypeOne/SocialIconsTypeOne.js'
 import socIconsData from './utils/socIconsData.js';
 
-if (document.querySelector('.common-home')) {
-  let some = `
-  <div>
-  <span>Наш инстаграм</span>
-  <br>
-  <span>Получите эстетическое удовольствие</span>
-</div>
-<br>
-<div class="container">
-  <div class="row">
-    <div class="col">
-      <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612444278/122393926_3332871193463703_632430044848993105_n_ix1ys7.png">
-      <div class="row">
-        <div class="col">
-          <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612880459/WhatsApp_Image_2021-02-01_at_09.59_4_1_ecg0pf.png">
-        </div>
-        <div class="col">
-          <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612880723/icons8-%D1%81%D0%B5%D1%80%D0%B4%D1%86%D0%B5_2_yhzkce.png">
-        </div>
-        <div class="col">
-        855
-        </div>
-      </div>
-    </div>
-    
-    <div class="col">
-      <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612444279/118702000_168735104783993_414454167277788697_n_qzwtwx.png">
-      <div class="row">
-        <div class="col">
-          <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612880459/WhatsApp_Image_2021-02-01_at_09.59_4_1_ecg0pf.png">
-        </div>
-        <div class="col">
-          <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612880723/icons8-%D1%81%D0%B5%D1%80%D0%B4%D1%86%D0%B5_2_yhzkce.png">
-        </div>
-        <div class="col">
-        765
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612444282/121780675_973066553157551_397272728753173198_n_ngqdtq.png">
-      <div class="row">
-        <div class="col">
-          <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612880459/WhatsApp_Image_2021-02-01_at_09.59_4_1_ecg0pf.png">
-        </div>
-        <div class="col">
-          <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612880723/icons8-%D1%81%D0%B5%D1%80%D0%B4%D1%86%D0%B5_2_yhzkce.png">
-        </div>
-        <div class="col">
-        984
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612444284/126316814_398284391223565_2871809762958162410_n_mhfatv.png">
-      <div class="row">
-        <div class="col">
-          <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612880459/WhatsApp_Image_2021-02-01_at_09.59_4_1_ecg0pf.png">
-        </div>
-        <div class="col">
-          <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612880723/icons8-%D1%81%D0%B5%D1%80%D0%B4%D1%86%D0%B5_2_yhzkce.png">
-        </div>
-        <div class="col">
-        763
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
 
 
 
+if (document.querySelector('.wrapper')) {
+  const footerContainer = document.querySelector('.footer-widgets>div>div');
 
+  new SocialIconsTypeOne(footerContainer, 'beforeend', socIconsData).render();
 
+  const socIconsContainer = document.querySelector('.soc-icons>.container');
+  socIconsContainer.insertAdjacentHTML('afterbegin', `<h4 class="widget-title soc-icons__title">Мы в соц.сетях</h4>`);
 
-
-
-
-
-
-
-
-
-
-
-
-
-<br>
-<div>
-  <span>Наши преимущества</span>
-  <br>
-  <span></span>
-</div>
-<br>
-
-<div class="container">
-  <div class="row">
-    <div class="col">
-      <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612445098/Frame_vbx8ci.png">
-      <div class="row">
-        <div class="col">
-        Полностью собственное производство
-        </div>
-      </div>
-    </div> 
-    
-    
-    <div class="col">
-      <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612445098/Frame_1_l0jgl3.png">
-      <div class="row">
-        <div class="col">
-        Только качественные материалы
-        </div>
-      </div>
-    </div>
-    
-    <div class="col">
-      <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612445100/Frame_2_bg7kab.png">
-      <div class="row">
-        <div class="col">
-        Вы можете заказать свой собственный дизайн
-        </div>
-      </div>
-    </div>
-    
-    <div class="col">
-      <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612445101/Frame_3_ykm90w.png">
-      <div class="row">
-        <div class="col">
-        У нас есть гарантия
-        </div>
-      </div>
-    </div>
-  </div>
-</div>`;
-
-document.querySelector('.custom_section').insertAdjacentHTML('afterbegin', some);
+  const socIcons = document.querySelector('.soc-icons');
+  socIcons.classList.add('col-md-3');
 }
 
-if (document.querySelector('.header-wrapper')) {
-  const oldNav = document.querySelector('.nav.sf-menu');
-  const newNav = oldNav.cloneNode(true);
-  document.querySelector('.logo').insertAdjacentHTML('beforebegin', newNav.outerHTML);
-  oldNav.remove();
+if (document.querySelector(".header-wrapper")) {
+  document.querySelector(".header-wrapper").classList.add("container")
+
+  document.querySelector('#search > div.search_field > input').placeholder = ""
+
+  //wish button header
+  document.querySelector("#home > div > header > div.header-wrapper.container > div.container-fluid > div.header-cart.sticky").insertAdjacentHTML("afterbegin", `<div class="header_wishlist">
+    <a id="wishlist" class="position-relative" href="https://brasletnata.ru/index.php?route=account/wishlist" role="link">
+      <i class="far fa-heart"></i>
+    </a>
+</div>`)
 }
+
+//Description block
+if (document.querySelector(".common-home")) {
+
+  if (screen.width < 480) {
+    document.querySelector('#home > div.wrapper.wrapper-closed > div > section').insertAdjacentHTML('afterbegin', `<div class="container">
+    <div class="row">
+          <div class="col-12">
+              <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613662311/20201017_101826_1_nbttwm.png">
+          </div>
+          <div class="col">
+              <div class="row desc-title">Здравствуйте, меня зовут Наталья!</div>
+              <div class="row desc-main">Я создаю волшебные, полезные украшения из натуральных камней! Более десяти лет я работаю с камнями, с их энергиями, настраиваю украшения для исполнения Ваших желаний!️
+  Нам всегда хочется чувствовать помощь, чувствовать, что мы не одни. И в этом случае помогают минералы. А правильно подобранные камни именно для Вас, для Вашей энергетики. Это огромная помощь в исполнении Ваших желаний.
+  Для подбора камней, украшений использую несколько техник, это для того, что бы более точно и быстро Вы достигли желаемого.</div>
+              <div class="row four-system">Я применяю четыре системы:</div>
+              <div class="row systems-one">
+                  <div class="col">- Психоматрица</div>
+                  <div class="col">- Китайскую метафизику</div>
+              </div>
+              <div class="row systems-two">
+                  <div class="col">- Натальная карта</div>
+                  <div class="col">- Просмотр Ваших полей</div>
+              </div>
+              <div class="row desc-footer"><i>При индивидуальном подборе браслета, колье, серёжек Ваши желания и мечты исполнятся значительно быстрее и в большем объёме.</i></div>
+          </div>
+      </div>
+  </div>`)
+  } else if (screen.width > 480) {
+
+    document.querySelector('#home > div.wrapper.wrapper-closed > div > section').insertAdjacentHTML('afterbegin', `<div class="container">
+  <div class="row">
+        <div class="col">
+            <img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613662311/20201017_101826_1_nbttwm.png">
+        </div>
+        <div class="col">
+            <div class="row desc-title">Здравствуйте, меня зовут Наталья!</div>
+            <div class="row desc-main">Я создаю волшебные, полезные украшения из натуральных камней! Более десяти лет я работаю с камнями, с их энергиями, настраиваю украшения для исполнения Ваших желаний!️
+Нам всегда хочется чувствовать помощь, чувствовать, что мы не одни. И в этом случае помогают минералы. А правильно подобранные камни именно для Вас, для Вашей энергетики. Это огромная помощь в исполнении Ваших желаний.
+Для подбора камней, украшений использую несколько техник, это для того, что бы более точно и быстро Вы достигли желаемого.</div>
+            <div class="row four-system">Я применяю четыре системы:</div>
+            <div class="row systems-one">
+                <div class="col">- Психоматрица</div>
+                <div class="col">- Китайскую метафизику</div>
+            </div>
+            <div class="row systems-two">
+                <div class="col">- Натальная карта</div>
+                <div class="col">- Просмотр Ваших полей</div>
+            </div>
+            <div class="row desc-footer"><i>При индивидуальном подборе браслета, колье, серёжек Ваши желания и мечты исполнятся значительно быстрее и в большем объёме.</i></div>
+        </div>
+    </div>
+</div>`)
+  }
+
+  if (screen.width > 1024) {
+    document.querySelector('.custom_section').insertAdjacentHTML('afterbegin', `<div class="container">
+    <div class="container"><span> Категории </span></div>  
+
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+                <a href="https://brasletnata.ru/%D0%B1%D1%80%D0%B0%D1%81%D0%BB%D0%B5%D1%82%D1%8B/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613724622/Component_4_3_veqqad.png"></a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a href="https://brasletnata.ru/%D1%81%D0%B5%D1%80%D1%8C%D0%B3%D0%B8/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613724622/Component_5_3_bzhesh.png"></a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a href="https://brasletnata.ru/%D0%BA%D0%BE%D0%BB%D1%8C%D0%B5/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613724622/Component_6_2_deqcxi.png"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+                <a href="https://brasletnata.ru/%D0%B1%D1%80%D0%B0%D1%81%D0%BB%D0%B5%D1%82%D1%8B/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613724623/Group_7423524_q1hsbd.png"></a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a href="https://brasletnata.ru/%D0%B1%D1%80%D0%B0%D1%81%D0%BB%D0%B5%D1%82%D1%8B/%D0%B0%D0%BD%D0%BA%D0%BB%D0%B5%D1%82%D1%8B-%D0%B1%D1%80%D0%B0%D1%81%D0%BB%D0%B5%D1%82%D1%8B-%D0%BD%D0%B0-%D0%BD%D0%BE%D0%B3%D1%83/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613724622/Group_7423525_zcmhwg.png"></a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a href="https://brasletnata.ru/%D0%BA%D0%BE%D0%BB%D1%8C%D1%86%D0%B0/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613724622/Group_7423526_vkn0fd.png"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+                <a href="https://brasletnata.ru/%D0%B1%D1%80%D0%B5%D0%BB%D0%BE%D0%BA%D0%B8/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613724622/Group_7423527_z4lsvi.png"></a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a href="https://brasletnata.ru/%D0%B1%D1%80%D0%BE%D1%88%D0%B8/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613724622/Group_7423528_inh6xw.png"></a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a href="https://brasletnata.ru/%D0%B8%D0%BD%D0%B4%D0%B8%D0%B2%D0%B8%D0%B4%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613724622/Group_7423529_ktsnbo.png"></a>
+            </div>
+        </div>
+    </div>`)
+  } else if (screen.width < 480) {
+    document.querySelector('.custom_section').insertAdjacentHTML('afterbegin', `<div class="container">
+    <div class="container"><span> Категории </span></div>  
+
+    <div class="row">
+    <div class="col-sm-6">
+        <a href="https://brasletnata.ru/%D0%B1%D1%80%D0%B0%D1%81%D0%BB%D0%B5%D1%82%D1%8B/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613982785/Group_7423521_1_kvjrjw.png"></a>
+    </div>
+    <div class="col-sm-6">
+        <a href="https://brasletnata.ru/%D1%81%D0%B5%D1%80%D1%8C%D0%B3%D0%B8/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613982750/Group_7423522_bncemm.png"></a>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-6">
+        <a href="https://brasletnata.ru/%D0%BA%D0%BE%D0%BB%D1%8C%D0%B5/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613982719/Group_7423523_lhygac.png"></a>
+    </div>
+    <div class="col-sm-6">
+        <a href="https://brasletnata.ru/%D0%B1%D1%80%D0%B0%D1%81%D0%BB%D0%B5%D1%82%D1%8B/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613982638/Group_7423524_1_xasley.png"></a>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-6">
+        <a href="https://brasletnata.ru/%D0%B1%D1%80%D0%B0%D1%81%D0%BB%D0%B5%D1%82%D1%8B/%D0%B0%D0%BD%D0%BA%D0%BB%D0%B5%D1%82%D1%8B-%D0%B1%D1%80%D0%B0%D1%81%D0%BB%D0%B5%D1%82%D1%8B-%D0%BD%D0%B0-%D0%BD%D0%BE%D0%B3%D1%83/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613982414/Group_7423525_1_lgzmro.png"></a>
+    </div>
+    <div class="col-sm-6">
+        <a href="https://brasletnata.ru/%D0%BA%D0%BE%D0%BB%D1%8C%D1%86%D0%B0/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613982413/Group_7423526_1_wirmff.png"></a>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-6">
+        <a href="https://brasletnata.ru/%D0%B1%D1%80%D0%B5%D0%BB%D0%BE%D0%BA%D0%B8/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613982333/Group_7423527_1_dq86ys.png"></a>
+    </div>
+    <div class="col-sm-6">
+        <a href="https://brasletnata.ru/%D0%B1%D1%80%D0%BE%D1%88%D0%B8/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613982335/Group_7423528_1_zf41lj.png"></a>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        <a href="https://brasletnata.ru/%D0%B8%D0%BD%D0%B4%D0%B8%D0%B2%D0%B8%D0%B4%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7/"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613916074/Group_7423529_xvgiks.png"></a>
+    </div>
+</div>
+</div>`) 
+  }
+}
+
 
 if (location.href.includes('term-of-use')) {
   const req = new Requisites(requsites, 'beforeend', 'ИП Горелова Мария Сергеевна');
@@ -167,6 +189,9 @@ if (location.href.includes('contact')) {
   req.setTemplate();
 }
 
+if (document.querySelector('.checkout-checkout')) {
+    document.querySelector('#home > div.content-area > section > div.flexwrap.checkout_form > div.register_block > div.checkout_title > h2').innerHTML = 'Шаг 1: Данные плательщика'
+}
 
 if (location.href.includes('policy')) {
   const req = new Requisites(
@@ -189,64 +214,12 @@ if (location.href.includes('pay-delivery')) {
   req.setTemplate();
 }
 
-if (document.querySelector('.wrapper')) {
-  const footerContainer = document.querySelector('.footer-widgets>div>div');
 
-  new SocialIconsTypeOne(footerContainer, 'beforeend', socIconsData).render();
-
-  const socIconsContainer = document.querySelector('.soc-icons>.container');
-  socIconsContainer.insertAdjacentHTML('afterbegin', `<h4 class="widget-title soc-icons__title">Мы в соц.сетях</h4>`);
-
-  const socIcons = document.querySelector('.soc-icons');
-  socIcons.classList.add('col-md-3');
+if (document.querySelector(".footer-widgets")) {
+    document.querySelector("#home > div > footer > div.footer-widgets > div.container > div > div:nth-child(1) > div > div > ul > li:nth-child(1) > a").innerHTML = "О мастере"
 }
 
-if (document.querySelector('.checkout-checkout')) {
-  document.querySelector('#home > div.content-area > section > div.flexwrap.checkout_form > div.register_block > div.form_checkout > div.seller-comments.form-group > label').innerHTML = 'Добавить комментарий';
+if (document.querySelector(".information-information-6862")) {
+    document.querySelector("#content > div > div").insertAdjacentHTML("beforebegin", `<div class="col-md-4"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1613662311/20201017_101826_1_nbttwm.png"></div>`)
+    document.querySelector("#content > div > div.col-md-12").classList = "col-md-8"
 }
-
-//Скрипт для удаления пробелов в строках от мобильной менюхи
-if (document.querySelector('#collapse-7117')) {
-  let x = document.querySelectorAll('#collapse-7117 > ul > li > a');
-
-  // x.map((index, item) => {
-  //   let c = item[index]
-  // })
-
-  for (let i = 0; i < x.length; i++) {
-    let c = x[i].innerHTML.replace('-', '');
-    x[i].innerHTML = c;
- }
-
- //Скрипт для удаления пробелов в строках от мобильной менюхи
-if (document.querySelector('#collapse-7050')) {
-  let x = document.querySelectorAll('#collapse-7050 > ul > li > a');
-
-  // x.map((index, item) => {
-  //   let c = item[index]
-  // })
-
-  for (let i = 0; i < x.length; i++) {
-    let c = x[i].innerHTML.replace('-', '');
-    x[i].innerHTML = c;
- }
-}
-}
-
-if (document.querySelector('.common-home')) {
-  document.querySelector('body').insertAdjacentHTML('afterbegin', '<div><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612528588/18716257_1_zpl8vr.png"></div>');
-  document.querySelector('body').insertAdjacentHTML('afterbegin', '<div><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612528588/455_5_qxtd9t.png"></div>');
-  document.querySelector('body').insertAdjacentHTML('afterbegin', '<div><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612528588/455_2_wgwrju.png"></div>');
-  document.querySelector('body').insertAdjacentHTML('afterbegin', '<div><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1612528588/455_3_f62gqq.png"></div>');
-}
-
-window.onload = () => {
-  if (document.querySelector('.custom_banner')) {
-    document.querySelector('.custom_banner').insertAdjacentHTML('afterbegin', `
-  <div class="container">
-      <video width="1100" autoplay loop muted>
-          <source src="https://res.cloudinary.com/richelle/video/upload/v1613390750/VID-20210214-WA0004_kxxz4g.mp4">Sorry, your browser doesn't support embedded videos.
-      </video> 
-  </div>`);
-  } 
-} 
