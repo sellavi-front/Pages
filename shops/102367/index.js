879efee5728b5ef6a2628aaca852a2df1075a576
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-23 13:02:44 +0300

    styles

--- shops/102367/index.js
+++ shops/102367/index.js
@@ -97,7 +97,7 @@ if (document.querySelector('.product-product')) {
         const cut = sel.textContent.match(regexRepalce);
         sel.textContent = sel.textContent.replace(regexRepalce, '');
         if (cut) {
-          console.log('PRICE', cut[1]);
+          console.log('PRICE', cut[1].replace(regex, ''));
           // return +cut[1].replace(/\s/i, '');
         }
       });
