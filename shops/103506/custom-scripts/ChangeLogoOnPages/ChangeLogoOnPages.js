a07ef89aff864d3e2a4c7f1fede714d639ddc91c
Coospir <eugene.starodubov@gmail.com>
2021-02-24 11:09:40 +0300

    Updated

--- shops/103506/custom-scripts/ChangeLogoOnPages/ChangeLogoOnPages.js
+++ shops/103506/custom-scripts/ChangeLogoOnPages/ChangeLogoOnPages.js
@@ -1,7 +1,9 @@
 import Paths from "../../path/paths.js"
 
+if( Paths.checkout ) {
+  document.querySelector("#home > div.content-area > section > div.logo_mobile_checkout > a > figure > img").setAttribute("src", 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612967822/sellavi/103506/Group_277_c28c6u.png')
+}
+
 if ( !Paths.home ) {
   document.querySelector("body.ltr> div > header > div.header-wrapper > div.container-fluid.d-flex > div.logo > a > img").setAttribute("src", 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612967822/sellavi/103506/Group_277_c28c6u.png')
-} else if( Paths.checkout ) {
-  document.querySelector("#home > div.content-area > section > div.logo_mobile_checkout > a > figure > img").setAttribute("src", 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612967822/sellavi/103506/Group_277_c28c6u.png')
 }
