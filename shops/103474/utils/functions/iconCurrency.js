51db583b574611343f70ee01c4e3b382a2cbf5ab
Закир Гафиятов <gafiyatovz@gmail.com>
2021-02-24 15:11:29 +0300

    styles

--- shops/103474/utils/functions/iconCurrency.js
+++ shops/103474/utils/functions/iconCurrency.js
@@ -78,4 +78,11 @@ import pages from '../pages/pages.js'
     const observer = new MutationObserver(callback);
     observer.observe(target, config);
   }
+
+  if (pages.all) {
+    if (document.querySelector("#home > div > header > div.header-wrapper > div > div.header_widgets > div.header-cart.sticky > div > div.dropdown.cart_drop_down.show > div > div > div > div > div")) {
+      const icons = document.querySelectorAll("#home > div > header > div.header-wrapper > div > div.header_widgets > div.header-cart.sticky > div > div.dropdown.cart_drop_down.show > div > div > div > div > div .item-price");
+      console.log( icons);
+    }
+  }
 })();
