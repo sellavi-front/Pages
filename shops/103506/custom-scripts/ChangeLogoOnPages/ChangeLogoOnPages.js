ca2931fdc4cc13528c2cae39954e6ff370d064fa
Coospir <eugene.starodubov@gmail.com>
2021-02-24 10:28:29 +0300

    Updated

--- shops/103506/custom-scripts/ChangeLogoOnPages/ChangeLogoOnPages.js
+++ shops/103506/custom-scripts/ChangeLogoOnPages/ChangeLogoOnPages.js
@@ -1,5 +1,5 @@
 import Paths from "../../path/paths.js"
 
-if ( !Paths.home ) {
+if ( !Paths.home || Paths.checkout) {
   document.querySelector("body.ltr> div > header > div.header-wrapper > div.container-fluid.d-flex > div.logo > a > img").setAttribute("src", 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612967822/sellavi/103506/Group_277_c28c6u.png')
 }
