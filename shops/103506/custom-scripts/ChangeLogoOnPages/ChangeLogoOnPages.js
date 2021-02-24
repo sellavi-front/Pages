f5cf6ddf2729f18d64501b19fde6cac44e815e2f
Coospir <eugene.starodubov@gmail.com>
2021-02-24 11:26:09 +0300

    Updated

--- shops/103506/custom-scripts/ChangeLogoOnPages/ChangeLogoOnPages.js
+++ shops/103506/custom-scripts/ChangeLogoOnPages/ChangeLogoOnPages.js
@@ -1,7 +1,7 @@
 import Paths from "../../path/paths.js"
 
 if( Paths.checkout ) {
-  document.querySelector("#home > div.content-area > section > div.logo_mobile_checkout > a > figure > img").setAttribute("src", 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612967822/sellavi/103506/Group_277_c28c6u.png')
+  document.querySelector("body.ltr > div.content-area > section > div.flexwrap.checkout_form > div.register_block > div.logo.logo-edited > a > figure > img").setAttribute("src", 'https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1612967822/sellavi/103506/Group_277_c28c6u.png')
 }
 
 if ( !Paths.home ) {
