ac14451e8ce78226bd0742375d18db07a661eeb7
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-02-25 13:41:49 +0300

    new styles

--- shops/102273/js/pages/innerPages/tourismPage.js
+++ shops/102273/js/pages/innerPages/tourismPage.js
@@ -233,8 +233,8 @@ const tourismPage = () => {
       <div class="row">
         <video controls class="col-md-4 col-12" src="https://res.cloudinary.com/depgheppz/video/upload/v1613749862/102273/%D0%9A%D0%BE%D0%BC%D0%BF%D0%BB%D0%B5%D0%BA%D1%81_Adel_%D0%B2_%D0%BF%D1%80%D0%B8%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%B5_%D0%9D%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE_%D0%90%D1%84%D0%BE%D0%BD%D0%B0_sbncbg.mp4"></video>
         <div class="col-8">
-          <img src="https://res.cloudinary.com/depgheppz/image/upload/v1614249288/102273/%D0%9C%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D0%B8%D0%BD%D1%8B_%D0%BD%D0%B0_%D1%81%D0%BD%D0%B5%D0%B3%D1%83_2_wlewfz.jpg">
-          <img src="https://res.cloudinary.com/depgheppz/image/upload/v1614249289/102273/%D0%9C%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D0%B8%D0%BD%D1%8B_%D0%BD%D0%B0_%D1%81%D0%BD%D0%B5%D0%B3%D1%83_1_zfksud.jpg">
+          <img class="w-50" src="https://res.cloudinary.com/depgheppz/image/upload/v1614249288/102273/%D0%9C%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D0%B8%D0%BD%D1%8B_%D0%BD%D0%B0_%D1%81%D0%BD%D0%B5%D0%B3%D1%83_2_wlewfz.jpg">
+          <img class="w-50" src="https://res.cloudinary.com/depgheppz/image/upload/v1614249289/102273/%D0%9C%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D0%B8%D0%BD%D1%8B_%D0%BD%D0%B0_%D1%81%D0%BD%D0%B5%D0%B3%D1%83_1_zfksud.jpg">
         </div>
         <div class="w-100 row">
           <video controls class="col-md-4 col-12" src="https://res.cloudinary.com/depgheppz/video/upload/v1614249297/102273/%D0%9A%D0%B0%D0%B4%D0%BE%D1%80%D1%81%D0%BA%D0%BE%D0%B5_%D1%83%D1%89%D0%B5%D0%BB%D1%8C%D0%B5_%D0%97%D0%B4%D0%BE%D1%80%D0%BE%D0%B2%D1%8C%D0%B5_%D0%B4%D0%BE%D0%BB%D0%B3%D0%BE%D0%B6%D0%B8%D1%82%D0%B5%D0%BB%D0%B8_dr8hcl.mp4"></video>
