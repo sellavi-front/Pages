59198779c8bee0281490f3dd30fccbced008775c
Coospir <eugene.starodubov@gmail.com>
2021-02-25 11:04:23 +0300

    Created 104573

--- fixes/js/ChangeAgreement/ChangeAgreement.js
+++ fixes/js/ChangeAgreement/ChangeAgreement.js
@@ -7,11 +7,7 @@ export default class ChangeAgreement {
     this.checkbox.textContent = '';
     this.checkbox.insertAdjacentHTML(
       'beforeend',
-<<<<<<< HEAD
-      `Согласен (-а) с условиями <a href="/term-of-use/" style="text-decoration: underline !important;"><b>Публичной оферты</b></a>`,
-=======
       `Согласен (-а) с условиями <a href="/term-of-use/" class="agree-checkbox"><b>Публичной оферты</b></a>`,
->>>>>>> 686227069cde8a3273a53f09350b9f1e653c3a52
     );
   }
 }
