4f71eb6e466718a58e1c66fc755d76d581f80d3e
m4medbekov <m4medbekov@gmail.com>
2021-02-26 14:41:58 +0300

    fix

--- shops/104488/index.js
+++ shops/104488/index.js
@@ -1,21 +1,20 @@
 import Requisites from '../../modules/Requisites/Requisites.js';
-import { requsites, dataContact, options } from './utils/requsites.js';
-
+import {requsites, dataContact } from './utils/requsites.js'
 import '../../fixes/js/FooterCopy/FooterCopy.js';
-import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js';
-import '../../fixes/js/ChangeAgreement/ChangeAgreement.js';
+import '../../fixes/js/TranslateAboutUs/TranslateAboutUs.js'
 import '../../templates/TemplateTypeTwo/TemplateTypeTwo.js';
+import '../../fixes/js/ChangeAgreement/ChangeAgreement.js';
 import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
 
-
-
-if (document.querySelector('.product-product')) {
-  const colorChoise = new ColorChoise();
-  colorChoise.render();
-}
+// if (document.querySelector('.header-wrapper')) {
+//   const oldNav = document.querySelector('.nav.sf-menu');
+//   const newNav = oldNav.cloneNode(true);
+//   document.querySelector('.logo').insertAdjacentHTML('beforebegin', newNav.outerHTML);
+//   oldNav.remove();
+// }
 
 if (location.href.includes('term-of-use')) {
-  const req = new Requisites(requsites, 'beforeend', 'ИП Орлова Елена Геннадьевна');
+  const req = new Requisites(requsites, 'beforeend', 'ИП Горелова Мария Сергеевна');
 
   req.setTemplate();
 }
@@ -23,11 +22,31 @@ if (location.href.includes('term-of-use')) {
 if (location.href.includes('contact')) {
   const media = document.querySelector('.contact-info > .media-list > .media:last-child');
 
-  const req = new Requisites(requsites, 'afterend', 'ИП Орлова Елена Геннадьевна', media);
+  const req = new Requisites(requsites, 'afterend', 'ИП Горелова Мария Сергеевна', media);
   req.setTemplate();
 }
 
+
 if (location.href.includes('policy')) {
-  const req = new Requisites(requsites, 'afterend', 'ИП Орлова Елена Геннадьевна', document.querySelector("#content > div > div"));
+  const req = new Requisites(
+    requsites,
+    'afterend',
+    'ИП Горелова Мария Сергеевна',
+    document.querySelector('#content > div > div'),
+  );
   req.setTemplate();
 }
+
+if (location.href.includes('pay-delivery')) {
+  const req = new Requisites(
+    requsites,
+    'afterend',
+    'ИП Горелова Мария Сергеевна',
+    document.querySelector('#content > div > div'),
+    dataContact,
+  );
+  req.setTemplate();
+}
+
+
+
