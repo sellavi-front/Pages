f5cec7ff8e5a13b9a96b59e9ced61960e245478f
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-02-26 13:07:23 +0300

    new styles

--- shops/103474/utils/index.js
+++ shops/103474/utils/index.js
@@ -178,7 +178,7 @@ jQuery(function () {
 
 jQuery(() => {
   if (document.querySelector('.wrapper')) {
-    const target = document.querySelector("#home > div > header > div.header-wrapper > div > div.header_widgets > div.header-cart.sticky > div.cart-wrapper > div.dropdown.cart_drop_down .cart-content");
+    const target = document.querySelector("header > div.header-wrapper > div > div.header_widgets > div.header-cart.sticky > div.cart-wrapper > div.dropdown.cart_drop_down .cart-content");
 
     // Конфигурация observer (за какими изменениями наблюдать)
     const config = {
