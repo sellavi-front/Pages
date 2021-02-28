e61ba7925ec04e8f4db824c47dbe91033dc08b88
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-02-28 13:06:29 +0300

    styles

--- shops/103474/utils/index.js
+++ shops/103474/utils/index.js
@@ -181,7 +181,7 @@ jQuery(function () {
     // if (document.querySelector('.price_actions .add_to_cart')) {
     //   document.querySelectorAll('.price_actions .add_to_cart')
     // }
-    const target = document.querySelector("#home > div > header > div.header-wrapper > div > div.header_widgets > div.header-cart.sticky > div.cart-wrapper > div.dropdown.cart_drop_down .dropdown-menu .cart-content .products_row");
+    const target = document.querySelector("#home > div > header > div.header-wrapper > div > div.header_widgets > div.header-cart.sticky > div.cart-wrapper > div.dropdown.cart_drop_down .dropdown-menu .cart-content .products_row div");
     console.log(target);
     const config = {
       childList: true,
