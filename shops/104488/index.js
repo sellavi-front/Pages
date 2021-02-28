fa5c2c1670f781b6c95657c20a04741ce40f8278
m4medbekov <m4medbekov@gmail.com>
2021-02-28 09:59:06 +0300

    fi

--- shops/104488/index.js
+++ shops/104488/index.js
@@ -6,6 +6,43 @@ import '../../templates/TemplateTypeTwo/TemplateTypeTwo.js';
 import '../../fixes/js/ChangeAgreement/ChangeAgreement.js';
 import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
 
+if (document.querySelector('.common-home')) {
+  
+  document.querySelector('.page-section.latest-section').insertAdjacentHTML('afterend', `<div class="contactsSection">
+  <div class="container">
+    <div class="contacts-flex">
+      <div class="contacts-title"><span>Контакты</span></div>
+      <div class="otherInfo">
+        <div class="time">
+          <div class="time-img"><img
+              src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1614495343/Group_7423528_zmhbqf.png" alt=""></div>
+          <div class="title-and-time">
+            <div class="time-title"><span>Часы работы</span></div>
+            <div class="real-time"><span>с 9.00 до 21.00</span></div>
+          </div>
+        </div>
+        <div class="phones">
+          <div class="phone-img"><img
+              src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1614495342/Group_208_klihn8.png" alt=""></div>
+          <div class="title-and-phone">
+            <div class="phone-title"><span>Номер телефона</span></div>
+            <div class="real-phone"><span>+7 906 442 49 53</span></div>
+          </div>
+        </div>
+        <div class="email">
+          <div class="email-img"><img
+              src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1614495341/Group_209_i1fx0a.png" alt=""></div>
+          <div class="title-and-email">
+            <div class="email-title"><span>Почтовый адрес</span></div>
+            <div class="real-email"><span>negasonpost@yandex.com</span></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`);
+}
+
 if (document.querySelector('.header-wrapper')) {
   const oldNav = document.querySelector('.nav.sf-menu');
   const newNav = oldNav.cloneNode(true);
