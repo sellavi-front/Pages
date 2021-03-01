eb6da0b1ba392f33c1a27a409aaf9926be4ad454
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-03-01 10:42:58 +0300

    styles

--- shops/104659/index.js
+++ shops/104659/index.js
@@ -50,7 +50,7 @@ if (location.href.includes('pay-delivery')) {
 }
 
 if (document.querySelector('.wrapper')) {
-  const changesText = [document.querySelector("#home > nav.navigation.mobile_side_nav > ul > li.flexy > a"), document.querySelector("#undefined-sticky-wrapper > header > div.navigation-wrapper > div > nav > ul > li.flexy.no_children > a") ]
+  const changesText = [document.querySelector("#home > nav.navigation.mobile_side_nav > ul > li.flexy > a"), document.querySelector("body .wrapper > header > div.navigation-wrapper > div > nav > ul > li.flexy.no_children > a") ]
 
   changesText.forEach(el => {
     console.log(el);
