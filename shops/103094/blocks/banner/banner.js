f0fd2c45687da2c7e83cccb3fdc871770ce59fc5
m4medbekov <m4medbekov@gmail.com>
2021-03-01 15:35:01 +0300

    fix

--- shops/103094/blocks/banner/banner.js
+++ shops/103094/blocks/banner/banner.js
@@ -1,5 +1,5 @@
 let resetSizeBanner = () => {
-  let newLink = "https://cdn.sellavi.com/image/upload/v1612349974/ru/clients/103094/71b24d86245d92e4b65b7c49fa8142c0a77bf550.webp"
+  let newLink = "https://res.cloudinary.com/dtqqfmyqb/image/upload/v1614602083/Component_1_19_ofb8eg.png"
   document.querySelector("#banner0 > div > div > div > div > picture > source").setAttribute("srcset", newLink)
 }
 
