1329e74499d3e45512e1c47fa081cac54750e2aa
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-03-02 13:44:28 +0300

    styles

--- shops/104659/index.js
+++ shops/104659/index.js
@@ -1,6 +1,8 @@
 import './utils/fixes.js'
 import './utils/accuring/accuring.js'
 
+import '../../components/HeaderTypeTwo/HeaderTypeTwo.js';
+
 import pages from './config/conditions.js'
 import { changeTitleBlogToNews } from './scripts/main.js';
 
--- shops/104659/scripts/main.js
+++ shops/104659/scripts/main.js
@@ -1,10 +1,8 @@
 import changeTitleBlogToNews from './changeTitleBlogToNews.js'
 import BtnWishListToBtnAddToCart from './BtnWishListToBtnAddToCart.js'
-import buttonListInHeader from './buttonListInHeader.js'
 
 
 export {
   changeTitleBlogToNews,
   BtnWishListToBtnAddToCart,
-  // buttonListInHeader
 }
