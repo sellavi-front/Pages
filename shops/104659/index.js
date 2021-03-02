2eaf34a109b08b9095bb6fee65ed43ac5e5fb01a
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-03-02 13:23:14 +0300

    styles

--- shops/104659/index.js
+++ shops/104659/index.js
@@ -1,5 +1,6 @@
 import './utils/fixes.js'
 import './utils/accuring/accuring.js'
+import '../../modules/ButtonListInHeader/ButtonListInHeader.js'
 
 import pages from './config/conditions.js'
 import { changeTitleBlogToNews } from './scripts/main.js';
