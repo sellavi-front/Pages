42670059201f04b096d208ab47405dc35068f0b9
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-03-02 13:12:50 +0300

    styles

--- shops/104659/scripts/BtnWishListToBtnAddToCart.js
+++ shops/104659/scripts/BtnWishListToBtnAddToCart.js
@@ -1,11 +1,29 @@
 const BtnWishListToBtnAddToCart = (() => {
-  const cards = document.querySelectorAll('.products.grid .product-item');
 
-  cards.forEach(card => {
-    const wishList = card.querySelector('.add_to_wishlist');
-    card.querySelector('.add_to_cart').insertAdjacentHTML('beforebegin', wishList.outerHTML)
-    wishList.remove();
-  })
+  const addBtn = (arr) => {
+    arr.forEach(card => {
+      const wishList = card.querySelector('.add_to_wishlist');
+      card.querySelector('.add_to_cart').insertAdjacentHTML('afterend', wishList.outerHTML)
+      wishList.remove();
+    })
+  }
+  const cards = document.querySelectorAll('.products.grid .product-item');
+  let target = document.querySelector('body div.products');
+  const config = {
+    childList: true,
+  };
+  const callback = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        addBtn(cards);
+      }
+    }
+  };
+  const observer = new MutationObserver(callback);
+
+
+  addBtn(cards)
+  observer.observe(target, config);
 })();
 
 export default BtnWishListToBtnAddToCart;
