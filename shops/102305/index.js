76bc9ef911e1114ef9e9c776f3cd6c851c7752cd
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-03-02 18:31:06 +0300

    styles

--- shops/102305/index.js
+++ shops/102305/index.js
@@ -40,6 +40,25 @@ if (document.querySelector(".wrapper")) {
 
 
 
+  let target = document.querySelector('#search_results');
+  console.log(target);
+  const config = {
+    childList: true,
+  };
+  const callback = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        const btnSearchText = document.querySelector("#search_results > div > a > span");
+        const num = btnSearchText.textContent.match(/\d/gi);
+
+        btnSearchText.textContent = `Смотреть все (${num}) результаты`
+
+      }
+    }
+  };
+  const observer = new MutationObserver(callback);
+  observer.observe(target, config);
+
 
 }
 
@@ -85,25 +104,3 @@ if (document.querySelector(".product-product")) {
 if (document.querySelector('.checkout-checkout')) {
   document.querySelector("#payment-address-new > div:nth-child(3) > div > label").textContent = 'Полный адрес (требуется)';
 }
-
-
-// setTimeout(() => {
-  let target = document.querySelector('#search_results');
-  console.log(target);
-  const config = {
-    childList: true,
-  };
-  const callback = function (mutationsList, observer) {
-    for (let mutation of mutationsList) {
-      if (mutation.type === 'childList') {
-        const btnSearchText = document.querySelector("#search_results > div > a > span");
-        const num = btnSearchText.textContent.match(/\d/gi);
-
-        btnSearchText.textContent = `Смотреть все (${num}) результаты`
-
-      }
-    }
-  };
-  const observer = new MutationObserver(callback);
-  observer.observe(target, config);
-// }, 800)
