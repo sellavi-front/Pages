10e2f0af4a1e355989873000b2ffd18c41a87c76
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-03-02 15:17:25 +0300

    styles

--- shops/102305/index.js
+++ shops/102305/index.js
@@ -55,13 +55,13 @@ if (document.querySelector('.product-item')) {
 }*/
 
 if (document.querySelector('.common-home')) {
-  //const customSection = document.querySelector('.custom_section')
-  //customSection.classList.add('container')
-  //const map = new ContactsWithMap(customSection, 'beforeend', contactData)
-  //map.render()
+  const customSection = document.querySelector('.custom_section')
+  customSection.classList.add('container')
+  const map = new ContactsWithMap(customSection, 'beforeend', contactData)
+  map.render()
 
-  //let advantages = new AdvantagesTypeTwo(document.querySelector("#home > div.wrapper.wrapper-closed > div.content-area > section.page-section.latest-section > div"), adv)
-  //advantages.render()
+  let advantages = new AdvantagesTypeTwo(document.querySelector("#home > div.wrapper.wrapper-closed > div.content-area > section.page-section.latest-section > div"), adv)
+  advantages.render()
 
   if(screen.width >= 993) {
     document.querySelector(".homefeatured_category").insertAdjacentHTML("afterend", `<div class='container'><img class='ft-img' style="width: 100%" src='https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1613390702/sellavi/102305/Component_1_1_da26cx.png' alt='features' /></div>`)
