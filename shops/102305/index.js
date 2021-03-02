214a56f9c684dc7d01427cb0e7a63b6696bb3949
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-03-02 18:00:38 +0300

    styles

--- shops/102305/index.js
+++ shops/102305/index.js
@@ -39,24 +39,27 @@ if (document.querySelector(".wrapper")) {
 
 
 
-  let target = document.querySelector('.search_results');
-  console.log(target);
-  const config = {
-    childList: true,
-  };
-  const callback = function (mutationsList, observer) {
-    for (let mutation of mutationsList) {
-      if (mutation.type === 'childList') {
-        const btnSearchText = document.querySelector("#search_results > div > a > span");
-        const num = btnSearchText.textContent.match(/\d/gi);
+  document.addEventListener('DOMContentLoaded', () => {
+    let target = document.querySelector('.search_results');
+    console.log(target);
+    const config = {
+      childList: true,
+    };
+    const callback = function (mutationsList, observer) {
+      for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          const btnSearchText = document.querySelector("#search_results > div > a > span");
+          const num = btnSearchText.textContent.match(/\d/gi);
 
-        btnSearchText.textContent = `Смотреть все (${num}) результаты`
+          btnSearchText.textContent = `Смотреть все (${num}) результаты`
 
+        }
       }
-    }
-  };
-  const observer = new MutationObserver(callback);
-  observer.observe(target, config);
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(target, config);
+
+  })
 
 }
 
