3ab0ee7b0a9342ad33b7c59938693cf081464366
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-03-02 17:48:52 +0300

    styles

--- shops/102305/index.js
+++ shops/102305/index.js
@@ -36,6 +36,25 @@ if (document.querySelector(".wrapper")) {
   headerIcons.cart.insertAdjacentHTML('afterbegin', icons.cartBlack)
   headerIcons.like.classList.remove('far', 'fa-heart')
   headerIcons.like.insertAdjacentHTML('afterbegin', icons.like)
+
+
+  let target = document.querySelector('.search_results');
+  const config = {
+    childList: true,
+  };
+  const callback = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        const btnSearchText = document.querySelector("#search_results > div > a > span");
+        const num = btnSearchText.textContent.match(/\d/gi);
+
+        btnSearchText.textContent = `Смотреть все (${num}) результаты`
+
+      }
+    }
+  };
+  const observer = new MutationObserver(callback);
+  observer.observe(target, config);
 }
 
 /*
