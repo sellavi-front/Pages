2c8eef64ddd0e8aed94010593cfaaf44d9ed937f
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-03-02 19:01:43 +0300

    styles

--- shops/104659/utils/fixes.js
+++ shops/104659/utils/fixes.js
@@ -1,3 +1,3 @@
 import '../../../fixes/js/FooterCopy/FooterCopy.js';
-import "../../../fixes/js/RemoveBlur/RemoveBlur.js"
-import "../../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
+// import "../../../fixes/js/RemoveBlur/RemoveBlur.js"
+// import "../../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
