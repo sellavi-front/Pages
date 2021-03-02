5ccf3ffd5d46ad3250efd164ede0c34fe2ce09df
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-03-02 13:32:26 +0300

    styles

--- shops/104659/index.js
+++ shops/104659/index.js
@@ -1,6 +1,5 @@
 import './utils/fixes.js'
 import './utils/accuring/accuring.js'
-import '../../modules/ButtonListInHeader/ButtonListInHeader.js'
 
 import pages from './config/conditions.js'
 import { changeTitleBlogToNews } from './scripts/main.js';
--- shops/104659/scripts/buttonListInHeader.js
+++ shops/104659/scripts/buttonListInHeader.js
@@ -1,3 +1,6 @@
+import ButtonListInHeader from '../../modules/ButtonListInHeader/ButtonListInHeader.js'
+
+
 const buttonListInHeader = (() => {
   const headerWrapper = document.querySelector('.header-wrapper>div:first-child');
   const hideCart = document.querySelector('.header-wrapper>div>.header-cart');
