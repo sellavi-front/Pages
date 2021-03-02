7ed9561db1896cdb8011eae52ab59c23da0da667
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2021-03-02 17:27:49 +0300

    styles

--- shops/104659/scripts/changeTextBannerBtn.js
+++ shops/104659/scripts/changeTextBannerBtn.js
@@ -2,7 +2,7 @@
 const changeTextBannerBtn = (() => {
   const btnCategory = document.querySelector("#banner0 > div > div > div.owl-item.owl-fade-in > div > div > div > div > div > div > p > a");
   btnCategory.forEach(btn => btn.textContent = 'Каталог');
-})
+})()
 
 
 export default changeTextBannerBtn;
