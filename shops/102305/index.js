0f07fb3a13b8dadab4df0f5f31fca2107d092e5b
Закир Гафиятов <gafiyatovz@gmail.com>
2021-03-03 11:50:41 +0300

    styles

--- shops/102305/index.js
+++ shops/102305/index.js
@@ -1,42 +1,39 @@
-import "../../fixes/js/RemoveBlur/RemoveBlur.js"
-import "../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
+import '../../fixes/js/RemoveBlur/RemoveBlur.js';
+import '../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js';
 
-import icons from './utils/icons.js'
+import icons from './utils/icons.js';
 
-import '../../components/HeaderTypeTwo/HeaderTypeTwoV2.js'
-import '../../components/PictureCategories/PictureCategories.js'
+import '../../components/HeaderTypeTwo/HeaderTypeTwoV2.js';
+import '../../components/PictureCategories/PictureCategories.js';
 // import "../../fixes/js/ChangeAgreement/ChangeAgreement.js"
 //import ContactsWithMap from "../../components/ContactsWithMap/ContactsWithMap.js"
-import Fixes from "./fixes/fixes.js"
-import AdvantagesTypeTwo from "../../components/AdvantagesTypeTwo/AdvantagesTypeTwo.js"
-import { adv } from "./utils/utils.js"
-import { contactData } from "./utils/utils.js"
-import Footer from "./blocks/footer.js"
-import ProductPage from "./blocks/product-page.js"
+import Fixes from './fixes/fixes.js';
+import AdvantagesTypeTwo from '../../components/AdvantagesTypeTwo/AdvantagesTypeTwo.js';
+import { adv } from './utils/utils.js';
+import { contactData } from './utils/utils.js';
+import Footer from './blocks/footer.js';
+import ProductPage from './blocks/product-page.js';
 
+let fixes = new Fixes();
+let footer = new Footer();
 
+fixes.deleteUnnecessaryClasses();
+fixes.updateImgCategories();
+fixes.insertTitleToCategories();
+fixes.updateClasses();
 
-let fixes = new Fixes()
-let footer = new Footer()
+footer.addIcons();
 
-fixes.deleteUnnecessaryClasses()
-fixes.updateImgCategories()
-fixes.insertTitleToCategories()
-fixes.updateClasses()
-
-footer.addIcons()
-
-if (document.querySelector(".wrapper")) {
+if (document.querySelector('.wrapper')) {
   const headerIcons = {
     cart: document.querySelector('body .header_widgets .header-cart i'),
-    like: document.querySelector('body .header_widgets .header_wishlist i')
-  }
+    like: document.querySelector('body .header_widgets .header_wishlist i'),
+  };
 
   headerIcons.cart.classList.remove('fa-shopping-cart', 'far');
-  headerIcons.cart.insertAdjacentHTML('afterbegin', icons.cartBlack)
-  headerIcons.like.classList.remove('far', 'fa-heart')
-  headerIcons.like.insertAdjacentHTML('afterbegin', icons.like)
-
+  headerIcons.cart.insertAdjacentHTML('afterbegin', icons.cartBlack);
+  headerIcons.like.classList.remove('far', 'fa-heart');
+  headerIcons.like.insertAdjacentHTML('afterbegin', icons.like);
 
   let target = document.querySelector('#search_results');
   const config = {
@@ -45,30 +42,38 @@ if (document.querySelector(".wrapper")) {
   const callback = function (mutationsList, observer) {
     for (let mutation of mutationsList) {
       if (mutation.type === 'childList') {
-        const btnSearchText = document.querySelector("#search_results > div > a > span");
+        const btnSearchText = document.querySelector('#search_results > div > a > span');
         const num = btnSearchText.textContent.match(/\d/gi);
-        btnSearchText.textContent = `Смотреть все (${num.join('')})`
-
+        btnSearchText.textContent = `Смотреть все (${num.join('')})`;
       }
     }
   };
   const observer = new MutationObserver(callback);
   observer.observe(target, config);
 
-  const links = [document.querySelector(".navigation-wrapper > div > nav > ul > li:nth-child(4) > ul > li > a"), document.querySelector(".navigation-wrapper > div > nav > li:nth-child(5) > li > ul > li > a"), ]
-  document.querySelectorAll("#home > div > header > div.navigation-wrapper > div > nav.sf-menu > li > a").forEach(a => {
-    console.log('A', a);
-    if(a.textContent.trim().toLocaleLowerCase() === 'детям' || a.textContent.trim().toLocaleLowerCase() === 'мужчинам' || a.textContent.trim().toLocaleLowerCase() === 'женщинам') {
-      console.log('sib', a.nextSibling);
-      if(a.nextSibling) {
-        console.log(a.nextSibling.querySelector('li > a'));
+  const links = [
+    document.querySelector('.navigation-wrapper > div > nav > ul > li:nth-child(4) > ul > li > a'),
+    document.querySelector('.navigation-wrapper > div > nav > li:nth-child(5) > li > ul > li > a'),
+  ];
+  console.log(
+    document.querySelectorAll('header > div.navigation-wrapper > div  .nav.sf-menu > li > a'),
+  );
+  document
+    .querySelectorAll('header > div.navigation-wrapper > div  .nav.sf-menu > li > a')
+    .forEach((a) => {
+      console.log('A', a);
+      if (
+        a.textContent.trim().toLocaleLowerCase() === 'детям' ||
+        a.textContent.trim().toLocaleLowerCase() === 'мужчинам' ||
+        a.textContent.trim().toLocaleLowerCase() === 'женщинам'
+      ) {
+        console.log('sib', a.nextSibling);
+        if (a.nextSibling) {
+          console.log(a.nextSibling.querySelector('li > a'));
+        }
       }
-
-    }
-  })
+    });
 }
-
-
 
 /*
 if (document.querySelector('.product-item')) {
@@ -93,21 +98,28 @@ if (document.querySelector('.common-home')) {
   // const map = new ContactsWithMap(customSection, 'beforeend', contactData)
   // map.render()
 
-  let advantages = new AdvantagesTypeTwo(document.querySelector("#home > div.wrapper .homefeatured_category"), adv, 'afterend')
-  advantages.render()
+  let advantages = new AdvantagesTypeTwo(
+    document.querySelector('#home > div.wrapper .homefeatured_category'),
+    adv,
+    'afterend',
+  );
+  advantages.render();
 
   // if(screen.width >= 993) {
   //   document.querySelector(".homefeatured_category").insertAdjacentHTML("afterend", `<div class='container'><img class='ft-img' style="width: 100%" src='https://res.cloudinary.com/sellavi-estarodubov/image/upload/v1613390702/sellavi/102305/Component_1_1_da26cx.png' alt='features' /></div>`)
   // }
 }
 
-if (document.querySelector(".product-product")) {
-  let product = new ProductPage()
-  product.renderColor()
-  product.errorMessage()
+if (document.querySelector('.product-product')) {
+  let product = new ProductPage();
+  product.renderColor();
+  product.errorMessage();
 }
 
 if (document.querySelector('.checkout-checkout')) {
-  document.querySelector("#payment-address-new > div:nth-child(3) > div > label").textContent = 'Полный адрес (требуется)';
-  document.querySelector("#home > div.content-area > section > div.flexwrap.checkout_form > div.register_block > div.form_checkout > div.payment-method > div > div > div > label").textContent = 'ONLINE';
+  document.querySelector('#payment-address-new > div:nth-child(3) > div > label').textContent =
+    'Полный адрес (требуется)';
+  document.querySelector(
+    '#home > div.content-area > section > div.flexwrap.checkout_form > div.register_block > div.form_checkout > div.payment-method > div > div > div > label',
+  ).textContent = 'ONLINE';
 }
