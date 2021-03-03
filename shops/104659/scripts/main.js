c89de512ec3e8938c43efb941a26f53f81c0f924
Закир Гафиятов <gafiyatovz@gmail.com>
2021-03-03 14:54:26 +0300

    styles

--- shops/104659/scripts/main.js
+++ shops/104659/scripts/main.js
@@ -1,5 +1,5 @@
 import changeTitleBlogToNews from './changeTitleBlogToNews.js';
-// import btnWishListToBtnAddToCart from './btnWishListToBtnAddToCart.js'
+import btnWishListToBtnAddToCart from './btnWishListToBtnAddToCart.js'
 import cutSrcImg from './cutSrcImg.js';
 import changeTextCategoryBtn from './changeTextCategoryBtn.js';
 // import changeTextBannerBtn from './changeTextBannerBtn.js'
