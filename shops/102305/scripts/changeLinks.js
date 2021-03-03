4a45ec18bac78403acfcfa05ef3734cc0bc9f878
Закир Гафиятов <gafiyatovz@gmail.com>
2021-03-03 13:07:22 +0300

    styles

--- shops/102305/scripts/changeLinks.js
+++ shops/102305/scripts/changeLinks.js
@@ -1,9 +1,10 @@
-const changeLinks = () => {
+const changeLinks = (() => {
+
   const findSetLink = (arr, href, check) => {
     arr.nextElementSibling.querySelectorAll('a').forEach((link) => {
-      if (link.textContent.trim().toLocaleLowerCase() === check) link.setAttribute('href', href);
+      if (link.textContent.trim().toLocaleLowerCase() === check) link.setAttribute('href', href)
     });
-  };
+  }
   const links = document.querySelectorAll(
     'header > div.navigation-wrapper > div .nav.sf-menu > li > a',
   );
@@ -27,8 +28,7 @@ const changeLinks = () => {
   });
 
   shoesLinks.forEach((a) => {
-    if (a.textContent.trim().toLocaleLowerCase() === 'подростковая')
-      kidShoes = a.getAttribute('href');
+    if (a.textContent.trim().toLocaleLowerCase() === 'подростковая')kidShoes = a.getAttribute('href');
     if (a.textContent.trim().toLocaleLowerCase() === 'мужская') manShoes = a.getAttribute('href');
     if (a.textContent.trim().toLocaleLowerCase() === 'женская') womanShoes = a.getAttribute('href');
   });
@@ -39,15 +39,14 @@ const changeLinks = () => {
       a.textContent.trim().toLocaleLowerCase() === 'мужчинам' ||
       a.textContent.trim().toLocaleLowerCase() === 'женщинам'
     ) {
-      findSetLink(a, linkBag, 'сумки');
 
-      if (a.textContent.trim().toLocaleLowerCase() === 'детям') findSetLink(a, kidShoes, 'обувь');
-      if (a.textContent.trim().toLocaleLowerCase() === 'мужчинам')
-        findSetLink(a, manShoes, 'обувь');
-      if (a.textContent.trim().toLocaleLowerCase() === 'женщинам')
-        findSetLink(a, womanShoes, 'обувь');
+      findSetLink(a, linkBag, 'сумки')
+
+      if (a.textContent.trim().toLocaleLowerCase() === 'детям') findSetLink(a, kidShoes, 'обувь')
+      if (a.textContent.trim().toLocaleLowerCase() === 'мужчинам') findSetLink(a, manShoes, 'обувь')
+      if (a.textContent.trim().toLocaleLowerCase() === 'женщинам') findSetLink(a, womanShoes, 'обувь')
     }
   });
-};
+})
 
 export default changeLinks;
