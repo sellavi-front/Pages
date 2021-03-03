ba37204d12c06dc75a613ba850da250ee346edf5
Закир Гафиятов <gafiyatovz@gmail.com>
2021-03-03 18:46:38 +0300

    styles

--- shops/102305/scripts/changeLinks.js
+++ shops/102305/scripts/changeLinks.js
@@ -8,6 +8,8 @@ const changeLinks = (() => {
   const links = document.querySelectorAll(
     'header > div.navigation-wrapper > div .nav.sf-menu > li > a',
   );
+  const mobLinks = document.querySelectorAll('.mobile_side_nav_menu > li > a');
+
   let linkBag;
   let shoesLinks;
   let manShoes;
@@ -19,11 +21,11 @@ const changeLinks = (() => {
       linkBag = a.getAttribute('href');
     }
   });
+  // mobile_side_nav_menu
 
   links.forEach((a) => {
     if (a.textContent.trim().toLocaleLowerCase() === 'обувь') {
       shoesLinks = a.nextElementSibling.querySelectorAll('.megaline .block-title.h4 a');
-      console.log('bag', a.nextElementSibling.querySelectorAll('.megaline .block-title.h4 a'));
     }
   });
 
@@ -34,6 +36,21 @@ const changeLinks = (() => {
   });
 
   links.forEach((a) => {
+    if (
+      a.textContent.trim().toLocaleLowerCase() === 'детям' ||
+      a.textContent.trim().toLocaleLowerCase() === 'мужчинам' ||
+      a.textContent.trim().toLocaleLowerCase() === 'женщинам'
+    ) {
+
+      findSetLink(a, linkBag, 'сумки')
+
+      if (a.textContent.trim().toLocaleLowerCase() === 'детям') findSetLink(a, kidShoes, 'обувь')
+      if (a.textContent.trim().toLocaleLowerCase() === 'мужчинам') findSetLink(a, manShoes, 'обувь')
+      if (a.textContent.trim().toLocaleLowerCase() === 'женщинам') findSetLink(a, womanShoes, 'обувь')
+    }
+  });
+
+  mobLinks.forEach((a) => {
     if (
       a.textContent.trim().toLocaleLowerCase() === 'детям' ||
       a.textContent.trim().toLocaleLowerCase() === 'мужчинам' ||
