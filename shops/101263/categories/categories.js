706343c9bd67e7fd91969a1a16413c05e8facfbc
Закир Гафиятов <gafiyatovz@gmail.com>
2021-03-04 16:16:51 +0300

    styles

--- shops/101263/categories/categories.js
+++ shops/101263/categories/categories.js
@@ -1,67 +1,67 @@
 const categories = [
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1614173861/Group_7423508_maietm.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7933',
+    link: '/index.php?route=product/category&path=7933',
     text: 'Кошер для Песах',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_50_bdvd2i.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7130',
+    link: '/index.php?route=product/category&path=7130',
     text: 'Мясная продукция',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7137',
+    link: '/index.php?route=product/category&path=7137',
     text: 'Хумусы и салаты',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_52_z3t2eg.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7466',
+    link: '/index.php?route=product/category&path=7466',
     text: 'Замороженная продукция',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_38_mcotzo.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7340',
+    link: '/index.php?route=product/category&path=7340',
     text: 'Снеки',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_49_aadszq.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7140',
+    link: '/index.php?route=product/category&path=7140',
     text: 'Выпечка',
   },
  {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1613638706/Frame_53_1_bea7sr.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7467',
+    link: '/index.php?route=product/category&path=7467',
     text: 'Сладости',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_56_tfxnce.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7775',
+    link: '/index.php?route=product/category&path=7775',
     text: 'Молочная продукция',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1613490169/Frame_49_2_rxujjr.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7002',
+    link: '/index.php?route=product/category&path=7002',
     text: 'Кофе, чай и какао',
   },
   // {
   //   img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784188/103474/Frame_42_pobk7l.png',
-  //   link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7774',
+  //   link: '/index.php?route=product/category&path=7774',
   //   text: 'Все для выпечки',
   // },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784188/103474/Frame_49_1_md8fwy.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7633',
+    link: '/index.php?route=product/category&path=7633',
     text: 'Бакалея',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1613638706/Frame_44_1_yszjsq.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7772',
+    link: '/index.php?route=product/category&path=7772',
     text: 'Детское питание и сухие завтраки',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1614173861/Frame_45_1_hghwfh.png',
-    link: 'https://kosher-brodsky.com/index.php?route=product/category&path=7773',
+    link: '/index.php?route=product/category&path=7773',
     text: 'Кидуш, шаббат и праздники',
   },
 ]
--- shops/101263/utils/socIcons.js
+++ shops/101263/utils/socIcons.js
@@ -2,11 +2,11 @@ const socIcons = [
 
   {
     src: 'https://res.cloudinary.com/gz-company/image/upload/v1606295327/Miru/assets/icons/icons8-instagram_1_rw7nv3.svg',
-    link: 'https://www.instagram.com/kosherbrodsky/',
+    link: 'https://www.instagram.com/kosher_gurme/',
   },
   {
     src: 'https://res.cloudinary.com/depgheppz/image/upload/v1613140765/icons8-facebook-f_1_1_vqui95.svg',
-    link: 'https://www.facebook.com/kosher.brodsky'
+    link: 'https://www.facebook.com/koshergurme.official'
   },
 
 ];
