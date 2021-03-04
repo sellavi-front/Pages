1c624b79ec26d809b99a9b5ca84d6fb496ad03f2
m4medbekov <m4medbekov@gmail.com>
2021-03-04 12:02:13 +0300

    text

--- shops/104328/scripts/main.js
+++ shops/104328/scripts/main.js
@@ -1,1 +1,5 @@
 import '../index.js';
+
+
+// @import('https://coospir.github.io/Pages/shops/101263/styles/index.css');
+// <script type="module" src="https://coospir.github.io/Pages/shops/101263/index.js"></script>
