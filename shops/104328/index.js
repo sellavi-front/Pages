22050eff980f1e8204f81238f08ad54ab1b56c8e
m4medbekov <m4medbekov@gmail.com>
2021-03-04 13:03:39 +0300

    fix

--- shops/104328/index.js
+++ shops/104328/index.js
@@ -1,3 +1,6 @@
 import './utils/fixes.js';
 import './utils/accuring/accuring.js';
 
+if (document.querySelector('.information-information-9296')) {
+    document.querySelector('#content > div > div > div > div:nth-child(2) > p:nth-child(2) > b:nth-child(7) > a').innerHTML = 'https://микроба.net'
+}
