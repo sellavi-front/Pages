0ff5d4cdb209124b9745c454aa181bdce4de2f56
m4medbekov <m4medbekov@gmail.com>
2021-03-04 18:07:18 +0300

    commited addTittleForCate

--- shops/104573/scripts/main.js
+++ shops/104573/scripts/main.js
@@ -2,7 +2,7 @@ import changeTitleBlogToNews from './changeTitleBlogToNews.js';
 import btnWishListToBtnAddToCart from './BtnWishListToBtnAddToCart.js'
 import cutSrcImg from './cutSrcImg.js';
 import changeTextCategoryBtn from './changeTextCategoryBtn.js';
-import addTitleForCategory from './addTitleForCategory.js';
+// import addTitleForCategory from './addTitleForCategory.js';
 // import changeTextBannerBtn from './changeTextBannerBtn.js'
 import pages from '../config/conditions.js';
 import addCategories from './addCategories.js';
@@ -19,7 +19,7 @@ if (document.querySelector('.product-item.product-layout')) {
 
 if (pages.main) {
   setBannerCaption('https://res.cloudinary.com/depgheppz/image/upload/v1614779607/Group_7423535_fvlseb.svg');
-  addTitleForCategory('Каталог')
+  // addTitleForCategory('Каталог')
   cutSrcImg();
   changeTextCategoryBtn();
   addCategories(dataCategories);
