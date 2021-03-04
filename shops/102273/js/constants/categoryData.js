3136aa0f25b6109bb6c4b9d09da620d67e8ef7e1
Закир Гафиятов <gafiyatovz@gmail.com>
2021-03-04 11:46:35 +0300

    styles

--- shops/102273/js/constants/categoryData.js
+++ shops/102273/js/constants/categoryData.js
@@ -62,6 +62,11 @@ const categoryData = {
       src: 'https://res.cloudinary.com/depgheppz/image/upload/v1611832430/102273/icons8-airport_gtx1o0.svg',
       link: '/tourism',
     },
+    {
+      text: 'Аэрозоли',
+      src: 'https://res.cloudinary.com/depgheppz/image/upload/v1614847581/102273/icons8-%D0%B4%D0%B5%D0%B7%D0%BE%D0%B4%D0%BE%D1%80%D0%B0%D0%BD%D1%82-%D1%81%D0%BF%D1%80%D0%B5%D0%B8%CC%86_1_a64r3r.svg',
+      link: '/aerosols',
+    },
   ],
 };
 
