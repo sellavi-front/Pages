8f5b32954313a5b47786391aacb16369f5bd175c
m4medbekov <m4medbekov@gmail.com>
2021-03-05 15:14:59 +0300

    fix

--- shops/104573/scripts/addSlider.js
+++ shops/104573/scripts/addSlider.js
@@ -1,8 +1,17 @@
 const nexGSlider = () => {
     document.querySelector('.custom_section').insertAdjacentHTML('afterbegin', `<div class="container"><div>Отзывы</div><div class="owl-carousel owl-theme">
-    <div class="item"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1614945483/118360270_156239069452181_1236850886805879660_n_1_iznhwe.png"><div><span>Синевская Татьяна</span><span>Саратов</span><span>Добрый вечер, спасибо большое за ваш труд, за прекрасное оформление! Радует каждый день такая красота.Желаю салона штор "Виктория" дальнейшего процветания и вдохновения!</span></div></div>
-    <div class="item"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1614945481/120095693_183858546573712_781689104529491764_n_1_xeo97p.png"><div><span>Синевская Татьяна</span><span>Саратов</span><span>Добрый вечер, спасибо большое за ваш труд, за прекрасное оформление! Радует каждый день такая красота.Желаю салона штор "Виктория" дальнейшего процветания и вдохновения!</span></div></div>
-    <div class="item"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1614945480/137613110_445067346631267_5280962858511166362_n_1_zzjbp5.png"><div><span>Синевская Татьяна</span><span>Саратов</span><span>Добрый вечер, спасибо большое за ваш труд, за прекрасное оформление! Радует каждый день такая красота.Желаю салона штор "Виктория" дальнейшего процветания и вдохновения!</span></div></div>
+    <div class="item"><h4>1</h4></div>
+    <div class="item"><h4>2</h4></div>
+    <div class="item"><h4>3</h4></div>
+    <div class="item"><h4>4</h4></div>
+    <div class="item"><h4>5</h4></div>
+    <div class="item"><h4>6</h4></div>
+    <div class="item"><h4>7</h4></div>
+    <div class="item"><h4>8</h4></div>
+    <div class="item"><h4>9</h4></div>
+    <div class="item"><h4>10</h4></div>
+    <div class="item"><h4>11</h4></div>
+    <div class="item"><h4>12</h4></div>
     </div></div>`)
 
     $('.owl-carousel').owlCarousel({
@@ -14,10 +23,10 @@ const nexGSlider = () => {
                 items:1
             },
             600:{
-                items:1
+                items:3
             },
             1000:{
-                items:1
+                items:3
             }
         }
     })
