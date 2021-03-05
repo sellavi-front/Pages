0db873690c66fac831a0df22799298c6ceead2aa
Закир Гафиятов <gafiyatovz@gmail.com>
2021-03-05 11:21:12 +0300

    styles

--- shops/104659/scripts/main.js
+++ shops/104659/scripts/main.js
@@ -3,7 +3,6 @@ import btnWishListToBtnAddToCart from './BtnWishListToBtnAddToCart.js'
 import cutSrcImg from './cutSrcImg.js';
 import changeTextCategoryBtn from './changeTextCategoryBtn.js';
 import addTitleForCategory from './addTitleForCategory.js';
-// import changeTextBannerBtn from './changeTextBannerBtn.js'
 import pages from '../config/conditions.js';
 import addCategories from './addCategories.js';
 import dataCategories from '../utils/dataCategories.js';
