1ea3b85f2004226a1a1ba46502f19c8c4f794909
m4medbekov <m4medbekov@gmail.com>
2021-03-05 15:43:12 +0300

    fix

--- shops/104573/scripts/addSlider.js
+++ shops/104573/scripts/addSlider.js
@@ -1,17 +1,8 @@
 const nexGSlider = () => {
     document.querySelector('.custom_section').insertAdjacentHTML('afterbegin', `<div class="container"><div>Отзывы</div><div class="owl-carousel owl-theme">
-    <div class="item"><h4>1</h4></div>
-    <div class="item"><h4>2</h4></div>
-    <div class="item"><h4>3</h4></div>
-    <div class="item"><h4>4</h4></div>
-    <div class="item"><h4>5</h4></div>
-    <div class="item"><h4>6</h4></div>
-    <div class="item"><h4>7</h4></div>
-    <div class="item"><h4>8</h4></div>
-    <div class="item"><h4>9</h4></div>
-    <div class="item"><h4>10</h4></div>
-    <div class="item"><h4>11</h4></div>
-    <div class="item"><h4>12</h4></div>
+    <div class="item"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1614945483/118360270_156239069452181_1236850886805879660_n_1_iznhwe.png"><div>Синевская Татьяна</div><div>Саратов</div><div>Добрый вечер, спасибо большое за ваш труд, за прекрасное оформление! Радует каждый день такая красота.Желаю салона штор "Виктория" дальнейшего процветания и вдохновения!</div></div>
+    <div class="item"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1614945483/118360270_156239069452181_1236850886805879660_n_1_iznhwe.png"><div>Синевская Татьяна</div><div>Саратов</div><div>Добрый вечер, спасибо большое за ваш труд, за прекрасное оформление! Радует каждый день такая красота.Желаю салона штор "Виктория" дальнейшего процветания и вдохновения!</div></div>
+    <div class="item"><img src="https://res.cloudinary.com/dtqqfmyqb/image/upload/v1614945483/118360270_156239069452181_1236850886805879660_n_1_iznhwe.png"><div>Синевская Татьяна</div><div>Саратов</div><div>Добрый вечер, спасибо большое за ваш труд, за прекрасное оформление! Радует каждый день такая красота.Желаю салона штор "Виктория" дальнейшего процветания и вдохновения!</div></div>
     </div></div>`)
 
     $('.owl-carousel').owlCarousel({
