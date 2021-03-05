053fbe206d0ef2338499275c4e719fc5016d62f5
Закир Гафиятов <gafiyatovz@gmail.com>
2021-03-05 18:18:08 +0300

    styles

--- shops/103506/custom-scripts/ChangeLogoOnPages/ChangeLogoOnPages.js
+++ shops/103506/custom-scripts/ChangeLogoOnPages/ChangeLogoOnPages.js
@@ -4,7 +4,7 @@ if( screen.width > 300 &&  Paths.checkout ) {
   document.querySelector("#home > div.content-area > section > div.logo_mobile_checkout > a > figure > img").setAttribute("src", 'https://res.cloudinary.com/depgheppz/image/upload/v1614957369/Frame_5_s3iyuq.png')
 }
 
-if ( !Paths.home ) {
+if ( Paths.home ) {
   document.querySelector("body.ltr> div > header > div.header-wrapper > div.container-fluid.d-flex > div.logo > a > img").setAttribute("src", 'https://res.cloudinary.com/depgheppz/image/upload/v1614957369/Frame_5_s3iyuq.png')
 }
 
