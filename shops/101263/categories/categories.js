87e433348b5dc1cbb0fac6c741e4aeca23566d9b
Закир Гафиятов <gafiyatovz@gmail.com>
2021-03-05 16:38:29 +0300

    styles

--- shops/101263/categories/categories.js
+++ shops/101263/categories/categories.js
@@ -2,22 +2,23 @@ const categories = [
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1614173861/Group_7423508_maietm.png',
     link: '/index.php?route=product/category&path=7933',
-    text: 'Кошер для Песах',
+    text: 'Песах',
   },
-  {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_50_bdvd2i.png',
-    link: '/index.php?route=product/category&path=7130',
-    text: 'Мясная продукция',
-  },
+
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_51_tyiyam.png',
     link: '/index.php?route=product/category&path=7137',
     text: 'Хумусы и салаты',
   },
   {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_52_z3t2eg.png',
-    link: '/index.php?route=product/category&path=7466',
-    text: 'Замороженная продукция',
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1613638706/Frame_53_1_bea7sr.png',
+    link: '/index.php?route=product/category&path=7467',
+    text: 'Сладости',
+  },
+  {
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_49_aadszq.png',
+    link: '/index.php?route=product/category&path=7140',
+    text: 'Выпечка',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_38_mcotzo.png',
@@ -25,45 +26,48 @@ const categories = [
     text: 'Снеки',
   },
   {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_49_aadszq.png',
-    link: '/index.php?route=product/category&path=7140',
-    text: 'Выпечка',
+    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1613490169/Frame_49_2_rxujjr.png',
+    link: '/index.php?route=product/category&path=7002',
+    text: 'Напитки',
   },
- {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1613638706/Frame_53_1_bea7sr.png',
-    link: '/index.php?route=product/category&path=7467',
-    text: 'Сладости',
+  {
+    img:
+      'https://res.cloudinary.com/depgheppz/image/upload/v1612784188/103474/Frame_49_1_md8fwy.png',
+    link: '/index.php?route=product/category&path=7633',
+    text: 'Бакалея',
   },
   {
     img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_56_tfxnce.png',
     link: '/index.php?route=product/category&path=7775',
     text: 'Молочная продукция',
   },
-  {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1613490169/Frame_49_2_rxujjr.png',
-    link: '/index.php?route=product/category&path=7002',
-    text: 'Кофе, чай и какао',
-  },
+
   // {
   //   img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784188/103474/Frame_42_pobk7l.png',
   //   link: '/index.php?route=product/category&path=7774',
   //   text: 'Все для выпечки',
   // },
-  {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784188/103474/Frame_49_1_md8fwy.png',
-    link: '/index.php?route=product/category&path=7633',
-    text: 'Бакалея',
-  },
-  {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1613638706/Frame_44_1_yszjsq.png',
-    link: '/index.php?route=product/category&path=7772',
-    text: 'Детское питание и сухие завтраки',
-  },
-  {
-    img: 'https://res.cloudinary.com/depgheppz/image/upload/v1614173861/Frame_45_1_hghwfh.png',
-    link: '/index.php?route=product/category&path=7773',
-    text: 'Кидуш, шаббат и праздники',
-  },
-]
 
-export default categories
+  // {
+  //   img: 'https://res.cloudinary.com/depgheppz/image/upload/v1613638706/Frame_44_1_yszjsq.png',
+  //   link: '/index.php?route=product/category&path=7772',
+  //   text: 'Детское питание и сухие завтраки',
+  // },
+  // {
+  //   img: 'https://res.cloudinary.com/depgheppz/image/upload/v1614173861/Frame_45_1_hghwfh.png',
+  //   link: '/index.php?route=product/category&path=7773',
+  //   text: 'Кидуш, шаббат и праздники',
+  // },
+  // {
+  //   img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_52_z3t2eg.png',
+  //   link: '/index.php?route=product/category&path=7466',
+  //   text: 'Замороженная продукция',
+  // },
+  // {
+  //   img: 'https://res.cloudinary.com/depgheppz/image/upload/v1612784189/103474/Frame_50_bdvd2i.png',
+  //   link: '/index.php?route=product/category&path=7130',
+  //   text: 'Мясная продукция',
+  // },
+];
+
+export default categories;
