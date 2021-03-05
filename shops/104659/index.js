3c074efe8cc552d380e64261ebae18238830dd4e
Закир Гафиятов <gafiyatovz@gmail.com>
2021-03-05 11:57:32 +0300

    styles

--- shops/104659/index.js
+++ shops/104659/index.js
@@ -1,4 +1,4 @@
 import './utils/fixes.js'
 import './utils/accuring/accuring.js'
-// import '../../modules/ButtonListInHeader/ButtonListInHeader.js';
+import '../../components/HeaderTypeTwo/HeaderTypeTwoV2.js';
 import './scripts/main.js'
