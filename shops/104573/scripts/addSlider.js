21fb5253fd8362fcfe43a3c096d0acb3d2fec5bc
m4medbekov <m4medbekov@gmail.com>
2021-03-05 15:53:40 +0300

    fix

--- shops/104573/scripts/addSlider.js
+++ shops/104573/scripts/addSlider.js
@@ -10,7 +10,7 @@ const nexGSlider = () => {
         margin:10,
         nav:true,
         autoplay: true,
-        autoplayTimeout: 2000,
+        // autoplayTimeout: 2000,
         responsive:{
             0:{
                 items:1
