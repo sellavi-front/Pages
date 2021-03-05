326bc7b4d5af2cff6f4539fa72567a4952ddf368
Закир Гафиятов <gafiyatovz@gmail.com>
2021-03-05 10:38:16 +0300

    styles

--- shops/104659/utils/fixes.js
+++ shops/104659/utils/fixes.js
@@ -1,3 +1,4 @@
 import '../../../fixes/js/FooterCopy/FooterCopy.js';
+import '../../../fixes/js/ChangeAgreement/ChangeAgreement.js'
 // import "../../../fixes/js/RemoveBlur/RemoveBlur.js"
 // import "../../../modules/ButtonContainerInProduct/ButtonContainerInProduct.js"
