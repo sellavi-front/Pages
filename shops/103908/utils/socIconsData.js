5bd877ab2bbffe632e6b08c11415c40be241dc6e
m4medbekov <m4medbekov@gmail.com>
2021-03-06 18:47:16 +0300

    fix

--- shops/103908/utils/socIconsData.js
+++ shops/103908/utils/socIconsData.js
@@ -6,7 +6,7 @@ const socIconsData = [
   {
     src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1615045142/Component_8_kt57xj.png',
     link: 'https://wa.me/79175751641',
-  },
+  },  
   {
     src: 'https://res.cloudinary.com/dtqqfmyqb/image/upload/v1615045144/Vector_xpkomv.png',
     link: 'https://t.me/79175751641',
