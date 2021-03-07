7de50ebfe465b74d991052ea4192655763d53666
m4medbekov <m4medbekov@gmail.com>
2021-03-07 13:49:22 +0300

    fix

--- shops/104573/index.js
+++ shops/104573/index.js
@@ -2,3 +2,4 @@ import './utils/fixes.js'
 import './utils/accuring/accuring.js'
 import '../../components/HeaderTypeOne/HeaderTypeOne.js';
 import './scripts/main.js'
+import '../../fixes/js/ChangeAgreement/ChangeAgreement.js';
