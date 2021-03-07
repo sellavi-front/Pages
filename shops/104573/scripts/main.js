c1e13901847d2d51d3127923ecd97ad6e122e370
m4medbekov <m4medbekov@gmail.com>
2021-03-07 14:49:21 +0300

    fix

--- shops/104573/scripts/main.js
+++ shops/104573/scripts/main.js
@@ -2,19 +2,11 @@ import changeTitleBlogToNews from './changeTitleBlogToNews.js';
 import btnWishListToBtnAddToCart from './BtnWishListToBtnAddToCart.js'
 import cutSrcImg from './cutSrcImg.js';
 import changeTextCategoryBtn from './changeTextCategoryBtn.js';
-// import addTitleForCategory from './addTitleForCategory.js';
-// import changeTextBannerBtn from './changeTextBannerBtn.js'
 import pages from '../config/conditions.js';
-// import addCategories from './addCategories.js';
-// import dataCategories from '../utils/dataCategories.js';
 import setBannerCaption from './setBannerCaption.js';
 import renderBanner from "../styles/banner/banner.js"
 import renderSlider from "./addSlider.js";
-import sendHomeForm from "../styles/banner/home-form.js";
 
-// if (pages.main) {
-//   renderBanner()
-// }
 
 if (pages.all) {
   changeTitleBlogToNews();
@@ -24,24 +16,9 @@ if (document.querySelector('.product-item')) {
   btnWishListToBtnAddToCart();
 }
 
-window.onload = () => {
-  if (pages.main) {
-    let homeFormBtn = document.querySelector(".banner__submit__form__btn")
-    homeFormBtn.addEventListener("click", () => {
-      sendHomeForm().call()
-    })
-  }
-}
 
 if (pages.main) {
   renderBanner()
 }
 renderSlider()
 
-// if (pages.main) {
-//   setBannerCaption('https://res.cloudinary.com/depgheppz/image/upload/v1614779607/Group_7423535_fvlseb.svg');
-//   // addTitleForCategory('Каталог')
-//   cutSrcImg();
-//   changeTextCategoryBtn();
-//   // addCategories(dataCategories);
-// }
